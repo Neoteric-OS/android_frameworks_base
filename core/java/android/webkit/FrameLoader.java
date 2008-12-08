c99ecaeb37e2c020569d60532b0242b61eed70e3
eden <eden1205.huang@gmail.com>
2008-12-08 10:49:02 +0800

    Add 'x-wap-profile' to http headers.

--- core/java/android/webkit/FrameLoader.java
+++ core/java/android/webkit/FrameLoader.java
@@ -18,6 +18,7 @@ package android.webkit;
 
 import android.net.http.EventHandler;
 import android.net.http.RequestHandle;
+import android.provider.Settings;
 import android.util.Config;
 import android.util.Log;
 import android.webkit.CacheManager.CacheResult;
@@ -52,6 +53,10 @@ class FrameLoader {
             "application/xhtml+xml, image/png, text/plain, */*;q=0.8";
 
     private static final String LOGTAG = "webkit";
+    
+    // for UAProf: add by Eden_Huang
+    private static final String HDR_KEY_UAPROFILE = "x-wap-profile";
+    private static String UAPROFILE_URL;
     
     /*
      * Construct the Accept_Language once. If the user changes language, then
@@ -90,6 +95,11 @@ class FrameLoader {
         mIsHighPriority = highPriority;
         mCacheMode = WebSettings.LOAD_NORMAL;
         mUserAgent = userAgent;
+        
+        // for UAProf: add by Eden_Huang
+        UAPROFILE_URL = Settings.Gservices.getString(
+                    mListener.getContext().getContentResolver(),
+                    Settings.Gservices.MMS_X_WAP_PROFILE_URL);
     }
 
     public void setReferrer(String ref) {
@@ -362,6 +372,14 @@ class FrameLoader {
         }
 
         mHeaders.put("User-Agent", mUserAgent);
+        
+        // for UAProf: add by Eden_Huang
+        if ((UAPROFILE_URL != null) && (UAPROFILE_URL.length() != 0)) {
+            String uaProf = mHeaders.get(HDR_KEY_UAPROFILE);
+            if (uaProf == null || uaProf.length() == 0) {
+                mHeaders.put(HDR_KEY_UAPROFILE, UAPROFILE_URL);
+            }
+        }
     }
 
     /**
