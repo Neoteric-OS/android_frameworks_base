c0c440be15e73e618827450e1c354b64d4bfb15e
Alan Jones <skyphyr@gmail.com>
2009-05-27 22:45:25 +1000

    Changes to support selecting only contacts with phone in PICK_ACTIVITY Requires the changes in packages/apps/Contacts/src/com/android/contacts/ContactsListActivity.java

--- core/java/android/provider/Contacts.java
+++ core/java/android/provider/Contacts.java
@@ -324,6 +324,13 @@ public class Contacts {
         public static final String PRIMARY_ORGANIZATION_ID = "primary_organization";
 
         /**
+         * An Extra to have the picker only display contacts with phone numbers
+         * <P>Used as a boolean extra field to indicate that only contacts with numbers
+         * should be shown</P>
+         */
+         public static final String EXTRA_WITH_PHONES_ONLY = "android.intent.extra.withPhones";
+
+        /**
          * Mark a person as having been contacted.
          *
          * @param resolver the ContentResolver to use
