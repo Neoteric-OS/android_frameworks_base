f9d9a0e8dcc0ef8b788c7c378bcfe8c58f8a1075
NoraBora <noranbora@gmail.com>
2009-12-25 19:51:34 -0500

    guess default mMaxHeight but allow layout to override this.
    without this code, progress bar height gets smaller when thumb gets bigger
    in AbsSeekBar.java onSizeChanged().

--- core/java/android/widget/ProgressBar.java
+++ core/java/android/widget/ProgressBar.java
@@ -177,7 +177,8 @@ public class ProgressBar extends View {
         Drawable drawable = a.getDrawable(R.styleable.ProgressBar_progressDrawable);
         if (drawable != null) {
             drawable = tileify(drawable, false);
-            setProgressDrawable(drawable);
+            setProgressDrawable(drawable); // will guess mMaxHeight.
+            // but allow XML layout to override this.
         }
 
 
@@ -421,6 +422,14 @@ public class ProgressBar extends View {
     public void setProgressDrawable(Drawable d) {
         if (d != null) {
             d.setCallback(this);
+
+            // without this code, progress bar height gets smaller
+            // when thumb height gets bigger.
+            int drawableHeight = drawable.getMinimumHeight();
+            if (mMaxHeight < drawableHeight) {
+                mMaxHeight = drawableHeight;
+                requestLayout();
+            }
         }
         mProgressDrawable = d;
         if (!mIndeterminate) {
