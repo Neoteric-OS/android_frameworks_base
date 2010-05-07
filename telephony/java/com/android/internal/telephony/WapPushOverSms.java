c1fb3b0efc642faaa2912bbb276f0d5546f29635
Billy <billy.leverington@synchronica.com>
2010-05-07 11:09:11 +0100

    Added support for EMN email notification WAP messaging. On recieving a EMN it will start a mail sync for the given account.
    Change-Id: Ia984515a6abb0ef3b6b3fbdfb30fa4cd16994de3

--- telephony/java/com/android/internal/telephony/WapPushOverSms.java
+++ telephony/java/com/android/internal/telephony/WapPushOverSms.java
@@ -132,6 +132,9 @@ public class WapPushOverSms {
                 case WspTypeDecoder.CONTENT_TYPE_B_VND_DOCOMO_PF:
                     mimeType = WspTypeDecoder.CONTENT_MIME_TYPE_B_VND_DOCOMO_PF;
                     break;
+               case WspTypeDecoder.CONTENT_TYPE_B_EMN:
+                	mimeType = WspTypeDecoder.CONTENT_MIME_TYPE_B_EMN;
+                	break;
                 default:
                     if (Config.LOGD) {
                         Log.w(LOG_TAG,
@@ -153,7 +156,10 @@ public class WapPushOverSms {
             } else if (mimeType.equals(WspTypeDecoder.CONTENT_MIME_TYPE_B_MMS)) {
                 binaryContentType = WspTypeDecoder.CONTENT_TYPE_B_MMS;
             } else if (mimeType.equals(WspTypeDecoder.CONTENT_MIME_TYPE_B_VND_DOCOMO_PF)) {
-                binaryContentType = WspTypeDecoder.CONTENT_TYPE_B_VND_DOCOMO_PF;
+                binaryContentType = WspTypeDecoder.CONTENT_TYPE_B_VND_DOCOMO_PF;                
+            } else if (mimeType.equals(WspTypeDecoder.CONTENT_MIME_TYPE_B_EMN)) {
+                binaryContentType = WspTypeDecoder.CONTENT_TYPE_B_EMN;
+                Log.w(LOG_TAG, "Received PDU. CONTENT_TYPE_B_EMN");
             } else {
                 if (Config.LOGD) Log.w(LOG_TAG, "Received PDU. Unknown Content-Type = " + mimeType);
                 return Intents.RESULT_SMS_HANDLED;
