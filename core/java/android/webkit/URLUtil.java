e8f7f5fc2f291585c38d6c8ba2011b2a4a6c811d
Melanie Clements <melanielc@google.com>
2010-06-09 22:12:43 -0400

    Fix for bug # 2337042  URL that starts with "/."
    This is a potential fix for the above bug, in which "." or "/." was
    being inserted into the beginning of urls.  This was not causing
    any particular problems but is inconsistent with other browsers
    and potentially confusing.  I have added a check to URLUtil to
    remove any leading periods.
    
    Change-Id: Ie0280faae257f6855a3e49d5f00c6742b9e9cf2d

--- core/java/android/webkit/URLUtil.java
+++ core/java/android/webkit/URLUtil.java
@@ -56,6 +56,14 @@ public final class URLUtil {
         if (inUrl.endsWith(".") == true) {
             inUrl = inUrl.substring(0, inUrl.length() - 1);
         }
+        
+        // bug 2337042 strip periop off beginning of url
+        if (inUrl.startsWith(".")) {
+            inUrl = inUrl.substring(1, inUrl.length());
+        }
+        if (inUrl.startsWith("/.")) {
+            inUrl = inUrl.substring(2, inUrl.length());
+        }
 
         try {
             webAddress = new WebAddress(inUrl);
