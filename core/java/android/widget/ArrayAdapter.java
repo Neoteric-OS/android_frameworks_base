bdbd51665304bebaca1354cbaf38d05d88a71f39
Christian Mehlmauer <FireFart@gmail.com>
2010-06-11 22:28:38 +0200

    Added an addAll to the ArrayAdapter
    It behaves the same as ArrayList.addAll, since
    the Object containing the Objects in the Adapter
    is a List. Now you can add multiple Objects
    at once, instead of looping over a Collection
    and add all items one by one.
    If this change gets approved, i will add UnitTests
    for the new function in the cts Project (ArrayAdapterTest.java)
    
    Change-Id: I16f3286a8ca4cda7eb9f1a99c2aab1cc45b2550f

--- core/java/android/widget/ArrayAdapter.java
+++ core/java/android/widget/ArrayAdapter.java
@@ -24,6 +24,7 @@ import android.view.ViewGroup;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.Comparator;
 import java.util.Collections;
@@ -83,7 +84,7 @@ public class ArrayAdapter<T> extends BaseAdapter implements Filterable {
      */
     private boolean mNotifyOnChange = true;
 
-    private Context mContext;    
+    private Context mContext;
 
     private ArrayList<T> mOriginalValues;
     private ArrayFilter mFilter;
@@ -181,6 +182,23 @@ public class ArrayAdapter<T> extends BaseAdapter implements Filterable {
     }
 
     /**
+     * Adds the specified Collection at the end of the array.
+     *
+     * @param collection The Collection to add at the end of the array.
+     */
+    public void addAll(Collection<? extends T> collection) {
+        if (mOriginalValues != null) {
+            synchronized (mLock) {
+                mOriginalValues.addAll(collection);
+                if (mNotifyOnChange) notifyDataSetChanged();
+            }
+        } else {
+            mObjects.addAll(collection);
+            if (mNotifyOnChange) notifyDataSetChanged();
+        }
+    }
+
+    /**
      * Inserts the specified object at the specified index in the array.
      *
      * @param object The object to insert into the array.
@@ -236,7 +254,7 @@ public class ArrayAdapter<T> extends BaseAdapter implements Filterable {
      */
     public void sort(Comparator<? super T> comparator) {
         Collections.sort(mObjects, comparator);
-        if (mNotifyOnChange) notifyDataSetChanged();        
+        if (mNotifyOnChange) notifyDataSetChanged();
     }
 
     /**
