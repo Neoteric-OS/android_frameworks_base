44c440d5858cd7d9ece6e7f6464679c455a21621
Kenny Root <kroot@google.com>
2010-07-13 07:53:32 -0700

    Add option to set ListPreference summaries from entryValues
    Currently when ListPreferences are used in a PreferenceActivity, the summary
    values are set to the same as the current index in mEntryValue. This patch
    add an option "summaryFromEntries" to allow the summary to be set from the
    corresponding index in mEntries to aid in localization.
    
    For example a preference may be named "color" with the following attributes
    in the locale "de" (German):
    
    mEntryValues = { "red", "green", "blue" }
    mEntries = { "rot", "grün", "blau" }
    
    Change-Id: Iea169ac3d1c9d6290d853fc7c67a7c4c8a11bb90

--- core/java/android/preference/ListPreference.java
+++ core/java/android/preference/ListPreference.java
@@ -34,12 +34,14 @@ import android.util.AttributeSet;
  * 
  * @attr ref android.R.styleable#ListPreference_entries
  * @attr ref android.R.styleable#ListPreference_entryValues
+ * @attr ref android.R.styleable#ListPreference_summaryFromEntries
  */
 public class ListPreference extends DialogPreference {
     private CharSequence[] mEntries;
     private CharSequence[] mEntryValues;
     private String mValue;
     private int mClickedDialogEntryIndex;
+    private boolean mSummaryFromEntries;
     
     public ListPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -48,6 +50,8 @@ public class ListPreference extends DialogPreference {
                 com.android.internal.R.styleable.ListPreference, 0, 0);
         mEntries = a.getTextArray(com.android.internal.R.styleable.ListPreference_entries);
         mEntryValues = a.getTextArray(com.android.internal.R.styleable.ListPreference_entryValues);
+        mSummaryFromEntries = a.getBoolean(
+                com.android.internal.R.styleable.ListPreference_summaryFromEntries, false);
         a.recycle();
     }
     
@@ -123,7 +127,25 @@ public class ListPreference extends DialogPreference {
     public void setValue(String value) {
         mValue = value;
         
+        if (mSummaryFromEntries) {
+            setSummaryFromValue();
+        }
+
         persistString(value);
+    }
+
+    /**
+     * Set the summary from the corresponding human-readable entry.
+     */
+    private void setSummaryFromValue() {
+        if (mEntryValues == null || mEntries == null) {
+            return;
+        }
+
+        int entryIndex = findIndexOfValue(mValue);
+        if (entryIndex >= 0) {
+            setSummary(mEntries[entryIndex]);
+        }
     }
 
     /**
@@ -157,6 +179,29 @@ public class ListPreference extends DialogPreference {
         return index >= 0 && mEntries != null ? mEntries[index] : null;
     }
     
+    /**
+     * Sets whether the summary for the preference will be set from the
+     * human-readable entry description that corresponds to the index
+     * of the entry value when {@link #setValue(String)} is called.
+     * When set to <code>true</code>, the summary will be immediately
+     * set if <code>entries</code> and <code>entryValues</code> are
+     * populated.  When set to <code>false</code>, the summary will be
+     * immediately set to <code>null</code>.
+     * 
+     * @param useSummaryFromEntries whether the human-readable strings
+     *            should be used for summaries.
+     * @attr ref android.R.styleable#ListPreference_summaryFromEntries
+     */
+    public void setSummaryFromEntries(boolean useSummaryFromEntries) {
+        mSummaryFromEntries = useSummaryFromEntries;
+
+        if (useSummaryFromEntries) {
+            setSummaryFromValue();
+        } else {
+            setSummary(null);
+        }
+    }
+
     /**
      * Returns the index of the given value (in the entry values array).
      * 
