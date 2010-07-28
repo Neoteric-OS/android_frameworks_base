bc6956c586a6ae9c130cb410eaeefc74a6013432
aimitakeshi <aimitakeshi@gmail.com>
2010-07-28 17:08:36 +0900

    Remove the tab after AID_DRM
    Change-Id: I74e0a3d1b3d70aaf4a3d112d2588deaae3d37035

--- cmds/servicemanager/service_manager.c
+++ cmds/servicemanager/service_manager.c
@@ -35,7 +35,7 @@ static struct {
     { AID_MEDIA, "media.camera" },
     { AID_MEDIA, "media.audio_policy" },
     { AID_DRMIO, "drm.drmIOService" },
-    { AID_DRM, 	 "drm.drmManager" },
+    { AID_DRM,   "drm.drmManager" },
     { AID_RADIO, "radio.phone" },
     { AID_RADIO, "radio.sms" },
     { AID_RADIO, "radio.phonesubinfo" },
