281b834c2b45dc0086e262cb4465f719e97a64eb
lge-aosp <lge-aosp@lge.com>
2011-01-06 13:12:01 +0900

    Prevent null pointer reference
    Change-Id: Ie2ad1e433f32de481ce556df6f3c5d0df1403f4f

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -426,9 +426,11 @@ class ServerThread extends Thread {
         mContentResolver.registerContentObserver(Settings.Secure.getUriFor(Settings.Secure.ADB_ENABLED),
                 false, new AdbSettingsObserver());
 
+        // WBT fix - CID363 prevent null pointer reference [START]
         // Before things start rolling, be sure we have decided whether
-        // we are in safe mode.
-        final boolean safeMode = wm.detectSafeMode();
+        // we are in safe mode.        
+        final boolean safeMode = (wm != null) ? wm.detectSafeMode() : false;
+        // WBT fix - CID363 prevent null pointer reference [END]
         if (safeMode) {
             try {
                 ActivityManagerNative.getDefault().enterSafeMode();
@@ -456,8 +458,17 @@ class ServerThread extends Thread {
         if (statusBar != null) {
             statusBar.systemReady();
         }
-        wm.systemReady();
-        power.systemReady();
+
+        // WBT fix - CID363 prevent null pointer reference [START]
+        if (wm != null) {
+            wm.systemReady();
+        }
+
+        if (power != null) {
+            power.systemReady();
+        }
+        // WBT fix - CID363 prevent null pointer reference [END]
+		
         try {
             pm.systemReady();
         } catch (RemoteException e) {
