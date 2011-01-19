e6b65eeeffe76b9c899f59c562e4c5ab539bd943
Mike Playle <mike.playle@realvnc.com>
2011-01-19 11:54:05 +0000

    Fix the screencap command to output data in the format that DDMS expects.
    This series of patches changes the mechanism used by the DDMS debugger
    to ensure that the screenshot tool always captures an accurate, atomic
    image of the screen.
    
    Change-Id: I0281bd4cdc5dc2b1d6bcba3017e62a64435635d1

--- cmds/screencap/screencap.cpp
+++ cmds/screencap/screencap.cpp
@@ -22,21 +22,56 @@
 
 using namespace android;
 
+/* This version number defines the format of the fbinfo struct.
+   It must match versioning in ddms where this data is consumed. */
+#define DDMS_RAWIMAGE_VERSION 1
+struct fbinfo {
+    unsigned int version;
+    unsigned int bpp;
+    unsigned int size;
+    unsigned int width;
+    unsigned int height;
+    unsigned int red_offset;
+    unsigned int red_length;
+    unsigned int blue_offset;
+    unsigned int blue_length;
+    unsigned int green_offset;
+    unsigned int green_length;
+    unsigned int alpha_offset;
+    unsigned int alpha_length;
+} __attribute__((packed));
+
 int main(int argc, char** argv)
 {
     ScreenshotClient screenshot;
     if (screenshot.update() != NO_ERROR)
-        return 0;
+        return -1;
 
-    void const* base = screenshot.getPixels();
-    uint32_t w = screenshot.getWidth();
-    uint32_t h = screenshot.getHeight();
     uint32_t f = screenshot.getFormat();
+    if (f != PIXEL_FORMAT_RGBA_8888)
+        return -1;
+
+    struct fbinfo fbinfo;
+    fbinfo.version = DDMS_RAWIMAGE_VERSION;
+
+    fbinfo.bpp = 32;
+    fbinfo.red_offset = 0;
+    fbinfo.red_length = 8;
+    fbinfo.green_offset = 8;
+    fbinfo.green_length = 8;
+    fbinfo.blue_offset = 16;
+    fbinfo.blue_length = 8;
+    fbinfo.alpha_offset = 0;
+    fbinfo.alpha_length = 0;
+    fbinfo.width = screenshot.getWidth();
+    fbinfo.height = screenshot.getHeight();
+    fbinfo.size = fbinfo.width * fbinfo.height * fbinfo.bpp / 8;
+
     int fd = dup(STDOUT_FILENO);
-    write(fd, &w, 4);
-    write(fd, &h, 4);
-    write(fd, &f, 4);
-    write(fd, base, w*h*4);
+    void const* base = screenshot.getPixels();
+
+    write(fd, &fbinfo, sizeof fbinfo);
+    write(fd, base, fbinfo.size);
     close(fd);
     return 0;
 }
