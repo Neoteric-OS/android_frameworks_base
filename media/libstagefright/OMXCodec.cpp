e5535a04552c6ea3c7cc5505ef8e7bcbe1104d9e
Vinay Kalia <vkalia@codeaurora.org>
2011-02-04 13:50:40 -0800

    libstagefright:Ignoring some events in ERROR state
    Port disable, port enable and flush complete events
    need to be ignored if the component is in ERROR
    state.
    
    Scenario for port enable:
    While the output port is being reconfigured,
    component might be parsing input buffers. If
    component encounters an error in parsing,
    it might move into Invalid state and
    then generate a port enable complete on output
    port. Adding fix to handle this scenario.
    
    Scenario for port disable:
    Port settings disable command complete need to
    be ignored in error state. This scenario happens
    when the component moves into ERROR state and
    issue Port Disable command complete during Port
    Reconfiguration.
    
    Scenario for flush:
    Component may move into invalid state and then
    issue a FlushComplete command. In this case
    we should not send any buffers to component
    and report error to the client of OMXCodec.
    
    Change-Id: Ib5ea1d7a726d46b0094c5adc8f62487bd3a24461

--- media/libstagefright/OMXCodec.cpp
+++ media/libstagefright/OMXCodec.cpp
@@ -1985,6 +1985,10 @@ void OMXCodec::onCmdComplete(OMX_COMMANDTYPE cmd, OMX_U32 data) {
 
         case OMX_CommandPortDisable:
         {
+            if(mState == ERROR) {
+                CODEC_LOGI("Ignoring OMX_CommandPortDisable in ERROR state");
+                break;
+            }
             OMX_U32 portIndex = data;
             CODEC_LOGV("PORT_DISABLED(%ld)", portIndex);
 
@@ -2019,6 +2023,10 @@ void OMXCodec::onCmdComplete(OMX_COMMANDTYPE cmd, OMX_U32 data) {
             OMX_U32 portIndex = data;
             CODEC_LOGV("PORT_ENABLED(%ld)", portIndex);
 
+            if(mState == ERROR) {
+                CODEC_LOGI("Ignoring OMX_CommandPortEnable in ERROR state");
+                break;
+            }
             CHECK(mState == EXECUTING || mState == RECONFIGURING);
             CHECK_EQ(mPortStatus[portIndex], ENABLING);
 
@@ -2046,6 +2054,10 @@ void OMXCodec::onCmdComplete(OMX_COMMANDTYPE cmd, OMX_U32 data) {
             CHECK_EQ(countBuffersWeOwn(mPortBuffers[portIndex]),
                      mPortBuffers[portIndex].size());
 
+            if(mState == ERROR) {
+                CODEC_LOGI("Ignoring OMX_CommandFlush in ERROR state");
+                break;
+            }
             if (mState == RECONFIGURING) {
                 CHECK_EQ(portIndex, kPortIndexOutput);
 
