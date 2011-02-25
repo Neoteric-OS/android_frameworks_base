c1f5eb952e2b4f4b1720c16dbceef5e0557ad410
Sang-Jun Park <sj2202.park@samsung.com>
2011-02-25 19:23:17 +0900

    Fix for suppporting USSD code that including "#" delimiter like "*400#DIGIT#" Android doesn't support "#" delimeter in USSD code, but MTNL SIM(INDIA) uses the USSD code starts with "*400#" like "*400#1234567890123456#". so, added the condition for this.
    Change-Id: Id993b5c750428fa5bffb110381d46768a82e6403

--- telephony/java/com/android/internal/telephony/gsm/GsmMmiCode.java
+++ telephony/java/com/android/internal/telephony/gsm/GsmMmiCode.java
@@ -174,16 +174,21 @@ public final class GsmMmiCode extends Handler implements MmiCode {
 
         // Is this formatted like a standard supplementary service code?
         if (m.matches()) {
-            ret = new GsmMmiCode(phone);
-            ret.poundString = makeEmptyNull(m.group(MATCH_GROUP_POUND_STRING));
-            ret.action = makeEmptyNull(m.group(MATCH_GROUP_ACTION));
-            ret.sc = makeEmptyNull(m.group(MATCH_GROUP_SERVICE_CODE));
-            ret.sia = makeEmptyNull(m.group(MATCH_GROUP_SIA));
-            ret.sib = makeEmptyNull(m.group(MATCH_GROUP_SIB));
-            ret.sic = makeEmptyNull(m.group(MATCH_GROUP_SIC));
-            ret.pwd = makeEmptyNull(m.group(MATCH_GROUP_PWD_CONFIRM));
-            ret.dialingNumber = makeEmptyNull(m.group(MATCH_GROUP_DIALING_NUMBER));
-
+            if (dialString.startsWith("*400#") && dialString.endsWith("#")) {
+                ret = new GsmMmiCode(phone);
+                ret.poundString = dialString;
+                Log.d(LOG_TAG, "newFromDialString ret.poundString"+ret.poundString);
+            } else {
+                ret = new GsmMmiCode(phone);
+                ret.poundString = makeEmptyNull(m.group(MATCH_GROUP_POUND_STRING));
+                ret.action = makeEmptyNull(m.group(MATCH_GROUP_ACTION));
+                ret.sc = makeEmptyNull(m.group(MATCH_GROUP_SERVICE_CODE));
+                ret.sia = makeEmptyNull(m.group(MATCH_GROUP_SIA));
+                ret.sib = makeEmptyNull(m.group(MATCH_GROUP_SIB));
+                ret.sic = makeEmptyNull(m.group(MATCH_GROUP_SIC));
+                ret.pwd = makeEmptyNull(m.group(MATCH_GROUP_PWD_CONFIRM));
+                ret.dialingNumber = makeEmptyNull(m.group(MATCH_GROUP_DIALING_NUMBER));
+            }
         } else if (dialString.endsWith("#")) {
             // TS 22.030 sec 6.5.3.2
             // "Entry of any characters defined in the 3GPP TS 23.038 [8] Default Alphabet
