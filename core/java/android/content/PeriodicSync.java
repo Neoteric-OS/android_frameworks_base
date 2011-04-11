4244a7e75c83b90c9e8126a12b7010fd248c4c05
Jozef BABJAK <jozef.babjak@gmail.com>
2011-04-11 13:57:52 +0200

    Adding proper hashCode() implementation for PeriodicSync class.
    This class overrides equals(Object), but does not
    override hashCode(), and inherits the implementation
    of hashCode() from java.lang.Object. Therefore, the
    class is very likely to violate the invariant that
    equal objects must have equal hashcodes.
    
    This patch adds a propert hashCode() method implementaion
    compliant with general contract of equals() and hashCode()
    methods.
    
    Change-Id: I6f17daf95bed785ed36c4afaed53ecbd508ea434

--- core/java/android/content/PeriodicSync.java
+++ core/java/android/content/PeriodicSync.java
@@ -81,4 +81,13 @@ public class PeriodicSync implements Parcelable {
                 && period == other.period
                 && SyncStorageEngine.equals(extras, other.extras);
     }
+
+    @Override
+    public int hashCode() {
+        int result = account != null ? account.hashCode() : 0;
+        result = 31 * result + (authority != null ? authority.hashCode() : 0);
+        result = 31 * result + (extras != null ? extras.hashCode() : 0);
+        result = 31 * result + (int) (period ^ (period >>> 32));
+        return result;
+    }
 }
