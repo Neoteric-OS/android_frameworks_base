8f561f3d435b643d71020b94089f394f3df3730b
Martin Storsjo <martin@martin.st>
2011-05-18 12:29:28 +0300

    stagefright amrnb: Restart the p_xn pointer when retraversing the array on overflow
    This is a safeguard to avoid issues in a rare codepath.
    
    This was committed on the master branch of the opencore
    repository in commit f532d145194e474cb30d8644b8eee83873ad24cc,
    based on AOSP contribution 10905.
    
    Change-Id: I9688b009bcbe63d88ee5040e883a021aefa30664

--- media/libstagefright/codecs/amrnb/enc/src/g_pitch.cpp
+++ media/libstagefright/codecs/amrnb/enc/src/g_pitch.cpp
@@ -346,8 +346,9 @@ Word16 G_pitch(         /* o : Gain of pitch lag saturated to 1.2       */
     }
     else
     {
-        s = 0;                      /* Avoid case of all zeros */
+        s = 0;  /* re-initialize calculations '/
         p_y1 = &y1[0];
+        p_xn = &xn[0];
         for (i = (L_subfr >> 1); i != 0; i--)
         {
             tmp = *(p_y1++) >> 2;
