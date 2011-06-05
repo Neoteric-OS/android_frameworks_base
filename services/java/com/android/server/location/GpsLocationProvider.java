1f48efd4ca44b4fe53609ddf7de6cf4342202cff
Kevin Tang <zhikait@codeaurora.org>
2011-06-05 14:30:23 -0700

    GPS Provider Service changes
    GPS engine needs to receive the current APN from Android fw.
    
    Added db query for the current APN, also added a new parameter
    between JNI / HAL to the new method in AGpsRilInterface struct
    for gps engine to receive APN from GPSLocationService
    
    Change-Id: I5ccb7c738cb63c489fa387a46f753613e2a4585f

--- services/java/com/android/server/location/GpsLocationProvider.java
+++ services/java/com/android/server/location/GpsLocationProvider.java
@@ -34,6 +34,7 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.NetworkUtils;
 import android.net.SntpClient;
+import android.net.Uri;
 import android.os.Binder;
 import android.os.Bundle;
 import android.os.Handler;
@@ -47,12 +48,15 @@ import android.os.ServiceManager;
 import android.os.SystemClock;
 import android.os.WorkSource;
 import android.provider.Settings;
+import android.provider.Telephony.Carriers;
 import android.provider.Telephony.Sms.Intents;
 import android.telephony.SmsMessage;
 import android.telephony.TelephonyManager;
 import android.telephony.gsm.GsmCellLocation;
 import android.util.Log;
 import android.util.SparseIntArray;
+
+import android.database.Cursor;
 
 import com.android.internal.app.IBatteryStats;
 import com.android.internal.location.GpsNetInitiatedHandler;
@@ -503,9 +507,15 @@ public class GpsLocationProvider implements LocationProviderInterface {
             boolean dataEnabled = Settings.Secure.getInt(mContext.getContentResolver(),
                                                          Settings.Secure.MOBILE_DATA, 1) == 1;
             boolean networkAvailable = info.isAvailable() && dataEnabled;
+            String default_apn = getSelectedApn();
+            if (null == default_apn)
+            {
+                default_apn = "dummy-apn";
+            }
 
             native_update_network_state(info.isConnected(), info.getType(),
-                    info.isRoaming(), networkAvailable, info.getExtraInfo());
+                                        info.isRoaming(), networkAvailable,
+                                        info.getExtraInfo(), default_apn);
         }
 
         if (info != null && info.getType() == ConnectivityManager.TYPE_MOBILE_SUPL
@@ -1605,6 +1615,25 @@ public class GpsLocationProvider implements LocationProviderInterface {
         }
     }
 
+    private String getSelectedApn() {
+        Uri uri = Uri.parse("content://telephony/carriers/preferapn");
+        String apn = null;
+
+        Cursor cursor = mContext.getContentResolver().query(uri, new String[] {"apn"},
+                null, null, Carriers.DEFAULT_SORT_ORDER);
+
+        if (null != cursor) {
+            try {
+                if (cursor.moveToFirst()) {
+                    apn = cursor.getString(0);
+                }
+            } finally {
+                cursor.close();
+            }
+        }
+        return apn;
+    }
+
     // for GPS SV statistics
     private static final int MAX_SVS = 32;
     private static final int EPHEMERIS_MASK = 0;
@@ -1663,5 +1692,5 @@ public class GpsLocationProvider implements LocationProviderInterface {
     private native void native_agps_set_id(int type, String setid);
 
     private native void native_update_network_state(boolean connected, int type,
-            boolean roaming, boolean available, String extraInfo);
+            boolean roaming, boolean available, String extraInfo, String defaultAPN);
 }
--- services/jni/com_android_server_location_GpsLocationProvider.cpp
+++ services/jni/com_android_server_location_GpsLocationProvider.cpp
@@ -540,7 +540,7 @@ static jstring android_location_GpsLocationProvider_get_internal_state(JNIEnv* e
 }
 
 static void android_location_GpsLocationProvider_update_network_state(JNIEnv* env, jobject obj,
-        jboolean connected, int type, jboolean roaming, jboolean available, jstring extraInfo)
+        jboolean connected, int type, jboolean roaming, jboolean available, jstring extraInfo, jstring apn)
 {
 
     if (sAGpsRilInterface && sAGpsRilInterface->update_network_state) {
@@ -553,7 +553,9 @@ static void android_location_GpsLocationProvider_update_network_state(JNIEnv* en
         }
 
         if (sAGpsRilInterface->update_network_vailability) {
-            sAGpsRilInterface->update_network_vailability(available);
+            const char *c_apn = env->GetStringUTFChars(apn, NULL);
+            sAGpsRilInterface->update_network_vailability(available, c_apn);
+            env->ReleaseStringUTFChars(apn, c_apn);
         }
     }
 }
@@ -583,7 +585,7 @@ static JNINativeMethod sMethods[] = {
     {"native_send_ni_response", "(II)V", (void*)android_location_GpsLocationProvider_send_ni_response},
     {"native_agps_ni_message", "([BI)V", (void *)android_location_GpsLocationProvider_agps_send_ni_message},
     {"native_get_internal_state", "()Ljava/lang/String;", (void*)android_location_GpsLocationProvider_get_internal_state},
-    {"native_update_network_state", "(ZIZZLjava/lang/String;)V", (void*)android_location_GpsLocationProvider_update_network_state },
+    {"native_update_network_state", "(ZIZZLjava/lang/String;Ljava/lang/String;)V", (void*)android_location_GpsLocationProvider_update_network_state },
 };
 
 int register_android_server_location_GpsLocationProvider(JNIEnv* env)
