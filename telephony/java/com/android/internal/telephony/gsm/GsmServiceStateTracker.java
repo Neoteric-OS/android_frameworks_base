d9fc225fc3ea9a2fb59ccdea7fa8de61dceb4bb3
Banavathu, Srinivas Naik <snb@codeaurora.org>
2012-01-19 01:07:31 +0530

    Managed Roaming support.
    If the network registration is denied with reject cause "Persistent location
    update reject" in Manual Network Selection mode, "Managed Roaming" dialog is
    displayed. User can selection an operator from the available Networks list.
    
    Change-Id: I78fb07a662fe963dbb75a75ff09811399c7dcee0

--- telephony/java/com/android/internal/telephony/gsm/GsmServiceStateTracker.java
+++ telephony/java/com/android/internal/telephony/gsm/GsmServiceStateTracker.java
@@ -30,12 +30,14 @@ import com.android.internal.telephony.TelephonyIntents;
 import com.android.internal.telephony.TelephonyProperties;
 
 import android.app.AlarmManager;
+import android.app.AlertDialog;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.res.Resources;
@@ -48,6 +50,7 @@ import android.os.Registrant;
 import android.os.RegistrantList;
 import android.os.SystemClock;
 import android.os.SystemProperties;
+import android.preference.PreferenceManager;
 import android.provider.Settings;
 import android.provider.Settings.SettingNotFoundException;
 import android.provider.Telephony.Intents;
@@ -58,11 +61,15 @@ import android.text.TextUtils;
 import android.util.EventLog;
 import android.util.Log;
 import android.util.TimeUtils;
+import android.view.KeyEvent;
+import android.view.WindowManager;
 
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.TimeZone;
+
+import com.android.internal.R;
 
 /**
  * {@hide}
@@ -88,6 +95,10 @@ final class GsmServiceStateTracker extends ServiceStateTracker {
      * handlePollStateResult to store CREG roaming result.
      */
     private boolean mGsmRoaming = false;
+
+    // Key used to read and write the saved network selection numeric value
+    private static final String NETWORK_SELECTION_KEY = "network_selection_key";
+    private static boolean isManagedRoamingDialogDisplayed = false;
 
     /**
      * Data roaming status solely based on TS 27.007 10.1.19 CGREG. Only used by
@@ -585,6 +596,19 @@ final class GsmServiceStateTracker extends ServiceStateTracker {
                     }
 
                     mGsmRoaming = regCodeIsRoaming(regState);
+
+                    if ((regState == 3 || regState == 13) && (states.length >= 14)) {
+                        try {
+                            int rejCode = Integer.parseInt(states[13]);
+                            // Check if rejCode is "Persistent location update reject",
+                            if (rejCode == 10) {
+                                createManagedRoamingDialog();
+                            }
+                        } catch (NumberFormatException ex) {
+                            Log.w(LOG_TAG, "error parsing regCode: " + ex);
+                        }
+                    }
+
                     newSS.setState (regCodeToServiceState(regState));
 
                     if (regState == 10 || regState == 12 || regState == 13 || regState == 14) {
@@ -1599,6 +1623,65 @@ final class GsmServiceStateTracker extends ServiceStateTracker {
             notificationManager.notify(notificationId, mNotification);
         }
     }
+
+    /*
+     * Show Managed Roaming dialog if user preferred Network Selection mode is 'Manual'
+     */
+    private void createManagedRoamingDialog() {
+        Resources r = Resources.getSystem();
+        String networkSelection = PreferenceManager.getDefaultSharedPreferences(phone.getContext())
+            .getString(NETWORK_SELECTION_KEY, "");
+
+        Log.i(LOG_TAG, "Managed Roaming case, networkSelection " + networkSelection);
+        // networkSelection will be empty for 'Automatic' mode.
+        if (!TextUtils.isEmpty(networkSelection) && !isManagedRoamingDialogDisplayed) {
+            Log.i(LOG_TAG, "Show Managed Roaming Dialog");
+            AlertDialog managedRoamingDialog = new AlertDialog.Builder(phone.getContext())
+                    .setTitle(r.getString(R.string.managed_roaming_title))
+                    .setMessage(r.getString(R.string.managed_roaming_dialog_content))
+                    .setPositiveButton(r.getString(R.string.managed_roaming_dialog_ok_button),
+                        onManagedRoamingDialogClick)
+                    .setNegativeButton(r.getString(R.string.managed_roaming_dialog_cancel_button),
+                        onManagedRoamingDialogClick)
+                    .create();
+
+            managedRoamingDialog.setOnKeyListener(mManagedRoamingDialogOnKeyListener);
+            isManagedRoamingDialogDisplayed = true;
+            managedRoamingDialog.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
+            managedRoamingDialog.show();
+        }
+    }
+
+    DialogInterface.OnClickListener onManagedRoamingDialogClick =
+        new DialogInterface.OnClickListener() {
+        public void onClick(DialogInterface dialog, int which) {
+            dialog.dismiss();
+            switch (which) {
+                case DialogInterface.BUTTON_POSITIVE:
+                    Intent networkSettingIntent = new Intent(Intent.ACTION_MAIN);
+                    networkSettingIntent.setClassName("com.android.phone",
+                            "com.android.phone.NetworkSetting");
+                    networkSettingIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    phone.getContext().startActivity(networkSettingIntent);
+                    break;
+
+                case DialogInterface.BUTTON_NEGATIVE:
+                    break;
+            }
+            isManagedRoamingDialogDisplayed = false;
+        }
+    };
+
+    DialogInterface.OnKeyListener mManagedRoamingDialogOnKeyListener =
+        new DialogInterface.OnKeyListener() {
+        public boolean onKey(DialogInterface dialog, int keyCode, KeyEvent event) {
+            // Handle the back key to reset the global variable.
+            if (keyCode == KeyEvent.KEYCODE_BACK) {
+                isManagedRoamingDialogDisplayed = false;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void log(String s) {
