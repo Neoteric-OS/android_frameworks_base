4ed2eb46a23f5adae77c6b0e7e0971e631fc8cb9
Kito Cheng <kito@0xlab.org>
2012-01-20 02:35:10 +0800

    Aprof support for JNI libraries
    Signed-off-by: Kito Cheng <kito@0xlab.org>
    
    Change-Id: If958d11bac3506c4616a90d2c74f6d9d0aa6d02d

--- core/java/android/app/ActivityThread.java
+++ core/java/android/app/ActivityThread.java
@@ -130,6 +130,16 @@ final class RemoteServiceException extends AndroidRuntimeException {
     }
 }
 
+final class Aprof {
+    static {
+        System.loadLibrary("aprof_jni_runtime");
+    }
+    public static final String TAG = "aprof";
+    public static final int APROF_ON = 1;
+    public static final int APROF_OFF = 0;
+    public static native void control(int c);
+}
+
 /**
  * This manages the execution of the main thread in an
  * application process, scheduling and executing activities,
@@ -2212,6 +2222,7 @@ public final class ActivityThread {
     }
 
     private void handleLaunchActivity(ActivityClientRecord r, Intent customIntent) {
+        Aprof.control(Aprof.APROF_ON);
         // If we are getting ready to gc after going to the background, well
         // we are back active so skip it.
         unscheduleGcIdler();
@@ -3206,6 +3217,7 @@ public final class ActivityThread {
         info.activity = r;
         info.state = r.state;
         mH.post(info);
+        Aprof.control(Aprof.APROF_OFF);
     }
 
     final void performRestartActivity(IBinder token) {
@@ -3521,6 +3533,7 @@ public final class ActivityThread {
                 // If the system process has died, it's game over for everyone.
             }
         }
+        Aprof.control(Aprof.APROF_OFF);
     }
 
     public final void requestRelaunchActivity(IBinder token,
@@ -3580,6 +3593,7 @@ public final class ActivityThread {
     }
 
     private void handleRelaunchActivity(ActivityClientRecord tmp) {
+        Aprof.control(Aprof.APROF_ON);
         // If we are getting ready to gc after going to the background, well
         // we are back active so skip it.
         unscheduleGcIdler();
