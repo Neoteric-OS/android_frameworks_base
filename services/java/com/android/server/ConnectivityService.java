a362662b13c95e82e2a7844ddc23847a0c75ab86
Daniel Drown <dan-android@drown.org>
2012-02-16 09:54:54 -0600

    Start clatd (464xlat) when on a ipv6-only default pdp data context
    This adds the class com.android.internal.telephony.Nat464xlatService which
    listens for connectivity change broadcast intents to determine when to
    start/stop clatd.  When clatd detects dns64/nat64 are in use, it creates
    a network interface, "clat".  Nat464xlatService listens for an interfaceAdded
    to signal clat is running, and broadcasts the intent
    com.android.server.connectivity.nat464xlatservice
    
    LinkProperties was modified to include the type of connection (IP/IPV6/IPV4V6)
    as well as being able to handle a different network interface for v4 and v6
    traffic.
    
    Tethering was modified to tether to the v4 interface (since tethering is
    currently v4 only)
    
    ConnectivityService was modified to choose the interface to route out based on
    protocol (used by MMS, HIPRI, and other services when a data connection is
    already active).
    
    MobileDataStateTracker listens for the nat464xlatservice broadcast intent to
    update the relevant connection's LinkProperties
    
    Change-Id: I2dd8ed9c7f18c654cc20343120f1f4c23559138c
    Signed-off-by: Daniel Drown <dan-android@drown.org>
    Depends-on: I4b041d1f13c25108476621d2e2a1e5bac69841f5

--- services/java/com/android/server/ConnectivityService.java
+++ services/java/com/android/server/ConnectivityService.java
@@ -98,6 +98,7 @@ import com.android.internal.net.VpnProfile;
 import com.android.internal.telephony.Phone;
 import com.android.internal.telephony.PhoneConstants;
 import com.android.internal.util.IndentingPrintWriter;
+import com.android.internal.telephony.Nat464xlatService;
 import com.android.server.am.BatteryStatsService;
 import com.android.server.connectivity.Tethering;
 import com.android.server.connectivity.Vpn;
@@ -156,6 +157,8 @@ public class ConnectivityService extends IConnectivityManager.Stub {
 
     private boolean mLockdownEnabled;
     private LockdownVpnTracker mLockdownTracker;
+
+    private Nat464xlatService mNat464xlatService;
 
     /** Lock around {@link #mUidRules} and {@link #mMeteredIfaces}. */
     private Object mRulesLock = new Object();
@@ -534,6 +537,8 @@ public class ConnectivityService extends IConnectivityManager.Stub {
 
         mVpn = new Vpn(mContext, mVpnCallback, mNetd);
         mVpn.startMonitoring(mContext, mTrackerHandler);
+
+	mNat464xlatService = new Nat464xlatService(mContext, nmService, this);
 
         try {
             mNetd.registerObserver(mTethering);
@@ -1397,11 +1402,11 @@ public class ConnectivityService extends IConnectivityManager.Stub {
     }
 
     private boolean addRoute(LinkProperties p, RouteInfo r, boolean toDefaultTable) {
-        return modifyRoute(p.getInterfaceName(), p, r, 0, ADD, toDefaultTable);
+        return modifyRoute(p, r, 0, ADD, toDefaultTable);
     }
 
     private boolean removeRoute(LinkProperties p, RouteInfo r, boolean toDefaultTable) {
-        return modifyRoute(p.getInterfaceName(), p, r, 0, REMOVE, toDefaultTable);
+        return modifyRoute(p, r, 0, REMOVE, toDefaultTable);
     }
 
     private boolean addRouteToAddress(LinkProperties lp, InetAddress addr) {
@@ -1427,13 +1432,13 @@ public class ConnectivityService extends IConnectivityManager.Stub {
                 bestRoute = RouteInfo.makeHostRoute(addr, bestRoute.getGateway());
             }
         }
-        return modifyRoute(lp.getInterfaceName(), lp, bestRoute, 0, doAdd, toDefaultTable);
+        return modifyRoute(lp, bestRoute, 0, doAdd, toDefaultTable);
     }
 
-    private boolean modifyRoute(String ifaceName, LinkProperties lp, RouteInfo r, int cycleCount,
+    private boolean modifyRoute(LinkProperties lp, RouteInfo r, int cycleCount,
             boolean doAdd, boolean toDefaultTable) {
-        if ((ifaceName == null) || (lp == null) || (r == null)) {
-            if (DBG) log("modifyRoute got unexpected null: " + ifaceName + ", " + lp + ", " + r);
+        if ((lp == null) || (r == null)) {
+            if (DBG) log("modifyRoute got unexpected null: " + lp + ", " + r);
             return false;
         }
 
@@ -1441,6 +1446,17 @@ public class ConnectivityService extends IConnectivityManager.Stub {
             loge("Error modifying route - too much recursion");
             return false;
         }
+
+	String ifaceName;
+	if(r.getDestination().getAddress() instanceof Inet4Address) {
+	    ifaceName = lp.getIPv4InterfaceName();
+	} else {
+	    ifaceName = lp.getIPv6InterfaceName();
+	}
+	if(ifaceName == null) {
+	    loge("Error modifying route - no interface name");
+	    return false;
+	}
 
         if (r.isHostRoute() == false) {
             RouteInfo bestRoute = RouteInfo.selectBestRoute(lp.getRoutes(), r.getGateway());
@@ -1453,7 +1469,7 @@ public class ConnectivityService extends IConnectivityManager.Stub {
                     // route to it's gateway
                     bestRoute = RouteInfo.makeHostRoute(r.getGateway(), bestRoute.getGateway());
                 }
-                modifyRoute(ifaceName, lp, bestRoute, cycleCount+1, doAdd, toDefaultTable);
+                modifyRoute(lp, bestRoute, cycleCount+1, doAdd, toDefaultTable);
             }
         }
         if (doAdd) {
