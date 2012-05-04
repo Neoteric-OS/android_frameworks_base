8c3a05a4ccdbe6b752e36e64def21a818b627730
Ovidiu Beldie <ovidiux.beldie@intel.com>
2012-05-04 16:17:11 +0200

    [BT]: fixes null pointer issue in BluetoothService
    This null dereference issue was detected by Klockwork.
    The fix consists in adding a sanity check.
    
    Change-Id: Ia295892e373bae3c69a12386233ba5f71cd3bd4c
    Author: Ovidiu Beldie <ovidiux.beldie@intel.com>
    Signed-off-by: Ovidiu Beldie <ovidiux.beldie@intel.com>
    Singed-off-by: Shuo Gao <shuo.gao@intel.com>
    Signed-off-by: Bruce Beare <bruce.j.beare@intel.com>
    Signed-off-by: Jack Ren <jack.ren@intel.com>
    Author-tracking-BZ: 34725

--- core/java/android/server/BluetoothService.java
+++ core/java/android/server/BluetoothService.java
@@ -645,13 +645,10 @@ public class BluetoothService extends IBluetooth.Stub {
     /*package*/ void initBluetoothAfterTurningOn() {
         String discoverable = getProperty("Discoverable", false);
         String timeout = getProperty("DiscoverableTimeout", false);
-        if (timeout == null) {
-            Log.w(TAG, "Null DiscoverableTimeout property");
-            // assign a number, anything not 0
-            timeout = "1";
-        }
-        if (discoverable.equals("true") && Integer.valueOf(timeout) != 0) {
-            setAdapterPropertyBooleanNative("Discoverable", 0);
+        if ((discoverable != null) && (timeout != null)) {
+            if (discoverable.equals("true") && !timeout.equals("null") && Integer.valueOf(timeout) != 0) {
+                setAdapterPropertyBooleanNative("Discoverable", 0);
+            }
         }
         mBondState.initBondState();
         initProfileState();
