14bfe3266bff3961dd8d777ec5273eb2e2bb550d
Mårten Kongstad <marten.kongstad@sonymobile.com>
2012-07-23 16:47:00 +0200

    Runtime resource overlay for any package
    This commit allows packages other than framework-res.apk to be targeted
    by runtime resource overlay. For reasons related to security (package
    signatures) and version matching (is version A of a package compatible
    with version B of an overlay package?), overlay packages are required
    to be installed in /vendor/overlay/{system,app}. This constraint may be
    gradually lifted in future patches by adding the missing support.
    
    Overlay packages and their targets are matched based on package name;
    overlay packages specify their target and priority relative other
    overlay packages in their manifest files:
    
        <manifest ...>
            <overlay target="com.foo.bar" priority="1234"/>
            ...
        </manifest>
    
    Resources from overlay packages with higher priority number have
    precedence over those with lower.
    
    To simplify package loading by the (native) AssetManager, each overlay
    contains a symlink from a directory in /data/system/overlay/ to its
    idmap file. A side benefit of this design is that overlays may be
    disabled [enabled] by the removal [creation] of a single symlink, while
    its package remains installed.
    
    Change-Id: Ic8d6b845f149d737357b7f044a9ef683e72165bc

--- cmds/installd/installd.c
+++ cmds/installd/installd.c
@@ -134,7 +134,7 @@ static int do_unlinklib(char **arg, char reply[REPLY_MAX])
 
 static int do_idmap(char **arg, char reply[REPLY_MAX])
 {
-    return idmap(arg[0], arg[1], atoi(arg[2]));
+    return idmap(arg[0], arg[1], atoi(arg[2]), atoi(arg[3]));
 }
 
 struct cmdinfo {
@@ -163,7 +163,7 @@ struct cmdinfo cmds[] = {
     { "mkuserdata",           3, do_mk_user_data },
     { "rmuser",               1, do_rm_user },
     { "cloneuserdata",        3, do_clone_user_data },
-    { "idmap",                3, do_idmap },
+    { "idmap",                4, do_idmap },
 };
 
 static int readx(int s, void *_buf, int count)
