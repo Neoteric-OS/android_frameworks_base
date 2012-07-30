1f1e8c4fd28811866bae596d4f13965ce3819475
yijunx.zhu <yijunx.zhu@intel.com>
2012-07-30 10:21:29 -0400

    [ACS-Monkey] Javacrash java.lang.NullPointerException in com.android.browser.
    Provide a WebView interface in WebSettings, for BrowserSettings need to access it.
    
    Change-Id: I375561867f3eb05681d579dfa173b5e298f7649e
    Author: Yijun Zhu <yijunx.zhu@intel.com>
    Signed-off-by: Yijun Zhu <yijunx.zhu@intel.com>
    Signed-off-by: Shuo Gao <shuo.gao@intel.com>
    Signed-off-by: Bruce Beare <bruce.j.beare@intel.com>
    Signed-off-by: Jack Ren <jack.ren@intel.com>
    Author-tracking-BZ: 34962

--- core/java/android/webkit/WebSettingsClassic.java
+++ core/java/android/webkit/WebSettingsClassic.java
@@ -1660,6 +1660,14 @@ public class WebSettingsClassic extends WebSettings {
         return mAutoFillProfile;
     }
 
+    /**
+     * @hide
+     * @return
+     */
+    public synchronized WebViewClassic getWebView(){
+        return mWebView;
+    }
+
     int getDoubleTapToastCount() {
         return mDoubleTapToastCount;
     }
