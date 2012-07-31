2ba16ba5f52bedf806ba3de190530e4bdf338238
Alex Yakavenka <ayakav@codeaurora.org>
2012-07-31 16:42:01 -0700

    Telephony: Move uicc classes into uicc package
    Reduce constructor visibility to package where
    possible
    
    Change-Id: I80204a2f3dc57cac875abeab390bb9db7a636ff7

--- telephony/java/com/android/internal/telephony/IccCardConstants.java
+++ telephony/java/com/android/internal/telephony/IccCardConstants.java
@@ -22,6 +22,8 @@ public class IccCardConstants {
 
     /* The extra data for broacasting intent INTENT_ICC_STATE_CHANGE */
     public static final String INTENT_KEY_ICC_STATE = "ss";
+    /* UNKNOWN means the ICC state is unknown */
+    static public final String INTENT_VALUE_ICC_UNKNOWN = "UNKNOWN";
     /* NOT_READY means the ICC interface is not ready (eg, radio is off or powering on) */
     public static final String INTENT_VALUE_ICC_NOT_READY = "NOT_READY";
     /* ABSENT means ICC is missing */
@@ -68,6 +70,32 @@ public class IccCardConstants {
             return ((this == PIN_REQUIRED) || (this == PUK_REQUIRED)
                     || (this == NETWORK_LOCKED) || (this == READY)
                     || (this == PERM_DISABLED));
+        }
+        public String getIntentString() {
+            switch (this) {
+                case ABSENT: return INTENT_VALUE_ICC_ABSENT;
+                case PIN_REQUIRED: return INTENT_VALUE_ICC_LOCKED;
+                case PUK_REQUIRED: return INTENT_VALUE_ICC_LOCKED;
+                case NETWORK_LOCKED: return INTENT_VALUE_ICC_LOCKED;
+                case READY: return INTENT_VALUE_ICC_READY;
+                case NOT_READY: return INTENT_VALUE_ICC_NOT_READY;
+                case PERM_DISABLED: return INTENT_VALUE_ICC_LOCKED;
+                default: return INTENT_VALUE_ICC_UNKNOWN;
+            }
+        }
+
+        /**
+         * Locked state have a reason (PIN, PUK, NETWORK, PERM_DISABLED)
+         * @return reason
+         */
+        public String getReason() {
+            switch (this) {
+                case PIN_REQUIRED: return INTENT_VALUE_LOCKED_ON_PIN;
+                case PUK_REQUIRED: return INTENT_VALUE_LOCKED_ON_PUK;
+                case NETWORK_LOCKED: return INTENT_VALUE_LOCKED_NETWORK;
+                case PERM_DISABLED: return INTENT_VALUE_ABSENT_ON_PERM_DISABLED;
+                default: return null;
+           }
         }
     }
 }
