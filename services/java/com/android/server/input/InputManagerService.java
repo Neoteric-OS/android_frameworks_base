6f86417196e9d74217819a873cbf498d1cb04145
Jason Gerecke <killertofu@gmail.com>
2012-09-11 16:28:06 -0700

    Allow persistence of input device calibration
    This patch extends the PersistentDataStore store to read and write
    input device calibration data. A new SET_INPUT_CALIBRATION permission
    grants apps the ability to update this information, and a new
    TouchCalibration class is used to wrap the raw calibration data.
    
    Change-Id: I0582cd7da962857a9033a3e5a4f9efa4596e007d

--- services/java/com/android/server/input/InputManagerService.java
+++ services/java/com/android/server/input/InputManagerService.java
@@ -46,6 +46,7 @@ import android.hardware.input.IInputManager;
 import android.hardware.input.IInputDevicesChangedListener;
 import android.hardware.input.InputManager;
 import android.hardware.input.KeyboardLayout;
+import android.hardware.input.TouchCalibration;
 import android.os.Binder;
 import android.os.Bundle;
 import android.os.Environment;
@@ -635,6 +636,37 @@ public class InputManagerService extends IInputManager.Stub implements Watchdog.
             }
         }
         mTempFullKeyboards.clear();
+    }
+
+    @Override // Binder call
+    public TouchCalibration getTouchCalibrationForInputDevice(String inputDeviceDescriptor) {
+        if (inputDeviceDescriptor == null) {
+            throw new IllegalArgumentException("inputDeviceDescriptor must not be null");
+        }
+
+        synchronized (mDataStore) {
+            return mDataStore.getTouchCalibration(inputDeviceDescriptor);
+        }
+    }
+
+    @Override // Binder call
+    public void setTouchCalibrationForInputDevice(String inputDeviceDescriptor,
+            TouchCalibration calibration) {
+        if (!checkCallingPermission(android.Manifest.permission.SET_INPUT_CALIBRATION,
+                "setTouchCalibrationForInputDevice()")) {
+            throw new SecurityException("Requires SET_INPUT_CALIBRATION permission");
+        }
+        if (inputDeviceDescriptor == null) {
+            throw new IllegalArgumentException("inputDeviceDescriptor must not be null");
+        }
+
+        synchronized (mDataStore) {
+            try {
+                mDataStore.setTouchCalibration(inputDeviceDescriptor, calibration);
+            } finally {
+                mDataStore.saveIfNeeded();
+            }
+        }
     }
 
     // Must be called on handler.
