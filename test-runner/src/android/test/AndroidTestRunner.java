69bd1b30c051696c278a54b6b88b9a218a509541
Kenny Root <kroot@google.com>
2012-10-05 23:26:04 -0700

    EXPERIMENTAL: find OpenSSL error leaks
    Change-Id: I2a1a85f9f63f87c2ca063d721386b1b5e9b48f03

--- test-runner/src/android/test/AndroidTestRunner.java
+++ test-runner/src/android/test/AndroidTestRunner.java
@@ -21,6 +21,9 @@ import android.content.Context;
 import android.os.PerformanceCollector.PerformanceResultsWriter;
 
 import com.google.android.collect.Lists;
+
+import org.apache.harmony.xnet.provider.jsse.NativeCrypto;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestListener;
@@ -177,6 +180,28 @@ public class AndroidTestRunner extends BaseTestRunner {
 
     public void runTest(TestResult testResult) {
         mTestResult = testResult;
+
+        mTestResult.addListener(new TestListener() {
+
+            @Override
+            public void startTest(Test test) {
+            }
+
+            @Override
+            public void endTest(Test test) {
+                if (NativeCrypto.ERR_peek_last_error() != 0) {
+                    mTestResult.addError(test, new Exception("Lingering OpenSSL errors"));
+                }
+            }
+
+            @Override
+            public void addFailure(Test test, AssertionFailedError t) {
+            }
+
+            @Override
+            public void addError(Test test, Throwable t) {
+            }
+        });
 
         for (TestListener testListener : mTestListeners) {
             mTestResult.addListener(testListener);
