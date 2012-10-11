036c83c1e38bd7e69ee65ed9dc35fc203f65466f
Cuihtlauac ALVARADO <cuihtlauac.alvarado@orange.com>
2012-10-11 10:12:28 +0200

    Expose requestRouteToHostAddress(int, InetAddress)
    Public API change
    
    The method requestRouteToHost(int, int) force apps to manually
    convert InetAddress into an int, which is reverted back into an
    InetAddress internally. Additionally, int is not IPv6 friendly.
    
    Fix public issue http://code.google.com/p/android/issues/detail?id=7376
    by exposing requestRouteToHostAddress(int, java.net.InetAddress).
    
    Change-Id: I44e47a26519b4c4722ea2f998aec497c0cea67d4

--- core/java/android/net/ConnectivityManager.java
+++ core/java/android/net/ConnectivityManager.java
@@ -557,11 +557,12 @@ public class ConnectivityManager {
      * Ensure that a network route exists to deliver traffic to the specified
      * host via the specified network interface. An attempt to add a route that
      * already exists is ignored, but treated as successful.
+     * <p>This method requires the caller to hold the permission
+     * {@link android.Manifest.permission#CHANGE_NETWORK_STATE}.
      * @param networkType the type of the network over which traffic to the specified
      * host is to be routed
      * @param hostAddress the IP address of the host to which the route is desired
      * @return {@code true} on success, {@code false} on failure
-     * @hide
      */
     public boolean requestRouteToHostAddress(int networkType, InetAddress hostAddress) {
         byte[] address = hostAddress.getAddress();
