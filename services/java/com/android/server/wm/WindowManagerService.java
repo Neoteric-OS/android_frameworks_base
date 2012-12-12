54dd9659b1bfef0e45eab919e61c417caf75d8a6
takeda.masayuki <takeda.masayuki@sharp.co.jp>
2012-12-12 11:06:24 +0900

    Uploaded patch set 2.
    Change-Id: I28a506f17fcd99c801a3faa17cca66fffb291596

--- services/java/com/android/server/wm/WindowManagerService.java
+++ services/java/com/android/server/wm/WindowManagerService.java
@@ -161,6 +161,7 @@ import java.io.StringWriter;
 import java.net.Socket;
 import java.text.DateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -4919,23 +4920,33 @@ public class WindowManagerService extends IWindowManager.Stub
         final WindowList windows = win.getWindowList();
         final int NCW = win.mChildWindows.size();
         boolean added = false;
+
+        // Sort all child windows by ascending order of mSubLayer or mLayer.
+        final WindowState[] children =
+            (WindowState[]) win.mChildWindows.toArray(new WindowState[NCW]);
+        Arrays.sort(children, msWindowZOrderComparator);
+
         for (int j=0; j<NCW; j++) {
-            WindowState cwin = win.mChildWindows.get(j);
-            if (!added && cwin.mSubLayer >= 0) {
-                if (DEBUG_WINDOW_MOVEMENT) Slog.v(TAG, "Re-adding child window at "
-                        + index + ": " + cwin);
+            WindowState cwin = children[j];
+            // Find the position at which the parent window is.
+            if (!added && cwin.mSubLayer > 0) {
+                if (DEBUG_WINDOW_MOVEMENT) Slog.v(TAG, "Re-adding window at "
+                        + index + ": " + win);
                 win.mRebuilding = false;
                 windows.add(index, win);
                 index++;
                 added = true;
             }
-            if (DEBUG_WINDOW_MOVEMENT) Slog.v(TAG, "Re-adding window at "
+            // Children with negative sublayer should be before the parent,
+            // then the parent, then children with positive sublayer.
+            if (DEBUG_WINDOW_MOVEMENT) Slog.v(TAG, "Re-adding child window at "
                     + index + ": " + cwin);
             cwin.mRebuilding = false;
             windows.add(index, cwin);
             index++;
         }
         if (!added) {
+            // Parent has no children or all children are placed below parent.
             if (DEBUG_WINDOW_MOVEMENT) Slog.v(TAG, "Re-adding window at "
                     + index + ": " + win);
             win.mRebuilding = false;
@@ -4945,6 +4956,22 @@ public class WindowManagerService extends IWindowManager.Stub
         mWindowsChanged = true;
         return index;
     }
+
+    private static final java.util.Comparator<WindowState> msWindowZOrderComparator =
+            new java.util.Comparator<WindowState>() {
+        public int compare(final WindowState lhs, final WindowState rhs) {
+            if (lhs.mSubLayer != rhs.mSubLayer) {
+                return lhs.mSubLayer - rhs.mSubLayer;
+            }
+            if (lhs.mLayer == 0) {
+                return 1;
+            }
+            if (lhs.mLayer != rhs.mLayer) {
+                return lhs.mLayer - rhs.mLayer;
+            }
+            return 0;
+        }
+    };
 
     private final int reAddAppWindowsLocked(final DisplayContent displayContent, int index,
                                             WindowToken token) {
