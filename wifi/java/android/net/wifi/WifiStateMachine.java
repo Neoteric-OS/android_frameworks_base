789081f908b4916a74a490983632e375f4eca9eb
Bruno Randolf <br1@einfach.org>
2013-03-27 12:11:22 +0000

    Wifi: Get the list of supported channels
    Get the list of supported channels/frequencies from wpa_supplicant and provide
    them thru WifiManager.getSupportedChannels();
    
    One channel is represented thru the class WifiChannel which includes channel
    number, frequency in MHz and a flag if IBSS mode is allowed on this channel.
    
    Like isIbssSupported() we can only expect a result after WIFI_STATE_ENABLED has
    been reached.
    
    Change-Id: Ib32c8a1c78cb28933985f70aee7ce4ee2966d074

--- wifi/java/android/net/wifi/WifiStateMachine.java
+++ wifi/java/android/net/wifi/WifiStateMachine.java
@@ -154,6 +154,8 @@ public class WifiStateMachine extends StateMachine {
 
     private PowerManager.WakeLock mSuspendWakeLock;
 
+    private List<WifiChannel> mSupportedChannels;
+
     /**
      * Interval in milliseconds between polling for RSSI
      * and linkspeed information
@@ -355,6 +357,9 @@ public class WifiStateMachine extends StateMachine {
 
     /* Is IBSS mode supported by the driver? */
     public static final int CMD_GET_IBSS_SUPPORTED        = BASE + 135;
+
+    /* Get supported channels */
+    public static final int CMD_GET_SUPPORTED_CHANNELS    = BASE + 136;
 
     public static final int CONNECT_MODE                   = 1;
     public static final int SCAN_ONLY_MODE                 = 2;
@@ -1065,6 +1070,13 @@ public class WifiStateMachine extends StateMachine {
     public int syncIsIbssSupported(AsyncChannel channel) {
         Message resultMsg = channel.sendMessageSynchronously(CMD_GET_IBSS_SUPPORTED);
         int result = resultMsg.arg1;
+        resultMsg.recycle();
+        return result;
+    }
+
+    public List<WifiChannel> syncGetSupportedChannels(AsyncChannel channel) {
+        Message resultMsg = channel.sendMessageSynchronously(CMD_GET_SUPPORTED_CHANNELS);
+        List<WifiChannel> result = (List<WifiChannel>) resultMsg.obj;
         resultMsg.recycle();
         return result;
     }
@@ -1943,6 +1955,9 @@ public class WifiStateMachine extends StateMachine {
                 case CMD_GET_IBSS_SUPPORTED:
                     replyToMessage(message, message.what, FAILURE);
                     break;
+                case CMD_GET_SUPPORTED_CHANNELS:
+                    replyToMessage(message, message.what, (List<WifiChannel>) null);
+                    break;
                 case CMD_GET_CONFIGURED_NETWORKS:
                     replyToMessage(message, message.what, (List<WifiConfiguration>) null);
                     break;
@@ -2210,6 +2225,7 @@ public class WifiStateMachine extends StateMachine {
                     initializeWpsDetails();
 
                     mIbssSupported = mWifiNative.getModeCapability("IBSS");
+                    mSupportedChannels = mWifiNative.getSupportedChannels();
 
                     sendSupplicantConnectionChangedBroadcast(true);
                     transitionTo(mDriverStartedState);
@@ -2240,6 +2256,7 @@ public class WifiStateMachine extends StateMachine {
                 case CMD_START_PACKET_FILTERING:
                 case CMD_STOP_PACKET_FILTERING:
                 case CMD_GET_IBSS_SUPPORTED:
+                case CMD_GET_SUPPORTED_CHANNELS:
                     deferMessage(message);
                     break;
                 default:
@@ -2304,6 +2321,7 @@ public class WifiStateMachine extends StateMachine {
                     mOperationalMode = message.arg1;
                     break;
                 case CMD_GET_IBSS_SUPPORTED:
+                case CMD_GET_SUPPORTED_CHANNELS:
                     deferMessage(message);
                     break;
                 default:
@@ -2622,6 +2640,9 @@ public class WifiStateMachine extends StateMachine {
                     break;
                 case CMD_GET_IBSS_SUPPORTED:
                     replyToMessage(message, message.what, mIbssSupported ? 1 : 0);
+                    break;
+                case CMD_GET_SUPPORTED_CHANNELS:
+                    replyToMessage(message, message.what, mSupportedChannels);
                     break;
                 default:
                     return NOT_HANDLED;
