b1aa61d9ab2189eb06700f65219b4f2bfd9df0ef
Richard Hansen <aosp@rhansen.org>
2013-03-27 17:15:54 -0400

    fix missing comment in @deprecated javadoc tag
    Fixes the following build error when building full-eng:
    
    Docs droiddoc: out/target/common/docs/doc-comment-check
    frameworks/base/core/java/com/android/internal/widget/LockPatternUtils.java:1280: error 114: @deprecated tag with no explanatory comment
    
    Change-Id: I58085049311982a66565e53c85fbdf87ec7edca9

--- core/java/com/android/internal/widget/LockPatternUtils.java
+++ core/java/com/android/internal/widget/LockPatternUtils.java
@@ -1277,7 +1277,7 @@ public class LockPatternUtils {
     }
 
     /**
-     * @deprecated
+     * @deprecated @see #updateEmergencyCallButtonState(Button, int, boolean, boolean, boolean)
      * @param button
      * @param phoneState
      * @param shown
