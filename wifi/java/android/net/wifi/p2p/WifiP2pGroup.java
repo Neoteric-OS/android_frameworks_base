3cc6d5585a1f67719961e29848b66e7a55f6b753
Deepthi Gowri <deepthi@codeaurora.org>
2013-04-19 13:50:10 +0530

    frameworks/base: Fix for connection failure between third client and GC.
    When third device sends a connection request to the group client, the
    existing connection is terminating and also the ongoing connection not
    successful.Cuurently is there is  no use case where third device is
    connecting to GC,and the existing onnection egtting terminated.This
    is addressed by ignoring the new connection request from third client
    to GC and the existing connection is intact.
    
    Change-Id: I486cab9a6286b358b6a8a9ab85b6a7c0977a8a0b

--- wifi/java/android/net/wifi/p2p/WifiP2pGroup.java
+++ wifi/java/android/net/wifi/p2p/WifiP2pGroup.java
@@ -49,8 +49,9 @@ public class WifiP2pGroup implements Parcelable {
     /** Group owner */
     private WifiP2pDevice mOwner;
 
-    /** Device is group owner */
-    private boolean mIsGroupOwner;
+    /** @hide Device is group owner
+     */
+    public boolean mIsGroupOwner;
 
     /** Group clients */
     private List<WifiP2pDevice> mClients = new ArrayList<WifiP2pDevice>();
