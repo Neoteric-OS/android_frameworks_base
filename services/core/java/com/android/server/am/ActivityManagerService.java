edaad18247c84cf8449078790ae037e4eaa6184c
Maunik Shah <mshah@codeaurora.org>
2013-08-27 18:38:29 +0530

    frameworks/base: Fix for concurrency issue in AMS
    List of services maintained by AMS may get updated/iterated
    concurrently if serviceTimeout() is scheduled simultaneously
    with launch/stop of new services for the same package. To fix
    the issue , iteration of proc.executingServices set in
    serviceTimeout() should be in synchronized with same lock used
    by updating the set during start/launch of service.
    
    Change-Id: Ic230ad0d16e6ec64a20cf394aa1c1ce777c37a2b

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -1387,7 +1387,11 @@ public final class ActivityManagerService extends ActivityManagerNative
                     mHandler.sendMessageDelayed(nmsg, ActiveServices.SERVICE_TIMEOUT);
                     return;
                 }
-                mServices.serviceTimeout((ProcessRecord)msg.obj);
+                // Synchronising to avoid proc.executingServices set
+                // getting updated while being iterated in serviceTimeout()
+                synchronized(ActivityManagerService.this){
+                    mServices.serviceTimeout((ProcessRecord)msg.obj);
+                }
             } break;
             case UPDATE_TIME_ZONE: {
                 synchronized (ActivityManagerService.this) {
