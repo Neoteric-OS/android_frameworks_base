f20bc9ef24351fc8d387c2ab29444c444991123c
You Kim <you.kim72@gmail.com>
2013-09-05 00:20:19 +0900

    Return value of substring ignored in removeTrailingPeriod.
    Change-Id: I7f7347db862995bef675c84ca652e54db96941e4

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsModel.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsModel.java
@@ -413,7 +413,7 @@ class QuickSettingsModel implements BluetoothStateChangeCallback,
         if (string == null) return null;
         final int length = string.length();
         if (string.endsWith(".")) {
-            string.substring(0, length - 1);
+            return string.substring(0, length - 1);
         }
         return string;
     }
