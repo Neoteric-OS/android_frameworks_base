dbf15cc102586d5e9219bb8c63404dc5b4eed88c
Robert Craig <rpcraig@tycho.ncsc.mil>
2014-03-27 10:00:58 -0400

    Change when the selinux relabel of /data/data occurs.
    Perform the relabel of the /data/data/<pkg> directories
    when the app is being scanned by the PMS. The impetus
    for this change was that the data directories of forward
    locked apps were receiving the wrong label during an
    OTA. Because the PMS doesn't actually scan forward locked
    apps til later in the boot process, the prior restorecon
    call was actually applying the default label of
    system_data_file for all such apps. This mechanism instead
    passes down the seinfo tag as part of the restorecon call
    instead of relying on the contents of packages.list.
    
    Change-Id: Ie440cba2c96f0907458086348197e1506d31c1b6
    Signed-off-by: rpcraig <rpcraig@tycho.ncsc.mil>

--- services/java/com/android/server/pm/Installer.java
+++ services/java/com/android/server/pm/Installer.java
@@ -397,7 +397,14 @@ public final class Installer {
         return execute(builder.toString());
     }
 
-    public boolean restoreconData() {
-        return (execute("restorecondata") == 0);
+    public boolean restoreconData(String pkgDir, String seinfo, int uid) {
+        StringBuilder builder = new StringBuilder("restorecondata");
+        builder.append(' ');
+        builder.append(pkgDir);
+        builder.append(' ');
+        builder.append(seinfo != null ? seinfo : "!");
+        builder.append(' ');
+        builder.append(uid);
+        return (execute(builder.toString()) == 0);
     }
 }
