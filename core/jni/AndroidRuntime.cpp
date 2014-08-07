ef4b7ada7133f99b402eaf03ec3f0304659322c4
Calin Juravle <calin@google.com>
2014-08-07 14:45:53 +0100

    Pass the name of the native bridge library to the runtime
    Bug: 16843953
    Change-Id: Ia0f57a9b3e30a74fe4e93eb4453f841c1ef512eb

--- core/jni/AndroidRuntime.cpp
+++ core/jni/AndroidRuntime.cpp
@@ -584,6 +584,7 @@ int AndroidRuntime::startVm(JavaVM** pJavaVM, JNIEnv** pEnv)
     char lockProfThresholdBuf[sizeof("-Xlockprofthreshold:")-1 + PROPERTY_VALUE_MAX];
     char jitOpBuf[sizeof("-Xjitop:")-1 + PROPERTY_VALUE_MAX];
     char jitMethodBuf[sizeof("-Xjitmethod:")-1 + PROPERTY_VALUE_MAX];
+    char nativeBridgeLibray[sizeof("-XX:NativeBridgeLibrary=") + PROPERTY_VALUE_MAX];
 
     bool checkJni = false;
     property_get("dalvik.vm.checkjni", propBuf, "");
@@ -875,6 +876,9 @@ int AndroidRuntime::startVm(JavaVM** pJavaVM, JNIEnv** pEnv)
                            profileMaxStackDepth,
                            "-Xprofile-max-stack-depth:");
     }
+
+    parseRuntimeOption("dalvik.vm.native.bridge.library",
+                       nativeBridgeLibray, "-XX:NativeBridgeLibrary=");
 
     initArgs.version = JNI_VERSION_1_4;
     initArgs.options = mOptions.editArray();
