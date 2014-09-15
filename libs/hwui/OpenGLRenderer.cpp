c09487f424a49efba65823ed4619f304fb3988e6
Chih-Hung Hsieh <chh@google.com>
2014-09-15 14:28:52 -0700

    Suppress Clang compilation warnings.
    Ignore warnings about unused parameters, deprecated declarations,
    mismatched struct vs class tags, and initialization of static
    constant float data members.
    Also change one potential error of allocating variable length
    array of non-POD element type on stack.
    
    BUG: 17512760
    Change-Id: I3a09c945fb1c17f27aff74a7e439dd4c35d1fb32

--- libs/hwui/OpenGLRenderer.cpp
+++ libs/hwui/OpenGLRenderer.cpp
@@ -2239,7 +2239,7 @@ status_t OpenGLRenderer::drawBitmapMesh(SkBitmap* bitmap, int meshWidth, int mes
 
     const uint32_t count = meshWidth * meshHeight * 6;
 
-    ColorTextureVertex mesh[count];
+    ColorTextureVertex *mesh = new ColorTextureVertex[count];
     ColorTextureVertex* vertex = mesh;
 
     bool cleanupColors = false;
