5bf500f4e1198e73c1f963346714281bdda58688
tiger_huang <tiger_huang@htc.com>
2014-12-01 23:39:11 +0800

    Not to change the layout while forcing hiding system bars
    If we override the layout of certain kinds of window, the position of
    the window would become unexpected.
    
    https://code.google.com/p/android/issues/detail?id=81359
    
    Change-Id: I78853d69db6fe7cc8579dfb7a0bf0ae04aa6fae6

--- policy/src/com/android/internal/policy/impl/PolicyControl.java
+++ policy/src/com/android/internal/policy/impl/PolicyControl.java
@@ -67,14 +67,12 @@ public class PolicyControl {
         int vis = win != null ? win.getSystemUiVisibility() : attrs.systemUiVisibility;
         if (sImmersiveStatusFilter != null && sImmersiveStatusFilter.matches(attrs)) {
             vis |= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                    | View.SYSTEM_UI_FLAG_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN;
             vis &= ~(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                     | View.STATUS_BAR_TRANSLUCENT);
         }
         if (sImmersiveNavigationFilter != null && sImmersiveNavigationFilter.matches(attrs)) {
             vis |= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                     | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
             vis &= ~(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                     | View.NAVIGATION_BAR_TRANSLUCENT);
