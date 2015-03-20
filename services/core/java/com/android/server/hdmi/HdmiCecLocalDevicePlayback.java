753652c31a2ed2ef9e0335ee8a3f9b1af5a6de24
Spencer Sutterlin <ssutterlin@nvidia.com>
2015-03-19 21:09:09 -0700

    CEC: Do not send inactive source on sleep
    Change-Id: Ib01de7a270aa67d5921b005649510b7f4a0b6ef0

--- services/core/java/com/android/server/hdmi/HdmiCecLocalDevicePlayback.java
+++ services/core/java/com/android/server/hdmi/HdmiCecLocalDevicePlayback.java
@@ -364,10 +364,7 @@ final class HdmiCecLocalDevicePlayback extends HdmiCecLocalDevice {
         super.disableDevice(initiatedByCec, callback);
 
         assertRunOnServiceThread();
-        if (!initiatedByCec && mIsActiveSource) {
-            mService.sendCecCommand(HdmiCecMessageBuilder.buildInactiveSource(
-                    mAddress, mService.getPhysicalAddress()));
-        }
+        // Do not send <Inactive Source> command as some TVs switch inputs improperly
         setActiveSource(false);
         checkIfPendingActionsCleared();
     }
