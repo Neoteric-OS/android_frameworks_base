1aeae98006c1721e01704d1bf4519768e4dd04c2
Alex Klyubin <klyubin@google.com>
2015-03-23 10:51:20 -0700

    Address pending comments for NetworkSecurityPolicy.
    This is a follow-up to
    https://android-review.googlesource.com/#/c/131920/, addressing some
    outstanding comments.
    
    This CL also switches the source file to the correct indentation.
    
    Bug: 19215516
    Change-Id: Ia48455ab351081e11619afcccadb8fa90340391d

--- core/java/android/security/NetworkSecurityPolicy.java
+++ core/java/android/security/NetworkSecurityPolicy.java
@@ -19,48 +19,58 @@ package android.security;
 /**
  * Network security policy.
  *
+ * <p>Network stacks/components should honor this policy to make it possible to centrally control
+ * the relevant aspects of network security behavior.
+ *
+ * <p>The policy currently consists of a single flag: whether cleartext network traffic is
+ * permitted. See {@link #isCleartextTrafficPermitted()}.
+ *
  * @hide
  */
 public class NetworkSecurityPolicy {
 
-  private static final NetworkSecurityPolicy INSTANCE = new NetworkSecurityPolicy();
+    private static final NetworkSecurityPolicy INSTANCE = new NetworkSecurityPolicy();
 
-  private boolean mCleartextTrafficPermitted = true;
+    private volatile boolean mCleartextTrafficPermitted = true;
 
-  private NetworkSecurityPolicy() {}
+    private NetworkSecurityPolicy() {}
 
-  /**
-   * Gets the policy.
-   */
-  public static NetworkSecurityPolicy getInstance() {
-    return INSTANCE;
-  }
-
-  /**
-   * Checks whether cleartext network traffic (e.g., HTTP, WebSockets, XMPP, IMAP, SMTP -- without
-   * TLS or STARTTLS) is permitted for this process.
-   *
-   * <p>When cleartext network traffic is not permitted, the platform's components (e.g., HTTP
-   * stacks, {@code WebView}, {@code MediaPlayer}) will refuse this process's requests to use
-   * cleartext traffic. Third-party libraries are encouraged to honor this setting as well.
-   */
-  public boolean isCleartextTrafficPermitted() {
-    synchronized (this) {
-      return mCleartextTrafficPermitted;
+    /**
+     * Gets the policy.
+     */
+    public static NetworkSecurityPolicy getInstance() {
+        return INSTANCE;
     }
-  }
 
-  /**
-   * Sets whether cleartext network traffic is permitted for this process.
-   *
-   * <p>This method is used by the platform early on in the application's initialization to set the
-   * policy.
-   *
-   * @hide
-   */
-  public void setCleartextTrafficPermitted(boolean permitted) {
-    synchronized (this) {
-      mCleartextTrafficPermitted = permitted;
+    /**
+     * Returns whether cleartext network traffic (e.g., HTTP, FTP WebSockets, XMPP, IMAP, SMTP --
+     * without TLS or STARTTLS) is permitted for this process.
+     *
+     * <p>When cleartext network traffic is not permitted, the platform's components (e.g., HTTP
+     * and stacks, {@code WebView}, {@code MediaPlayer}) will refuse this process's requests to use
+     * cleartext traffic. Third-party libraries are encouraged to honor this setting as well.
+     *
+     * <p>This flag is honored on a best effort basis because it's impossible to prevent all
+     * cleartext traffic from Android applications given the level of access provided to
+     * applications. For example, there's no expectation that {@link java.net.Socket} API will honor
+     * this flag because it doesn't have enough visibility into whether its traffic is in cleartext.
+     * Luckily, most network traffic from apps is handled by higher-level network stacks/components
+     * which do have sufficient visibility into whether traffic is in cleartext and can thus honor
+     * this aspect of the policy.
+     */
+    public boolean isCleartextTrafficPermitted() {
+        return mCleartextTrafficPermitted;
     }
-  }
+
+    /**
+     * Sets whether cleartext network traffic is permitted for this process.
+     *
+     * <p>This method is used by the platform early on in the application's initialization to set
+     * the policy.
+     *
+     * @hide
+     */
+    public void setCleartextTrafficPermitted(boolean permitted) {
+        mCleartextTrafficPermitted = permitted;
+    }
 }
