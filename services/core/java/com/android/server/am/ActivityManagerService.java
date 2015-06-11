9e926404ea0f84436a68599eab072f9a8cfecb5e
jinchul.kim <jinchul.kim@lge.com>
2015-06-11 18:15:06 +0900

    KillProcessGroup on background thread
    Killing a process takes time, especially on killProcessGroup. If it is
    called by updateOomAdjLocked, it keeps lock of mServices for moments and
    other requests would be delayed, like attachApplicationNeed.
    
    Do killProcessGroup on background thread to void unnecessary waiting.
    
    Change-Id: Ie4a9b3979ff5cc78614efd85bb958fffb814137c

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -1290,6 +1290,8 @@ public final class ActivityManagerService extends ActivityManagerNative
     final MainHandler mHandler;
     final UiHandler mUiHandler;
 
+    static KillProcessBackground mKillProcessHandler;
+
     final class UiHandler extends Handler {
         public UiHandler() {
             super(com.android.server.UiThread.get().getLooper(), null, true);
@@ -1835,7 +1837,23 @@ public final class ActivityManagerService extends ActivityManagerNative
         }
     };
 
+    final class KillProcessBackground extends Handler {
+        public KillProcessBackground(Looper looper) {
+            super(looper);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+            case KILL_PROCESS_GROUP_MSG:
+                killProcessGroupBackground(msg.arg1, msg.arg2);
+            break;
+            }
+        }
+    };
+
     static final int COLLECT_PSS_BG_MSG = 1;
+    static final int KILL_PROCESS_GROUP_MSG = 44;
 
     final Handler mBgHandler = new Handler(BackgroundThread.getHandler().getLooper()) {
         @Override
@@ -2093,6 +2111,8 @@ public final class ActivityManagerService extends ActivityManagerNative
         mHandlerThread.start();
         mHandler = new MainHandler(mHandlerThread.getLooper());
         mUiHandler = new UiHandler();
+
+        mKillProcessHandler = new KillProcessBackground(BackgroundThread.getHandler().getLooper());
 
         mFgBroadcastQueue = new BroadcastQueue(this, mHandler,
                 "foreground", BROADCAST_FG_TIMEOUT, false);
@@ -2504,7 +2524,7 @@ public final class ActivityManagerService extends ActivityManagerNative
             if (!app.killed) {
                 Slog.wtfStack(TAG, "Removing process that hasn't been killed: " + app);
                 Process.killProcessQuiet(app.pid);
-                Process.killProcessGroup(app.info.uid, app.pid);
+                killProcessGroup(app.info.uid, app.pid);
             }
             if (lrui <= mLruProcessActivityStart) {
                 mLruProcessActivityStart--;
@@ -2862,7 +2882,7 @@ public final class ActivityManagerService extends ActivityManagerNative
             // clean it up now.
             if (DEBUG_PROCESSES || DEBUG_CLEANUP) Slog.v(TAG, "App died: " + app);
             checkTime(startTime, "startProcess: bad proc running, killing");
-            Process.killProcessGroup(app.info.uid, app.pid);
+            killProcessGroup(app.info.uid, app.pid);
             handleAppDiedLocked(app, true, true);
             checkTime(startTime, "startProcess: done killing old proc");
         }
@@ -4727,7 +4747,7 @@ public final class ActivityManagerService extends ActivityManagerNative
             if (!fromBinderDied) {
                 Process.killProcessQuiet(pid);
             }
-            Process.killProcessGroup(app.info.uid, pid);
+            killProcessGroup(app.info.uid, pid);
             app.killed = true;
         }
 
@@ -12134,7 +12154,7 @@ public final class ActivityManagerService extends ActivityManagerNative
                             } else {
                                 // Huh.
                                 Process.killProcess(pid);
-                                Process.killProcessGroup(uid, pid);
+                                killProcessGroup(uid, pid);
                             }
                         }
                         return;
@@ -17014,6 +17034,24 @@ public final class ActivityManagerService extends ActivityManagerNative
         if (ass.mNesting == 0) {
             ass.mTime += SystemClock.uptimeMillis() - ass.mStartTime;
         }
+    }
+
+    static final boolean DEBUG_KILL_ASYNC = true;
+    static public void killProcessGroup(final int uid , final int pid) {
+        if (mKillProcessHandler == null) {
+            Slog.w(TAG, "thread for killProcessGroup is not ready");
+            Process.killProcessGroup(uid, pid);
+            return;
+        }
+        mKillProcessHandler.sendMessage(mKillProcessHandler.obtainMessage(KILL_PROCESS_GROUP_MSG, uid, pid));
+    }
+
+    private void killProcessGroupBackground(int uid , int pid) {
+        long now = SystemClock.uptimeMillis();
+        Process.killProcessGroup(uid, pid);
+        if (DEBUG_KILL_ASYNC) Slog.v(TAG, "killProcessGroupAsync took "
+            + (SystemClock.uptimeMillis() - now) + " ms for PID " + pid
+            + " on thread " + Thread.currentThread().getId());
     }
 
     private final int computeOomAdjLocked(ProcessRecord app, int cachedAdj, ProcessRecord TOP_APP,
