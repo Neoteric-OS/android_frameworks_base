71a88c300c255790bf96c0a50e718fa3f6cb2130
Zhenjun Zhang <zhenjun85@gmail.com>
2016-04-02 09:39:29 +0800

    Remove the unnecessary window animation request
    If the OpeningApp's onDraw is blocked for some reason, the mClosingApp's window
    will always set as animating. Then updateWallpaperLocked will set mAnimating to true
    and request window animation until APP_TRANSITION_TIMEOUT, which is unnecessary.
    
    Solution:
    remove the animation request in updateWallpaperLocked

--- services/core/java/com/android/server/wm/WindowAnimator.java
+++ services/core/java/com/android/server/wm/WindowAnimator.java
@@ -546,7 +546,6 @@ public class WindowAnimator {
                         }
                     }
                 }
-                mAnimating = true;
             }
 
             // If this window's app token is running a detached wallpaper
