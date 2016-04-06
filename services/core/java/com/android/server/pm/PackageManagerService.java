5c0bbb622d662682ce316bfecbf8eec78ba587fc
Scott Mertz <scott@cyngn.com>
2016-04-06 12:34:04 -0700

    PackageManager: add independent lock for media mounted state
    There is a potential deadlock between PackageManagerService &
    MountService if PackageManagerService.isExternalMediaAvailable()
    is called with mPackages locked. This change adds a new lock for
    mMediaMounted independent of the mPackages lock.
    
    Currently, PackageManagerService.isExternalMediaAvailable() will
    eventually call into MountService.getVolumeList() with mPackages
    held. Within MountService.getVolumeList(), it tries to acquire
    it's own lock. If at the same time MountService.forgetAllVolumes()
    is called, it acquires the lock and calls into
    UserManagerService.getUsers(). Within UserManagerService.getUsers(),
    it tries to acquire the PackageManagerService mPackages lock. This
    results in a deadlock with PackageManagerService holding mPackages
    while trying to acquire MountService mLock & MountService holding
    mLock while trying to acquire PackageManagerService mPackages.
    
    Change-Id: I7f1d1b01d5300ac3f664256d5b0a7b366ff323a9

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -9453,6 +9453,8 @@ public class PackageManagerService extends IPackageManager.Stub {
      * Check if the external storage media is available. This is true if there
      * is a mounted external storage medium or if the external storage is
      * emulated.
+     *
+     * MUST NOT BE CALLED WITH mPackages LOCK HELD
      */
     private boolean isExternalMediaAvailable() {
         return mMediaMounted || Environment.isExternalStorageEmulated();
@@ -9460,14 +9462,17 @@ public class PackageManagerService extends IPackageManager.Stub {
 
     @Override
     public PackageCleanItem nextPackageToClean(PackageCleanItem lastPackage) {
-        // writer
-        synchronized (mPackages) {
+        // reader
+        synchronized(mMediaMountedLock) {
             if (!isExternalMediaAvailable()) {
                 // If the external storage is no longer mounted at this point,
                 // the caller may not have been able to delete all of this
                 // packages files and can not delete any more.  Bail.
                 return null;
             }
+        }
+        // writer
+        synchronized (mPackages) {
             final ArrayList<PackageCleanItem> pkgs = mSettings.mPackagesToBeCleaned;
             if (lastPackage != null) {
                 pkgs.remove(lastPackage);
@@ -9494,10 +9499,13 @@ public class PackageManagerService extends IPackageManager.Stub {
 
     void startCleaningPackages() {
         // reader
-        synchronized (mPackages) {
+        synchronized (mMediaMountedLock) {
             if (!isExternalMediaAvailable()) {
                 return;
             }
+        }
+        // reader
+        synchronized (mPackages) {
             if (mSettings.mPackagesToBeCleaned.isEmpty()) {
                 return;
             }
@@ -15398,7 +15406,9 @@ public class PackageManagerService extends IPackageManager.Stub {
 
     private static final String SD_ENCRYPTION_ALGORITHM = "AES";
 
+    @GuardedBy("mMediaMountedLock")
     private boolean mMediaMounted = false;
+    private final Object mMediaMountedLock = new Object();
 
     static String getEncryptKey() {
         try {
@@ -15431,9 +15441,8 @@ public class PackageManagerService extends IPackageManager.Stub {
         if (callingUid != 0 && callingUid != Process.SYSTEM_UID) {
             throw new SecurityException("Media status can only be updated by the system");
         }
-        // reader; this apparently protects mMediaMounted, but should probably
-        // be a different lock in that case.
-        synchronized (mPackages) {
+        // reader
+        synchronized (mMediaMountedLock) {
             Log.i(TAG, "Updating external media status from "
                     + (mMediaMounted ? "mounted" : "unmounted") + " to "
                     + (mediaStatus ? "mounted" : "unmounted"));
