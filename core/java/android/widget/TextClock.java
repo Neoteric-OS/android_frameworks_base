a1c1ebaff151522b8fc5f2f9a9730a476602d874
xu teresa <xiaoyan.xu@orange.com>
2016-04-27 10:44:32 +0800

    Allow adding TextClock widget from user profiles.
    The goal of this change is to enable support for appwidget from
    user profiles to the user main profile.
    
    Change-Id: I559b0666375978fc09e8585661e7a4b5284706ce

--- core/java/android/widget/TextClock.java
+++ core/java/android/widget/TextClock.java
@@ -549,7 +549,15 @@ public class TextClock extends TextView {
         filter.addAction(Intent.ACTION_TIME_CHANGED);
         filter.addAction(Intent.ACTION_TIMEZONE_CHANGED);
 
-        getContext().registerReceiver(mIntentReceiver, filter, null, getHandler());
+        // This class is supported by the remote views machanism and as
+        // a part of that the remote views can be inflated by a context
+        // for another user without the app having interact users permission
+        // just for loading resources.
+        // For exmaple, when adding widgets from a user profile to the
+        // home screen. Therefore, we register the receiver as the current
+        // user not the one the context is for.
+        getContext().registerReceiverAsUser(mIntentReceiver,
+                android.os.Process.myUserHandle(), filter, null, getHandler());
     }
 
     private void registerObserver() {
