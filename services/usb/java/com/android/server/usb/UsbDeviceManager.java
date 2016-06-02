785b04ae02174a977280c82609a6ad1bd0e95db8
Tom Marshall <tdm@cyngn.com>
2016-06-02 10:40:40 -0700

    UsbDeviceManager: Default persist.sys.usb.config to none instead of mtp
    Change-Id: I296f8af0a5125f0cfb43175e908720a0aa970c4e

--- services/usb/java/com/android/server/usb/UsbDeviceManager.java
+++ services/usb/java/com/android/server/usb/UsbDeviceManager.java
@@ -838,9 +838,6 @@ public class UsbDeviceManager {
         private String getDefaultFunctions() {
             String func = SystemProperties.get(USB_PERSISTENT_CONFIG_PROPERTY,
                     UsbManager.USB_FUNCTION_NONE);
-            if (UsbManager.USB_FUNCTION_NONE.equals(func)) {
-                func = UsbManager.USB_FUNCTION_MTP;
-            }
             return func;
         }
 
