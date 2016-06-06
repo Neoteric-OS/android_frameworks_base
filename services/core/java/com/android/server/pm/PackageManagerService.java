196200acb0c313f05a5fbdc17d64af6d75b6dc2a
liulvping <liulvping@xiaomi.com>
2016-06-06 10:20:48 +0800

    Skip uninstalled package for getPackagesForUid()
    If a package with sharedUserId had been setInstalled
    with false, then the result of getPackagesForUid()
    should not include it.
    
    https://code.google.com/p/android/issues/detail?id=212255
    
    Change-Id: I7b984051db34ecc2d0ef9518786d51100ab9ac79
    Signed-off-by: liulvping <liulvping@xiaomi.com>

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -4072,6 +4072,7 @@ public class PackageManagerService extends IPackageManager.Stub {
 
     @Override
     public String[] getPackagesForUid(int uid) {
+        final int userId = UserHandle.getUserId(uid);
         uid = UserHandle.getAppId(uid);
         // reader
         synchronized (mPackages) {
@@ -4079,11 +4080,16 @@ public class PackageManagerService extends IPackageManager.Stub {
             if (obj instanceof SharedUserSetting) {
                 final SharedUserSetting sus = (SharedUserSetting) obj;
                 final int N = sus.packages.size();
-                final String[] res = new String[N];
+                String[] res = new String[N];
                 final Iterator<PackageSetting> it = sus.packages.iterator();
                 int i = 0;
                 while (it.hasNext()) {
-                    res[i++] = it.next().name;
+                    PackageSetting ps = it.next();
+                    if (ps.getInstalled(userId)) {
+                        res[i++] = ps.name;
+                    } else {
+                        res = ArrayUtils.removeElement(String.class, res, res[i]);
+                    }
                 }
                 return res;
             } else if (obj instanceof PackageSetting) {
