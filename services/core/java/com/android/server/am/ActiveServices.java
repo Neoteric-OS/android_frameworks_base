6c77594b48f8394ba29e002b610c1ff03c946eec
yuanhuihui <yuanhuihui@xiaomi.com>
2016-07-14 10:55:51 +0800

    fix to bring up service failed
    when bind remote service running in an isolated process.
    for trim memory and am kill cause service's process died and restart with delay.
    when restartDelay >0 but don't contain in mRestartingServices list,
    then bring up service failed.
    
    https://code.google.com/p/android/issues/detail?id=215980
    
    Change-Id: I1fafe868a7e2ff854a0ad403f6dc0e99f48a9d39
    Signed-off-by: yuanhuihui <yuanhuihui@xiaomi.com>

--- services/core/java/com/android/server/am/ActiveServices.java
+++ services/core/java/com/android/server/am/ActiveServices.java
@@ -2364,7 +2364,9 @@ public final class ActiveServices {
                 ServiceRecord r = mRestartingServices.get(i);
                 if (r.processName.equals(app.processName) &&
                         r.serviceInfo.applicationInfo.uid == app.info.uid) {
-                    mRestartingServices.remove(i);
+                    if (mRestartingServices.remove(i)) {
+                        r.resetRestartCounter();
+                    }
                     clearRestartingIfNeededLocked(r);
                 }
             }
