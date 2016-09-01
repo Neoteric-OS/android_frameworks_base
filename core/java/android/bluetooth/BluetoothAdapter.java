a5b36d6c6f6f0d2975bcccd4d194d45e6122eb85
Joseph Pirozzo <pirozzoj@google.com>
2016-09-01 14:19:28 -0700

    MAP MCE
    Add MAP client code into packages/apps/Bluetooth.  Changes here are to
    define the MAP MCE interface and enable its selection when running on a
    device that is also running a PBAP client (Car Kitt).
    
    Bug: 30467210
    Change-Id: Ifa2cdea7d67f63a2b5f3d971df8ec6d321dc5fee
    (cherry picked from commit 433b3054847951e8e7b3864d11990604a66b8651)

--- core/java/android/bluetooth/BluetoothAdapter.java
+++ core/java/android/bluetooth/BluetoothAdapter.java
@@ -1925,6 +1925,9 @@ public final class BluetoothAdapter {
         } else if (profile == BluetoothProfile.PBAP_CLIENT) {
             BluetoothPbapClient pbapClient = new BluetoothPbapClient(context, listener);
             return true;
+        } else if (profile == BluetoothProfile.MAP_CLIENT) {
+            BluetoothMapClient mapClient = new BluetoothMapClient(context, listener);
+            return true;
         } else {
             return false;
         }
@@ -1996,6 +1999,10 @@ public final class BluetoothAdapter {
             case BluetoothProfile.PBAP_CLIENT:
                 BluetoothPbapClient pbapClient = (BluetoothPbapClient)proxy;
                 pbapClient.close();
+                break;
+            case BluetoothProfile.MAP_CLIENT:
+                BluetoothMapClient mapClient = (BluetoothMapClient)proxy;
+                mapClient.close();
                 break;
         }
     }
