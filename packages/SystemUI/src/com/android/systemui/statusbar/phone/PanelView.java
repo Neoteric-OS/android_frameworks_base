6c402613956efeddad72645378e0cf89289f4ff1
zhang sanshan <sanshan.zhang@nxp.com>
2016-10-26 23:03:45 +0800

    Screen Lock: Can't unlock keyguard with USB mouse
    The last point is on top of screen usually when unlock the keyguard with mouse.
    Event ACTION_UP will be ignored by system and endMotionEvent will not be called.
    When current bar state is KEYGUARD, we do not need enable below feature:
    "single mouse click expands the panel instead of dragging"
    
    Signed-off-by: zhang sanshan <sanshan.zhang@nxp.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PanelView.java
@@ -218,13 +218,6 @@ public abstract class PanelView extends FrameLayout {
             return false;
         }
 
-        // On expanding, single mouse click expands the panel instead of dragging.
-        if (isFullyCollapsed() && event.isFromSource(InputDevice.SOURCE_MOUSE)) {
-            if (event.getAction() == MotionEvent.ACTION_UP) {
-                expand(true);
-            }
-            return true;
-        }
 
         /*
          * We capture touch events here and update the expand height here in case according to
@@ -404,6 +397,12 @@ public abstract class PanelView extends FrameLayout {
                 mUpdateFlingVelocity = vel;
             }
         } else {
+            // On expanding, single mouse click expands the panel instead of dragging.
+            if (isFullyCollapsed() && event.isFromSource(InputDevice.SOURCE_MOUSE)) {
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    expand(true);
+                }
+            }
             boolean expands = onEmptySpaceClick(mInitialTouchX);
             onTrackingStopped(expands);
         }
