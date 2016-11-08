83b43dd4c81c53cc9b30876e3dc6302ab47218ff
liangweikang <liangweikang@xiaomi.com>
2016-11-08 18:29:08 +0800

    fix java.lang.IllegalArgumentException: tried recording negative data
    https://code.google.com/p/android/issues/detail?id=227298&thanks=227298&ts=1478600867
    
    Test: compile and monkey test
    
    Signed-off-by: liangweikang <liangweikang900911@gmail.com>

--- core/java/android/net/NetworkStats.java
+++ core/java/android/net/NetworkStats.java
@@ -656,18 +656,17 @@ public class NetworkStats implements Parcelable {
                 entry.txBytes = left.txBytes[i] - right.txBytes[j];
                 entry.txPackets = left.txPackets[i] - right.txPackets[j];
                 entry.operations = left.operations[i] - right.operations[j];
-
-                if (entry.rxBytes < 0 || entry.rxPackets < 0 || entry.txBytes < 0
-                        || entry.txPackets < 0 || entry.operations < 0) {
-                    if (observer != null) {
-                        observer.foundNonMonotonic(left, i, right, j, cookie);
-                    }
-                    entry.rxBytes = Math.max(entry.rxBytes, 0);
-                    entry.rxPackets = Math.max(entry.rxPackets, 0);
-                    entry.txBytes = Math.max(entry.txBytes, 0);
-                    entry.txPackets = Math.max(entry.txPackets, 0);
-                    entry.operations = Math.max(entry.operations, 0);
+            }          
+            if (entry.rxBytes < 0 || entry.rxPackets < 0 || entry.txBytes < 0
+                    || entry.txPackets < 0 || entry.operations < 0) {
+                if (observer != null) {
+                    observer.foundNonMonotonic(left, i, right, j, cookie);
                 }
+                entry.rxBytes = Math.max(entry.rxBytes, 0);
+                entry.rxPackets = Math.max(entry.rxPackets, 0);
+                entry.txBytes = Math.max(entry.txBytes, 0);
+                entry.txPackets = Math.max(entry.txPackets, 0);
+                entry.operations = Math.max(entry.operations, 0);
             }
 
             result.addValues(entry);
