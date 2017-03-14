cb48f81b8848361cfd57e79dbcd6aa12fa4ac9f2
Jesse Fuentes <jessef@quicinc.com>
2017-03-14 10:53:04 +0800

    Telephony: Get SIM card capacity count of SMS
    Add an API to get the capacity of the stored SMS on ICC card.
    
    Change-Id: I1193367008b19c3b1e017ecd854a75b2f3f70c40

--- telephony/java/android/telephony/SmsManager.java
+++ telephony/java/android/telephony/SmsManager.java
@@ -1382,6 +1382,26 @@ public final class SmsManager {
         }
     }
 
+    /**
+     * Get the capacity count of sms on Icc card
+     *
+     * @return the capacity count of sms on Icc card
+     * @hide
+     */
+    @SystemApi
+    public int getSmsCapacityOnIcc() {
+        int ret = -1;
+        try {
+            ISms iccISms = getISmsService();
+            if (iccISms != null) {
+                ret = iccISms.getSmsCapacityOnIccForSubscriber(getSubscriptionId());
+            }
+        } catch (RemoteException ex) {
+            //ignore it
+        }
+        return ret;
+    }
+
     // see SmsMessage.getStatusOnIcc
 
     /** Free space (TS 51.011 10.5.3 / 3GPP2 C.S0023 3.4.27). */
