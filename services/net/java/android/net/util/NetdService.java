5c5be0c7298c1f366856b0c8c33b946688b92d8b
Erik Kline <ek@google.com>
2017-03-14 17:40:00 +0900

    Add blocking and retrying wrappers for INetd uses.
    Test: as follows
        - built (bullhead)
        - flashed
        - booted
        - runtest frameworks-net passes
    
    Change-Id: I1a8e15149bf9dbedc6a09c0d8ab6abb2ef4f8fee

--- services/net/java/android/net/util/NetdService.java
+++ services/net/java/android/net/util/NetdService.java
@@ -17,7 +17,9 @@
 package android.net.util;
 
 import android.net.INetd;
+import android.os.RemoteException;
 import android.os.ServiceManager;
+import android.os.ServiceSpecificException;
 import android.util.Log;
 
 
@@ -27,20 +29,74 @@ import android.util.Log;
 public class NetdService {
     private static final String TAG = NetdService.class.getSimpleName();
     private static final String NETD_SERVICE_NAME = "netd";
+    private static final int BASE_TIMEOUT_MS = 100;
+    private static final int MAX_TIMEOUT_MS = 1000;
+
 
     /**
+     * Return an INetd instance, or null if not available.
+     *
      * It is the caller's responsibility to check for a null return value
      * and to handle RemoteException errors from invocations on the returned
      * interface if, for example, netd dies and is restarted.
      *
+     * Returned instances of INetd should not be cached.
+     *
      * @return an INetd instance or null.
      */
     public static INetd getInstance() {
+        // NOTE: ServiceManager does no caching for the netd service,
+        // because netd is not one of the defined common services.
         final INetd netdInstance = INetd.Stub.asInterface(
                 ServiceManager.getService(NETD_SERVICE_NAME));
         if (netdInstance == null) {
             Log.w(TAG, "WARNING: returning null INetd instance.");
         }
         return netdInstance;
+    }
+
+    /**
+     * Blocks until an INetd instance is available.
+     *
+     * It is the caller's responsibility to handle RemoteException errors
+     * from invocations on the returned interface if, for example, netd
+     * dies after this interface was returned.
+     *
+     * Returned instances of INetd should not be cached.
+     *
+     * @return an INetd instance or null.
+     */
+    public static INetd get() {
+        for (int i = 0; ; i++) {
+            final INetd netdInstance = getInstance();
+            if (netdInstance != null) {
+                return netdInstance;
+            }
+
+            // No netdInstance was received; sleep and retry.
+            final int timeoutMs = (i < (MAX_TIMEOUT_MS / BASE_TIMEOUT_MS))
+                    ? (i * BASE_TIMEOUT_MS)
+                    : MAX_TIMEOUT_MS;
+            try {
+                Thread.sleep(timeoutMs);
+            } catch (InterruptedException e) {}
+        }
+    }
+
+    public static interface NetdCommand {
+        void run(INetd netd) throws RemoteException, ServiceSpecificException;
+    }
+
+    /**
+     * Blocks until an INetd instance is availabe, and retries until either
+     * the command succeeds or a ServiceSpecificError is thrown.
+     */
+    public static void run(NetdCommand cmd) {
+        while (true) {
+            try {
+                cmd.run(get());
+                return;
+            } catch (RemoteException re) {}
+        }
     }
 }
