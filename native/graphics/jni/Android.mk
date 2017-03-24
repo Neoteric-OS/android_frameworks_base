f7a47319cc39800bfa619bee552a43e6a9f54485
Dimitry Ivanov <dimitry@google.com>
2017-03-23 17:46:22 -0700

    Add elf-hash to x86 version of the library
    Bug: http://b/36571076
    Bug: http://b/24465209
    Test: run bionic-unit-tests --gtest_filter=dlext.compat_elf_hash_and_relocation_tables on fugu
    Change-Id: I3c5026c32da018b472b9a90e1e2a252c837a489f

--- native/graphics/jni/Android.mk
+++ native/graphics/jni/Android.mk
@@ -34,6 +34,7 @@ LOCAL_CFLAGS += -Wall -Werror -Wunused -Wunreachable-code
 
 # TODO: This is to work around b/24465209. Remove after root cause is fixed
 LOCAL_LDFLAGS_arm := -Wl,--hash-style=both
+LOCAL_LDFLAGS_x86 := -Wl,--hash-style=both
 
 include $(BUILD_SHARED_LIBRARY)
 
