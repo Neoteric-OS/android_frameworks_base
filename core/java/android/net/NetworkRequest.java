57389ed4355c3970b1438c8a23d0a764ddbd944d
Etan Cohen <etancohen@google.com>
2017-03-29 13:57:08 -0700

    [CM] Unhide the NetworkSpecifier as object API
    API visibility change: unhide allowing NetworkSpecifier
    to be an arbitrary object.
    
    Bug: 27533960
    Test: builds and runs
    Change-Id: I1d1705cca7ece077ef8d7c674c62d5369fedbb03

--- core/java/android/net/NetworkRequest.java
+++ core/java/android/net/NetworkRequest.java
@@ -271,8 +271,6 @@ public class NetworkRequest implements Parcelable {
          *
          * @param networkSpecifier A concrete, parcelable framework class that extends
          *                         NetworkSpecifier.
-         *
-         * @hide
          */
         public Builder setNetworkSpecifier(NetworkSpecifier networkSpecifier) {
             if (networkSpecifier instanceof MatchAllNetworkSpecifier) {
