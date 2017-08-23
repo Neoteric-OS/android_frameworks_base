c556c32c9f417675ad976a0f2b53b94988537455
Qiushi.Han <qiushi.han@spreadtrum.com>
2017-08-23 08:58:37 +0800

    Bug #000000
    [root cause  ]
    [changes     ]
    [side effects]
    [self test   ]:
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:no
    [board]:xxxx
    [test case]:xxxx
    [reviewers   ]

--- services/core/java/com/android/server/wm/TaskSnapshotSurface.java
+++ services/core/java/com/android/server/wm/TaskSnapshotSurface.java
@@ -20,6 +20,7 @@ import static android.graphics.Color.WHITE;
 import static android.graphics.Color.alpha;
 import static android.view.SurfaceControl.HIDDEN;
 import static android.view.WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM;
+import static android.view.WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS;
 import static android.view.WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED;
 import static android.view.WindowManager.LayoutParams.FLAG_IGNORE_CHEEK_PRESSES;
 import static android.view.WindowManager.LayoutParams.FLAG_LOCAL_FOCUS_MODE;
@@ -163,7 +164,8 @@ class TaskSnapshotSurface implements StartingSurface {
             layoutParams.format = snapshot.getSnapshot().getFormat();
             layoutParams.flags = (windowFlags & ~FLAG_INHERIT_EXCLUDES)
                     | FLAG_NOT_FOCUSABLE
-                    | FLAG_NOT_TOUCHABLE;
+                    | FLAG_NOT_TOUCHABLE
+                    | FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS;
             layoutParams.privateFlags = PRIVATE_FLAG_TASK_SNAPSHOT
                     | (windowPrivateFlags & PRIVATE_FLAG_INHERITS);
             layoutParams.token = token.token;
