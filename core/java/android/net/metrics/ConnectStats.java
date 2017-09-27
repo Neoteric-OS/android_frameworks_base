2d45acc7b0c0625a07b95003c9c2e443bd2c6312
Hugo Benichi <hugobenichi@google.com>
2017-09-27 23:28:59 +0900

    Investigating better connect and dns logging
    Bug:
    Test: runtest frameworks-net
    Change-Id: I599e271b0347f7e52b07a4c2f552d1cdd88bc27d

--- core/java/android/net/metrics/ConnectStats.java
+++ core/java/android/net/metrics/ConnectStats.java
@@ -20,6 +20,7 @@ import android.net.NetworkCapabilities;
 import android.system.OsConstants;
 import android.util.IntArray;
 import android.util.SparseIntArray;
+
 import com.android.internal.util.BitUtils;
 import com.android.internal.util.TokenBucket;
 
@@ -43,6 +44,8 @@ public class ConnectStats {
     public final TokenBucket mLatencyTb;
     /** Maximum number of latency values recorded. */
     public final int mMaxLatencyRecords;
+    /** Total count of events */
+    public int eventCount = 0;
     /** Total count of successful connects. */
     public int connectCount = 0;
     /** Total count of successful connects done in blocking mode. */
@@ -58,6 +61,7 @@ public class ConnectStats {
     }
 
     public void addEvent(int errno, int latencyMs, String ipAddr) {
+        eventCount++;
         if (isSuccess(errno)) {
             countConnect(errno, ipAddr);
             countLatency(errno, latencyMs);
@@ -117,6 +121,7 @@ public class ConnectStats {
         for (int t : BitUtils.unpackBits(transports)) {
             builder.append(NetworkCapabilities.transportNameOf(t)).append(", ");
         }
+        builder.append(String.format("%d events, ", eventCount));
         builder.append(String.format("%d success, ", connectCount));
         builder.append(String.format("%d blocking, ", connectBlockingCount));
         builder.append(String.format("%d IPv6 dst", ipv6ConnectCount));
