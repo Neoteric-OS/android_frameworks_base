5addb4df168529d96d214b75a03ca123d2edfbda
Benedict Wong <benedictwong@google.com>
2017-09-28 13:37:24 -0700

    Add IpTables rules to ignore UDP-encap overhead
    Allows IpSecService to dynamically add IpTables rules to selectively
    ignore UDP encapsulation overhead. Ignores all UDP inbound packets with
    SPI > 0; This allows us to do data accounting for IKE, but ignore the
    UDP-encap'd packets, which will be accounted for after the second pass
    through IpTables.
    
    Test: IpSecServiceTest updated to test binder call, CTS test updated to
    test IpTables rules
    Bug: 62994731
    Change-Id: I665d55944fab6935bfeb9641180d38e8ee272707

--- services/core/java/com/android/server/IpSecService.java
+++ services/core/java/com/android/server/IpSecService.java
@@ -34,6 +34,7 @@ import android.net.IpSecTransform;
 import android.net.IpSecTransformResponse;
 import android.net.IpSecUdpEncapResponse;
 import android.net.NetworkUtils;
+import android.net.TrafficStats;
 import android.net.util.NetdService;
 import android.os.Binder;
 import android.os.IBinder;
@@ -50,7 +51,7 @@ import android.util.SparseArray;
 
 import com.android.internal.annotations.GuardedBy;
 import com.android.internal.annotations.VisibleForTesting;
-
+import dalvik.system.SocketTagger;
 import java.io.FileDescriptor;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -560,6 +561,12 @@ public class IpSecService extends IIpSecService.Stub {
             Log.d(TAG, "Closing port " + mPort);
             IoUtils.closeQuietly(mSocket);
             mSocket = null;
+
+            try {
+                mSrvConfig.getNetdInstance().ipSecRemoveUdpEncapExemption(mPort);
+            } catch (RemoteException e) {
+                Log.e(TAG, "Failed to cleanup IPtables UDP exemption for port " + mPort);
+            }
         }
 
         @Override
@@ -792,12 +799,14 @@ public class IpSecService extends IIpSecService.Stub {
 
         int resourceId = mNextResourceId.getAndIncrement();
         FileDescriptor sockFd = null;
+        long identityToken = 0;
         try {
             if (!mUserQuotaTracker.getUserRecord(Binder.getCallingUid()).socket.isAvailable()) {
                 return new IpSecUdpEncapResponse(IpSecManager.Status.RESOURCE_UNAVAILABLE);
             }
 
             sockFd = Os.socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+            mSrvConfig.getNetdInstance().ipSecSetSocketOwner(sockFd, Binder.getCallingUid());
 
             if (port != 0) {
                 Log.v(TAG, "Binding to port " + port);
@@ -811,6 +820,9 @@ public class IpSecService extends IIpSecService.Stub {
                     OsConstants.IPPROTO_UDP,
                     OsConstants.UDP_ENCAP,
                     OsConstants.UDP_ENCAP_ESPINUDP);
+
+            mSrvConfig.getNetdInstance().ipSecAddUdpEncapExemption(port);
+            setSockUid(sockFd, Binder.getCallingUid());
 
             mUdpSocketRecords.put(
                     resourceId, new UdpSocketRecord(resourceId, binder, sockFd, port));
@@ -826,8 +838,13 @@ public class IpSecService extends IIpSecService.Stub {
     /** close a socket that has been been allocated by and registered with the system server */
     @Override
     public void closeUdpEncapsulationSocket(int resourceId) throws RemoteException {
-
         releaseManagedResource(mUdpSocketRecords, resourceId, "UdpEncapsulationSocket");
+    }
+
+    @VisibleForTesting
+    void setSockUid(FileDescriptor fd, int uid) throws IOException {
+        TrafficStats.setThreadStatsUid(uid);
+        SocketTagger.get().tag(fd);
     }
 
     /**
