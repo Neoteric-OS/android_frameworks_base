187d375fa39860fa05f7ac6f5928f46d0eb1b004
Erik Kline <ek@google.com>
2017-10-04 20:28:54 +0900

    IpManager: add display name for helpful logging
    Test: none
    Bug: 62476366
    Change-Id: I509506814fa704e54f3d67158203276312a24fa1

--- services/net/java/android/net/ip/IpManager.java
+++ services/net/java/android/net/ip/IpManager.java
@@ -26,6 +26,7 @@ import android.net.IpPrefix;
 import android.net.LinkAddress;
 import android.net.LinkProperties.ProvisioningChange;
 import android.net.LinkProperties;
+import android.net.Network;
 import android.net.ProxyInfo;
 import android.net.RouteInfo;
 import android.net.StaticIpConfiguration;
@@ -348,6 +349,11 @@ public class IpManager extends StateMachine {
                 return this;
             }
 
+            public Builder withDisplayName(String displayName) {
+                mConfig.mDisplayName = displayName;
+                return this;
+            }
+
             public ProvisioningConfiguration build() {
                 return new ProvisioningConfiguration(mConfig);
             }
@@ -362,6 +368,7 @@ public class IpManager extends StateMachine {
         /* package */ ApfCapabilities mApfCapabilities;
         /* package */ int mProvisioningTimeoutMs = DEFAULT_TIMEOUT_MS;
         /* package */ int mIPv6AddrGenMode = INetd.IPV6_ADDR_GEN_MODE_STABLE_PRIVACY;
+        /* package */ String mDisplayName = null;
 
         public ProvisioningConfiguration() {} // used by Builder
 
@@ -374,6 +381,8 @@ public class IpManager extends StateMachine {
             mStaticIpConfig = other.mStaticIpConfig;
             mApfCapabilities = other.mApfCapabilities;
             mProvisioningTimeoutMs = other.mProvisioningTimeoutMs;
+            mIPv6AddrGenMode = other.mIPv6AddrGenMode;
+            mDisplayName = other.mDisplayName;
         }
 
         @Override
@@ -388,6 +397,7 @@ public class IpManager extends StateMachine {
                     .add("mApfCapabilities: " + mApfCapabilities)
                     .add("mProvisioningTimeoutMs: " + mProvisioningTimeoutMs)
                     .add("mIPv6AddrGenMode: " + mIPv6AddrGenMode)
+                    .add("mDisplayName: " + mDisplayName)
                     .toString();
         }
 
@@ -532,8 +542,9 @@ public class IpManager extends StateMachine {
     private static final int CMD_UPDATE_TCP_BUFFER_SIZES          = 7;
     private static final int CMD_UPDATE_HTTP_PROXY                = 8;
     private static final int CMD_SET_MULTICAST_FILTER             = 9;
-    private static final int EVENT_PROVISIONING_TIMEOUT           = 10;
-    private static final int EVENT_DHCPACTION_TIMEOUT             = 11;
+    private static final int CMD_SET_NETWORK                      = 10;
+    private static final int EVENT_PROVISIONING_TIMEOUT           = 11;
+    private static final int EVENT_DHCPACTION_TIMEOUT             = 12;
 
     private static final int MAX_LOG_RECORDS = 500;
     private static final int MAX_PACKET_RECORDS = 100;
@@ -788,6 +799,10 @@ public class IpManager extends StateMachine {
      */
     public void setMulticastFilter(boolean enabled) {
         sendMessage(CMD_SET_MULTICAST_FILTER, enabled);
+    }
+
+    public void setNetwork(int netId) {
+        sendMessage(CMD_SET_NETWORK, new Network(netId));
     }
 
     public void dump(FileDescriptor fd, PrintWriter writer, String[] args) {
@@ -1437,14 +1452,15 @@ public class IpManager extends StateMachine {
     class RunningState extends State {
         private ConnectivityPacketTracker mPacketTracker;
         private boolean mDhcpActionInFlight;
+        private Network mNetwork;
 
         @Override
         public void enter() {
             // Get the Configuration for ApfFilter from Context
-            boolean filter802_3Frames =
+            final boolean filter802_3Frames =
                     mContext.getResources().getBoolean(R.bool.config_apfDrop802_3Frames);
 
-            int[] ethTypeBlackList = mContext.getResources().getIntArray(
+            final int[] ethTypeBlackList = mContext.getResources().getIntArray(
                     R.array.config_apfEthTypeBlackList);
 
             mApfFilter = ApfFilter.maybeCreate(mConfiguration.mApfCapabilities, mNetworkInterface,
@@ -1456,7 +1472,9 @@ public class IpManager extends StateMachine {
             }
 
             mPacketTracker = createPacketTracker();
-            if (mPacketTracker != null) mPacketTracker.start();
+            if (mPacketTracker != null) mPacketTracker.start(mConfiguration.mDisplayName);
+
+            mNetwork = null;
 
             if (mConfiguration.mEnableIPv6 && !startIPv6()) {
                 doImmediateProvisioningFailure(IpManagerEvent.ERROR_STARTING_IPV6);
@@ -1470,7 +1488,7 @@ public class IpManager extends StateMachine {
                 return;
             }
 
-            InitialConfiguration config = mConfiguration.mInitialConfig;
+            final InitialConfiguration config = mConfiguration.mInitialConfig;
             if ((config != null) && !applyInitialConfig(config)) {
                 // TODO introduce a new IpManagerEvent constant to distinguish this error case.
                 doImmediateProvisioningFailure(IpManagerEvent.ERROR_INVALID_PROVISIONING);
@@ -1499,6 +1517,8 @@ public class IpManager extends StateMachine {
                 mDhcpClient.sendMessage(DhcpClient.CMD_STOP_DHCP);
                 mDhcpClient.doQuit();
             }
+
+            mNetwork = null;
 
             if (mPacketTracker != null) {
                 mPacketTracker.stop();
@@ -1595,6 +1615,19 @@ public class IpManager extends StateMachine {
                     } else {
                         mCallback.setFallbackMulticastFilter(mMulticastFiltering);
                     }
+                    break;
+                }
+
+                case CMD_SET_NETWORK: {
+                    final Network network = (Network) msg.obj;
+                    if (mNetwork != null && !mNetwork.equals(network)) {
+                        final String errmsg = String.format(
+                                "Changing networks (%s -> %s) while running is BAD",
+                                mNetwork, network);
+                        mLog.e(errmsg);
+                        Log.wtf(mTag, errmsg);
+                    }
+                    mNetwork = network;
                     break;
                 }
 
