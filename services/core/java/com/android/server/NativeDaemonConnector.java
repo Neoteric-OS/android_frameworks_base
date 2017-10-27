01647ab67ede16e59859438c498a768990dd8d44
Tetsutoki Shiozawa <tetsutoki.shiozawa@sony.com>
2017-10-27 14:03:00 +0900

    Definitely abort reconnecting to native daemon during shutdown
    Symptom:
    System_server crashed due to unsolved "netd" service.
    
    Root cause:
    My previous patch was not enough for avoiding this crash.
    e83724e Abort connecting to native daemon during shutdown
    
    listenToSocket loop can be finished without throwing exception.
    
    Solution:
    It's better to test shutdown condition before reconnection even
    though listenToSockat is finished without exception.
    
    Change-Id: Ifdfe43d52bef891f55bd7b07cc1aa0fa248a6030

--- services/core/java/com/android/server/NativeDaemonConnector.java
+++ services/core/java/com/android/server/NativeDaemonConnector.java
@@ -138,13 +138,13 @@ final class NativeDaemonConnector implements Runnable, Handler.Callback, Watchdo
                 listenToSocket();
             } catch (Exception e) {
                 loge("Error in NativeDaemonConnector: " + e);
-                String shutdownAct = SystemProperties.get(
-                        ShutdownThread.SHUTDOWN_ACTION_PROPERTY, "");
-                if (shutdownAct != null && shutdownAct.length() > 0) {
-                    // The device is in middle of shutdown.
-                    break;
-                }
                 SystemClock.sleep(5000);
+            }
+            String shutdownAct = SystemProperties.get(
+                    ShutdownThread.SHUTDOWN_ACTION_PROPERTY, "");
+            if (shutdownAct != null && shutdownAct.length() > 0) {
+                // The device is in middle of shutdown.
+                break;
             }
         }
     }
