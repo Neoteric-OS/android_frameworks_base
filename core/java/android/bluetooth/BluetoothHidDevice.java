3c59a7e4e1a674c76342dbcea1650bc21858769f
Hansong Zhang <hsz@google.com>
2017-11-08 09:57:12 -0800

    Bluetooth HID device API fix
    * Add a builder for BluetoothHidDeviceAppQosSettings
    * Add documentation for BluetoothHidDeviceAppSdpSettings
    and BluetoothHidDeviceAppQosSettings
    * Fix the behavior of and BluetoothHidDeviceAppSdpSettings.equals()
    and BluetoothHidDeviceAppQosSettings.equals()
    
    Test: test with HID device apps
    Change-Id: I933f4ec3034e1f704ddd614e48b76d27fda8b6ff

--- core/java/android/bluetooth/BluetoothHidDevice.java
+++ core/java/android/bluetooth/BluetoothHidDevice.java
@@ -63,7 +63,7 @@ public final class BluetoothHidDevice implements BluetoothProfile {
      * receive.
      */
     @SdkConstant(SdkConstantType.BROADCAST_INTENT_ACTION)
-    public static final String ACTION_CONNECTION_STATE_CHANGED =
+    public static final String HIDDEVICE_PROFILE_ACTION_CONNECTION_STATE_CHANGED =
             "android.bluetooth.hiddevice.profile.action.CONNECTION_STATE_CHANGED";
 
     /**
@@ -352,10 +352,16 @@ public final class BluetoothHidDevice implements BluetoothProfile {
      * {@link #unregisterApp(BluetoothHidDeviceAppConfiguration)}.
      *
      * @param sdp {@link BluetoothHidDeviceAppSdpSettings} object of HID Device SDP record.
+     * The HID Device SDP record is required.
      * @param inQos {@link BluetoothHidDeviceAppQosSettings} object of Incoming QoS Settings.
+     * The Incoming QoS Settings is not required. Use null or default
+     * BluetoothHidDeviceAppQosSettings.Builder for default values.
      * @param outQos {@link BluetoothHidDeviceAppQosSettings} object of Outgoing QoS Settings.
+     * The Outgoing QoS Settings is not required. Use null or default
+     * BluetoothHidDeviceAppQosSettings.Builder for default values.
      * @param callback {@link BluetoothHidDeviceCallback} object to which callback messages will be
      * sent.
+     * The BluetoothHidDeviceCallback object is required.
      * @return
      */
     public boolean registerApp(BluetoothHidDeviceAppSdpSettings sdp,
