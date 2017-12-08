d67952cef2d7aaa54ab15697aa7e367b42e48b1f
Ruchi Kandoi <kandoiruchi@google.com>
2017-12-08 15:07:03 -0800

    Open Mobile API for frameworks/base
    This contribution is an implementation of the GlobalPlatform
    Open Mobile API for Android with some modifications to
    namespaces and packages to make it suitable as a core
    Android component.
    
    This contribution is based on
    0001-Open-Source-Contribution-of-Smartcard-Service-for-fr.patch
    which can be found in
    https://portland.source.codeaurora.org/patches/quic/la/PATCH_217881_OpenMobileAPI_20171206.tar.gz
    
    The submitted patch was derived from
    https://source.codeaurora.org/quic/la/platform/packages/apps/SmartCardService/commit/?h=LA.BF64.1.2.1&id=06ecea9abb8264049f52c7e31c0bc13330a425d5.
    
    Test: To be completed
    
    Change-Id: Iac5206bd84798ca0fcdb504c89e1da5383012a5a
    Signed-off-by: Jeremy O'Donoghue <jodonogh@codeaurora.org>

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -63,7 +63,6 @@ import static android.os.Process.ROOT_UID;
 import static android.os.Process.SCHED_FIFO;
 import static android.os.Process.SCHED_OTHER;
 import static android.os.Process.SCHED_RESET_ON_FORK;
-import static android.os.Process.SE_UID;
 import static android.os.Process.SHELL_UID;
 import static android.os.Process.SIGNAL_QUIT;
 import static android.os.Process.SIGNAL_USR1;
@@ -19185,7 +19184,6 @@ public class ActivityManagerService extends IActivityManager.Stub
             case PHONE_UID:
             case BLUETOOTH_UID:
             case NFC_UID:
-            case SE_UID:
                 isCallerSystem = true;
                 break;
             default:
