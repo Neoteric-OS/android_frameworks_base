f100fbb3aec9ead5e07fb4e49777a2cd5e164942
Holly Jiuyu Sun <jiuyu@google.com>
2017-12-11 13:28:03 -0800

    Add asn1 to platform.
    Add unit test.
    
    Bug: 38206971
    Test: unit test
    Change-Id: Ifb7c7d13ad0f980ba986b3aa6c3213ad26658489

---
+++ telephony/tests/Android.mk
@@ -0,0 +1,29 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := tests
+
+LOCAL_SRC_FILES := $(call all-subdir-java-files)
+
+#LOCAL_STATIC_JAVA_LIBRARIES := librilproto-java
+
+LOCAL_JAVA_LIBRARIES := \
+    android.test.runner \
+    telephony-common \
+    ims-common \
+    services.core \
+    bouncycastle \
+    android.test.base \
+    android.test.mock \
+
+LOCAL_STATIC_JAVA_LIBRARIES := guava \
+                               frameworks-base-testutils \
+			       mockito-target-minus-junit4 \
+			       android-support-test \
+			       platform-test-annotations
+
+LOCAL_PACKAGE_NAME := FrameworksUiccTests
+
+LOCAL_COMPATIBILITY_SUITE := device-tests
+
+include $(BUILD_PACKAGE)
