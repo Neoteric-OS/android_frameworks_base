046074b659f2a0842e86acaf29cadfdcc9284d79
Benedict Wong <benedictwong@google.com>
2017-12-13 18:26:40 -0800

    Add checks to ensure SPIs are not reused
    This change adds an additional check in CheckIpsecConfig to prevent
    users from using the same SPI twice. This allows for a more granular
    error message.
    
    Bug: 70642141
    Test: Tests added in IpSecServiceParameterizedTest
    Change-Id: I9621fb05c6b162bd8ae8db4ac1e64feaa9d0ac73

--- tests/net/java/com/android/server/IpSecServiceParameterizedTest.java
+++ tests/net/java/com/android/server/IpSecServiceParameterizedTest.java
@@ -268,6 +268,31 @@ public class IpSecServiceParameterizedTest {
                         anyInt());
     }
 
+    public void testCreateTwoTransformsWithSameSpis() throws Exception {
+        IpSecConfig ipSecConfig = new IpSecConfig();
+        addDefaultSpisAndRemoteAddrToIpSecConfig(ipSecConfig);
+        addAuthAndCryptToIpSecConfig(ipSecConfig);
+
+        IpSecTransformResponse createTransformResp =
+                mIpSecService.createTransportModeTransform(ipSecConfig, new Binder());
+        assertEquals(IpSecManager.Status.OK, createTransformResp.status);
+
+        // Attempting to create transform a second time with the same SPIs should throw an error...
+        try {
+                mIpSecService.createTransportModeTransform(ipSecConfig, new Binder());
+                fail("IpSecService should have thrown an error for reuse of SPI");
+        } catch (IllegalStateException expected) {
+        }
+
+        // ... even if the transform is deleted
+        mIpSecService.deleteTransportModeTransform(createTransformResp.resourceId);
+        try {
+                mIpSecService.createTransportModeTransform(ipSecConfig, new Binder());
+                fail("IpSecService should have thrown an error for reuse of SPI");
+        } catch (IllegalStateException expected) {
+        }
+    }
+
     @Test
     public void testDeleteTransform() throws Exception {
         IpSecConfig ipSecConfig = new IpSecConfig();
