d55685db430206bf5c60afefbde97a36749e1342
Masaho Nishikawa <masaho.nishikawa@sony.com>
2017-12-18 14:33:38 +0900

    Add config to show an alert dialog for video charges
    Add config to show an alert dialog for video charges when user starts
    a video call. The default value is false.
    
    Test: manual - Verified that a warning dialog about charges when a
    video call is started if KEY_SHOW_VIDEO_CALL_CHARGES_ALERT_DIALOG_BOOL
    is true.
    Bug: 67832837
    
    Change-Id: I11ab3ea9a8971a9d76b47714638f1fe0a9c980c2

--- telephony/java/android/telephony/CarrierConfigManager.java
+++ telephony/java/android/telephony/CarrierConfigManager.java
@@ -1712,6 +1712,13 @@ public class CarrierConfigManager {
     public static final String KEY_SPN_DISPLAY_RULE_USE_ROAMING_FROM_SERVICE_STATE_BOOL =
             "spn_display_rule_use_roaming_from_service_state_bool";
 
+    /**
+     * Flag specifying whether to show an alert dialog for video call charges.
+     * By default this value is {@code false}.
+     */
+    public static final String KEY_SHOW_VIDEO_CALL_CHARGES_ALERT_DIALOG_BOOL =
+            "show_video_call_charges_alert_dialog_bool";
+
     /** The default value for every variable. */
     private final static PersistableBundle sDefaults;
 
@@ -1995,6 +2002,7 @@ public class CarrierConfigManager {
         sDefaults.putBoolean(KEY_IDENTIFY_HIGH_DEFINITION_CALLS_IN_CALL_LOG_BOOL, false);
         sDefaults.putBoolean(KEY_SPN_DISPLAY_RULE_USE_ROAMING_FROM_SERVICE_STATE_BOOL, false);
         sDefaults.putBoolean(KEY_ALWAYS_SHOW_DATA_RAT_ICON_BOOL, false);
+        sDefaults.putBoolean(KEY_SHOW_VIDEO_CALL_CHARGES_ALERT_DIALOG_BOOL, false);
     }
 
     /**
