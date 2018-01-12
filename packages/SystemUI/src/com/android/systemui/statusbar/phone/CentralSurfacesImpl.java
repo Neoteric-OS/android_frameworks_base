87e9b5bcf496d8a2af0f5969dfd8cceb99e4241c
Kensuke Matsui <kensuke.matsui@sony.com>
2018-01-12 12:14:50 +0900

    Move StatusBar and NavigationBar to prevent burn in
    To prevent screen burn in we move StatusBar and NavigationBar
    a few dots when the screen is turned ON.
    
    Test: Manual
    Bug: 138172569
    Change-Id: I590c6866be3ffd6589b84a533e24f8afd4942895

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -289,6 +289,11 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
      */
     private static final int HINT_RESET_DELAY_MS = 1200;
 
+    /**
+     * The threshold sleep time of moving system bars to avoid burn in.
+     */
+    private static final int THRESHOLD_SLEEP_TIME_MILLIS = 10000;
+
     private static final UiEventLogger sUiEventLogger = new UiEventLoggerImpl();
 
     /**
@@ -881,6 +886,8 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
         mLightRevealScrimViewModelLazy = lightRevealScrimViewModelLazy;
     }
 
+    private boolean mIsMoveSystemBarsEnabled;
+
     @Override
     public void start() {
         mScreenLifecycle.addObserver(mScreenObserver);
@@ -1112,6 +1119,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
                     }
                 }, OverlayPlugin.class, true /* Allow multiple plugins */);
 
+        mIsMoveSystemBarsEnabled = mContext.getResources().getBoolean(R.bool.config_enableMoveSystemBars);
         mStartingSurfaceOptional.ifPresent(startingSurface -> startingSurface.setSysuiProxy(
                 (requestTopUi, componentTag) -> mMainExecutor.execute(() ->
                         mNotificationShadeWindowController.setRequestTopUi(
@@ -3440,6 +3448,8 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
 
     @VisibleForTesting
     final WakefulnessLifecycle.Observer mWakefulnessObserver = new WakefulnessLifecycle.Observer() {
+        private long mStartSleepTime;
+
         @Override
         public void onFinishedGoingToSleep() {
             mCameraLauncherLazy.get().setLaunchingAffordance(false);
@@ -3469,6 +3479,8 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
                         () -> mCommandQueueCallbacks.onEmergencyActionLaunchGestureDetected());
             }
             updateIsKeyguard();
+
+            mStartSleepTime = SystemClock.uptimeMillis();
         }
 
         @Override
@@ -3524,6 +3536,18 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
 
             });
             DejankUtils.stopDetectingBlockingIpcs(tag);
+
+            if (mIsMoveSystemBarsEnabled) {
+                mStatusBarView.moveStatusBar();
+                NavigationBarView navigationBarView =
+                        mNavigationBarController.getDefaultNavigationBarView();
+                if (SystemClock.uptimeMillis() - mStartSleepTime >= THRESHOLD_SLEEP_TIME_MILLIS) {
+                    mStatusBarView.moveStatusBar();
+                    if (navigationBarView != null) {
+                        navigationBarView.moveNavigationBar();
+                    }
+                }
+            }
         }
 
         @Override
