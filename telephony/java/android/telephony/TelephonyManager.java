23d818067373877d5a3009d5edf751ae455a4f3a
Eric Schwarzenbach <easchwar@google.com>
2018-02-13 17:45:27 -0800

    Add constants for reporting criteria
    Adds RILConstants and TelephonyManager constants for reporting and
    setting indication filter bits.
    
    Bug: 72117365
    Test: make
    Change-Id: Ia52ffdbc9ea2499893ece518aba4a99c6fe4ee90

--- telephony/java/android/telephony/TelephonyManager.java
+++ telephony/java/android/telephony/TelephonyManager.java
@@ -7714,6 +7714,18 @@ public class TelephonyManager {
      */
     public static final int INDICATION_FILTER_DATA_CALL_DORMANCY_CHANGED    = 0x4;
 
+    /**
+     * The indication for link capacity estimate update.
+     * @hide
+     */
+    public static final int INDICATION_FILTER_LINK_CAPACITY_ESTIMATE    = 0x8;
+
+    /**
+     * The indication for physical channel config update.
+     * @hide
+     */
+    public static final int INDICATION_FILTER_PHYSICAL_CHANNEL_CONFIG = 0x10;
+
     /** @hide */
     @IntDef(flag = true, prefix = { "INDICATION_FILTER_" }, value = {
             INDICATION_FILTER_SIGNAL_STRENGTH,
