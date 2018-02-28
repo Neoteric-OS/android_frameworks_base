d92f8bfac9f782a4477ea5a5c95ef90c148f4548
Erik Kline <ek@google.com>
2018-02-28 15:01:35 +0900

    Don't unnecessarily reevaluate tethering provisioning
    Registering for carrier config changes can deliver a sticky broadcast
    and can cause Tethering to think something has changed and reevaluate
    provisioning status, even though this has been checked before it
    entered tethering mode alive state.
    
    Additionally, move the provisioning_app{,no_ui} resources into the
    TetheringConfiguration, if for no other reason than now we can log
    it in .toString().
    
    Test: as follows
        - built
        - flashed
        - booted
        - runtest frameworks-net passes
        - manual USB tethering toward WiFi works
    Bug: 69565814
    Change-Id: Ib8b2620ce44c55e5eb0afd3f00f3f5aa4fc8a593

--- tests/net/java/com/android/server/connectivity/TetheringTest.java
+++ tests/net/java/com/android/server/connectivity/TetheringTest.java
@@ -204,6 +204,7 @@ public class TetheringTest {
     @Test
     public void canRequireProvisioning() {
         setupForRequiredProvisioning();
+        sendConfigurationChanged();
         assertTrue(mTethering.isTetherProvisioningRequired());
     }
 
@@ -212,6 +213,7 @@ public class TetheringTest {
         setupForRequiredProvisioning();
         when(mContext.getSystemService(Context.CARRIER_CONFIG_SERVICE))
                 .thenReturn(null);
+        sendConfigurationChanged();
         // Couldn't get the CarrierConfigManager, but still had a declared provisioning app.
         // We therefore still require provisioning.
         assertTrue(mTethering.isTetherProvisioningRequired());
@@ -221,6 +223,7 @@ public class TetheringTest {
     public void toleratesCarrierConfigMissing() {
         setupForRequiredProvisioning();
         when(mCarrierConfigManager.getConfig()).thenReturn(null);
+        sendConfigurationChanged();
         // We still have a provisioning app configured, so still require provisioning.
         assertTrue(mTethering.isTetherProvisioningRequired());
     }
@@ -257,6 +260,11 @@ public class TetheringTest {
         intent.putExtra(USB_CONNECTED, connected);
         intent.putExtra(USB_CONFIGURED, configured);
         intent.putExtra(USB_FUNCTION_RNDIS, rndisFunction);
+        mServiceContext.sendStickyBroadcastAsUser(intent, UserHandle.ALL);
+    }
+
+    private void sendConfigurationChanged() {
+        final Intent intent = new Intent(Intent.ACTION_CONFIGURATION_CHANGED);
         mServiceContext.sendStickyBroadcastAsUser(intent, UserHandle.ALL);
     }
 
