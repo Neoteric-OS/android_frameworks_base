fbe00d9f66387db6049826e72a26f8946759fc2d
Nathan Harold <nharold@google.com>
2018-03-01 13:03:53 -0800

    Expose LCE Callback in DataService API
    Add LCE to the DataService SystemApi so that
    system apps implementing data services can provide
    link capacity estimates.
    
    Bug: 72117292
    Test: runtest frameworks-telephony
    Change-Id: I5612a7e6cc756a7bf21373fff860c5682b5f264f

--- telephony/java/android/telephony/data/DataServiceCallback.java
+++ telephony/java/android/telephony/data/DataServiceCallback.java
@@ -171,4 +171,20 @@ public class DataServiceCallback {
             }
         }
     }
+
+    /**
+     * Called to indicate that data connection list changed.
+     *
+     * @param dataCallList List of the current active data connection.
+     */
+    public void onLinkCapacityEstimateChanged(LinkCapacityEstimate lce) {
+        IDataServiceCallback callback = mCallback.get();
+        if (callback != null) {
+            try {
+                callback.onLinkCapacityEstimateChanged(lce);
+            } catch (RemoteException e) {
+                Rlog.e(TAG, "Failed to onLinkCapacityEstimateChanged on the remote");
+            }
+        }
+    }
 }
