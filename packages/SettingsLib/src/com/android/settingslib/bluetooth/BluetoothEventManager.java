227c78b203c4852253a70ca35aa5c666b1bc549e
Stanley Tng <stng@google.com>
2018-03-02 11:43:57 -0800

    WIP Add code to do the BluetoothHearingAid.ACTION_CONNECTION_STATE_CHANGED
    Change-Id: I0681fe977b70fcf18845e30782cf3900cd6422c4

--- packages/SettingsLib/src/com/android/settingslib/bluetooth/BluetoothEventManager.java
+++ packages/SettingsLib/src/com/android/settingslib/bluetooth/BluetoothEventManager.java
@@ -115,6 +115,12 @@ public class BluetoothEventManager {
                    new ActiveDeviceChangedHandler());
         addHandler(BluetoothHeadset.ACTION_ACTIVE_DEVICE_CHANGED,
                    new ActiveDeviceChangedHandler());
+        addHandler(BluetoothHearingAid.ACTION_ACTIVE_DEVICE_CHANGED,
+                   new ActiveDeviceChangedHandler());
+
+        // Individual profile broadcasts (for special handling)
+        addHandler(BluetoothHearingAid.ACTION_CONNECTION_STATE_CHANGED,
+                new ConnectionStateChangedHandler());
 
         mContext.registerReceiver(mBroadcastReceiver, mAdapterIntentFilter, null, mReceiverHandler);
         mContext.registerReceiver(mProfileBroadcastReceiver, mProfileIntentFilter, null, mReceiverHandler);
@@ -240,9 +246,19 @@ public class BluetoothEventManager {
         @Override
         public void onReceive(Context context, Intent intent, BluetoothDevice device) {
             CachedBluetoothDevice cachedDevice = mDeviceManager.findDevice(device);
-            int state = intent.getIntExtra(BluetoothAdapter.EXTRA_CONNECTION_STATE,
-                    BluetoothAdapter.ERROR);
-            dispatchConnectionStateChanged(cachedDevice, state);
+            String action = intent.getAction();
+
+            if (Objects.equals(action, BluetoothAdapter.ACTION_CONNECTION_STATE_CHANGED)) {
+                int state = intent.getIntExtra(BluetoothAdapter.EXTRA_CONNECTION_STATE,
+                        BluetoothAdapter.ERROR);
+                dispatchConnectionStateChanged(cachedDevice, state);
+            }
+            else if (Objects.equals(action, BluetoothHearingAid.ACTION_CONNECTION_STATE_CHANGED)) {
+                Log.d(TAG, "ConnectionStateChangedHandler: action=" + action);
+            }
+            else {
+                Log.d(TAG, "ConnectionStateChangedHandler: Unhandled intent. action=" + action);
+            }
         }
     }
 
@@ -434,6 +450,8 @@ public class BluetoothEventManager {
                 bluetoothProfile = BluetoothProfile.A2DP;
             } else if (Objects.equals(action, BluetoothHeadset.ACTION_ACTIVE_DEVICE_CHANGED)) {
                 bluetoothProfile = BluetoothProfile.HEADSET;
+            } else if (Objects.equals(action, BluetoothHearingAid.ACTION_ACTIVE_DEVICE_CHANGED)) {
+                bluetoothProfile = BluetoothProfile.HEARING_AID;
             } else {
                 Log.w(TAG, "ActiveDeviceChangedHandler: unknown action " + action);
                 return;
@@ -444,6 +462,8 @@ public class BluetoothEventManager {
 
     private void dispatchActiveDeviceChanged(CachedBluetoothDevice activeDevice,
                                              int bluetoothProfile) {
+        Log.d(TAG, "dispatchActiveDeviceChanged: activeDevice=" + activeDevice
+              + ", bluetoothProfile=" + bluetoothProfile);
         synchronized (mCallbacks) {
             for (BluetoothCallback callback : mCallbacks) {
                 callback.onActiveDeviceChanged(activeDevice, bluetoothProfile);
