4657265b03dbd1dfc4b53c874fff91d041520bae
pip.zhang <pip.zhang@cloudminds.com>
2018-03-27 15:55:55 +0800

    SystemUI: dismiss keyguard when sharing screenShot
    1.changing the type to broadcast caused this bug. This fake broadcast
    alse need dismiss keyguard like activity.
    
    Change-Id: I2baee7c424a0e69241882e0cc17d0d758662d1b6
    Bug-Id: HO-9922
    Depends-On: N/A
    Test: take a screenShot. then lockscreen. then swipe down from
    statusbar. then try to share the screenShoted picture from its
    notification 'SHARE' button. then the chooser activity should
    be stated.

--- packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
@@ -406,9 +406,9 @@ class DeleteImageInBackgroundTask extends AsyncTask<Uri, Void, Void> {
     }
 }
 
-class GlobalScreenshot {
+public class GlobalScreenshot {
     static final String SCREENSHOT_URI_ID = "android:screenshot_uri_id";
-    static final String SHARING_INTENT = "android:screenshot_sharing_intent";
+    public static final String SHARING_INTENT = "android:screenshot_sharing_intent";
 
     private static final int SCREENSHOT_FLASH_TO_PEAK_DURATION = 130;
     private static final int SCREENSHOT_DROP_IN_DURATION = 430;
