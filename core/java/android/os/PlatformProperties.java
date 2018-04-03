e6b14b776a7954ac6d80b0d9e56020523b634b60
Jiyong Park <jiyong@google.com>
2018-04-03 23:57:13 +0900

    Make system properties typed
    Bug: 73750660
    Test: m -j framework
    Change-Id: I2bbd339a8829d334240ea416918bd36d4bc6deeb

---
+++ core/java/android/os/PlatformProperties.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.os;
+
+import android.os.TypedSystemProperty.BooleanSystemProperty;
+import android.os.TypedSystemProperty.EnumSystemProperty;
+import android.os.TypedSystemProperty.IntegerSystemProperty;
+import android.os.TypedSystemProperty.Owner;
+import android.os.TypedSystemProperty.StringSystemProperty;
+
+import java.util.Optional;
+
+/**
+ * Set of platform defined system properties.
+ * @hide
+ */
+public final class PlatformProperties {
+    private PlatformProperties() {
+    }
+
+    private static class IntegerProperty extends IntegerSystemProperty {
+        protected IntegerProperty(String name, Owner owner, boolean isWriteOnce,
+                Optional<Integer> defaultValue, Accessor accessor) {
+            super(name, owner, isWriteOnce, defaultValue, accessor);
+        }
+    }
+
+    private static class BooleanProperty extends BooleanSystemProperty {
+        protected BooleanProperty(String name, Owner owner, boolean isWriteOnce,
+                Optional<Boolean> defaultValue, Accessor accessor) {
+            super(name, owner, isWriteOnce, defaultValue, accessor);
+        }
+    }
+
+    private static class StringProperty extends StringSystemProperty {
+        protected StringProperty(String name, Owner owner, boolean isWriteOnce,
+                Optional<String> defaultValue, Accessor accessor) {
+            super(name, owner, isWriteOnce, defaultValue, accessor);
+        }
+    }
+
+    private static class EnumProperty<T extends Enum<T>>
+            extends EnumSystemProperty<T> {
+        protected EnumProperty(String name, Owner owner, boolean isWriteOnce,
+                Class<T> type, Optional<T> defaultValue, Accessor accessor) {
+            super(name, owner, isWriteOnce, type, defaultValue, accessor);
+        }
+    }
+}
