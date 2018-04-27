3f4cdba2f724e0d4742fb536b60b23709a6785ba
Erik Kline <ek@google.com>
2018-04-27 22:48:33 +0900

    Remove ResolveUtil from frameworks/base callers
    Use the Private DNS bypass logic that was moved into Network.
    
    Once all callers of ResolvUtil are updated to use this interface
    ResolvUtil can be deleted.
    
    Test: as follows
        - built, flashed, booted
        - runtest frameworks-net passes
        - connection to captive portal network detects portal correctly
          and the login activity functions as expected
    
    Bug: 64133961
    Bug: 72345192
    Bug: 73872000
    Bug: 78548486
    
    Change-Id: If11ef2b5ffdc729f8449cf18dccd5f1eccbc51e6

--- core/java/android/net/SntpClient.java
+++ core/java/android/net/SntpClient.java
@@ -86,18 +86,17 @@ public class SntpClient {
      */
     public boolean requestTime(String host, int timeout, Network network) {
         // This flag only affects DNS resolution and not other socket semantics,
-        // therefore it's safe to set unilaterally rather than take more
-        // defensive measures like making a copy.
-        network.setPrivateDnsBypass(true);
+        // therefore it's safe to set unilaterally.
+        final Network networkForResolv = network.getPrivateDnsBypassingCopy();
         InetAddress address = null;
         try {
-            address = network.getByName(host);
+            address = networkForResolv.getByName(host);
         } catch (Exception e) {
             EventLogTags.writeNtpFailure(host, e.toString());
             if (DBG) Log.d(TAG, "request time failed: " + e);
             return false;
         }
-        return requestTime(address, NTP_PORT, timeout, network);
+        return requestTime(address, NTP_PORT, timeout, networkForResolv);
     }
 
     public boolean requestTime(InetAddress address, int port, int timeout, Network network) {
