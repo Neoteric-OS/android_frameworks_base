c090b7f5c084de8592145d713220ecad32d2889b
arty.ho <arty.ho@mstarsemi.com>
2018-05-03 10:55:16 +0800

    Allocate new native handle to store sidebandStream
    When we get sidebandStream from TvInput, we only store the address of
    native handle, the address point to native_handle_t in _hidl_replay.
    After openStream is finished, the _hidl_replay will be freed
    automatically, therefoce we will use the freed memory in
    setSidebandStrem, It will cause the writeNativeHandle fail in
    setSidebandStrem.
    
    Change-Id: Idd2e8343670ef5a3048836f1db978cd7c04378b7

--- services/core/jni/com_android_server_tv_TvInputHal.cpp
+++ services/core/jni/com_android_server_tv_TvInputHal.cpp
@@ -390,7 +390,14 @@ int JTvInputHal::addOrUpdateStream(int deviceId, int streamId, const sp<Surface>
                 [&result, &sidebandStream](Result res, const native_handle_t* handle) {
                     result = res;
                     if (res == Result::OK) {
-                        sidebandStream = handle;
+                        if(handle) {
+                            sidebandStream = (native_handle_t *) native_handle_create(handle->numFds, handle->numInts);
+                            if(sidebandStream) {
+                                memcpy((void*)sidebandStream, handle, sizeof(native_handle_t) + (sizeof(int) * (handle->numFds + handle->numInts)));
+                            }
+                        } else {
+                            sidebandStream = NULL;
+                        }
                     }
                 });
         if (result != Result::OK) {
@@ -398,7 +405,7 @@ int JTvInputHal::addOrUpdateStream(int deviceId, int streamId, const sp<Surface>
                     result);
             return UNKNOWN_ERROR;
         }
-        connection.mSourceHandle = NativeHandle::create((native_handle_t*)sidebandStream, false);
+        connection.mSourceHandle = NativeHandle::create((native_handle_t*)sidebandStream, true);
     }
     connection.mSurface = surface;
     if (connection.mSurface != nullptr) {
