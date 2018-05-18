9aa1381734ee1d4076c641508755996401df6f77
Chalard Jean <jchalard@google.com>
2018-05-18 23:48:49 +0900

    Fix setCapabilities.
    P introduced setSSID, UIDs and unwanted capabilities.
    None of these exhibit commutative behavior through combineCapabilities
    because their semantics don't allow it. Therefore
    NetworkRequest.setCapabilities() is badly broken around any of
    these. Look at the comments in the new tests to realize the
    extent of the damage.
    
    Clean cherry-pick of ag/4083952
    
    Bug: 79748782
    Test: new tests written, old tests pass
    Change-Id: Iafe074126132a82af37f4bf056c4a7b8d56bdc83
    Merged-In: Ia5bebf8a233775367bbf1b788870528934ecbcfb
    Merged-In: I13d7782a6c0c7b1f94137995bbb0d257a58d89c1

--- core/java/android/net/NetworkRequest.java
+++ core/java/android/net/NetworkRequest.java
@@ -199,8 +199,7 @@ public class NetworkRequest implements Parcelable {
          * @hide
          */
         public Builder setCapabilities(NetworkCapabilities nc) {
-            mNetworkCapabilities.clearAll();
-            mNetworkCapabilities.combineCapabilities(nc);
+            mNetworkCapabilities.set(nc);
             return this;
         }
 
