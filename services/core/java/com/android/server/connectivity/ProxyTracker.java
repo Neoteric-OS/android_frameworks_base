20fb1cbbc26d2a49a6ab9be78723d91b61873ddd
Chalard Jean <jchalard@google.com>
2018-06-07 18:02:37 +0900

    [PT03] Move some more code into ProxyTracker.
    Add finals and annotations. Remove comments that have lost their
    context (they were in the context of disabling a permission check
    that had been added, but constituted an API change that would not
    serve any real purpose).
    
    Test: runtest
    Change-Id: I1f879b2c105d2127072b88233d72097a0d78fe14

--- services/core/java/com/android/server/connectivity/ProxyTracker.java
+++ services/core/java/com/android/server/connectivity/ProxyTracker.java
@@ -73,4 +73,22 @@ public class ProxyTracker {
         // hosts even when PAC URLs are present to account for the legacy PAC resolver.
         return Objects.equals(ca, cb) && (ca == null || Objects.equals(ca.getHost(), cb.getHost()));
     }
+
+    @Nullable
+    public ProxyInfo getDefaultProxy() {
+        // This information is already available as a world read/writable jvm property.
+        synchronized (mProxyLock) {
+            final ProxyInfo ret = mGlobalProxy;
+            if ((ret == null) && !mDefaultProxyDisabled) return mDefaultProxy;
+            return ret;
+        }
+    }
+
+    @Nullable
+    public ProxyInfo getGlobalProxy() {
+        // This information is already available as a world read/writable jvm property.
+        synchronized (mProxyLock) {
+            return mGlobalProxy;
+        }
+    }
 }
