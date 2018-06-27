3fcd938475a26db16deabb40d926343a360c9d6f
Benedict Wong <benedictwong@google.com>
2018-06-27 15:48:22 -0700

    Add @hide support for IPsec Networks
    This change adds support for building an Android Network for IPsec
    tunnel-mode interfaces. This allows us to integration-test the IPsec
    Tunnel Mode codepaths using standard API calls.
    
    Bug: 72950854
    Test: CTS tests added and passing.
    Change-Id: I5fe97d6c0b6c00447054619c833ffd0d03fe493f

---
+++ core/java/android/net/IpSecNetworkFactory.java
@@ -0,0 +1,354 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.net;
+
+import static android.net.ConnectivityManager.TYPE_IPSEC;
+
+import android.content.Context;
+import android.net.NetworkInfo.DetailedState;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.INetworkManagementService;
+import android.os.RemoteException;
+import android.os.ServiceManager;
+import android.text.TextUtils;
+import android.util.Log;
+
+import com.android.internal.util.IndentingPrintWriter;
+
+import java.io.FileDescriptor;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * {@link android.net.NetworkFactory} that represents IpSec networks.
+ *
+ * <p>This class reports a static network score of 75.
+ *
+ * @hide
+ */
+public class IpSecNetworkFactory extends NetworkFactory {
+    private static final String TAG = IpSecNetworkFactory.class.getSimpleName();
+    static final boolean DBG = true;
+
+    private final INetworkManagementService mNMService;
+
+    private static final int NETWORK_SCORE = 75;
+    private static final String NETWORK_TYPE = "IPsec";
+
+    private final ConcurrentHashMap<String, NetworkInterfaceState> mTrackingInterfaces =
+            new ConcurrentHashMap<>();
+    private final Handler mHandler;
+    private final Context mContext;
+
+    private static NetworkCapabilities getFilter() {
+        NetworkCapabilities filter = new NetworkCapabilities();
+        filter.clearAll(); // Remove default capabilities.
+        filter.addTransportType(NetworkCapabilities.TRANSPORT_IPSEC);
+
+        return filter;
+    }
+
+    public IpSecNetworkFactory(Handler handler, Context context) {
+        super(handler.getLooper(), context, NETWORK_TYPE, getFilter());
+
+        IBinder b = ServiceManager.getService(Context.NETWORKMANAGEMENT_SERVICE);
+        mNMService = INetworkManagementService.Stub.asInterface(b);
+
+        mHandler = handler;
+        mContext = context;
+    }
+
+    @Override
+    protected void needNetworkFor(NetworkRequest networkRequest, int score) {
+        NetworkInterfaceState network = networkForRequest(networkRequest);
+
+        if (network == null) {
+            return;
+        }
+
+        if (++network.mRefCount == 1) {
+            network.start();
+        }
+    }
+
+    @Override
+    protected void releaseNetworkFor(NetworkRequest networkRequest) {
+        NetworkInterfaceState network = networkForRequest(networkRequest);
+
+        if (network == null) {
+            return;
+        }
+
+        if (--network.mRefCount == 1) {
+            network.stop();
+        }
+    }
+
+    /** @hide */
+    public void addInterface(String ifaceName) {
+        NetworkCapabilities nc = new NetworkCapabilities();
+        nc.clearAll(); // Remove default capabilities.
+        nc.addTransportType(NetworkCapabilities.TRANSPORT_IPSEC);
+        nc.addCapability(NetworkCapabilities.NET_CAPABILITY_NOT_RESTRICTED);
+        nc.addCapability(NetworkCapabilities.NET_CAPABILITY_NOT_VPN);
+        nc.addCapability(NetworkCapabilities.NET_CAPABILITY_NOT_METERED);
+        nc.addCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED);
+        nc.addCapability(NetworkCapabilities.NET_CAPABILITY_NOT_ROAMING);
+        nc.addCapability(NetworkCapabilities.NET_CAPABILITY_NOT_CONGESTED);
+        nc.addCapability(NetworkCapabilities.NET_CAPABILITY_NOT_SUSPENDED);
+        nc.addCapability(NetworkCapabilities.NET_CAPABILITY_TRUSTED);
+        nc.setLinkUpstreamBandwidthKbps(1024 * 1024);
+        nc.setLinkDownstreamBandwidthKbps(1024 * 1024);
+
+        addInterface(ifaceName, "01:02:03:04:05:06", nc);
+    }
+
+    /** @hide */
+    public void addInterface(String ifaceName, String hwAddress, NetworkCapabilities capabilities) {
+        InterfaceConfiguration config = null;
+        // Bring up the interface so we get link status indications.
+        try {
+            mNMService.setInterfaceUp(ifaceName);
+            config = mNMService.getInterfaceConfig(ifaceName);
+        } catch (RemoteException | IllegalStateException e) {
+            // Either the system is crashing or the interface has disappeared. Just ignore the
+            // error; we haven't modified any state because we only do that if our calls succeed.
+            Log.e(TAG, "Error upping interface " + ifaceName, e);
+        }
+
+        if (mTrackingInterfaces.containsKey(ifaceName)) {
+            Log.e(TAG, "Interface with name " + ifaceName + " already exists.");
+            return;
+        }
+
+        if (DBG) {
+            Log.d(TAG, "addInterface, iface: " + ifaceName + ", capabilities: " + capabilities);
+        }
+
+        NetworkInterfaceState iface =
+                new NetworkInterfaceState(ifaceName, hwAddress, mHandler, mContext, capabilities);
+        mTrackingInterfaces.put(ifaceName, iface);
+
+        updateCapabilityFilter();
+    }
+
+    private void updateCapabilityFilter() {
+        NetworkCapabilities capabilitiesFilter = new NetworkCapabilities();
+        capabilitiesFilter.clearAll();
+
+        for (NetworkInterfaceState iface : mTrackingInterfaces.values()) {
+            capabilitiesFilter.combineCapabilities(iface.mCapabilities);
+        }
+
+        if (DBG) Log.d(TAG, "updateCapabilityFilter: " + capabilitiesFilter);
+        setCapabilityFilter(capabilitiesFilter);
+    }
+
+    /** @hide */
+    public void removeInterface(String interfaceName) {
+        NetworkInterfaceState iface = mTrackingInterfaces.remove(interfaceName);
+        if (iface != null) {
+            iface.stop();
+        }
+
+        updateCapabilityFilter();
+    }
+
+    boolean hasInterface(String interfaceName) {
+        return mTrackingInterfaces.containsKey(interfaceName);
+    }
+
+    private NetworkInterfaceState networkForRequest(NetworkRequest request) {
+        String requestedIface = null;
+
+        NetworkSpecifier specifier = request.networkCapabilities.getNetworkSpecifier();
+        if (specifier instanceof StringNetworkSpecifier) {
+            requestedIface = ((StringNetworkSpecifier) specifier).specifier;
+        }
+
+        NetworkInterfaceState network = null;
+        if (!TextUtils.isEmpty(requestedIface)) {
+            NetworkInterfaceState n = mTrackingInterfaces.get(requestedIface);
+            if (n != null && n.satisified(request.networkCapabilities)) {
+                network = n;
+            }
+        } else {
+            for (NetworkInterfaceState n : mTrackingInterfaces.values()) {
+                if (n.satisified(request.networkCapabilities)) {
+                    network = n;
+                    break;
+                }
+            }
+        }
+
+        if (DBG) {
+            Log.i(TAG, "networkForRequest, request: " + request + ", network: " + network);
+        }
+
+        return network;
+    }
+
+    private static class NetworkInterfaceState {
+        final String mInterfaceName;
+
+        private final String mHwAddress;
+        private final NetworkCapabilities mCapabilities;
+        private final Handler mHandler;
+        private final Context mContext;
+        private final NetworkInfo mNetworkInfo;
+
+        private boolean mLinkUp;
+        private LinkProperties mLinkProperties = new LinkProperties();
+
+        private NetworkAgent mNetworkAgent;
+
+        long mRefCount = 0;
+
+        NetworkInterfaceState(
+                String ifaceName,
+                String hwAddress,
+                Handler handler,
+                Context context,
+                NetworkCapabilities capabilities) {
+            mInterfaceName = ifaceName;
+            mCapabilities = capabilities;
+            mHandler = handler;
+            mContext = context;
+
+            mHwAddress = hwAddress;
+            mNetworkInfo = new NetworkInfo(TYPE_IPSEC, 0, NETWORK_TYPE, "");
+        }
+
+        boolean satisified(NetworkCapabilities requestedCapabilities) {
+            return requestedCapabilities.satisfiedByNetworkCapabilities(mCapabilities);
+        }
+
+        private void start() {
+            if (mNetworkAgent != null) {
+                Log.e(TAG, "Already have a NetworkAgent - aborting new request");
+                stop();
+                return;
+            }
+
+            mLinkProperties = getDefaultIpSecLinkProperties();
+            mNetworkInfo.setDetailedState(DetailedState.CONNECTED, null, mHwAddress);
+            mNetworkInfo.setIsAvailable(true);
+            mLinkUp = true;
+
+            // Create our NetworkAgent.
+            mNetworkAgent =
+                    new NetworkAgent(
+                            mHandler.getLooper(),
+                            mContext,
+                            NETWORK_TYPE,
+                            mNetworkInfo,
+                            mCapabilities,
+                            mLinkProperties,
+                            NETWORK_SCORE) {
+                        public void unwanted() {
+                            if (this == mNetworkAgent) {
+                                stop();
+                            } else if (mNetworkAgent != null) {
+                                Log.d(
+                                        TAG,
+                                        "Ignoring unwanted as we have a more modern " + "instance");
+                            } // Otherwise, we've already called stop.
+                        }
+                    };
+        }
+
+        LinkProperties getDefaultIpSecLinkProperties() {
+            LinkProperties lp = new LinkProperties();
+            lp.setInterfaceName(mInterfaceName);
+
+            return lp;
+        }
+
+        void updateLinkProperties(LinkProperties linkProperties) {
+            mLinkProperties = linkProperties;
+            if (mNetworkAgent != null) {
+                mNetworkAgent.sendLinkProperties(linkProperties);
+            }
+        }
+
+        void stop() {
+            // ConnectivityService will only forget our NetworkAgent if we send it a NetworkInfo
+            // object with a state of DISCONNECTED or SUSPENDED. So we can't simply clear our
+            // NetworkInfo here; that sets the state to IDLE, and ConnectivityService will still
+            // think we're connected.
+            //
+            mNetworkInfo.setDetailedState(DetailedState.DISCONNECTED, null, mHwAddress);
+            mNetworkInfo.setIsAvailable(false);
+            mLinkUp = false;
+
+            if (mNetworkAgent != null) {
+                updateAgent();
+                mNetworkAgent = null;
+            }
+            clear();
+        }
+
+        private void updateAgent() {
+            if (mNetworkAgent == null) return;
+            if (DBG) {
+                Log.i(
+                        TAG,
+                        "Updating mNetworkAgent with: "
+                                + mCapabilities
+                                + ", "
+                                + mNetworkInfo
+                                + ", "
+                                + mLinkProperties);
+            }
+            mNetworkAgent.sendNetworkCapabilities(mCapabilities);
+            mNetworkAgent.sendNetworkInfo(mNetworkInfo);
+            mNetworkAgent.sendLinkProperties(mLinkProperties);
+            // never set the network score below 0.
+            mNetworkAgent.sendNetworkScore(mLinkUp ? NETWORK_SCORE : 0);
+        }
+
+        private void clear() {
+            mLinkProperties.clear();
+            mNetworkInfo.setDetailedState(DetailedState.IDLE, null, null);
+            mNetworkInfo.setIsAvailable(false);
+        }
+
+        @Override
+        public String toString() {
+            return getClass().getSimpleName()
+                    + "{ iface: " + mInterfaceName
+                    + ", up: " + mLinkUp
+                    + ", hwAddress: " + mHwAddress
+                    + ", networkInfo: " + mNetworkInfo
+                    + ", networkAgent: " + mNetworkAgent
+                    + ", linkProperties: " + mLinkProperties
+                    + "}";
+        }
+    }
+
+    void dump(FileDescriptor fd, IndentingPrintWriter pw, String[] args) {
+        super.dump(fd, pw, args);
+        pw.println(getClass().getSimpleName());
+        pw.println("Tracking interfaces:");
+        pw.increaseIndent();
+        for (String iface : mTrackingInterfaces.keySet()) {
+            NetworkInterfaceState ifaceState = mTrackingInterfaces.get(iface);
+            pw.println(iface + ":" + ifaceState);
+        }
+        pw.decreaseIndent();
+    }
+}
---
+++ core/java/android/net/IpSecSecureTunnel.java
@@ -0,0 +1,219 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package android.net;
+
+import android.content.Context;
+import android.util.Log;
+
+import dalvik.system.CloseGuard;
+
+import java.io.IOException;
+import java.net.InetAddress;
+
+/**
+ * This class contains methods for building and managing IPsec-secured tunnels.
+ *
+ * @hide
+ */
+public final class IpSecSecureTunnel implements AutoCloseable {
+    private static final String TAG = "IpSecSecureTunnel";
+
+    public final IpSecManager.SecurityParameterIndex mSpiIn;
+    public final IpSecManager.SecurityParameterIndex mSpiOut;
+    public final IpSecTransform mTransformIn;
+    public final IpSecTransform mTransformOut;
+    public final IpSecManager.UdpEncapsulationSocket mEncapSocket;
+    public final IpSecManager.IpSecTunnelInterface mTunnelIntf;
+
+    private final CloseGuard mCloseGuard = CloseGuard.get();
+
+    private IpSecSecureTunnel(
+            IpSecManager.SecurityParameterIndex spiIn,
+            IpSecManager.SecurityParameterIndex spiOut,
+            IpSecTransform transformIn,
+            IpSecTransform transformOut,
+            IpSecManager.UdpEncapsulationSocket encapSocket,
+            IpSecManager.IpSecTunnelInterface tunnelInterface) {
+        mSpiIn = spiIn;
+        mSpiOut = spiOut;
+        mTransformIn = transformIn;
+        mTransformOut = transformOut;
+        mEncapSocket = encapSocket;
+        mTunnelIntf = tunnelInterface;
+    }
+
+    /**
+     * Add an address to the IpSecTunnelInterface
+     *
+     * @hide
+     */
+    public void addInnerAddress(InetAddress address, int prefixLen) throws IOException {
+        mTunnelIntf.addAddress(address, prefixLen);
+    }
+
+    /**
+     * Remove an address from the IpSecTunnelInterface
+     *
+     * @hide
+     */
+    public void removeInnerAddress(InetAddress address, int prefixLen) throws IOException {
+        mTunnelIntf.removeAddress(address, prefixLen);
+    }
+
+    /** Releases a SecureTunnel */
+    @Override
+    public void close() {
+        mTunnelIntf.close();
+        mTransformOut.close();
+        mTransformIn.close();
+        mSpiOut.close();
+        mSpiIn.close();
+        if (mEncapSocket != null) {
+            try {
+                mEncapSocket.close();
+            } catch (Exception e) {
+                // On close we swallow all random exceptions since failure to close is not
+                // actionable by the user.
+                Log.e(TAG, "Failed to close SecureTunnel, Exception=" + e);
+            }
+        }
+
+        mCloseGuard.close();
+    }
+
+    /** Check that the Secure Tunnel was closed properly. */
+    @Override
+    protected void finalize() throws Throwable {
+        if (mCloseGuard != null) {
+            mCloseGuard.warnIfOpen();
+        }
+
+        close();
+    }
+
+    /**
+     * Builder for IpSecSecureTunnel. Allows configuration of algorithms and encapsulation.
+     *
+     * <p>Validity of config checked at build time by IpSecService.
+     *
+     * @hide
+     */
+    public static class Builder {
+        private final IpSecManager mISM;
+        private final InetAddress mSourceAddr;
+        private final InetAddress mDestAddr;
+        private final Network mUnderlyingNetwork;
+        private final IpSecTransform.Builder mInTransformBuilder;
+        private final IpSecTransform.Builder mOutTransformBuilder;
+
+        private boolean mIsEncapEnabled = false;
+
+        /** Create a builder with the minimal required information. */
+        public Builder(
+                Context context,
+                InetAddress sourceAddr,
+                InetAddress destAddr,
+                Network underlyingNetwork) {
+            mISM = (IpSecManager) context.getSystemService(Context.IPSEC_SERVICE);
+            mSourceAddr = sourceAddr;
+            mDestAddr = destAddr;
+            mUnderlyingNetwork = underlyingNetwork;
+            mInTransformBuilder = new IpSecTransform.Builder(context);
+            mOutTransformBuilder = new IpSecTransform.Builder(context);
+
+            if (!context.getOpPackageName().equals("android.net.cts")) {
+                throw new SecurityException("Test methods for CTS only");
+            }
+        }
+
+        private IpSecTransform.Builder getBuilderForDirection(int direction) {
+            switch (direction) {
+                case IpSecManager.DIRECTION_IN:
+                    return mInTransformBuilder;
+                case IpSecManager.DIRECTION_OUT:
+                    return mOutTransformBuilder;
+                default:
+                    throw new IllegalArgumentException("Invalid direction");
+            }
+        }
+
+        /** Set the authentication algorithm for the given direction */
+        public Builder setAuthentication(int direction, IpSecAlgorithm auth) {
+            getBuilderForDirection(direction).setAuthentication(auth);
+            return this;
+        }
+
+        /** Set the encryption algorithm for the given direction */
+        public Builder setEncryption(int direction, IpSecAlgorithm crypt) {
+            getBuilderForDirection(direction).setEncryption(crypt);
+            return this;
+        }
+
+        /** Set the authenticated encryption algorithm for the given direction */
+        public Builder setAuthenticatedEncryption(int direction, IpSecAlgorithm aead) {
+            getBuilderForDirection(direction).setAuthenticatedEncryption(aead);
+            return this;
+        }
+
+        /** Set whether UDP encapsulation is used */
+        public Builder useEncap(boolean enabled) {
+            mIsEncapEnabled = enabled;
+            return this;
+        }
+
+        /**
+         * Build the relevant resource objects, returned and tracked in an instance of
+         * IpSecSecureTunnel
+         *
+         * In cases where the source and destination address are the same, the tunnel
+         * will be built using a single SPI and Transform (Since two Transforms cannot share
+         * the same SPI/destination pair)
+         */
+        public IpSecSecureTunnel build()
+                throws IOException, IpSecManager.SpiUnavailableException,
+                        IpSecManager.ResourceUnavailableException {
+            boolean useMirroredTransforms = mSourceAddr.equals(mDestAddr);
+
+            IpSecManager.SecurityParameterIndex spiIn =
+                    mISM.allocateSecurityParameterIndex(mSourceAddr);
+            IpSecManager.SecurityParameterIndex spiOut =
+                    useMirroredTransforms ? spiIn : mISM.allocateSecurityParameterIndex(mDestAddr);
+
+            IpSecManager.UdpEncapsulationSocket encapSocket = null;
+            if (mIsEncapEnabled) {
+                encapSocket = mISM.openUdpEncapsulationSocket();
+                mInTransformBuilder.setIpv4Encapsulation(encapSocket, encapSocket.getPort());
+                mOutTransformBuilder.setIpv4Encapsulation(encapSocket, encapSocket.getPort());
+            }
+
+            IpSecManager.IpSecTunnelInterface tunnelIntf =
+                    mISM.createIpSecTunnelInterface(mSourceAddr, mDestAddr, mUnderlyingNetwork);
+
+            IpSecTransform transformIn =
+                    mInTransformBuilder.buildTunnelModeTransform(mDestAddr, spiIn);
+            IpSecTransform transformOut =
+                    useMirroredTransforms
+                            ? transformIn
+                            : mOutTransformBuilder.buildTunnelModeTransform(mSourceAddr, spiOut);
+
+            mISM.applyTunnelModeTransform(tunnelIntf, IpSecManager.DIRECTION_IN, transformIn);
+            mISM.applyTunnelModeTransform(tunnelIntf, IpSecManager.DIRECTION_OUT, transformOut);
+
+            return new IpSecSecureTunnel(
+                    spiIn, spiOut, transformIn, transformOut, encapSocket, tunnelIntf);
+        }
+    }
+}
