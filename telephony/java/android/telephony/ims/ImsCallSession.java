4e7f2d270dbbbc6648a5743a347c5b8111100cd4
Ashit Sood <sooda@codeaurora.org>
2018-07-01 14:21:50 -0700

    IMS: Propagate call session property changed message
    - RTT Voice information will be passed to the UI via the call
      session property changed message
    
    - The RTT voice information - SPEECH/SILENCE, will be conveyed
      via the Connection.PROPERTY_RTT_AUDIO_SPEECH bit mask in the
      call session property
    
    - A connection event will be sent from frameworks to the UI -
      EVENT_RTT_AUDIO_INDICATION
    
    - Add listeners, connection event and extras to propagate the
      call session property to the UI
    
    Test: Manual
    Bug: 110976450
    Change-Id: I2b795a150cb485ea36d6e55b7355ae1535d3c3f8

--- telephony/java/android/telephony/ims/ImsCallSession.java
+++ telephony/java/android/telephony/ims/ImsCallSession.java
@@ -447,6 +447,16 @@ public class ImsCallSession {
         public void callSessionRttMessageReceived(String rttMessage) {
             // no-op
         }
+
+        /**
+         * While in call, there has been a change in the call session
+         * property.
+         * @param property - an integer containing masks for different properties
+         * {e.g. @see Connection#PROPERTY_RTT_AUDIO_SPEECH}
+         */
+        public void callSessionPropertyChanged(int property) {
+            // no-op
+        }
     }
 
     private final IImsCallSession miSession;
@@ -1399,6 +1409,19 @@ public class ImsCallSession {
         public void callSessionRttMessageReceived(String rttMessage) {
             if (mListener != null) {
                 mListener.callSessionRttMessageReceived(rttMessage);
+            }
+        }
+
+        /**
+         * While in call, there has been a change in the call session
+         * property.
+         * @param property - an integer containing masks for different properties
+         * {e.g. @see Connection#PROPERTY_RTT_AUDIO_SPEECH}
+         */
+        @Override
+        public void callSessionPropertyChanged(int property) {
+            if (mListener != null) {
+                mListener.callSessionPropertyChanged(property);
             }
         }
     }
