f09945ec37822557eaa7baab3f7c86328a2b9275
zhoutengteng <zhoutengteng@xiaomi.com>
2018-07-01 13:57:33 +0800

    Fix after disableBle, bluetooth does not move to OFF state
    1.Enable Ble by api enableBLE
    2.Disable Ble by api disableBLE
    3.From logcat, Bluetooth state is still BLE_ON, which should be OFF.
    
    Bug:111017410
    Test: EnableBle and DisableBle, Bluetooth state move to OFF state
    
    Change-Id: I7740e5b8088150f091f15e24714c2a5c9e93fbe1
    Signed-off-by: zhoutengteng <zhoutengteng@xiaomi.com>

--- core/java/android/bluetooth/BluetoothAdapter.java
+++ core/java/android/bluetooth/BluetoothAdapter.java
@@ -921,7 +921,7 @@ public final class BluetoothAdapter {
             if (DBG) {
                 Log.d(TAG, "enableBLE(): Calling enable");
             }
-            return mManagerService.enable(packageName);
+            return mManagerService.enable(packageName, true);
         } catch (RemoteException e) {
             Log.e(TAG, "", e);
         }
@@ -1051,7 +1051,7 @@ public final class BluetoothAdapter {
             return true;
         }
         try {
-            return mManagerService.enable(ActivityThread.currentPackageName());
+            return mManagerService.enable(ActivityThread.currentPackageName(), false);
         } catch (RemoteException e) {
             Log.e(TAG, "", e);
         }
