04d31aadb13c55bc9571b7783224b601da8f3aca
Nathan Harold <nharold@google.com>
2018-08-09 17:10:42 -0700

    Remove getNeighboringCell API
    Remove the obsolete getNeighboringCell API for callers
    targeting Q or higher SDK. This API was fully
    superceded by getAllCellInfo in KitKat, and
    maintaining it results in battery drain because calls
    to this API resulted in unrestricted modem calls.
    
    For legacy support, this API now routes to the newer
    CellInfo mechanism, which stems the battery drain but
    further emphasizes the redundancy. Thus, removing this
    is the best possible outcome for maintainers, users and
    OEMs.
    
    Bug: 62490173
    Test: compilation
    Change-Id: I8aa806bfcad2ceefc9dfbff58c0ce48d5a3cab5b

--- telephony/java/android/telephony/TelephonyManager.java
+++ telephony/java/android/telephony/TelephonyManager.java
@@ -1527,6 +1527,7 @@ public class TelephonyManager {
      *
      * @return List of NeighboringCellInfo or null if info unavailable.
      *
+     * @removed
      * @deprecated Use {@link #getAllCellInfo} which returns a superset of the information
      *             from NeighboringCellInfo, including LTE cell information.
      */
