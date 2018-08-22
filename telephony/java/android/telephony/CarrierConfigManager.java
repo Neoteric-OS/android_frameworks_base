fe67d29b7bd7c8d6ed6a5630a616c78d1938e43d
Saki Oomori <saki.x.oomori@sony.com>
2018-08-22 16:07:07 +0900

    Add new carrier config to extend DisconnectCause to play busy tone
    This carrier config is used to specify additional DisconnectCause for
    playing busy tone.
    
    Bug: 116189822
    Test: manual - Checked that busy tone is played according to
    "KEY_DISCONNECT_CAUSE_PLAY_BUSYTONE_INT_ARRAY".
    Change-Id: I58afb08ddc718bfb23d6c8b4f248c4c05d084829

--- telephony/java/android/telephony/CarrierConfigManager.java
+++ telephony/java/android/telephony/CarrierConfigManager.java
@@ -2368,6 +2368,13 @@ public class CarrierConfigManager {
     public static final String KEY_OPPORTUNISTIC_NETWORK_EXIT_THRESHOLD_RSSNR_INT =
             "opportunistic_network_exit_threshold_rssnr_int";
 
+    /**
+     * DisconnectCause array to play busy tone. Value should be array of
+     * {@link android.telephony.DisconnectCause}.
+     */
+    public static final String KEY_DISCONNECT_CAUSE_PLAY_BUSYTONE_INT_ARRAY =
+            "disconnect_cause_play_busytone_int_array";
+
     /** The default value for every variable. */
     private final static PersistableBundle sDefaults;
 
@@ -2735,6 +2742,8 @@ public class CarrierConfigManager {
         sDefaults.putInt(KEY_OPPORTUNISTIC_NETWORK_ENTRY_THRESHOLD_RSSNR_INT, 45);
         /* Default value is minimum RSSNR level needed for SIGNAL_STRENGTH_MODERATE */
         sDefaults.putInt(KEY_OPPORTUNISTIC_NETWORK_EXIT_THRESHOLD_RSSNR_INT, 10);
+        sDefaults.putIntArray(KEY_DISCONNECT_CAUSE_PLAY_BUSYTONE_INT_ARRAY,
+                new int[] {4 /* BUSY */});
     }
 
     /**
