b18d1bf59323abcd0b27eeea684fc0cdb3c26e1b
Niklas Lindgren <niklas.lindgren@sony.com>
2018-10-01 11:09:43 +0200

    Add carrier config support to operator name in statusbar
    Move configuration of showing operator name from resource to
    carrier config. By moving this to carrier config, this will also
    support Dual SIM scenario.
    
    Bug: 116847905
    Test: Add item for carrier in vendor.xml and swap sim cards
    between ones that have the item enabled and disabled. We have
    also tested to swap data sim cards on dual sim device.
    Change-Id: If3b7bbe165c8f0d0305b8ae4f77c91a41a98ca14

--- packages/SystemUI/src/com/android/systemui/statusbar/OperatorNameView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/OperatorNameView.java
@@ -17,9 +17,13 @@ package com.android.systemui.statusbar;
 import android.content.Context;
 import android.graphics.Rect;
 import android.net.ConnectivityManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.telephony.CarrierConfigManager;
 import android.telephony.ServiceState;
 import android.telephony.SubscriptionInfo;
+import android.telephony.SubscriptionManager;
 import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.widget.TextView;
@@ -38,7 +42,6 @@ import com.android.systemui.statusbar.policy.NetworkController.SignalCallback;
 import com.android.systemui.tuner.TunerService;
 import com.android.systemui.tuner.TunerService.Tunable;
 
-import java.util.List;
 
 public class OperatorNameView extends TextView implements DemoMode, DarkReceiver,
         SignalCallback, Tunable {
@@ -133,22 +136,42 @@ public class OperatorNameView extends TextView implements DemoMode, DarkReceiver
     }
 
     private void updateText() {
-        CharSequence displayText = null;
-        List<SubscriptionInfo> subs = mKeyguardUpdateMonitor.getFilteredSubscriptionInfo(false);
-        final int N = subs.size();
-        for (int i = 0; i < N; i++) {
-            int subId = subs.get(i).getSubscriptionId();
-            State simState = mKeyguardUpdateMonitor.getSimState(subId);
-            CharSequence carrierName = subs.get(i).getCarrierName();
-            if (!TextUtils.isEmpty(carrierName) && simState == State.READY) {
-                ServiceState ss = mKeyguardUpdateMonitor.getServiceState(subId);
-                if (ss != null && ss.getState() == ServiceState.STATE_IN_SERVICE) {
-                    displayText = carrierName;
-                    break;
-                }
-            }
+        final CarrierConfigManager configMgr = getContext()
+                .getSystemService(CarrierConfigManager.class);
+        if (configMgr == null) {
+            return;
         }
 
-        setText(displayText);
+        // Do the binder calls in background
+        AsyncTask<Void, Void, CharSequence> task =
+                new AsyncTask<Void, Void, CharSequence>() {
+            @Override
+            protected CharSequence doInBackground(Void... args) {
+                CharSequence displayText = null;
+                int defaultDataSubId = SubscriptionManager.getDefaultDataSubscriptionId();
+                State simState = mKeyguardUpdateMonitor.getSimState(defaultDataSubId);
+                final SubscriptionInfo subInfo = mKeyguardUpdateMonitor
+                        .getSubscriptionInfoForSubId(defaultDataSubId);
+                final PersistableBundle b = configMgr.getConfigForSubId(defaultDataSubId);
+
+                CharSequence carrierName = null;
+                if (subInfo != null && b != null && b.getBoolean(
+                        CarrierConfigManager.KEY_SHOW_OPERATOR_NAME_IN_STATUSBAR_BOOL)) {
+                    carrierName = subInfo.getCarrierName();
+                }
+                if (!TextUtils.isEmpty(carrierName) && simState == State.READY) {
+                    ServiceState ss = mKeyguardUpdateMonitor.getServiceState(defaultDataSubId);
+                    if (ss != null && ss.getState() == ServiceState.STATE_IN_SERVICE) {
+                        displayText = carrierName;
+                    }
+                }
+                return displayText;
+            }
+            @Override
+            protected void onPostExecute(CharSequence displayText) {
+                setText(displayText);
+            }
+        };
+        task.execute();
     }
 }
