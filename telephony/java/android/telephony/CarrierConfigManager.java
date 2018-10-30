45bc1cff0591db2aa7a6e46a5e4945ba4c75ea72
Chuck Liao <chuckliao@google.com>
2018-10-30 11:39:56 +0800

    Add a new carrier config to support emergency dialer shortcut feature.
    Test: manual
    Bug: 117865402
    Change-Id: I5d008af8d4dc73adc76630d7b6f47724544f068c

--- telephony/java/android/telephony/CarrierConfigManager.java
+++ telephony/java/android/telephony/CarrierConfigManager.java
@@ -2232,6 +2232,12 @@ public class CarrierConfigManager {
     public static final String KEY_CALL_WAITING_OVER_UT_WARNING_BOOL =
             "call_waiting_over_ut_warning_bool";
 
+    /**
+     * Determines whether the carrier want to support emergency dialer shortcut.
+     */
+    public static final String KEY_SUPPORT_EMERGENCY_DIALER_SHORTCUT_BOOL =
+            "support_emergency_dialer_shortcut_bool";
+
     /** The default value for every variable. */
     private final static PersistableBundle sDefaults;
 
@@ -2580,6 +2586,7 @@ public class CarrierConfigManager {
         sDefaults.putBoolean(KEY_CALL_BARRING_OVER_UT_WARNING_BOOL, false);
         sDefaults.putBoolean(KEY_CALLER_ID_OVER_UT_WARNING_BOOL, false);
         sDefaults.putBoolean(KEY_CALL_WAITING_OVER_UT_WARNING_BOOL, false);
+        sDefaults.putBoolean(KEY_SUPPORT_EMERGENCY_DIALER_SHORTCUT_BOOL, true);
     }
 
     /**
