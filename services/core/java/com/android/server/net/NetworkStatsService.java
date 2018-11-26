865747fa86fbd9976a88ce1657383b1cb1d5d371
Mike SU <suzh1@lenovo.com>
2018-11-26 15:05:13 +0800

    fix getIfaceStats and getTotalStats bug
    root cause: getIfaceStats and getTotalStats is directly reading
    iface_stat_fmt or eBPF, not include tether stats.
    solution: add tether stats to getIfaceStats and getTotalStats.
    
    Change-Id: I3d94acb71c142ec38b750e58822881ff383341cc

--- services/core/java/com/android/server/net/NetworkStatsService.java
+++ services/core/java/com/android/server/net/NetworkStatsService.java
@@ -941,12 +941,55 @@ public class NetworkStatsService extends INetworkStatsService.Stub {
 
     @Override
     public long getIfaceStats(String iface, int type) {
-        return nativeGetIfaceStat(iface, type, checkBpfStatsEnable());
+        long nativeIfaceStats = nativeGetIfaceStat(iface, type, checkBpfStatsEnable());
+        if (nativeIfaceStats == -1 ) {
+            return nativeIfaceStats;
+        } else {
+            return nativeIfaceStats + getTetherStats(iface, type);
+        }
     }
 
     @Override
     public long getTotalStats(int type) {
-        return nativeGetTotalStat(type, checkBpfStatsEnable());
+        long nativeTotalStats = nativeGetTotalStat(type, checkBpfStatsEnable());
+        if (nativeTotalStats == -1 ) {
+            return nativeTotalStats;
+        } else {
+            return nativeTotalStats + getTetherStats(IFACE_ALL, type);
+        }
+    }
+
+    private long getTetherStats(String iface, int type) {
+        final long token = Binder.clearCallingIdentity();
+        try {
+            final NetworkStats tetherSnapshot = getNetworkStatsTethering(STATS_PER_IFACE);
+            HashSet<String> limitIfaces;
+            if (iface == IFACE_ALL) {
+                limitIfaces = null;
+            } else {
+                limitIfaces = new HashSet<String>();
+                limitIfaces.add(iface);
+            }
+            NetworkStats.Entry entry = tetherSnapshot.getTotal(null, limitIfaces);
+            if (LOGD) Slog.d(TAG, "TetherStats: iface=" + iface + " type=" + type + " entry=" + entry);
+            switch (type) {
+            case 0: //TYPE_RX_BYTES
+                return entry.rxBytes;
+            case 1: //TYPE_RX_PACKETS
+                return entry.rxPackets;
+            case 2: //TYPE_TX_BYTES
+                return entry.txBytes;
+            case 3: //TYPE_TX_PACKETS
+                return entry.txPackets;
+            default:
+                return 0;
+            }
+        } catch (RemoteException e) {
+            Slog.w(TAG, "Error get TetherStats: " + e);
+            return 0;
+        } finally {
+            Binder.restoreCallingIdentity(token);
+        }
     }
 
     private boolean checkBpfStatsEnable() {
