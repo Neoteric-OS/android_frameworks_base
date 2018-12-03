335eec2999127153e31c2dc89ac297034583b169
Joshua Duong <joshuaduong@google.com>
2018-12-03 15:21:46 -0800

    [adbwifi] Make AdbManager changes for adb over WiFi.
    This CL has a couple of notable changes:
    - Add communication layer between adbd, system server, and Settings UI
    - Add system notification (Wireless debugging connected) when at least
      one device is connected.
    - Add trusted networks (BSSID) to the keystore.
      - Changed the keystore format to:
        <keyStore version="1">
        <adbKey ... />
        <wifiAP ... />
        </keyStore>
     - Currently, trusted networks don't have a expiration time. Also, only
       way to clear it is by blowing up the keystore (revoke permissions).
    - Add pairing mechanism:
      - Using libadbwifi_pairing_connection C++ library to pair a device
        using SPAKE2 protocol over TLS.
      - Register MDNS service for client discovery.
    
    Bug: 111434128, 119490154, 119492574
    
    Test: make
    
    Change-Id: I9f87679d195da99a55b6faf7131da1f1af65fe01

--- services/core/java/com/android/server/adb/AdbService.java
+++ services/core/java/com/android/server/adb/AdbService.java
@@ -24,6 +24,7 @@ import android.debug.AdbManagerInternal;
 import android.debug.AdbTransportType;
 import android.debug.IAdbManager;
 import android.debug.IAdbTransport;
+import android.debug.PairDevice;
 import android.hardware.usb.UsbManager;
 import android.net.Uri;
 import android.os.Binder;
@@ -52,6 +53,7 @@ import java.io.File;
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
 import java.util.Collections;
+import java.util.Map;
 
 /**
  * The Android Debug Bridge (ADB) service. This controls the availability of ADB and authorization
@@ -128,8 +130,8 @@ public class AdbService extends IAdbManager.Stub {
                 mAdbUsbEnabled = containsFunction(
                         SystemProperties.get(USB_PERSISTENT_CONFIG_PROPERTY, ""),
                         UsbManager.USB_FUNCTION_ADB);
-                // TODO: for mAdbWifiEnabled
-                mAdbWifiEnabled = false;
+                mAdbWifiEnabled = "1".equals(
+                        SystemProperties.get(WIFI_PERSISTENT_CONFIG_PROPERTY, "0"));
 
                 // register observer to listen for settings changes
                 mObserver = new AdbSettingsObserver();
@@ -212,6 +214,7 @@ public class AdbService extends IAdbManager.Stub {
      * May also contain vendor-specific default functions for testing purposes.
      */
     private static final String USB_PERSISTENT_CONFIG_PROPERTY = "persist.sys.usb.config";
+    private static final String WIFI_PERSISTENT_CONFIG_PROPERTY = "persist.sys.adb.wifi";
 
     private final Context mContext;
     private final ContentResolver mContentResolver;
@@ -228,7 +231,8 @@ public class AdbService extends IAdbManager.Stub {
 
         boolean secureAdbEnabled = AdbProperties.secure().orElse(false);
         boolean dataEncrypted = "1".equals(SystemProperties.get("vold.decrypt"));
-        if (secureAdbEnabled && !dataEncrypted) {
+        boolean emulatorWantsAuth = "1".equals(SystemProperties.get("qemu.adb.auth"));
+        if ((secureAdbEnabled && !dataEncrypted) || emulatorWantsAuth) {
             mDebuggingManager = new AdbDebuggingManager(context);
         }
 
@@ -313,6 +317,72 @@ public class AdbService extends IAdbManager.Stub {
                 android.Manifest.permission.MANAGE_DEBUGGING, "AdbService");
         return isAdbWifiSupported() && mContext.getPackageManager().hasSystemFeature(
                 PackageManager.FEATURE_CAMERA_ANY);
+    }
+
+    @Override
+    public void allowWirelessDebugging(boolean alwaysAllow, String bssid) {
+        mContext.enforceCallingOrSelfPermission(android.Manifest.permission.MANAGE_DEBUGGING, null);
+        if (mDebuggingManager != null) {
+            mDebuggingManager.allowWirelessDebugging(alwaysAllow, bssid);
+        }
+    }
+
+    @Override
+    public void denyWirelessDebugging() {
+        mContext.enforceCallingOrSelfPermission(android.Manifest.permission.MANAGE_DEBUGGING, null);
+        if (mDebuggingManager != null) {
+            mDebuggingManager.denyWirelessDebugging();
+        }
+    }
+
+    @Override
+    public Map<String, PairDevice> getPairedDevices() {
+        mContext.enforceCallingOrSelfPermission(android.Manifest.permission.MANAGE_DEBUGGING, null);
+        if (mDebuggingManager != null) {
+            return mDebuggingManager.getPairedDevices();
+        }
+        return null;
+    }
+
+    @Override
+    public void unpairDevice(String fingerprint) {
+        mContext.enforceCallingOrSelfPermission(android.Manifest.permission.MANAGE_DEBUGGING, null);
+        if (mDebuggingManager != null) {
+            mDebuggingManager.unpairDevice(fingerprint);
+        }
+    }
+
+    @Override
+    public void enablePairingByPairingCode() {
+        mContext.enforceCallingOrSelfPermission(android.Manifest.permission.MANAGE_DEBUGGING, null);
+        if (mDebuggingManager != null) {
+            mDebuggingManager.enablePairingByPairingCode();
+        }
+    }
+
+    @Override
+    public void enablePairingByQrCode(String serviceName, String password) {
+        mContext.enforceCallingOrSelfPermission(android.Manifest.permission.MANAGE_DEBUGGING, null);
+        if (mDebuggingManager != null) {
+            mDebuggingManager.enablePairingByQrCode(serviceName, password);
+        }
+    }
+
+    @Override
+    public void disablePairing() {
+        mContext.enforceCallingOrSelfPermission(android.Manifest.permission.MANAGE_DEBUGGING, null);
+        if (mDebuggingManager != null) {
+            mDebuggingManager.disablePairing();
+        }
+    }
+
+    @Override
+    public int getAdbWirelessPort() {
+        mContext.enforceCallingOrSelfPermission(android.Manifest.permission.MANAGE_DEBUGGING, null);
+        if (mDebuggingManager != null) {
+            return mDebuggingManager.getAdbWirelessPort();
+        }
+        return 0;
     }
 
     private void setAdbEnabled(boolean enable, byte transportType) {
