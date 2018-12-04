7fd90c6501d3d2627e087d894a58601f8f4229a0
Remi NGUYEN VAN <reminv@google.com>
2018-12-04 12:13:09 +0900

    Move DhcpServer to NetworkStack app
    Test: atest FrameworksNetTests && atest NetworkStackTests
    Bug: b/112869080
    
    Change-Id: I96c40e63e9ceb37b67705bdd4d120307e114715b

---
+++ services/net/java/android/net/shared/DhcpServerStatus.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.net.shared;
+
+import android.annotation.IntDef;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Collection of status codes returned by the DhcpServer.
+ *
+ * @hide
+ */
+public class DhcpServerStatus {
+    @IntDef({ STATUS_UNKNOWN, STATUS_SUCCESS, STATUS_INVALID_PARAMETER })
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface Status {}
+
+    public static final int STATUS_UNKNOWN = 0;
+    public static final int STATUS_SUCCESS = 1;
+    public static final int STATUS_INVALID_PARAMETER = 2;
+    public static final int STATUS_UNKNOWN_ERROR = 3;
+}
