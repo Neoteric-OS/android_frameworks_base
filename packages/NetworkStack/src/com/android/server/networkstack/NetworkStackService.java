86ca5da7c2392118d147a53470d42a57e16a8401
Remi NGUYEN VAN <reminv@google.com>
2018-12-04 12:13:09 +0900

    Move DhcpServer to NetworkStack app
    Test: atest FrameworksNetTests && atest NetworkStackUnitTests
    Bug: b/112869080
    
    Change-Id: I96c40e63e9ceb37b67705bdd4d120307e114715b

--- packages/NetworkStack/src/com/android/server/networkstack/NetworkStackService.java
+++ packages/NetworkStack/src/com/android/server/networkstack/NetworkStackService.java
@@ -5,7 +5,17 @@ import android.annotation.Nullable;
 import android.app.Service;
 import android.content.Intent;
 import android.net.INetworkStackConnector;
+import android.net.dhcp.DhcpServer;
+import android.net.dhcp.DhcpServingParams;
+import android.net.dhcp.DhcpServingParamsParcel;
+import android.net.dhcp.IDhcpServer;
+import android.net.dhcp.IDhcpServerCallbacks;
+import android.net.util.SharedLog;
+import android.os.Binder;
+import android.os.HandlerThread;
 import android.os.IBinder;
+import android.os.Process;
+import android.os.RemoteException;
 
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
@@ -35,7 +45,23 @@ public class NetworkStackService extends Service {
     }
 
     private static class NetworkStackConnector extends INetworkStackConnector.Stub {
-        // TODO: makeDhcpServer(), etc. will go here.
+        @NonNull
+        private final SharedLog mLog = new SharedLog(TAG);
+
+        @Override
+        public void makeDhcpServer(@NonNull String ifName, @NonNull DhcpServingParamsParcel params,
+                @NonNull IDhcpServerCallbacks cb) throws RemoteException {
+            checkCallerIsSystemServer();
+            final HandlerThread tetheringThread = new HandlerThread(
+                    DhcpServer.class.getSimpleName() + "." + ifName);
+            tetheringThread.start();
+            final DhcpServer server = new DhcpServer(
+                    tetheringThread.getLooper(),
+                    ifName,
+                    DhcpServingParams.fromParcel(params),
+                    mLog.forSubComponent(ifName + ".DHCP"));
+            cb.onDhcpServerCreated(new DhcpServerImpl(server));
+        }
 
         @Override
         public int getInterfaceVersion() {
@@ -45,8 +71,45 @@ public class NetworkStackService extends Service {
         @Override
         protected void dump(@NonNull FileDescriptor fd, @NonNull PrintWriter fout,
                 @Nullable String[] args) {
-            // TODO: dump logs here
-            super.dump(fd, fout, args);
+            mLog.dump(fd, fout, args);
+        }
+    }
+
+    private static class DhcpServerImpl extends IDhcpServer.Stub {
+        private final DhcpServer mServer;
+
+        private DhcpServerImpl(@NonNull DhcpServer server) {
+            mServer = server;
+        }
+
+        @Override
+        public void start() {
+            checkCallerIsSystemServer();
+            mServer.start();
+        }
+
+        @Override
+        public void updateParams(@NonNull DhcpServingParamsParcel params) {
+            checkCallerIsSystemServer();
+            mServer.updateParams(DhcpServingParams.fromParcel(params));
+        }
+
+        @Override
+        public void stop() throws RemoteException {
+            checkCallerIsSystemServer();
+            mServer.stop();
+        }
+
+        @Override
+        public int getInterfaceVersion() {
+            return IDhcpServer.VERSION;
+        }
+    }
+
+    private static void checkCallerIsSystemServer() {
+        if (Binder.getCallingUid() != Process.SYSTEM_UID) {
+            throw new SecurityException("Uid " + Binder.getCallingUid()
+                    + " is not allowed to access this service.");
         }
     }
 }
