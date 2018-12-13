30956b2c8c93e022a94943ebdad9fc25080a0b87
junyulai <junyulai@google.com>
2018-12-13 12:47:51 +0800

    Deprecate NetworkInfo
    Currently NetworkInfo is used by Apps to get information of
    network. However, to get such information, Apps need to poll
    NetworkInfo frequently from ConnectivityService.
    
    In order to increase the stability and reduce the maintain
    effort, all functionalities provided by NetworkInfo are targeted
    to be replaced or removed entirely.
    
    Apps should use ConnectivityManager.NetworkCallback instead, to
    get faster and more detailed updates from connectivity changes.
    
    Or, apps could use getNetworkCapabilities or getLinkProperties
    to get information synchronously, but should not mix the
    callbacks and synchronous methods together.
    
    Bug: 113629330
    Test: atest FrameworksNetTests
    Change-Id: Ie8faf620958c3fa0a4a2f233b35b825de0e99ffc

--- core/java/android/net/NetworkInfo.java
+++ core/java/android/net/NetworkInfo.java
@@ -28,7 +28,14 @@ import java.util.EnumMap;
  * Describes the status of a network interface.
  * <p>Use {@link ConnectivityManager#getActiveNetworkInfo()} to get an instance that represents
  * the current network connection.
+ *
+ * @deprecated Callers should instead use the {@link ConnectivityManager.NetworkCallback} API to
+ *             learn about connectivity changes, or switch to use
+ *             {@link ConnectivityManager#getNetworkCapabilities} or
+ *             {@link ConnectivityManager#getLinkProperties} to get information synchronously, but
+ *             should not mix the callbacks and synchronous methods together.
  */
+@Deprecated
 public class NetworkInfo implements Parcelable {
 
     /**
@@ -52,7 +59,10 @@ public class NetworkInfo implements Parcelable {
      * <tr><td><code>FAILED</code></td><td><code>DISCONNECTED</code></td></tr>
      * <tr><td><code>BLOCKED</code></td><td><code>DISCONNECTED</code></td></tr>
      * </table>
+     *
+     * @deprecated See {@link NetworkInfo}.
      */
+    @Deprecated
     public enum State {
         CONNECTING, CONNECTED, SUSPENDED, DISCONNECTING, DISCONNECTED, UNKNOWN
     }
@@ -61,7 +71,10 @@ public class NetworkInfo implements Parcelable {
      * The fine-grained state of a network connection. This level of detail
      * is probably of interest to few applications. Most should use
      * {@link android.net.NetworkInfo.State State} instead.
+     *
+     * @deprecated See {@link NetworkInfo}.
      */
+    @Deprecated
     public enum DetailedState {
         /** Ready to start data connection setup. */
         IDLE,
@@ -463,8 +476,10 @@ public class NetworkInfo implements Parcelable {
      * Set the extraInfo field.
      * @param extraInfo an optional {@code String} providing addditional network state
      * information passed up from the lower networking layers.
+     * @deprecated See {@link NetworkInfo#getExtraInfo}.
      * @hide
      */
+    @Deprecated
     public void setExtraInfo(String extraInfo) {
         synchronized (this) {
             this.mExtraInfo = extraInfo;
@@ -488,7 +503,10 @@ public class NetworkInfo implements Parcelable {
      * Report the extra information about the network state, if any was
      * provided by the lower networking layers.
      * @return the extra information, or null if not available
+     * @deprecated Use other services e.g. WifiManager to get additional information passed up from
+     *             the lower networking layers.
      */
+    @Deprecated
     public String getExtraInfo() {
         synchronized (this) {
             return mExtraInfo;
