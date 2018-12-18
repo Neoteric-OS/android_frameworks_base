105b586610c04453e53b91424a8ed28a375ba7fd
Chalard Jean <jchalard@google.com>
2018-12-18 22:05:19 +0900

    [MS07] Implement storeNetworkAttributes and storeBlob.
    Test: New tests in IpMemoryStore
    Bug: 116512211
    
    Change-Id: I49bee0c903247e77ab93517efbe44548313cf1a4

--- core/java/android/net/ipmemorystore/Status.java
+++ core/java/android/net/ipmemorystore/Status.java
@@ -18,6 +18,8 @@ package android.net.ipmemorystore;
 
 import android.annotation.NonNull;
 
+import com.android.internal.annotations.VisibleForTesting;
+
 /**
  * A parcelable status representing the result of an operation.
  * Parcels as StatusParceled.
@@ -26,7 +28,9 @@ import android.annotation.NonNull;
 public class Status {
     public static final int SUCCESS = 0;
 
-    public static final int ERROR_DATABASE_CANNOT_BE_OPENED = -1;
+    public static final int ERROR_GENERIC = -1;
+    public static final int ERROR_ILLEGAL_ARGUMENT = -2;
+    public static final int ERROR_DATABASE_CANNOT_BE_OPENED = -3;
 
     public final int resultCode;
 
@@ -34,7 +38,8 @@ public class Status {
         this.resultCode = resultCode;
     }
 
-    Status(@NonNull final StatusParcelable parcelable) {
+    @VisibleForTesting
+    public Status(@NonNull final StatusParcelable parcelable) {
         this(parcelable.resultCode);
     }
 
