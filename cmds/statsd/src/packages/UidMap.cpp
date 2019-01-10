bdb844259f339b4a95526d781abfb70adc2127dd
Shawn Willden <swillden@google.com>
2019-01-10 07:56:55 -0700

    [WIP] Add IdentityCredential API
    Test: Not yet
    Change-Id: I2e540f37367384005b463944847ce7274956bd4f

--- cmds/statsd/src/packages/UidMap.cpp
+++ cmds/statsd/src/packages/UidMap.cpp
@@ -493,6 +493,7 @@ const std::map<string, uint32_t> UidMap::sAidToUidMapping = {{"AID_ROOT", 0},
                                                              {"AID_LMKD", 1069},
                                                              {"AID_LLKD", 1070},
                                                              {"AID_IORAPD", 1071},
+                                                             {"AID_CREDSTORE", 1074},
                                                              {"AID_SHELL", 2000},
                                                              {"AID_CACHE", 2001},
                                                              {"AID_DIAG", 2002}};
---
+++ identity/java/android/security/identity/AccessControlProfiles.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.security.identity;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+/**
+ * @hide
+ */
+public class AccessControlProfiles implements Parcelable {
+    public static final Parcelable.Creator<AccessControlProfiles> CREATOR =
+            new Parcelable.Creator<AccessControlProfiles>() {
+                @Override
+                public AccessControlProfiles createFromParcel(Parcel in) {
+                    // TODO(swillden): Auto-generated method stub
+                    return null;
+                }
+
+                @Override
+                public AccessControlProfiles[] newArray(int arg0) {
+                    // TODO(swillden): Auto-generated method stub
+                    return null;
+                }
+            };
+
+
+    @Override
+    public int describeContents() {
+        // TODO(swillden): Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel arg0, int arg1) {
+        // TODO(swillden): Auto-generated method stub
+
+    }
+}
