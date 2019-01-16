2ceffd1a04a33c087c3727c077ea1a3b1ee5fc51
junyulai <junyulai@google.com>
2019-01-16 20:23:34 +0800

    [KA02.5] Use binder thread and executor to invoke callback
    Currently, client side of keepalive event handling rely on a
    newly created thread, looper, messenger and handler per object.
    
    However, by creating oneway AIDL interface with the executor,
    the callbacks can be invoked on the binder thread with user
    specified context, which not only greatly simplify the design
    but also reduce the cost of current thread modeling.
    
    Bug: 114151147
    Bug: 123969871
    Test: 1. atest FrameworksNetTests --generate-new-metric 10
          2. atest-deflake.sh
    
    Change-Id: I27504074cd28d5b5eb94a7ec0e97ebaaaaa1ae3d

--- core/java/android/net/TcpSocketKeepalive.java
+++ core/java/android/net/TcpSocketKeepalive.java
@@ -17,7 +17,6 @@
 package android.net;
 
 import android.annotation.NonNull;
-import android.os.Binder;
 import android.os.RemoteException;
 import android.util.Log;
 
@@ -55,24 +54,28 @@ final class TcpSocketKeepalive extends SocketKeepalive {
      */
     @Override
     void startImpl(int intervalSec) {
-        try {
-            final FileDescriptor fd = mSocket.getFileDescriptor$();
-            mService.startTcpKeepalive(mNetwork, fd, intervalSec, mMessenger, new Binder());
-        } catch (RemoteException e) {
-            Log.e(TAG, "Error starting packet keepalive: ", e);
-            stopLooper();
-        }
+        mExecutor.execute(() -> {
+            try {
+                final FileDescriptor fd = mSocket.getFileDescriptor$();
+                mService.startTcpKeepalive(mNetwork, fd, intervalSec, mCallback);
+            } catch (RemoteException e) {
+                Log.e(TAG, "Error starting packet keepalive: ", e);
+                notifyErrorCallback(ERROR_BINDER_DIED);
+            }
+        });
     }
 
     @Override
     void stopImpl() {
-        try {
-            if (mSlot != null) {
-                mService.stopKeepalive(mNetwork, mSlot);
+        mExecutor.execute(() -> {
+            try {
+                if (mSlot != null) {
+                    mService.stopKeepalive(mNetwork, mSlot);
+                }
+            } catch (RemoteException e) {
+                Log.e(TAG, "Error stopping packet keepalive: ", e);
+                notifyErrorCallback(ERROR_BINDER_DIED);
             }
-        } catch (RemoteException e) {
-            Log.e(TAG, "Error stopping packet keepalive: ", e);
-            stopLooper();
-        }
+        });
     }
 }
