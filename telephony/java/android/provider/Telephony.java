00457c2a76d450e00d4f1532737fb2576333a8f9
Leland Miller <lelandmiller@google.com>
2019-01-24 16:09:10 -0800

    Add getOrCreateCanonicalAddressId
    This is used exclusively for RCS message storage to retrieve canonical
    address IDs for participants.
    
    Test: CtsRcsTestCases
    Change-Id: Idd935b8d7787db9597b643551714f134d63b33c0

--- telephony/java/android/provider/Telephony.java
+++ telephony/java/android/provider/Telephony.java
@@ -2653,6 +2653,40 @@ public final class Telephony {
             Uri RCS_EVENT_QUERY_URI = Uri.withAppendedPath(CONTENT_AND_AUTHORITY,
                     RCS_EVENT_QUERY_URI_PATH);
         }
+
+        /**
+         * Allows RCS specific canonical address handling
+         */
+        interface RcsCanonicalAddressHelper extends BaseColumns {
+            /**
+             * Returns the canonical address ID for a canonical address, if now row exists, this
+             * will add a row and return its ID.
+             *
+             * @throws IllegalArgumentException if unable to retrieve or create the canonical
+             *                                  address entry.
+             */
+            static long getOrCreateCanonicalAddressId(
+                    ContentResolver contentResolver, String canonicalAddress) {
+
+                Uri.Builder uriBuilder = CONTENT_AND_AUTHORITY.buildUpon();
+                uriBuilder.appendPath("canonical-address");
+                uriBuilder.appendQueryParameter("address", canonicalAddress);
+                Uri uri = uriBuilder.build();
+
+
+                try (Cursor cursor = contentResolver.query(uri, null, null, null)) {
+                    if (cursor != null && cursor.moveToFirst()) {
+                        return cursor.getLong(cursor.getColumnIndex(BaseColumns._ID));
+                    } else {
+                        Rlog.e(TAG, "getOrCreateCanonicalAddressId returned no rows");
+                    }
+                }
+
+                Rlog.e(TAG, "getOrCreateCanonicalAddressId failed");
+                throw new IllegalArgumentException(
+                        "Unable to find or allocate a canonical address ID");
+            }
+        }
     }
 
     /**
