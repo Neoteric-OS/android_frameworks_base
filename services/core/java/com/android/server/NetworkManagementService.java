2585a62d27638ed14fdf187817dec54ace4f368b
Remi NGUYEN VAN <reminv@google.com>
2019-01-29 15:38:52 +0900

    Compile NetworkStack as system_current
    Includes various small changes to stop using hidden APIs
    
    Test: make NetworkStack
    Test: flashed, booted, WiFi and tethering working
    Bug: 112869080
    Change-Id: Id2830795a444f484b377ed6437435a1cd833697a

--- services/core/java/com/android/server/NetworkManagementService.java
+++ services/core/java/com/android/server/NetworkManagementService.java
@@ -61,7 +61,7 @@ import android.net.RouteInfo;
 import android.net.TetherStatsParcel;
 import android.net.UidRange;
 import android.net.shared.NetdService;
-import android.net.shared.NetworkObserverRegistry;
+import android.net.util.NetdService;
 import android.os.BatteryStats;
 import android.os.Binder;
 import android.os.Handler;
