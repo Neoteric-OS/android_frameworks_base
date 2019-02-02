82f7957161a6424a2058341fa5a149c7a2742d43
Nathan Harold <nharold@google.com>
2019-02-01 17:43:09 -0800

    Track Multiple Event Instances in EventReporter
    To avoid spamming users or the debug app with debug
    events, the DebugEventReporter will now track each
    event it receives and only send a single intent for
    each event signature per boot. In the future, some
    other method of persistence (such as per-build) might
    be preferable, but this should mitigate any looping
    events.
    
    In addition, add dump() to the event reporter so that
    even if there is no debug app installed, or in case
    an event happens multiple times, it can be observed
    through a bugreport.
    
    Bug: 120941729
    Test: dump and observe that the events are printed.
    Change-Id: Iaf04a308a29bc074acfaa16b9e70947761759181

--- telephony/java/android/telephony/DebugEventReporter.java
+++ telephony/java/android/telephony/DebugEventReporter.java
@@ -24,6 +24,11 @@ import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.os.ParcelUuid;
 
+import com.android.internal.util.IndentingPrintWriter;
+
+import java.io.FileDescriptor;
+import java.io.PrintWriter;
+import java.util.HashMap;
 import java.util.List;
 import java.util.UUID;
 
@@ -47,6 +52,8 @@ public final class DebugEventReporter {
 
     private static Context sContext = null;
 
+    private static HashMap<UUID, Integer> sEvents = new HashMap<>();
+
     /*
      * Because this is only supporting system packages, once we find a package, it will be the
      * same package until the next system upgrade. Thus, to save time in processing debug events
@@ -69,6 +76,10 @@ public final class DebugEventReporter {
      *        static and must not contain any sensitive information (especially PII).
      */
     public static void sendEvent(@NonNull UUID eventId, String description) {
+        // If this event has already occurred, skip sending intents for it; regardless log its
+        // invocation here.
+        if (sEvents.compute(eventId, (id, count) -> count == null ? 1 : count++) > 1) return;
+
         if (sContext == null) {
             Rlog.w(TAG, "DebugEventReporter not yet initialized, dropping event=" + eventId);
             return;
@@ -139,5 +150,21 @@ public final class DebugEventReporter {
             break;
         }
         // Initialization may only be performed once.
+    }
+
+    /** Dump the contents of the DebugEventReporter */
+    public static void dump(FileDescriptor fd, PrintWriter printWriter, String[] args) {
+        if (sContext == null) return;
+        IndentingPrintWriter pw = new IndentingPrintWriter(printWriter, "  ");
+        sContext.enforceCallingOrSelfPermission(android.Manifest.permission.DUMP, "Requires DUMP");
+        pw.println("Initialized=" + (sContext != null ? "Yes" : "No"));
+        pw.println("Debug Package=" + sDebugPackageName);
+        pw.println("Event Counts:");
+        pw.increaseIndent();
+        for (UUID event : sEvents.keySet()) {
+            pw.println(event + ": " + sEvents.get(event));
+        }
+        pw.decreaseIndent();
+        pw.flush();
     }
 }
