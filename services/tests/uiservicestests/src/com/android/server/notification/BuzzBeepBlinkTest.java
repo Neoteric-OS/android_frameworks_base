8425e95b505f15d7ec9b24cd1f20fde468dad32f
Tetsutoki Shiozawa <tetsutoki.shiozawa@sony.com>
2019-02-20 17:15:09 +0900

    Missed call LED pulse notification is... missing
    When a call is received, the screen lights up and the InCallUI is
    shown.  If the call goes unanswered a 'missed call' notification
    is left, where the LED should also pulse; this pulsing is to indicate
    that a missed call occurred even after the screen turns off.
    
    This re-adds that functionality that was removed in:
    5436923 Be more strict about triggering notification lights
    
    Bug:
    Test: atest BuzzBeepBlinkTest
    Change-Id: If01e8f595affc874b2c9fee82a48283b6aeaceda

--- services/tests/uiservicestests/src/com/android/server/notification/BuzzBeepBlinkTest.java
+++ services/tests/uiservicestests/src/com/android/server/notification/BuzzBeepBlinkTest.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2016 The Android Open Source Project
+ * Copyright (C) 2019 Sony Mobile Communications Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,6 +13,9 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * NOTE: This file has been modified by Sony Mobile Communications Inc.
+ * Modifications are licensed under the License.
  */
 package com.android.server.notification;
 
@@ -1185,7 +1189,7 @@ public class BuzzBeepBlinkTest extends UiServiceTestCase {
         NotificationRecord r = getLightsNotification();
         mService.buzzBeepBlinkLocked(r);
         verifyNeverLights();
-        assertFalse(r.isInterruptive());
+        assertTrue(r.isInterruptive());
         assertEquals(-1, r.getLastAudiblyAlertedMs());
     }
 
