1f8e9ee6a7a940d1559cf0247f0ba6e28bcb8ce7
junyulai <junyulai@google.com>
2019-04-16 19:32:32 +0800

    [KA11] Verify fd ownership and allocate resource for NattKeepalive
    Since socket keepalive APIs for UDP encapsulation sockets are
    public to generic app. In order to ensure the given fd is valid,
    this change verifies the resource id inside the UDP
    encapsulation socket by using methods provided by IpSecService.
    
    Bug: 125517194
    Test: 1. atest FrameworksNetTests --generate-new-metrics 10
          2. atestcom.android.server.ConnectivityServiceTest \
             #testNattSocketKeepalives --generate-new-metrics 100
    Change-Id: I408aacc19b364683854d15a095c34e72389a6e5b

--- services/core/java/com/android/server/ConnectivityService.java
+++ services/core/java/com/android/server/ConnectivityService.java
@@ -27,6 +27,7 @@ import static android.net.ConnectivityManager.getNetworkTypeName;
 import static android.net.ConnectivityManager.isNetworkTypeValid;
 import static android.net.INetworkMonitor.NETWORK_TEST_RESULT_PARTIAL_CONNECTIVITY;
 import static android.net.INetworkMonitor.NETWORK_TEST_RESULT_VALID;
+import static android.net.IpSecManager.INVALID_RESOURCE_ID;
 import static android.net.NetworkCapabilities.NET_CAPABILITY_CAPTIVE_PORTAL;
 import static android.net.NetworkCapabilities.NET_CAPABILITY_FOREGROUND;
 import static android.net.NetworkCapabilities.NET_CAPABILITY_INTERNET;
@@ -6839,7 +6840,8 @@ public class ConnectivityService extends IConnectivityManager.Stub
         mKeepaliveTracker.startNattKeepalive(
                 getNetworkAgentInfoForNetwork(network), null /* fd */,
                 intervalSeconds, cb,
-                srcAddr, srcPort, dstAddr, NattSocketKeepalive.NATT_PORT);
+                srcAddr, srcPort, dstAddr, NattSocketKeepalive.NATT_PORT,
+                INVALID_RESOURCE_ID /* Unused */);
     }
 
     @Override
