ef371ba3662a9d105517da0fdd3d901bc32da14b
Chiachang Wang <chiachangwang@google.com>
2019-05-27 20:53:01 +0800

    Clean up for async dns query in NetworkStack
    This is a follow-up commit for aosp/944772 to
    1. Update error message for UNE
    2. Refactor identical code
    3. Update comments
    
    Bug: 123435238
    Test: atest com.android.server.connectivity.NetworkMonitorTest
    Change-Id: I11d5013497352cd32ff43fbdd88e39d12835277c

--- packages/NetworkStack/src/com/android/networkstack/util/DnsUtils.java
+++ packages/NetworkStack/src/com/android/networkstack/util/DnsUtils.java
@@ -21,12 +21,16 @@ import static android.net.DnsResolver.TYPE_A;
 import static android.net.DnsResolver.TYPE_AAAA;
 
 import android.annotation.NonNull;
+import android.annotation.Nullable;
 import android.net.DnsResolver;
 import android.net.Network;
 import android.net.TrafficStats;
+import android.net.shared.PrivateDnsConfig;
+import android.net.util.Stopwatch;
 import android.util.Log;
 
 import com.android.internal.util.TrafficStatsConstants;
+import com.android.server.connectivity.NetworkMonitor.DnsLogFunc;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
@@ -44,6 +48,7 @@ public class DnsUtils {
     // Decide what queries to make depending on what IP addresses are on the system.
     public static final int TYPE_ADDRCONFIG = -1;
     private static final String TAG = DnsUtils.class.getSimpleName();
+    private static final boolean DBG = Log.isLoggable(TAG, Log.DEBUG);
 
     /**
      * Return both A and AAAA query results regardless the ip address type of the giving network.
@@ -51,27 +56,35 @@ public class DnsUtils {
      */
     @NonNull
     public static InetAddress[] getAllByName(@NonNull final DnsResolver dnsResolver,
-            @NonNull final Network network, @NonNull String host, int timeout)
-            throws UnknownHostException {
+            @NonNull final Network network, @NonNull String host, int timeout,
+            @NonNull final DnsLogFunc logger) throws UnknownHostException {
         final List<InetAddress> result = new ArrayList<InetAddress>();
+        final StringBuffer errorMsg = new StringBuffer(host);
 
         try {
             result.addAll(Arrays.asList(
                     getAllByName(dnsResolver, network, host, TYPE_AAAA, FLAG_NO_CACHE_LOOKUP,
-                    timeout)));
+                    timeout, logger)));
         } catch (UnknownHostException e) {
             // Might happen if the host is v4-only, still need to query TYPE_A
+            errorMsg.append(String.format(" (%s)%s", dnsTypeToStr(TYPE_AAAA), e.getMessage()));
         }
         try {
             result.addAll(Arrays.asList(
                     getAllByName(dnsResolver, network, host, TYPE_A, FLAG_NO_CACHE_LOOKUP,
-                    timeout)));
+                    timeout, logger)));
         } catch (UnknownHostException e) {
             // Might happen if the host is v6-only, still need to return AAAA answers
+            errorMsg.append(String.format(" (%s)%s", dnsTypeToStr(TYPE_A), e.getMessage()));
         }
+
+        PrivateDnsConfig privateDnsConfig =
+                new PrivateDnsConfig(host, result.toArray(new InetAddress[0]));
         if (result.size() == 0) {
-            throw new UnknownHostException(host);
+            logger.log("FAIL: " + errorMsg.toString());
+            throw new UnknownHostException(errorMsg.toString());
         }
+        logger.log("OK: " + privateDnsConfig);
         return result.toArray(new InetAddress[0]);
     }
 
@@ -82,10 +95,11 @@ public class DnsUtils {
     @NonNull
     public static InetAddress[] getAllByName(@NonNull final DnsResolver dnsResolver,
             @NonNull final Network network, @NonNull final String host, int type, int flag,
-            int timeoutMs) throws UnknownHostException {
+            int timeoutMs, @Nullable final DnsLogFunc logger) throws UnknownHostException {
         final CountDownLatch latch = new CountDownLatch(1);
         final AtomicReference<List<InetAddress>> resultRef = new AtomicReference<>();
-
+        final AtomicReference<String> errorMsg = new AtomicReference<>();
+        final Stopwatch watch = new Stopwatch().start();
         final DnsResolver.Callback<List<InetAddress>> callback =
                 new DnsResolver.Callback<List<InetAddress>>() {
             @Override
@@ -98,10 +112,17 @@ public class DnsUtils {
 
             @Override
             public void onError(@NonNull DnsResolver.DnsException e) {
-                Log.d(TAG, "DNS error resolving " + host + ": " + e.getMessage());
+                errorMsg.set(e.getMessage());
+                if (DBG) {
+                    Log.d(TAG, "DNS error resolving " + errorMsg);
+                }
                 latch.countDown();
             }
         };
+        // TODO: Investigate whether this is still useful.
+        // The packets that actually do the DNS queries are sent by netd, but netd doesn't
+        // look at the tag at all. Given that this is a library, the tag should be passed in by the
+        // caller.
         final int oldTag = TrafficStats.getAndSetThreadStatsTag(
                 TrafficStatsConstants.TAG_SYSTEM_PROBE);
 
@@ -121,10 +142,47 @@ public class DnsUtils {
         }
 
         final List<InetAddress> result = resultRef.get();
-        if (result == null || result.size() == 0) {
-            throw new UnknownHostException(host);
+        final boolean succeed = !((result == null || result.size() == 0));
+        final String msg = (errorMsg.get() == null || errorMsg.get().length() == 0)
+                ? "Timeout" : errorMsg.toString();
+        logDnsResult(result, watch, succeed, logger, msg, type);
+        if (!succeed) {
+            throw new UnknownHostException(msg);
         }
 
         return result.toArray(new InetAddress[0]);
+    }
+
+    private static void logDnsResult(final List<InetAddress> result, @NonNull final Stopwatch watch,
+            boolean succeed, @Nullable final DnsLogFunc logger, @NonNull final String errorMsg,
+            int type) {
+        if (logger == null) {
+            return;
+        }
+
+        final long latency = watch.stop();
+        if (succeed) {
+            StringBuffer buffer = new StringBuffer();
+            for (InetAddress address : result) {
+                buffer.append(',').append(address.getHostAddress());
+            }
+            logger.log(String.format("%dms OK %s", latency, buffer.substring(1)));
+        } else {
+            logger.log(String.format("%dms FAIL in type %s %s", latency, dnsTypeToStr(type),
+                    errorMsg));
+        }
+    }
+
+    private static String dnsTypeToStr(int type) {
+        switch (type) {
+            case TYPE_A:
+                return "A";
+            case TYPE_AAAA:
+                return "AAAA";
+            case TYPE_ADDRCONFIG:
+                return "ADDRCONFIG";
+            default:
+        }
+        return "UNDEFINED";
     }
 }
