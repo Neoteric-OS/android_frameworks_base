769c96514f9daba6313f6db434d9fdb83720773b
Satish Chandra <satish.chandra@mediatek.com>
2019-06-05 14:32:14 +0800

    Fix for, BT Carkit position issue when music app is killed
    When Music application is killed by user or lower memory killer,
    Playback state is not set to null (or to default).
    So, BT AVRCP display (or remote display) continues to display with
    last music song position.
    
    Bug:
    Test: 1. Connect to BT carkit
    Test: 2. Play music with music apk
    Test: 3. Force stop music apk in phone settings->app->force stop
    Test: 4. Start navigation with map apk.
    Test:  Manuallytested the above scenerio on two carkits.
    
    Change-Id: I84a8c245f4c2fc258a316f498e2737942d438af6

--- services/core/java/com/android/server/media/MediaSessionRecord.java
+++ services/core/java/com/android/server/media/MediaSessionRecord.java
@@ -421,6 +421,7 @@ public class MediaSessionRecord implements IBinder.DeathRecipient {
                 return;
             }
             mDestroyed = true;
+            mPlaybackState = null;
             mHandler.post(MessageHandler.MSG_DESTROYED);
         }
     }
