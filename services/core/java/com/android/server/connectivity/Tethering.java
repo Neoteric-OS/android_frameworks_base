34cf6cdabb008596c2988ef526e9906fdd66c80f
markchien <markchien@google.com>
2019-07-19 17:28:04 +0800

    Fix the race between entitlement start/stop
    For very quickly hotspot OFF/ON, there is race between tethering
    start entitlement from Settings and stop entitlement from wifi's
    intent broadcast. Entitlement would only be started when Settings
    call #startTethering(). But there are two points that tethering
    would stop entitlement. One is #stopTethering() from Settings.
    Another one is wifi tell tethering that it is shuting down.
    In this change, second point of stop entitlement is removed to avoid
    this race problem. This change should be safe because hotspot would
    always be stopped by Settings.
    
    Bug: 137034691
    Test: -build, flash, boot
          -atest FrameworkNetTests
          -Enable hotspot with entitlement supported SIM,
           then changing the AP band.
    
    Change-Id: I63f858356b4543b5bdda41d03e45adc944b86da3

--- services/core/java/com/android/server/connectivity/Tethering.java
+++ services/core/java/com/android/server/connectivity/Tethering.java
@@ -772,7 +772,16 @@ public class Tethering extends BaseNetworkObserver {
                     case WifiManager.WIFI_AP_STATE_FAILED:
                     default:
                         disableWifiIpServingLocked(ifname, curState);
-                        mEntitlementMgr.stopProvisioningIfNeeded(TETHERING_WIFI);
+                        // There is race condition between #startTethering() and
+                        // WIFI_AP_STATE_DISABLED when hotspot is off/on quickly. Calling
+                        // #stopProvisioningIfNeeded in here may have problem due to the race.
+                        // See b/137034691#comment31. WIFI_AP_STATE_{DISABLED, DISABLING} is
+                        // always called after #stopTethering(), which already disabled it; and
+                        // for WIFI_AP_STATE_FAILED, callers are expected to call #stopTethering()
+                        // when it is received, as Settings does now.
+                        // TODO: Don't reply on caller to stop entitlement. Refactor the design
+                        // to call #stopProvisioningIfNeeded when IpServer leaving TETHERED state.
+                        // TODO: mWifiTetherRequested may also have this race condition.
                         break;
                 }
             }
