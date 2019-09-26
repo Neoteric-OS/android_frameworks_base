a528686b98d3b5b5e44696f14d21c8daa9628a32
Chen Xu <fionaxu@google.com>
2019-09-26 16:11:59 -0700

    new telephony annotation class
    This is a preparatory work for mainline. since Intdef is hidden, so we
    have to move some annotations (applied in SDK/system API) to a separate
    annotation class and having other module statically link to it.
    
    TODO: include telephony annotation in framework-non-updatable-sources
    
    Bug: 140908357
    Test: Build
    Change-Id: I37f8a0624bbf27f264870ee9dbf03d3aaa5cadc1
    (cherry picked from commit c9d4ee112e5441ae01502d597f6661f0feecc363)
    Merged-in: I37f8a0624bbf27f264870ee9dbf03d3aaa5cadc1
    (cherry picked from commit 4712711d3d91621083bf92f5a1647b92c20a8b81)

--- core/java/android/os/telephony/TelephonyRegistryManager.java
+++ core/java/android/os/telephony/TelephonyRegistryManager.java
@@ -21,23 +21,25 @@ import android.net.NetworkCapabilities;
 import android.os.Bundle;
 import android.os.RemoteException;
 import android.os.ServiceManager;
+import android.telephony.Annotation;
+import android.telephony.Annotation.ApnType;
+import android.telephony.Annotation.CallState;
+import android.telephony.Annotation.DataActivityType;
+import android.telephony.Annotation.DataFailureCause;
+import android.telephony.Annotation.DataState;
+import android.telephony.Annotation.NetworkType;
+import android.telephony.Annotation.RadioPowerState;
+import android.telephony.Annotation.SimActivationState;
+import android.telephony.Annotation.SrvccState;
 import android.telephony.CallQuality;
 import android.telephony.CellInfo;
-import android.telephony.DataFailCause;
 import android.telephony.DisconnectCause;
 import android.telephony.PhoneCapability;
 import android.telephony.PreciseCallState.State;
 import android.telephony.ServiceState;
 import android.telephony.SignalStrength;
 import android.telephony.TelephonyManager;
-import android.telephony.TelephonyManager.CallState;
-import android.telephony.TelephonyManager.DataActivityType;
-import android.telephony.TelephonyManager.DataState;
-import android.telephony.TelephonyManager.NetworkType;
-import android.telephony.TelephonyManager.RadioPowerState;
-import android.telephony.TelephonyManager.SimActivationState;
 import android.telephony.data.ApnSetting;
-import android.telephony.data.ApnSetting.ApnType;
 import android.telephony.ims.ImsReasonInfo;
 import com.android.internal.telephony.ITelephonyRegistry;
 import java.util.List;
@@ -100,9 +102,7 @@ public class TelephonyRegistryManager {
      *
      * @hide
      */
-    public void notifyCallStateChanged(int subId, int slotIndex, @CallState int state,
-        String incomingNumer) {
-        try {
+    public void notifyCallStateChanged(int subId, int slotIndex, @CallState int statee  {
             sRegistry.notifyCallState(slotIndex, subId, state, incomingNumer);
         } catch (RemoteException ex) {
             // system server crash
@@ -410,7 +410,7 @@ public class TelephonyRegistryManager {
      * @hide
      */
     public void notifyPreciseDataConnectionFailed(int subId, int slotIndex, String apnType,
-        String apn, @DataFailCause.FailCause int failCause) {
+        String apn, @DataFailureCause int failCause) {
         try {
             sRegistry.notifyPreciseDataConnectionFailed(slotIndex, subId, apnType, apn, failCause);
         } catch (RemoteException ex) {
@@ -427,7 +427,7 @@ public class TelephonyRegistryManager {
      *
      * @hide
      */
-    public void notifySrvccStateChanged(int subId, @TelephonyManager.SrvccState int state) {
+    public void notifySrvccStateChanged(int subId, @SrvccState int state) {
         try {
             sRegistry.notifySrvccStateChanged(subId, state);
         } catch (RemoteException ex) {
