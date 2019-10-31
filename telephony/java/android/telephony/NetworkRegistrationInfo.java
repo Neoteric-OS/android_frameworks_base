a109612b1221934989b3593ee4018714bc85934f
shilu <shilu@google.com>
2019-10-31 16:50:43 -0700

    Exposing Network Registration Info for PS service
    bug: 123099466
    
    Test: manual
    
    Change-Id: I1421b638f1835962de8e4ba18bdf75615d37ebe5

--- telephony/java/android/telephony/NetworkRegistrationInfo.java
+++ telephony/java/android/telephony/NetworkRegistrationInfo.java
@@ -24,8 +24,8 @@ import android.annotation.TestApi;
 import android.os.Parcel;
 import android.os.Parcelable;
 import android.telephony.AccessNetworkConstants.TransportType;
-
 import android.telephony.Annotation.NetworkType;
+
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.util.ArrayList;
@@ -36,10 +36,7 @@ import java.util.stream.Collectors;
 
 /**
  * Description of a mobile network registration info
- * @hide
  */
-@SystemApi
-@TestApi
 public final class NetworkRegistrationInfo implements Parcelable {
     /**
      * Network domain
@@ -49,9 +46,18 @@ public final class NetworkRegistrationInfo implements Parcelable {
     @IntDef(prefix = "DOMAIN_", value = {DOMAIN_CS, DOMAIN_PS})
     public @interface Domain {}
 
-    /** Circuit switching domain */
+    /**
+     * Circuit switching domain
+     * @hide
+     */
+    @SystemApi @TestApi
     public static final int DOMAIN_CS = 1;
-    /** Packet switching domain */
+
+    /**
+     * Packet switching domain
+     * @hide
+     */
+    @SystemApi @TestApi
     public static final int DOMAIN_PS = 2;
 
     /**
@@ -65,17 +71,29 @@ public final class NetworkRegistrationInfo implements Parcelable {
                     REGISTRATION_STATE_UNKNOWN, REGISTRATION_STATE_ROAMING})
     public @interface RegistrationState {}
 
-    /** Not registered. The device is not currently searching a new operator to register. */
+    /**
+     * Not registered. The device is not currently searching a new operator to register.
+     */
     public static final int REGISTRATION_STATE_NOT_REGISTERED_OR_SEARCHING = 0;
-    /** Registered on home network. */
+    /**
+     * Registered on home network.
+     */
     public static final int REGISTRATION_STATE_HOME = 1;
-    /** Not registered. The device is currently searching a new operator to register. */
+    /**
+     * Not registered. The device is currently searching a new operator to register.
+     */
     public static final int REGISTRATION_STATE_NOT_REGISTERED_SEARCHING = 2;
-    /** Registration denied. */
+    /**
+     * Registration denied.
+     */
     public static final int REGISTRATION_STATE_DENIED = 3;
-    /** Registration state is unknown. */
+    /**
+     * Registration state is unknown.
+     */
     public static final int REGISTRATION_STATE_UNKNOWN = 4;
-    /** Registered on roaming network. */
+    /**
+     * Registered on roaming network.
+     */
     public static final int REGISTRATION_STATE_ROAMING = 5;
 
     /** @hide */
@@ -125,22 +143,34 @@ public final class NetworkRegistrationInfo implements Parcelable {
                     SERVICE_TYPE_VIDEO, SERVICE_TYPE_EMERGENCY})
     public @interface ServiceType {}
 
-    /** Unkown service */
+    /**
+     * Unkown service
+     */
     public static final int SERVICE_TYPE_UNKNOWN    = 0;
 
-    /** Voice service */
+    /**
+     * Voice service
+     */
     public static final int SERVICE_TYPE_VOICE      = 1;
 
-    /** Data service */
+    /**
+     * Data service
+     */
     public static final int SERVICE_TYPE_DATA       = 2;
 
-    /** SMS service */
+    /**
+     * SMS service
+     */
     public static final int SERVICE_TYPE_SMS        = 3;
 
-    /** Video service */
+    /**
+     * Video service
+     */
     public static final int SERVICE_TYPE_VIDEO      = 4;
 
-    /** Emergency service */
+    /**
+     * Emergency service
+     */
     public static final int SERVICE_TYPE_EMERGENCY  = 5;
 
     @Domain
@@ -344,7 +374,9 @@ public final class NetworkRegistrationInfo implements Parcelable {
 
     /**
      * @return {@code true} if registered on roaming network, {@code false} otherwise.
+     * @hide
      */
+    @SystemApi @TestApi
     public boolean isRoaming() {
         return mRoamingType != ServiceState.ROAMING_TYPE_NOT_ROAMING;
     }
@@ -369,22 +401,27 @@ public final class NetworkRegistrationInfo implements Parcelable {
 
     /**
      * @return the current network roaming type.
+     * @hide
      */
-
+    @SystemApi @TestApi
     public @ServiceState.RoamingType int getRoamingType() {
         return mRoamingType;
     }
 
     /**
      * @return Whether emergency is enabled.
+     * @hide
      */
+    @SystemApi @TestApi
     public boolean isEmergencyEnabled() { return mEmergencyOnly; }
 
     /**
      * @return List of available service types.
+     * @hide
      */
     @NonNull
     @ServiceType
+    @SystemApi @TestApi
     public List<Integer> getAvailableServices() {
         return Collections.unmodifiableList(mAvailableServices);
     }
@@ -432,19 +469,26 @@ public final class NetworkRegistrationInfo implements Parcelable {
      * @hide
      */
     @Nullable
+    @SystemApi @TestApi
     public VoiceSpecificRegistrationInfo getVoiceSpecificInfo() {
         return mVoiceSpecificInfo;
     }
 
     /**
      * @return Data registration related info
+     * @hide
      */
     @Nullable
+    @SystemApi @TestApi
     public DataSpecificRegistrationInfo getDataSpecificInfo() {
         return mDataSpecificInfo;
     }
 
+    /**
+     * @hide
+     */
     @Override
+    @SystemApi @TestApi
     public int describeContents() {
         return 0;
     }
@@ -554,7 +598,11 @@ public final class NetworkRegistrationInfo implements Parcelable {
                 && mNrState == other.mNrState;
     }
 
+    /**
+     * @hide
+     */
     @Override
+    @SystemApi @TestApi
     public void writeToParcel(Parcel dest, int flags) {
         dest.writeInt(mDomain);
         dest.writeInt(mTransportType);
@@ -642,7 +690,9 @@ public final class NetworkRegistrationInfo implements Parcelable {
      *     .setRegistrationState(REGISTRATION_STATE_HOME)
      *     .build();
      * </code></pre>
+     * @hide
      */
+    @SystemApi @TestApi
     public static final class Builder{
         @Domain
         private int mDomain;
@@ -742,7 +792,9 @@ public final class NetworkRegistrationInfo implements Parcelable {
          * @param emergencyOnly True if this network registration is for emergency use only.
          *
          * @return The same instance of the builder.
+         * @hide
          */
+        @SystemApi @TestApi
         public @NonNull Builder setEmergencyOnly(boolean emergencyOnly) {
             mEmergencyOnly = emergencyOnly;
             return this;
@@ -754,7 +806,9 @@ public final class NetworkRegistrationInfo implements Parcelable {
          * @param availableServices Available services.
          *
          * @return The same instance of the builder.
+         * @hide
          */
+        @SystemApi @TestApi
         public @NonNull Builder setAvailableServices(
                 @NonNull @ServiceType List<Integer> availableServices) {
             mAvailableServices = availableServices;
@@ -767,7 +821,9 @@ public final class NetworkRegistrationInfo implements Parcelable {
          * @param cellIdentity The cell identity.
          *
          * @return The same instance of the builder.
+         * @hide
          */
+        @SystemApi @TestApi
         public @NonNull Builder setCellIdentity(@Nullable CellIdentity cellIdentity) {
             mCellIdentity = cellIdentity;
             return this;
@@ -775,9 +831,10 @@ public final class NetworkRegistrationInfo implements Parcelable {
 
         /**
          * Build the NetworkRegistrationInfo.
-         *
          * @return the NetworkRegistrationInfo object.
+         * @hide
          */
+        @SystemApi @TestApi
         public @NonNull NetworkRegistrationInfo build() {
             return new NetworkRegistrationInfo(mDomain, mTransportType, mRegistrationState,
                     mAccessNetworkTechnology, mRejectCause, mEmergencyOnly, mAvailableServices,
