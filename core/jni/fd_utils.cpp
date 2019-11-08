ec6166bde7edd391d49f57799df510586f1faac7
Mark Chien <markchien@google.com>
2019-11-08 15:00:15 +0900

    [Tether13] Put Tethering apk and library into apex
    Bug:144320626
    
    Change-Id: I1b4b039ff9de57f8e91c36cd0a9f3295db651351

--- core/jni/fd_utils.cpp
+++ core/jni/fd_utils.cpp
@@ -35,6 +35,7 @@
 static const char* kPathWhitelist[] = {
   "/apex/com.android.conscrypt/javalib/conscrypt.jar",
   "/apex/com.android.media/javalib/updatable-media.jar",
+  "/apex/com.android.tetheringstack/javalib/tethering-client.jar",
   "/dev/null",
   "/dev/socket/zygote",
   "/dev/socket/zygote_secondary",
