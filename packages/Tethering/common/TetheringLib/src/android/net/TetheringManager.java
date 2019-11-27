0c7730ce408d591cc3fb19a51120aad997aece06
markchien <markchien@google.com>
2019-11-27 21:20:33 +0800

    [Tether10]Remove UserManagerInternal usage in Tethering
    To be the unbundled application, tethering can not use UserManagerInternal
    method anymore. Replace it by listening user restriction change intent.
    
    Also fix some cleanup from
    https://android-review.googlesource.com/c/platform/frameworks/base/+/1131002
    
    Bug: 143195885
    Test: -build, flash, boot
          -atest TetheringTests
    
    Change-Id: I4ed08d20f8b7476b8ecd5d018b75806dd2f22cc1

--- packages/Tethering/common/TetheringLib/src/android/net/TetheringManager.java
+++ packages/Tethering/common/TetheringLib/src/android/net/TetheringManager.java
@@ -168,11 +168,12 @@ public class TetheringManager {
      * #getTetherableIfaces() to ensure corresponding interface is available for
      * tethering before calling #tether().
      *
-     * TODO: Deprecate this API. The only usages should be in PanService and Wifi P2P which
+     * @deprecated The only usages should be in PanService and Wifi P2P which
      * need direct access.
      *
      * {@hide}
      */
+    @Deprecated
     public int tether(@NonNull String iface) {
         try {
             mConnector.tether(iface);
@@ -185,8 +186,10 @@ public class TetheringManager {
     /**
      * Stop tethering the named interface.
      *
+     * @deprecated
      * {@hide}
      */
+    @Deprecated
     public int untether(@NonNull String iface) {
         try {
             mConnector.untether(iface);
@@ -202,9 +205,10 @@ public class TetheringManager {
      * encapsulate proper entitlement logic. If the API is used and an entitlement check is needed,
      * downstream USB tethering will be enabled but will not have any upstream.
      *
-     * @Deprecated
+     * @deprecated
      * {@hide}
      */
+    @Deprecated
     public int setUsbTethering(boolean enable) {
         try {
             mConnector.setUsbTethering(enable);
@@ -220,7 +224,7 @@ public class TetheringManager {
      *
      * {@hide}
      */
-    // TODO: improve the usage of ResultReciever, b/145096122
+    // TODO: improve the usage of ResultReceiver, b/145096122
     public void startTethering(int type, @NonNull ResultReceiver receiver,
             boolean showProvisioningUi) {
         try {
@@ -386,8 +390,11 @@ public class TetheringManager {
     /**
      * Get the set of tethered dhcp ranges.
      *
+     * @deprecated This API just return the default value which is not used in
+     * DhcpServer.
      * {@hide}
      */
+    @Deprecated
     public @NonNull String[] getTetheredDhcpRanges() {
         if (!mCallback.awaitCallbackCreation()) {
             throw new NullPointerException("callback was not ready yet");
