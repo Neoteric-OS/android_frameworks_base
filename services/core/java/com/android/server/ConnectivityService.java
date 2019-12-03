eef50e11827aa92d91d39b653f0db9a3e159c63b
Chalard Jean <jchalard@google.com>
2019-12-03 23:16:10 +0900

    [NS A40] Read current reassignment state instead of global state
    The reassignment state has the future value, but is transient
    instead of global. With this, the reassignment loop does not
    read from global state any more, so it's not affected by the
    side effects from the loop.
    
    This means future patches will be able to move the side effects
    out of the computation.
    
    Test: ConnectivityServiceTest
    Change-Id: I43499f19730bcd94b691cfea83aa1ca1d3de75db

--- services/core/java/com/android/server/ConnectivityService.java
+++ services/core/java/com/android/server/ConnectivityService.java
@@ -6379,6 +6379,7 @@ public class ConnectivityService extends IConnectivityManager.Stub
     }
 
     private ArrayMap<NetworkRequestInfo, NetworkAgentInfo> computeRequestReassignmentForNetwork(
+            @NonNull final NetworkReassignment changes,
             @NonNull final NetworkAgentInfo newNetwork) {
         final int score = newNetwork.getCurrentScore();
         final ArrayMap<NetworkRequestInfo, NetworkAgentInfo> reassignedRequests = new ArrayMap<>();
@@ -6389,7 +6390,7 @@ public class ConnectivityService extends IConnectivityManager.Stub
             // requests or not, and doesn't affect the network's score.
             if (nri.request.isListen()) continue;
 
-            final NetworkAgentInfo currentNetwork = nri.mSatisfier;
+            final NetworkAgentInfo currentNetwork = changes.getNewSatisfier(nri);
             final boolean satisfies = newNetwork.satisfies(nri.request);
             if (newNetwork == currentNetwork && satisfies) continue;
 
@@ -6439,7 +6440,7 @@ public class ConnectivityService extends IConnectivityManager.Stub
         if (VDBG || DDBG) log("rematching " + newNetwork.name());
 
         final ArrayMap<NetworkRequestInfo, NetworkAgentInfo> reassignedRequests =
-                computeRequestReassignmentForNetwork(newNetwork);
+                computeRequestReassignmentForNetwork(reassignment, newNetwork);
 
         // Find and migrate to this Network any NetworkRequests for
         // which this network is now the best.
