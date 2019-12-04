ce198830b2b50e8df9c1a830fce5d2c68d9160e8
Chalard Jean <jchalard@google.com>
2019-12-04 20:01:46 +0900

    [NS A44 1/2] Update linger state before processing listens
    To compute accurately whether a network is in the background, the
    linger state needs to be updated. Do that before computing whether
    a network is in the background in l.6580.
    
    However ! As of this patch, l.6443 computes a wrong value, because
    it is looking at intermediate global state. Somehow this used to
    compensate exactly for the wrongness in the computation in l.6580.
    There might have been a few undetected bugs there, but none is
    known.
    
    As such, as of this patch, l.6443 still computes a wrong value
    while l.6580 now computes the right one, so the tests do not pass.
    This patch must be checked in together with A44 2/2 which will
    fix the computation in l.6443, but is kept separate for ease of
    review.
    
    Bug: 113554781
    Test: ConnectivityServiceTest
    Change-Id: Iaeed0d11bfa09f292f232ae020e944e430bc0184

--- services/core/java/com/android/server/ConnectivityService.java
+++ services/core/java/com/android/server/ConnectivityService.java
@@ -6568,17 +6568,9 @@ public class ConnectivityService extends IConnectivityManager.Stub
             }
         }
 
-        for (final NetworkReassignment.AffectedNetwork event : changes.getAffectedNetworks()) {
-            // Process listen requests and update capabilities if the background state has
-            // changed for this network. For consistency with previous behavior, send onLost
-            // callbacks before onAvailable.
-            processNewlyLostListenRequests(event.mNetwork);
-            if (event.mOldBackground != event.mNetwork.isBackgroundNetwork()) {
-                applyBackgroundChangeForRematch(event.mNetwork);
-            }
-            processNewlySatisfiedListenRequests(event.mNetwork);
-        }
-
+        // Update the linger state before processing listen callbacks, because the background
+        // computation depends on whether the network is lingering. Don't send the LOSING callbacks
+        // just yet though, because they have to be sent later.
         final ArrayList<NetworkAgentInfo> lingeredNetworks = new ArrayList<>();
         for (final NetworkAgentInfo nai : nais) {
             // Rematching may have altered the linger state of some networks, so update all linger
@@ -6589,6 +6581,17 @@ public class ConnectivityService extends IConnectivityManager.Stub
             if (updateLingerState(nai, now)) {
                 lingeredNetworks.add(nai);
             }
+        }
+
+        for (final NetworkReassignment.AffectedNetwork event : changes.getAffectedNetworks()) {
+            // Process listen requests and update capabilities if the background state has
+            // changed for this network. For consistency with previous behavior, send onLost
+            // callbacks before onAvailable.
+            processNewlyLostListenRequests(event.mNetwork);
+            if (event.mOldBackground != event.mNetwork.isBackgroundNetwork()) {
+                applyBackgroundChangeForRematch(event.mNetwork);
+            }
+            processNewlySatisfiedListenRequests(event.mNetwork);
         }
 
         for (final NetworkAgentInfo nai : lingeredNetworks) {
