15a3fba306a2cabff96f9250fa87aa6430554cf5
markchien <markchien@google.com>
2019-12-05 00:55:39 +0800

    Expose some tethering constants in Settings
    Make some tethering constants @systemApi because tethering
    cannot use hide Api.
    
    Bug: 144814607
    Test: build, flash, boot
    Change-Id: I8b46e2483eec575cddaad392d055551b75ca71ef

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -231,7 +231,9 @@ public final class Settings {
      * @hide
      */
     @SdkConstant(SdkConstantType.ACTIVITY_INTENT_ACTION)
-    public static final String ACTION_TETHER_PROVISIONING =
+    @SystemApi
+    @TestApi
+    public static final String ACTION_TETHER_PROVISIONING_UI =
             "android.settings.TETHER_PROVISIONING_UI";
 
     /**
@@ -10441,6 +10443,7 @@ public final class Settings {
          * is interpreted as |false|.
          * @hide
          */
+        @SystemApi
         public static final String TETHER_OFFLOAD_DISABLED = "tether_offload_disabled";
 
         /**
@@ -10450,6 +10453,7 @@ public final class Settings {
          * is interpreted as |false|.
          * @hide
          */
+        @SystemApi
         public static final String TETHER_ENABLE_LEGACY_DHCP_SERVER =
                 "tether_enable_legacy_dhcp_server";
 
