d508fc685730501b0336c24a8cfdb66f60179843
Zongheng Wang <wangzongheng@google.com>
2019-12-11 15:09:48 -0800

    Add addOnSubscriptionsChangedListener with an executor as a parameter
    Bug: 140562205
    Test: Manual
    Change-Id: I4c81d9f3a8a3c7d11261cdfe1ae27b49abc33e43

--- telephony/java/android/telephony/SubscriptionManager.java
+++ telephony/java/android/telephony/SubscriptionManager.java
@@ -1040,6 +1040,22 @@ public class SubscriptionManager {
      */
     public void addOnSubscriptionsChangedListener(OnSubscriptionsChangedListener listener) {
         if (listener == null) return;
+        addOnSubscriptionsChangedListener(listener, listener.mExecutor);
+    }
+
+    /**
+     * Register for changes to the list of active {@link SubscriptionInfo} records or to the
+     * individual records themselves. When a change occurs the onSubscriptionsChanged method of
+     * the listener will be invoked immediately if there has been a notification. The
+     * onSubscriptionChanged method will also be triggered once initially when calling this
+     * function.
+     *
+     * @param listener an instance of {@link OnSubscriptionsChangedListener} with
+     *                 onSubscriptionsChanged overridden.
+     * @param executor the executor that will execute callbacks.
+     */
+    public void addOnSubscriptionsChangedListener(@NonNull OnSubscriptionsChangedListener listener,
+                                                  @NonNull Executor executor) {
         String pkgName = mContext != null ? mContext.getOpPackageName() : "<unknown>";
         if (DBG) {
             logd("register OnSubscriptionsChangedListener pkgName=" + pkgName
@@ -1051,7 +1067,7 @@ public class SubscriptionManager {
                 mContext.getSystemService(Context.TELEPHONY_REGISTRY_SERVICE);
         if (telephonyRegistryManager != null) {
             telephonyRegistryManager.addOnSubscriptionsChangedListener(listener,
-                    listener.mExecutor);
+                    executor);
         }
     }
 
