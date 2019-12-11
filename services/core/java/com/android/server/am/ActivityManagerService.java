6b936a85e27238eb2525cfa41e328b201c53bab6
liulvping <liulvping@xiaomi.com>
2019-12-11 17:55:48 +0800

    Moved ProcessRecord.appNotResponding handling on Handler
    appNotResponding occupied too much time, which could
    cause binder thread starvation of waiting AM lock, then
    leads to more serious anr exceptions.
    
    Test: manual
    
    Change-Id: Icb8b8202d19c656f6d796375ba343ada927b1d32
    Signed-off-by: liulvping <liulvping@xiaomi.com>

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -18586,8 +18586,10 @@ public class ActivityManagerService extends IActivityManager.Stub
                     return true;
                 }
             }
-            proc.appNotResponding(activityShortComponentName, aInfo,
-                    parentShortComponentName, parentProcess, aboveSystem, annotation);
+            mHandler.post(() -> {
+                    proc.appNotResponding(activityShortComponentName, aInfo,
+                        parentShortComponentName, parentProcess, aboveSystem, annotation);
+            });
         }
 
         return true;
