a71c697a7e86fb67a9ea4c141ecf5a4a641840eb
Peter Wang <tpwang@google.com>
2019-12-17 19:11:23 -0800

    [Telephony Mainline] Expose getActiveSubscriptionIdList
    Bug: 146362577
    Test: Build
    Change-Id: Ic2886c8af0ef176cffcd674eb5b1a09cb49cc8f0
    Merged-In: Ic2886c8af0ef176cffcd674eb5b1a09cb49cc8f0

--- services/core/java/com/android/server/TelephonyRegistry.java
+++ services/core/java/com/android/server/TelephonyRegistry.java
@@ -1290,8 +1290,9 @@ public class TelephonyRegistry extends ITelephonyRegistry.Stub {
     public void notifyCarrierNetworkChange(boolean active) {
         // only CarrierService with carrier privilege rule should have the permission
         int[] subIds = Arrays.stream(SubscriptionManager.from(mContext)
-                    .getActiveSubscriptionIdList(false))
-                    .filter(i -> TelephonyPermissions.checkCarrierPrivilegeForSubId(i)).toArray();
+                    .getActiveAndHiddenSubscriptionIdList())
+                    .filter(i -> TelephonyPermissions.checkCarrierPrivilegeForSubId(i))
+                    .toArray();
         if (ArrayUtils.isEmpty(subIds)) {
             loge("notifyCarrierNetworkChange without carrier privilege");
             // the active subId does not have carrier privilege.
