faba861b8738f582dcf5da217991fd1150bb94e2
Nikita Ioffe <ioffe@google.com>
2019-12-18 13:45:15 +0000

    Add an API to tell whenever device supports userspace reboot
    If device doesn't support userspace reboot then call to
    PowerManager.reboot("userspace") will throw an
    UnsupportedOperationException.
    
    For the case of upgrading devices, Treble enforces that previous vendor
    image should work with the new system image. Since userspace reboot
    requires services to be stopped and restarted cleanly, which upgrading
    devices might not be able to comply with, we need to have an API to
    distinguish between devices that support userspace reboot and the ones
    that do not.
    
    Test: atest PowerManagerTest
    Test: atest CtsUserspaceRebootHostSideTestCases
    Bug: 138605273
    Bug: 135984674
    Change-Id: I8303f43ab29499eb2995f0256854c787055d9560

--- core/java/android/os/PowerManager.java
+++ core/java/android/os/PowerManager.java
@@ -605,6 +605,12 @@ public final class PowerManager {
     public static final String REBOOT_SAFE_MODE = "safemode";
 
     /**
+     * The 'reason' value used for userspace reboot.
+     * @hide
+     */
+    public static final String REBOOT_USERSPACE = "userspace";
+
+    /**
      * The 'reason' value used when rebooting the device without turning on the screen.
      * @hide
      */
@@ -1383,6 +1389,14 @@ public final class PowerManager {
     }
 
     /**
+     * Returns {@code true} if tis devices supports userspace reboot.
+     */
+    // TODO(b/138605180): add link to documentation once it's ready.
+    public boolean isUserspaceRebootSupported() {
+        return SystemProperties.getBoolean("ro.init.userspace_reboot.is_supported", false);
+    }
+
+    /**
      * Reboot the device.  Will not return if the reboot is successful.
      * <p>
      * Requires the {@link android.Manifest.permission#REBOOT} permission.
@@ -1390,8 +1404,15 @@ public final class PowerManager {
      *
      * @param reason code to pass to the kernel (e.g., "recovery") to
      *               request special boot modes, or null.
+     * @throws UnsupportedOperationException if userspace reboot was requested on a device that
+     *                                       doesn't support it.
      */
+    // TODO(b/138605273): consider a new api with well defined reasons.
     public void reboot(String reason) {
+        if (reason.equals(REBOOT_USERSPACE) && !isUserspaceRebootSupported()) {
+            throw new UnsupportedOperationException(
+                    "Attempted userspace reboot on a device that doesn't support it");
+        }
         try {
             mService.reboot(false, reason, true);
         } catch (RemoteException e) {
