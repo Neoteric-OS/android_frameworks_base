5b88b008cabb69857d01d6ae719666fa8ff93331
George Chang <georgekgchang@google.com>
2019-12-30 14:53:16 +0800

    Catch internal IllegalStateException on multiple connects
    Only one TagTechnology can be connected at a time.
    Other calls to connect() will return IOException.
    
    Bug: 144398423
    Test: check error on multiple connects
    Change-Id: I19a5f33c7f0d3e7592fe7791c1e04ddc2c9d6125

--- core/java/android/nfc/Tag.java
+++ core/java/android/nfc/Tag.java
@@ -455,12 +455,13 @@ public final class Tag implements Parcelable {
      *
      * @hide
      */
-    public synchronized void setConnectedTechnology(int technology) {
+    public synchronized boolean setConnectedTechnology(int technology) {
         if (mConnectedTechnology == -1) {
             mConnectedTechnology = technology;
         } else {
-            throw new IllegalStateException("Close other technology first!");
+            return false;
         }
+        return true;
     }
 
     /**
