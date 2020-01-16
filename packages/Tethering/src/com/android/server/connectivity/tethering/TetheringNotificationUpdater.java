68add6415e8b218b807056f17cc0f80160a4580b
paulhu <paulhu@google.com>
2020-01-16 23:47:49 +0800

    [TNU03] Add TetheringNotificationUpdaterTest
    Add new test for TetheringNotificationUpdater
    
    Bug: 122085773
    Bug: 130596698
    Test: atest TetheringTests
    Change-Id: I0db3df3e85dd6a8c3989c8bc66a06c50f45a0c15

--- packages/Tethering/src/com/android/server/connectivity/tethering/TetheringNotificationUpdater.java
+++ packages/Tethering/src/com/android/server/connectivity/tethering/TetheringNotificationUpdater.java
@@ -128,8 +128,9 @@ public class TetheringNotificationUpdater {
      *
      * @return downstream types mask value.
      */
+    @VisibleForTesting
     @IntRange(from = 0, to = 7)
-    private int getDownstreamTypesMask(@NonNull final String types) {
+    int getDownstreamTypesMask(@NonNull final String types) {
         int downstreamTypesMask = DOWNSTREAM_NONE;
         final String[] downstreams = types.split("\\|");
         for (String downstream : downstreams) {
@@ -153,7 +154,8 @@ public class TetheringNotificationUpdater {
      * @return {@link android.util.SparseArray} with downstream types and icon id info.
      */
     @NonNull
-    private SparseArray<Integer> getIcons(@ArrayRes int id) {
+    @VisibleForTesting
+    SparseArray<Integer> getIcons(@ArrayRes int id) {
         final Resources res = getResourcesForSubId(mContext, mActiveDataSubId);
         final String[] array = res.getStringArray(id);
         final SparseArray<Integer> icons = new SparseArray<>();
