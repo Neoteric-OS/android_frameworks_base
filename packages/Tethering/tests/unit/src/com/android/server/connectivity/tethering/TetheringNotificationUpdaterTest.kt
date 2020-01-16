5652bd562a7fd0ffde793ef99894ec273b973fc9
paulhu <paulhu@google.com>
2020-01-16 23:47:49 +0800

    [TNU03] Add TetheringNotificationUpdaterTest
    Add new test for TetheringNotificationUpdater
    
    Bug: 122085773
    Test: atest TetheringTests
    Change-Id: I0db3df3e85dd6a8c3989c8bc66a06c50f45a0c15

---
+++ packages/Tethering/tests/unit/src/com/android/server/connectivity/tethering/TetheringNotificationUpdaterTest.kt
@@ -0,0 +1,163 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.server.connectivity.tethering
+
+import android.app.Notification
+import android.app.NotificationManager
+import android.content.Context
+import android.content.res.Resources
+import android.content.res.TypedArray
+import android.net.ConnectivityManager.TETHERING_USB
+import android.net.ConnectivityManager.TETHERING_WIFI
+import android.os.UserHandle
+import android.telephony.SubscriptionManager.INVALID_SUBSCRIPTION_ID
+import androidx.test.InstrumentationRegistry
+import androidx.test.filters.SmallTest
+import androidx.test.runner.AndroidJUnit4
+import com.android.internal.util.test.BroadcastInterceptingContext
+import com.android.networkstack.tethering.R
+import com.android.server.connectivity.tethering.TetheringNotificationUpdater.DOWNSTREAM_NONE
+import com.android.server.connectivity.tethering.TetheringNotificationUpdater.NO_ICON_ID
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.ArgumentCaptor
+import org.mockito.ArgumentMatchers.any
+import org.mockito.ArgumentMatchers.anyInt
+import org.mockito.ArgumentMatchers.eq
+import org.mockito.Mock
+import org.mockito.Mockito.doReturn
+import org.mockito.Mockito.never
+import org.mockito.Mockito.reset
+import org.mockito.Mockito.spy
+import org.mockito.Mockito.times
+import org.mockito.Mockito.verify
+import org.mockito.MockitoAnnotations
+
+const val TEST_SUBID_1 = 0
+const val WIFI_DOWNSTREAM_TYPE_MASK = 1 shl TETHERING_WIFI
+const val USB_DOWNSTREAM_TYPE_MASK = 1 shl TETHERING_USB
+const val OVERLAY_TITTLE = "Tethering active"
+const val OVERLAY_TEXT_NO_CLIENT = "Tap here to set up."
+
+@RunWith(AndroidJUnit4::class)
+@SmallTest
+class TetheringNotificationUpdaterTest {
+    // lateinit used here for mocks as they need to be reinitialized between each test and the test
+    // should crash if they are used before being initialized.
+    @Mock private lateinit var mockContext: Context
+    @Mock private lateinit var notificationManager: NotificationManager
+    @Mock private lateinit var overlayRes: Resources
+    @Mock private lateinit var overlayIcon: TypedArray
+    private lateinit var context: Context
+    private lateinit var notificationUpdater: TetheringNotificationUpdater
+
+    fun setupOverlayResources() {
+        doReturn(overlayIcon).`when`(overlayRes)
+                .obtainTypedArray(R.array.tethering_notification_icons)
+        doReturn(8).`when`(overlayIcon).length()
+        doReturn(OVERLAY_TITTLE).`when`(overlayRes)
+                .getString(R.string.tethering_notification_title_noclients)
+        doReturn(OVERLAY_TEXT_NO_CLIENT).`when`(overlayRes)
+                .getString(R.string.tethering_notification_text_noclients)
+        doReturn(R.drawable.stat_sys_tether_general).`when`(overlayIcon)
+                .getResourceId(eq(WIFI_DOWNSTREAM_TYPE_MASK), eq(NO_ICON_ID))
+    }
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+
+        context = spy(BroadcastInterceptingContext(InstrumentationRegistry.getContext())).also {
+            doReturn(mockContext).`when`(it).createContextAsUser(eq(UserHandle.ALL), eq(0))
+            doReturn(it).`when`(it).createContextAsUser(eq(UserHandle.CURRENT), eq(0))
+            doReturn(notificationManager).`when`(mockContext)
+                    .getSystemService(Context.NOTIFICATION_SERVICE)
+        }
+
+        notificationUpdater = spy(TetheringNotificationUpdater(context)).also {
+            doReturn(overlayRes).`when`(it).getResourcesForSubId(any(), eq(TEST_SUBID_1))
+        }
+    }
+
+    private fun Notification.title() = this.extras.getString(Notification.EXTRA_TITLE)
+    private fun Notification.text() = this.extras.getString(Notification.EXTRA_TEXT)
+
+    private fun expectShowNotification(title: String, text: String) {
+        verify(notificationManager, never()).cancel(any(), anyInt())
+
+        val notificationCaptor = ArgumentCaptor.forClass(Notification::class.java)
+        verify(notificationManager, times(1)).notify(any(), anyInt(), notificationCaptor.capture())
+
+        val notification = notificationCaptor.getValue()
+        assertEquals(title, notification.title())
+        assertEquals(text, notification.text())
+
+        reset(notificationManager)
+    }
+
+    private fun expectClearNotification() {
+        verify(notificationManager, times(1)).cancel(any(), anyInt())
+        verify(notificationManager, never()).notify(any(), anyInt(), any())
+        reset(notificationManager)
+    }
+
+    @Test
+    fun testNotificationWithDefaultResources() {
+        val title = context.getResources().getString(R.string.tethered_notification_title)
+        val message = context.getResources().getString(R.string.tethered_notification_message)
+
+        // Hotspot enabled, no notification showed.
+        notificationUpdater.onDownstreamChanged(WIFI_DOWNSTREAM_TYPE_MASK)
+        expectClearNotification()
+
+        // Usb tethering enabled, showed enable notification
+        notificationUpdater.onDownstreamChanged(
+                WIFI_DOWNSTREAM_TYPE_MASK or USB_DOWNSTREAM_TYPE_MASK)
+        expectShowNotification(title, message)
+
+        // Remove wifi downstream, showed enable notification.
+        notificationUpdater.onDownstreamChanged(USB_DOWNSTREAM_TYPE_MASK)
+        expectShowNotification(title, message)
+
+        // No downstream, no notification showed.
+        notificationUpdater.onDownstreamChanged(DOWNSTREAM_NONE)
+        expectClearNotification()
+    }
+
+    @Test
+    fun testNotificationWithActiveDataSubscriptionIdChanged() {
+        setupOverlayResources()
+
+        // Hotspot enabled, no notification showed.
+        notificationUpdater.onDownstreamChanged(WIFI_DOWNSTREAM_TYPE_MASK)
+        expectClearNotification()
+
+        // Set overlay resource sub id, showed enable notification with overlay text.
+        notificationUpdater.onActiveDataSubscriptionIdChanged(TEST_SUBID_1)
+        expectShowNotification(OVERLAY_TITTLE, OVERLAY_TEXT_NO_CLIENT)
+
+        // Set invalid sub id, no notification for hotspot.
+        notificationUpdater.onActiveDataSubscriptionIdChanged(INVALID_SUBSCRIPTION_ID)
+        expectClearNotification()
+
+        // No downstream, no notification showed.
+        notificationUpdater.onDownstreamChanged(DOWNSTREAM_NONE)
+        expectClearNotification()
+    }
+}
