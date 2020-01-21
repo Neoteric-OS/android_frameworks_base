4dd184fed222df1a9ea562b572e10dba1b9cee42
Cody Kesting <ckesting@google.com>
2020-01-20 18:03:41 -0800

    Define Bundle keys for ConnectivityDiagnostics.
    ConnectivityDiagnosticsManager defines several callbacks that can be
    invoked for registered ConnectivityDiagnosticsCallback instances.
    onConnectivityReport and onDataStallSuspected include ConnectivityReport
    and DataStallReport, respectively, both of which contain a
    PersistableBundle to be used for passing information from the
    NetworkStack module to the registered callback. When values will be
    consistently passed inside these bundles, their keys are defined inside
    ConnectivityReport and DataStallReport.
    
    Bug: 147249364
    Test: compiles
    Test: atest FrameworksNetTests
    Change-Id: Icc6bd4337865692b6ca7c18eacabff59ba3a5bb9

--- core/java/android/net/ConnectivityDiagnosticsManager.java
+++ core/java/android/net/ConnectivityDiagnosticsManager.java
@@ -19,6 +19,7 @@ package android.net;
 import android.annotation.IntDef;
 import android.annotation.NonNull;
 import android.annotation.Nullable;
+import android.annotation.StringDef;
 import android.content.Context;
 import android.os.Parcel;
 import android.os.Parcelable;
@@ -79,6 +80,106 @@ public class ConnectivityDiagnosticsManager {
 
     /** Class that includes connectivity information for a specific Network at a specific time. */
     public static final class ConnectivityReport implements Parcelable {
+        /**
+         * The overall status of the network is that it is invalid; it neither provides
+         * connectivity nor is it deemed to be a captive portal.
+         */
+        public static final int NETWORK_VALIDATION_RESULT_INVALID = 0;
+
+        /**
+         * The overall status of the network is that it is valid, either because it provides
+         * full Internet access (all probes succeeded) or is deemed to be a captive portal.
+         */
+        // TODO: link to INetworkMonitor.NETWORK_VALIDATION_RESULT_VALID
+        public static final int NETWORK_VALIDATION_RESULT_VALID = 1;
+
+        /**
+         * The overall status of the network is that it provides partial connectivity; some
+         * probed services succeeded but others failed.
+         */
+        // TODO: link to INetworkMonitor.NETWORK_VALIDATION_RESULT_PARTIAL;
+        public static final int NETWORK_VALIDATION_RESULT_PARTIALLY_VALID = 2;
+
+        /** @hide */
+        @IntDef(
+                prefix = {"NETWORK_VALIDATION_RESULT_"},
+                value = {
+                        NETWORK_VALIDATION_RESULT_INVALID,
+                        NETWORK_VALIDATION_RESULT_VALID,
+                        NETWORK_VALIDATION_RESULT_PARTIALLY_VALID
+                })
+        @Retention(RetentionPolicy.SOURCE)
+        public @interface NetworkValidationResult {}
+
+        /**
+         * The overall validation result for the Network being reported on.
+         *
+         * <p>The possible values for this key are:
+         * {@link #NETWORK_VALIDATION_RESULT_INVALID},
+         * {@link #NETWORK_VALIDATION_RESULT_VALID},
+         * {@link #NETWORK_VALIDATION_RESULT_PARTIALLY_VALID}.
+         *
+         * @see android.net.NetworkCapabilities#CAPABILITY_VALIDATED
+         */
+        @NetworkValidationResult
+        public static final String KEY_NETWORK_VALIDATION_RESULT = "networkValidationResult";
+
+        /** DNS resolved successfully. */
+        // TODO: link to INetworkMonitor.NETWORK_VALIDATION_PROBE_DNS
+        public static final int NETWORK_PROBE_RESULT_DNS_SUCCESS = 0x04;
+
+        /** HTTP connected successfully. */
+        // TODO: link to INetworkMonitor.NETWORK_VALIDATION_PROBE_HTTP
+        public static final int NETWORK_PROBE_RESULT_HTTP_SUCCESS = 0x08;
+
+        /** HTTPS connected successfully. */
+        // TODO: link to INetworkMonitor.NETWORK_VALIDATION_PROBE_HTTPS;
+        public static final int NETWORK_PROBE_RESULT_HTTPS_SUCCESS = 0x10;
+
+        /** Captive portal fallback address probe succeeded. */
+        // TODO: link to INetworkMonitor.NETWORK_VALIDATION_FALLBACK
+        public static final int NETWORK_PROBE_RESULT_FALLBACK_SUCCESS = 0x20;
+
+        /** Private DNS (DNS over TLS) resolved successfully. */
+        // TODO: link to INetworkMonitor.NETWORK_VALIDATION_PROBE_PRIVDNS
+        public static final int NETWORK_PROBE_RESULT_PRIVATE_DNS_SUCCESS = 0x40;
+
+        /** @hide */
+        @IntDef(
+                prefix = {"NETWORK_PROBE_RESULT_"},
+                value = {
+                        NETWORK_PROBE_RESULT_DNS_SUCCESS,
+                        NETWORK_PROBE_RESULT_HTTP_SUCCESS,
+                        NETWORK_PROBE_RESULT_HTTPS_SUCCESS,
+                        NETWORK_PROBE_RESULT_FALLBACK_SUCCESS,
+                        NETWORK_PROBE_RESULT_PRIVATE_DNS_SUCCESS
+                })
+        @Retention(RetentionPolicy.SOURCE)
+        public @interface NetworkProbeResult {}
+
+        /**
+         * A bitmask of network validation probes that succeeded.
+         *
+         * <p>A result missing from this list does not necessarily mean that a test failed; it is
+         * possible that the probe was never performed.
+         *
+         * <p>The possible bits values reported by this key are:
+         * {@link #NETWORK_PROBE_RESULT_DNS_SUCCESS},
+         * {@link #NETWORK_PROBE_RESULT_HTTP_SUCCESS},
+         * {@link #NETWORK_PROBE_RESULT_HTTPS_SUCCESS},
+         * {@link #NETWORK_PROBE_RESULT_FALLBACK_SUCCESS},
+         * {@link #NETWORK_PROBE_RESULT_PRIVATE_DNS_SUCCESS}.
+         */
+        @NetworkProbeResult
+        public static final String KEY_NETWORK_PROBE_RESULT_BITMASK =
+                "networkProbeResultBitmask";
+
+        /** @hide */
+        @StringDef(prefix = {"KEY_"}, value = {
+                KEY_NETWORK_VALIDATION_RESULT, KEY_NETWORK_PROBE_RESULT_BITMASK})
+        @Retention(RetentionPolicy.SOURCE)
+        public @interface ConnectivityReportBundleKeys {}
+
         /** The Network for which this ConnectivityReport applied */
         @NonNull private final Network mNetwork;
 
@@ -246,6 +347,35 @@ public class ConnectivityDiagnosticsManager {
                 value = {DETECTION_METHOD_DNS_EVENTS, DETECTION_METHOD_TCP_METRICS})
         public @interface DetectionMethod {}
 
+        /**
+         * This key represents the fail rate of TCP packets when the suspected data stall was
+         * detected.
+         *
+         * <p>This key will be included if the data stall detection method is
+         * {@link #KEY_TCP_PACKET_FAIL_RATE}.
+         *
+         * <p>The value is an int percentage between 0 and 100.
+         */
+        public static final String KEY_TCP_PACKET_FAIL_RATE = "tcpPacketFailRate";
+
+        /**
+         * This key represents the consecutive number of DNS timeouts that have occurred.
+         *
+         * <p>This key will be included if the data stall detection method is
+         * {@link #DETECTION_METHOD_DNS_EVENTS}.
+         *
+         * <p>This value is an int.
+         */
+        public static final String KEY_DNS_CONSECUTIVE_TIMEOUTS = "dnsConsecutiveTimeouts";
+
+        /** @hide */
+        @Retention(RetentionPolicy.SOURCE)
+        @StringDef(prefix = {"KEY_"}, value = {
+                KEY_TCP_PACKET_FAIL_RATE,
+                KEY_DNS_CONSECUTIVE_TIMEOUTS
+        })
+        public @interface DataStallReportBundleKeys {}
+
         /** The Network for which this DataStallReport applied */
         @NonNull private final Network mNetwork;
 
@@ -314,6 +444,9 @@ public class ConnectivityDiagnosticsManager {
 
         /**
          * Returns a PersistableBundle with additional info for this report.
+         *
+         * <p>Get a bundle with detail about the suspected data stall including information
+         * specific to the monitoring method that detected the data stall.
          *
          * @return PersistableBundle that may contain additional information on the suspected data
          *     stall
