fde7fbaf62dfc1c7a0c4f69c17d0b1d6feadf389
Cody Kesting <ckesting@google.com>
2020-02-05 10:33:55 -0800

    Add support for registering test networks with administrators.
    CTS testing for ConnectivityDiagnosticsManager requires registering
    TestNetworks with the administrator UIDs set. This sets up the CTS
    process to be an administrator over the test network, which allows it to
    receive ConnectivityDiagnosticsCallback callbacks.
    
    Bug: 148032944
    Test: atest android.net.cts.ConnectivityDiagnosticsManagerTest
    Test: atest IpSecManagerTunnelTest
    Change-Id: I7a443c368cf1ac470fbe40883ca24bc3143153b0

--- services/core/java/com/android/server/TestNetworkService.java
+++ services/core/java/com/android/server/TestNetworkService.java
@@ -55,6 +55,7 @@ import java.net.InterfaceAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /** @hide */
@@ -231,6 +232,7 @@ class TestNetworkService extends ITestNetworkManager.Stub {
             @Nullable LinkProperties lp,
             boolean isMetered,
             int callingUid,
+            @NonNull int[] administratorUids,
             @NonNull IBinder binder)
             throws RemoteException, SocketException {
         checkNotNull(looper, "missing Looper");
@@ -249,6 +251,7 @@ class TestNetworkService extends ITestNetworkManager.Stub {
         nc.addCapability(NetworkCapabilities.NET_CAPABILITY_NOT_SUSPENDED);
         nc.addCapability(NetworkCapabilities.NET_CAPABILITY_NOT_RESTRICTED);
         nc.setNetworkSpecifier(new StringNetworkSpecifier(iface));
+        nc.setAdministratorUids(intArrayToList(administratorUids));
         if (!isMetered) {
             nc.addCapability(NetworkCapabilities.NET_CAPABILITY_NOT_METERED);
         }
@@ -291,6 +294,14 @@ class TestNetworkService extends ITestNetworkManager.Stub {
         return new TestNetworkAgent(looper, context, ni, nc, lp, callingUid, binder);
     }
 
+    private List<Integer> intArrayToList(@NonNull int[] array) {
+        final List<Integer> list = new ArrayList<>(array.length);
+        for (final int i : array) {
+            list.add(i);
+        }
+        return list;
+    }
+
     /**
      * Sets up a Network with extremely limited privileges, guarded by the MANAGE_TEST_NETWORKS
      * permission.
@@ -302,6 +313,7 @@ class TestNetworkService extends ITestNetworkManager.Stub {
             @NonNull String iface,
             @Nullable LinkProperties lp,
             boolean isMetered,
+            @NonNull int[] administratorUids,
             @NonNull IBinder binder) {
         enforceTestNetworkPermissions(mContext);
 
@@ -336,6 +348,7 @@ class TestNetworkService extends ITestNetworkManager.Stub {
                                             lp,
                                             isMetered,
                                             callingUid,
+                                            administratorUids,
                                             binder);
 
                             mTestNetworkTracker.put(agent.network.netId, agent);
