3e7b2894fd80b972a70aba47c3364f1b641d0b2c
Cody Kesting <ckesting@google.com>
2020-02-12 14:50:58 -0800

    Invoke onConnectivityReport on registering ConnectivityDiagnostics.
    This change updates the behavior for registering
    ConnectivityDiagnosticsCallbacks. Now, after a successful register()
    call, callbacks will receive cached ConnectivityReports for all
    matching, permissioned networks. This allows registrants to be updated
    with the network state for their networks without having to wait for the
    next network validation.
    
    Bug: 147849853
    Test: atest FrameworksNetTests
    Change-Id: I924ba8fdcc847f453557021591bde38602fe089c

--- services/core/java/com/android/server/ConnectivityService.java
+++ services/core/java/com/android/server/ConnectivityService.java
@@ -7807,6 +7807,34 @@ public class ConnectivityService extends IConnectivityManager.Stub
             cb.asBinder().linkToDeath(cbInfo, 0);
         } catch (RemoteException e) {
             cbInfo.binderDied();
+            return;
+        }
+
+        // Once registered, provide ConnectivityReports for matching Networks
+        final List<NetworkAgentInfo> matchingNetworks = new ArrayList<>();
+        synchronized (mNetworkForNetId) {
+            for (int i = 0; i < mNetworkForNetId.size(); i++) {
+                final NetworkAgentInfo nai = mNetworkForNetId.valueAt(i);
+                if (nai.satisfies(nri.request)) {
+                    matchingNetworks.add(nai);
+                }
+            }
+        }
+        for (final NetworkAgentInfo nai : matchingNetworks) {
+            final ConnectivityReport report = nai.getConnectivityReport();
+            if (report == null) {
+                continue;
+            }
+            if (!checkConnectivityDiagnosticsPermissions(
+                    nri.mPid, nri.mUid, nai, cbInfo.mCallingPackageName)) {
+                continue;
+            }
+
+            try {
+                cb.onConnectivityReport(report);
+            } catch (RemoteException e) {
+                // Exception while sending the ConnectivityReport. Move on to the next network.
+            }
         }
     }
 
@@ -7839,6 +7867,7 @@ public class ConnectivityService extends IConnectivityManager.Stub
                         nai.linkProperties,
                         nai.networkCapabilities,
                         extras);
+        nai.setConnectivityReport(report);
         final List<IConnectivityDiagnosticsCallback> results =
                 getMatchingPermissionedCallbacks(nai);
         for (final IConnectivityDiagnosticsCallback cb : results) {
