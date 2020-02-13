477f090e2ec5a3ac7ce50d6466142338e8eabc0c
Benedict Wong <benedictwong@google.com>
2020-02-12 18:00:32 -0800

    WIP: Make Ikev2VpnRunner not require a Thread
    This CL changes the Ikev2VpnRunner inheritance hierarchy to avoid the
    need for spawning an additional Thread that the Ikev2VpnRunner does not
    need. All callbacks are already proxied or called on the single-threaded
    executor
    
    Test: FrameworksNetTests passing
    Change-Id: I04f794e8cc0362a30f454ecd85f4513702124b4b

--- tests/net/java/com/android/server/connectivity/VpnTest.java
+++ tests/net/java/com/android/server/connectivity/VpnTest.java
@@ -1057,7 +1057,7 @@ public class VpnTest {
 
         synchronized (vpn) {
             runner = (IkeV2VpnRunner) vpn.mVpnRunner;
-            vpn.mVpnRunner.exit();
+            vpn.teardownVpn(runner);
 
             // Verify state is reset
             assertNull(vpn.mConfig);
