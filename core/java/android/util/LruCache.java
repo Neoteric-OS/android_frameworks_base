b06aa0dc774d156c2aaed8f9a9440924da71725f
Guray Alsac <guray@google.com>
2020-02-25 14:55:35 -0800

    add method to reset cache counters
    Test: modified unit tests
    Change-Id: Ib719037e76156f39d827f91c7fd0ee27a93ba6d1

--- core/java/android/util/LruCache.java
+++ core/java/android/util/LruCache.java
@@ -367,6 +367,18 @@ public class LruCache<K, V> {
     }
 
     /**
+     * Resets counters.
+     */
+    @SuppressLint("VisiblySynchronized")
+    public final synchronized void resetStatistics() {
+        hitCount = 0;
+        missCount = 0;
+        createCount = 0;
+        putCount = 0;
+        evictionCount = 0;
+    }
+
+    /**
      * Returns a copy of the current contents of the cache, ordered from least
      * recently accessed to most recently accessed.
      */
