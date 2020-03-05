e52a7a3779cb0558edcfb63f47fecf1195a7e9e9
Cody Kesting <ckesting@google.com>
2020-03-05 15:19:48 -0800

    Update ConnectivityDiagnosticsCallback function name.
    API Council feedback suggests renaming ConnectivityDiagnosticsCallback
    'onConnectivityReport' function to be 'onConnectivityReportAvailable'.
    
    Bug: 150598997
    Test: atest FrameworksNetTests
    Change-Id: Ieea727ecee6cf358a53a215fd4b06233dfe967ea

--- services/core/java/com/android/server/ConnectivityService.java
+++ services/core/java/com/android/server/ConnectivityService.java
@@ -7831,7 +7831,7 @@ public class ConnectivityService extends IConnectivityManager.Stub
             }
 
             try {
-                cb.onConnectivityReport(report);
+                cb.onConnectivityReportAvailable(report);
             } catch (RemoteException e) {
                 // Exception while sending the ConnectivityReport. Move on to the next network.
             }
@@ -7872,7 +7872,7 @@ public class ConnectivityService extends IConnectivityManager.Stub
                 getMatchingPermissionedCallbacks(nai);
         for (final IConnectivityDiagnosticsCallback cb : results) {
             try {
-                cb.onConnectivityReport(report);
+                cb.onConnectivityReportAvailable(report);
             } catch (RemoteException ex) {
                 loge("Error invoking onConnectivityReport", ex);
             }
