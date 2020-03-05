29cf3363ade6a7d29c972876611d5f69ca036eaf
Cody Kesting <ckesting@google.com>
2020-03-05 10:46:02 -0800

    Use IBinder as key for ConnectivityDiagnostics storage in CS.
    This change updates ConnectivityService to use IBinder instances as keys
    when storing ConnectivityDiagnosticsCallbacks.
    
    When storing ConnectivityDiagnosticsCallbacks in ConnectivityService,
    the IConnectivityDiagnsoticsCallback is used as the key for
    ConnectivityService.mConnectivityDiagnosticsCallbacks. However,
    IConnectivityDiagnosticsCallback instances are received as different
    objects. This causes them to produce different hashCode() values, so
    attempts to remove an IConnectivityDiagnosticsCallback fail.
    
    Bug: 150867635
    Test: atest FrameworksNetTests
    Change-Id: Ib99e68d5ae47fa27e12428f9a60a2c1204ac59a2

--- tests/net/java/com/android/server/ConnectivityServiceTest.java
+++ tests/net/java/com/android/server/ConnectivityServiceTest.java
@@ -6731,16 +6731,12 @@ public class ConnectivityServiceTest {
 
         verify(mIBinder).linkToDeath(any(ConnectivityDiagnosticsCallbackInfo.class), anyInt());
         verify(mConnectivityDiagnosticsCallback).asBinder();
-        assertTrue(
-                mService.mConnectivityDiagnosticsCallbacks.containsKey(
-                        mConnectivityDiagnosticsCallback));
+        assertTrue(mService.mConnectivityDiagnosticsCallbacks.containsKey(mIBinder));
 
         mService.unregisterConnectivityDiagnosticsCallback(mConnectivityDiagnosticsCallback);
         verify(mIBinder, timeout(TIMEOUT_MS))
                 .unlinkToDeath(any(ConnectivityDiagnosticsCallbackInfo.class), anyInt());
-        assertFalse(
-                mService.mConnectivityDiagnosticsCallbacks.containsKey(
-                        mConnectivityDiagnosticsCallback));
+        assertFalse(mService.mConnectivityDiagnosticsCallbacks.containsKey(mIBinder));
         verify(mConnectivityDiagnosticsCallback, atLeastOnce()).asBinder();
     }
 
@@ -6758,9 +6754,7 @@ public class ConnectivityServiceTest {
 
         verify(mIBinder).linkToDeath(any(ConnectivityDiagnosticsCallbackInfo.class), anyInt());
         verify(mConnectivityDiagnosticsCallback).asBinder();
-        assertTrue(
-                mService.mConnectivityDiagnosticsCallbacks.containsKey(
-                        mConnectivityDiagnosticsCallback));
+        assertTrue(mService.mConnectivityDiagnosticsCallbacks.containsKey(mIBinder));
 
         // Register the same callback again
         mService.registerConnectivityDiagnosticsCallback(
@@ -6769,9 +6763,7 @@ public class ConnectivityServiceTest {
         // Block until all other events are done processing.
         HandlerUtilsKt.waitForIdle(mCsHandlerThread, TIMEOUT_MS);
 
-        assertTrue(
-                mService.mConnectivityDiagnosticsCallbacks.containsKey(
-                        mConnectivityDiagnosticsCallback));
+        assertTrue(mService.mConnectivityDiagnosticsCallbacks.containsKey(mIBinder));
     }
 
     @Test
