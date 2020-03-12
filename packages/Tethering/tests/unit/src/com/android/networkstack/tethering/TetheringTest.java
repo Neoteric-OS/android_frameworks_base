8f028d99a70f3f7a879e65a5d6f2b5ff2d81c37a
Hungming Chen <nuccachen@google.com>
2020-03-12 21:24:01 +0800

    [BOT.2] Create a coordinator and stats provider to provide tether stats
    Make BPF tethering offload coordinator, BpfTetheringCoordinator,
    registers a network stats provider, BpfTetherStatsProvider, and
    provide the tethering stats from the BPF map.
    
    Test: manual
    
    Change-Id: I22e71f87b67668f7e733e4f215d93bf5b2c9380d

--- packages/Tethering/tests/unit/src/com/android/networkstack/tethering/TetheringTest.java
+++ packages/Tethering/tests/unit/src/com/android/networkstack/tethering/TetheringTest.java
@@ -196,6 +196,7 @@ public class TetheringTest {
     @Mock private ConnectivityManager mCm;
     @Mock private EthernetManager mEm;
     @Mock private TetheringNotificationUpdater mNotificationUpdater;
+    @Mock private BpfTetheringCoordinator mBpfTetheringCoordinator;
 
     private final MockIpServerDependencies mIpServerDependencies =
             spy(new MockIpServerDependencies());
@@ -326,6 +327,12 @@ public class TetheringTest {
         public void reset() {
             mUpstreamNetworkMonitorMasterSM = null;
             mIpv6CoordinatorNotifyList = null;
+        }
+
+        @Override
+        public BpfTetheringCoordinator getBpfTetheringCoordinator(Handler handler, INetd netd,
+                SharedLog log, BpfTetheringCoordinator.Dependencies deps) {
+            return mBpfTetheringCoordinator;
         }
 
         @Override
