b507317267b009e28f428a112e31fd736f574364
Chiachang Wang <chiachangwang@google.com>
2020-03-12 14:45:47 +0800

    Add KeepalivePacketDataTest
    Add test for constructor of KeepalivePacketData and
    InvalidPacketException
    
    Bug: 151109466
    Test: atest CtsNetTestCasesLatestSdk:android.net.KeepalivePacketDataTest
          on both Q and R devices
    Change-Id: Ic9db4b9b83ff96c87f41c5f381763c3ee8dd27c8

---
+++ tests/net/common/java/android/net/KeepalivePacketDataTest.kt
@@ -0,0 +1,142 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package android.net
+
+import android.net.InvalidPacketException.ERROR_INVALID_IP_ADDRESS
+import android.net.InvalidPacketException.ERROR_INVALID_PORT
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Assert.fail
+import android.os.Build
+import androidx.test.filters.SmallTest
+import androidx.test.runner.AndroidJUnit4
+import com.android.testutils.DevSdkIgnoreRule
+import com.android.testutils.DevSdkIgnoreRule.IgnoreUpTo
+import java.net.InetAddress
+import java.util.Arrays
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+const val TEST_SRC_PORT = 4243
+const val TEST_DST_PORT = 4244
+const val INVALID_PORT = 65537
+
+@RunWith(AndroidJUnit4::class)
+@SmallTest
+class KeepalivePacketDataTest {
+    @Rule @JvmField
+    val ignoreRule: DevSdkIgnoreRule = DevSdkIgnoreRule()
+
+    private val TESTBYTES = byteArrayOf(12, 31, 22, 44)
+    private val TEST_SRC_ADDRV4 = Address("198.168.0.2")
+    private val TEST_DST_ADDRV4 = Address("198.168.0.1")
+    private val TEST_ADDRV6 = Address("2001:db8::1")
+
+    private fun Address(addr: String): InetAddress = InetAddress.parseNumericAddress(addr)
+    private fun makeValidData() =
+            TestKeepalivePacketData(
+                    TEST_SRC_ADDRV4,
+                    TEST_SRC_PORT,
+                    TEST_DST_ADDRV4,
+                    TEST_DST_PORT,
+                    TESTBYTES)
+
+    // Add for test because constructor of KeepalivePacketData is protected.
+    internal inner class TestKeepalivePacketData(
+        srcAddress: InetAddress?,
+        srcPort: Int,
+        dstAddress: InetAddress?,
+        dstPort: Int,
+        data: ByteArray
+    ) : KeepalivePacketData(srcAddress, srcPort, dstAddress, dstPort, data)
+
+    @Test
+    @IgnoreUpTo(Build.VERSION_CODES.Q)
+    fun testConstructor() {
+        var data: TestKeepalivePacketData
+
+        try {
+            data = TestKeepalivePacketData(null, TEST_SRC_PORT, TEST_SRC_ADDRV4, TEST_DST_PORT,
+                    TESTBYTES)
+            fail("Null src address should cause exception")
+        } catch (e: InvalidPacketException) {
+            assertEquals(e.error, ERROR_INVALID_IP_ADDRESS)
+        }
+
+        try {
+            data = TestKeepalivePacketData(TEST_SRC_ADDRV4, TEST_SRC_PORT, null, TEST_DST_PORT,
+                    TESTBYTES)
+            fail("Null dst address should cause exception")
+        } catch (e: InvalidPacketException) {
+            assertEquals(e.error, ERROR_INVALID_IP_ADDRESS)
+        }
+
+        try {
+            data = TestKeepalivePacketData(TEST_SRC_ADDRV4, TEST_SRC_PORT, TEST_ADDRV6,
+                    TEST_DST_PORT, TESTBYTES)
+            fail("Ip family mismatched should cause exception")
+        } catch (e: InvalidPacketException) {
+            assertEquals(e.error, ERROR_INVALID_IP_ADDRESS)
+        }
+
+        try {
+            data = TestKeepalivePacketData(TEST_SRC_ADDRV4, INVALID_PORT, TEST_DST_ADDRV4,
+                    TEST_DST_PORT, TESTBYTES)
+            fail("Invalid srcPort should cause exception")
+        } catch (e: InvalidPacketException) {
+            assertEquals(e.error, ERROR_INVALID_PORT)
+        }
+
+        try {
+            data = TestKeepalivePacketData(TEST_SRC_ADDRV4, TEST_SRC_PORT, TEST_DST_ADDRV4,
+                    INVALID_PORT, TESTBYTES)
+            fail("Invalid dstPort should cause exception")
+        } catch (e: InvalidPacketException) {
+            assertEquals(e.error, ERROR_INVALID_PORT)
+        }
+    }
+
+    @Test
+    @IgnoreUpTo(Build.VERSION_CODES.Q)
+    fun testSrcAddress() {
+        assertEquals(TEST_SRC_ADDRV4, makeValidData().srcAddress)
+    }
+
+    @Test
+    @IgnoreUpTo(Build.VERSION_CODES.Q)
+    fun testDstAddress() {
+        assertEquals(TEST_DST_ADDRV4, makeValidData().dstAddress)
+    }
+
+    @Test
+    @IgnoreUpTo(Build.VERSION_CODES.Q)
+    fun testSrcPort() {
+        assertEquals(TEST_SRC_PORT, makeValidData().srcPort)
+    }
+
+    @Test
+    @IgnoreUpTo(Build.VERSION_CODES.Q)
+    fun testDstPort() {
+        assertEquals(TEST_DST_PORT, makeValidData().dstPort)
+    }
+
+    @Test
+    @IgnoreUpTo(Build.VERSION_CODES.Q)
+    fun testPacket() {
+        assertTrue(Arrays.equals(TESTBYTES, makeValidData().packet))
+    }
+}
