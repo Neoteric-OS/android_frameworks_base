f69af73af8de6eea25132d76e3098595c3bb7057
Cody Kesting <ckesting@google.com>
2020-04-07 16:07:33 -0700

    Set owner and administrator UIDs for test networks.
    This change sets the owner and administrator UIDs for test networks when
    their initial values match the UID for the app creating the test
    network. This ensures that apps registering test networks can only make
    themselves owners / administrators of the network.
    
    Bug: 153449964
    Test: atest NetworkAgentTest
    Change-Id: I3a974700aa1d83cb285295ed1de0aa263e2e5b58

--- core/java/android/net/NetworkCapabilities.java
+++ core/java/android/net/NetworkCapabilities.java
@@ -26,6 +26,7 @@ import android.annotation.SystemApi;
 import android.annotation.TestApi;
 import android.compat.annotation.UnsupportedAppUsage;
 import android.net.ConnectivityManager.NetworkCallback;
+import android.os.Binder;
 import android.os.Build;
 import android.os.Parcel;
 import android.os.Parcelable;
@@ -666,16 +667,42 @@ public final class NetworkCapabilities implements Parcelable {
      * restrictions.
      * @hide
      */
-    public void restrictCapabilitesForTestNetwork() {
+    public void restrictCapabilitesForTestNetwork(@Nullable NetworkCapabilities previous) {
         final long originalCapabilities = mNetworkCapabilities;
         final NetworkSpecifier originalSpecifier = mNetworkSpecifier;
         final int originalSignalStrength = mSignalStrength;
+        final int ownerUid = getOwnerUid();
+        final int[] administratorUids = getAdministratorUids();
         clearAll();
         // Reset the transports to only contain TRANSPORT_TEST.
         mTransportTypes = (1 << TRANSPORT_TEST);
         mNetworkCapabilities = originalCapabilities & TEST_NETWORKS_ALLOWED_CAPABILITIES;
         mNetworkSpecifier = originalSpecifier;
         mSignalStrength = originalSignalStrength;
+
+        // Only retain the owner and administrator UIDs if they match the app registering the test
+        // network.
+        if (previous == null) {
+            // The network is being registered. There is no previous NetworkCapabilities to compare
+            // against, so check the calling uid.
+            final int callingUid = Binder.getCallingUid();
+            if (callingUid == ownerUid) {
+                setOwnerUid(callingUid);
+            }
+            if (ArrayUtils.contains(administratorUids, callingUid)) {
+                setAdministratorUids(new int[] {callingUid});
+            }
+        } else {
+            // NetworkCapabilities are being updated. Only retain the uids if they match the
+            // previous values. If this is true, they implicitly match those of the app that
+            // registered the test network.
+            if (ownerUid == previous.getOwnerUid()) {
+                setOwnerUid(ownerUid);
+            }
+            if (Arrays.equals(administratorUids, previous.getAdministratorUids())) {
+                setAdministratorUids(administratorUids);
+            }
+        }
     }
 
     /**
