da00400e12b705abb93144357381669a9cecb31c
Tarundeep Singh <tarundeep.singh@ittiam.com>
2020-04-10 12:55:43 +0530

    Remove ro.hdmi.wake_on_hotplug
    wake_on_hotplug is not CEC specific
    
    Bug: 154122694
    Test: m services.core
    Change-Id: Ifd06566d958ef1b08aa84012366b4641ee5032f7

--- services/core/java/com/android/server/hdmi/HdmiCecLocalDevicePlayback.java
+++ services/core/java/com/android/server/hdmi/HdmiCecLocalDevicePlayback.java
@@ -43,9 +43,6 @@ import java.util.Locale;
 public class HdmiCecLocalDevicePlayback extends HdmiCecLocalDeviceSource {
     private static final String TAG = "HdmiCecLocalDevicePlayback";
 
-    private static final boolean WAKE_ON_HOTPLUG =
-            SystemProperties.getBoolean(Constants.PROPERTY_WAKE_ON_HOTPLUG, true);
-
     private static final boolean SET_MENU_LANGUAGE =
             SystemProperties.getBoolean(Constants.PROPERTY_SET_MENU_LANGUAGE, false);
 
@@ -147,9 +144,6 @@ public class HdmiCecLocalDevicePlayback extends HdmiCecLocalDeviceSource {
         assertRunOnServiceThread();
         mCecMessageCache.flushAll();
         // We'll not clear mIsActiveSource on the hotplug event to pass CETC 11.2.2-2 ~ 3.
-        if (WAKE_ON_HOTPLUG && connected && mService.isPowerStandbyOrTransient()) {
-            mService.wakeUp();
-        }
         if (!connected) {
             getWakeLock().release();
         }
