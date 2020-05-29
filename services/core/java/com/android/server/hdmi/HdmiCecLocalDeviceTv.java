6defaab890932fd97633c92cf90247a3288d13cc
daren.liao <daren.liao@mediatek.com>
2020-05-29 11:43:48 +0800

    Send SAM request to turn off SAM of AVR on standby
    [Description]
    2 known compatibiity issue
    Onkyo Amp
    Marantz SR5011
    
    When TV standby, these AVR will standby
    When TV resume, SAM of them are still ON
    and TV cannot turn on AVR in this case
    
    We should send SAM request with no parameter during standby
    so that TV could turn off SAM of AVR
    
    [Test Report]
    Tested on
     - Onkyo TX-RZ840
     - Sony soundbar
     - Yamaha X3500H
    DC off on 5 times PASS
    
    Change-Id: I701ed194d2e03282de684706bd2397c7a9cbd6ad

--- services/core/java/com/android/server/hdmi/HdmiCecLocalDeviceTv.java
+++ services/core/java/com/android/server/hdmi/HdmiCecLocalDeviceTv.java
@@ -1650,9 +1650,14 @@ final class HdmiCecLocalDeviceTv extends HdmiCecLocalDevice {
     @ServiceThreadOnly
     private void disableSystemAudioIfExist() {
         assertRunOnServiceThread();
-        if (getAvrDeviceInfo() == null) {
+        HdmiDeviceInfo avr = getAvrDeviceInfo();
+        if (avr == null) {
             return;
         }
+
+        HdmiLogger.debug("Send SystemAudioModeRequest to turn of SAM of AVR");
+        mService.sendCecCommand(HdmiCecMessageBuilder.buildSystemAudioModeRequest(
+                    mAddress, avr.getLogicalAddress(), Constants.PATH_INTERNAL, false));
 
         // Seq #31.
         removeAction(SystemAudioActionFromAvr.class);
