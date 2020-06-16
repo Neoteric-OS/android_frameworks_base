89f9c0b1ea97eb17e1abac9f472ba2ad15c9aa48
Sato Yutaka <yutaka.xb.sato@sonymobile.com>
2020-06-16 12:53:50 +0900

    The default wallpaper should not be set
    Symptom:
    When a default wallpaper resource is updated, the wallpaper
    set by user is overwritten.
    
    Root cause:
    Upon receiving DEVICE_CUSTOMIZATION_READY broadcast,
    WallpaperUpdateReceiver unconditionally updates the wallpaper to
    the default_wallpaper resource.
    
    Solution:
    Do not set a default wallpaper.
    Instead request ImageWallpaepr to redraw the wallpaper.
    
    Test: set a wallpaper and run
     adb shell am broadcast -a android.intent.action.DEVICE_CUSTOMIZATION_READY -f 0x01000000
    Bug: 159673283
    Change-Id: I385c28be84b9146e8620910258495f5d71b9c04e

--- services/core/java/com/android/server/WallpaperUpdateReceiver.java
+++ services/core/java/com/android/server/WallpaperUpdateReceiver.java
@@ -16,18 +16,16 @@
 
 package com.android.server;
 
-import android.annotation.RequiresPermission;
-import android.app.ActivityThread;
-import android.app.WallpaperInfo;
-import android.app.WallpaperManager;
 import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.os.AsyncTask;
-import android.os.ParcelFileDescriptor;
+import android.os.ServiceManager;
 import android.util.Slog;
+
+import com.android.server.wallpaper.WallpaperManagerService;
+
 
 /**
  * Receiver responsible for updating the wallpaper when the device
@@ -50,49 +48,13 @@ public class WallpaperUpdateReceiver extends BroadcastReceiver {
     }
 
     private void updateWallpaper() {
-        try {
-            ActivityThread currentActivityThread = ActivityThread.currentActivityThread();
-            Context uiContext = currentActivityThread.getSystemUiContext();
-            WallpaperManager wallpaperManager = WallpaperManager.getInstance(uiContext);
-            if (isUserSetWallpaper(wallpaperManager, uiContext)) {
-                Slog.i(TAG, "User has set wallpaper, skip to resetting");
-                return;
-            }
-            if (DEBUG) Slog.d(TAG, "Set customized default_wallpaper.");
-            Bitmap blank = Bitmap.createBitmap(1, 1, Bitmap.Config.ALPHA_8);
-            // set a blank wallpaper to force a redraw of default_wallpaper
-            wallpaperManager.setBitmap(blank);
-            wallpaperManager.setResource(com.android.internal.R.drawable.default_wallpaper);
-        } catch (Exception e) {
-            Slog.w(TAG, "Failed to customize system wallpaper." + e);
+        if (DEBUG) Slog.d(TAG, "Set customized default_wallpaper.");
+        WallpaperManagerService service =
+                (WallpaperManagerService) ServiceManager.getService(Context.WALLPAPER_SERVICE);
+        if (service == null) {
+            Slog.w(TAG, "WallpaperManagerService is not started");
+            return;
         }
-    }
-
-    /**
-     * A function to validate if users have set customized (live)wallpaper
-     * <p>
-     * return true if users have customized their wallpaper
-     **/
-    @RequiresPermission(android.Manifest.permission.READ_WALLPAPER_INTERNAL)
-    private boolean isUserSetWallpaper(WallpaperManager wm, Context context) {
-        WallpaperInfo info = wm.getWallpaperInfo();
-        if (info == null) {
-            //Image Wallpaper
-            ParcelFileDescriptor sysWallpaper =
-                    wm.getWallpaperFile(WallpaperManager.FLAG_SYSTEM);
-            ParcelFileDescriptor lockWallpaper =
-                    wm.getWallpaperFile(WallpaperManager.FLAG_LOCK);
-            if (sysWallpaper != null || lockWallpaper != null) {
-                return true;
-            }
-        } else {
-            //live wallpaper
-            ComponentName currCN = info.getComponent();
-            ComponentName defaultCN = WallpaperManager.getDefaultWallpaperComponent(context);
-            if (!currCN.equals(defaultCN)) {
-                return true;
-            }
-        }
-        return false;
+        service.requestUpdateImageWallpaper();
     }
 }
