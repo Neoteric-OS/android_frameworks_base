40d50f2b37e0ae2284d7e35d6ddceb5907a4d9b1
Eduardo Rocha <edro@google.com>
2020-07-16 18:35:06 +0000

    Fuzzer for libandroidfw
    Bug: 161463861
    Test: Builds and runs on host and device.
    
    Change-Id: I78b19376275dc5a29afcab48c70eea479bff8728

---
+++ libs/androidfw/fuzz/resourcefile_fuzzer/resourcefile_fuzzer.cpp
@@ -0,0 +1,21 @@
+#include <stddef.h>
+#include <stdint.h>
+#include <string.h>
+#include <string>
+#include <fuzzer/FuzzedDataProvider.h>
+#include <androidfw/ApkAssets.h>
+#include <androidfw/LoadedArsc.h>
+#include <memory>
+#include <androidfw/StringPiece.h>
+
+using android::ApkAssets;
+using android::LoadedArsc;
+using android::StringPiece;
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+
+  std::unique_ptr<const LoadedArsc> loaded_arsc = 
+    LoadedArsc::Load(StringPiece(reinterpret_cast<const char*>(data), size));
+
+  return 0;
+}
