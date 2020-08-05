1dd37a8946480b97ec6f243aaf73dcec7094697d
Yan Yan <evitayan@google.com>
2020-08-05 12:16:20 -0700

    Add new mandatory IPsec algorithms
    This CL adds new mandatory IPsec algorithms and allows
    OEM to enable them on old devices using resource overlay
    
    Bug: 161716062
    Test: FrameworksNetTests:IpSecAlgorithmTest
    Change-Id: Ib827b05ea27dbe61b14ad236e858f825293ed994

--- tests/net/java/android/net/IpSecAlgorithmTest.java
+++ tests/net/java/android/net/IpSecAlgorithmTest.java
@@ -16,9 +16,14 @@
 
 package android.net;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
 
+import android.content.res.Resources;
+import android.os.Build;
 import android.os.Parcel;
 
 import androidx.test.filters.SmallTest;
@@ -29,20 +34,30 @@ import org.junit.runner.RunWith;
 
 import java.util.AbstractMap.SimpleEntry;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Map.Entry;
 import java.util.Random;
+import java.util.Set;
 
 /** Unit tests for {@link IpSecAlgorithm}. */
 @SmallTest
 @RunWith(AndroidJUnit4.class)
 public class IpSecAlgorithmTest {
+    private static final int ALGO_MANDATORY_FOR_ALL_COUNT = 7;
+    private static final int ALGO_MANDATORY_SINCE_S_COUNT = 3;
 
     private static final byte[] KEY_MATERIAL;
+
+    private final Resources mMockResources = mock(Resources.class);
 
     static {
         KEY_MATERIAL = new byte[128];
         new Random().nextBytes(KEY_MATERIAL);
     };
+
+    private static byte[] generateKey(int keyLenInBits) {
+        return Arrays.copyOf(KEY_MATERIAL, keyLenInBits / 8);
+    }
 
     @Test
     public void testNoTruncLen() throws Exception {
@@ -53,7 +68,7 @@ public class IpSecAlgorithmTest {
                     new SimpleEntry<>(IpSecAlgorithm.AUTH_HMAC_SHA256, 256),
                     new SimpleEntry<>(IpSecAlgorithm.AUTH_HMAC_SHA384, 384),
                     new SimpleEntry<>(IpSecAlgorithm.AUTH_HMAC_SHA512, 512),
-                    new SimpleEntry<>(IpSecAlgorithm.AUTH_CRYPT_AES_GCM, 224)
+                    new SimpleEntry<>(IpSecAlgorithm.AUTH_CRYPT_AES_GCM, 224),
                 };
 
         // Expect auth and aead algorithms to throw errors if trunclen is omitted.
@@ -68,6 +83,52 @@ public class IpSecAlgorithmTest {
 
         // Ensure crypt works with no truncation length supplied.
         new IpSecAlgorithm(IpSecAlgorithm.CRYPT_AES_CBC, Arrays.copyOf(KEY_MATERIAL, 256 / 8));
+    }
+
+    private void checkAuthKeyAndTruncLenValidation(String algoName, int keyLen, int truncLen)
+            throws Exception {
+        new IpSecAlgorithm(algoName, generateKey(keyLen), truncLen);
+
+        try {
+            new IpSecAlgorithm(algoName, generateKey(keyLen));
+            fail("Expected exception on unprovided auth trunclen");
+        } catch (IllegalArgumentException pass) {
+        }
+
+        try {
+            new IpSecAlgorithm(algoName, generateKey(keyLen + 8), truncLen);
+            fail("Invalid key length not validated");
+        } catch (IllegalArgumentException pass) {
+        }
+
+        try {
+            new IpSecAlgorithm(algoName, generateKey(keyLen), truncLen + 1);
+            fail("Invalid truncation length not validated");
+        } catch (IllegalArgumentException pass) {
+        }
+    }
+
+    private void checkCryptKeyLenValidation(String algoName, int keyLen) throws Exception {
+        new IpSecAlgorithm(algoName, generateKey(keyLen));
+
+        try {
+            new IpSecAlgorithm(algoName, generateKey(keyLen + 8));
+            fail("Invalid key length not validated");
+        } catch (IllegalArgumentException pass) {
+        }
+    }
+
+    @Test
+    public void testValidationForAlgosAddedInS() throws Exception {
+        if (Build.VERSION.FIRST_SDK_INT <= Build.VERSION_CODES.R) {
+            return;
+        }
+
+        for (int len : new int[] {160, 224, 288}) {
+            checkCryptKeyLenValidation(IpSecAlgorithm.CRYPT_AES_CTR, len);
+        }
+        checkAuthKeyAndTruncLenValidation(IpSecAlgorithm.AUTH_AES_XCBC, 128, 96);
+        checkAuthKeyAndTruncLenValidation(IpSecAlgorithm.AUTH_CRYPT_CHACHA20_POLY1305, 288, 128);
     }
 
     @Test
@@ -126,5 +187,56 @@ public class IpSecAlgorithmTest {
         IpSecAlgorithm fin = IpSecAlgorithm.CREATOR.createFromParcel(p);
         assertTrue("Parcel/Unparcel failed!", IpSecAlgorithm.equals(init, fin));
         p.recycle();
+    }
+
+    @Test
+    public void testGetSupportedAlgorithms() throws Exception {
+        int mandatoryAlgoCount = ALGO_MANDATORY_FOR_ALL_COUNT;
+
+        if (Build.VERSION.FIRST_SDK_INT > Build.VERSION_CODES.R) {
+            mandatoryAlgoCount += ALGO_MANDATORY_SINCE_S_COUNT;
+        }
+
+        assertEquals(
+                "Missing mandatory algorithm(s) in supported algorithm list",
+                mandatoryAlgoCount,
+                IpSecAlgorithm.getSupportedAlgorithms().size());
+    }
+
+    @Test
+    public void testLoadOptionalAlgos() throws Exception {
+        if (Build.VERSION.FIRST_SDK_INT > Build.VERSION_CODES.R) {
+            return;
+        }
+
+        final String[] optionalAlgos =
+                new String[] {
+                    IpSecAlgorithm.CRYPT_AES_CTR,
+                    IpSecAlgorithm.AUTH_AES_XCBC,
+                    IpSecAlgorithm.AUTH_CRYPT_CHACHA20_POLY1305
+                };
+
+        doReturn(optionalAlgos)
+                .when(mMockResources)
+                .getStringArray(com.android.internal.R.array.config_optionalIpSecAlgorithms);
+
+        final Set<String> enabledAlgos = IpSecAlgorithm.loadOptionalAlgos(mMockResources);
+        final Set<String> expectedAlgos = new HashSet<>(Arrays.asList(optionalAlgos));
+
+        assertEquals(expectedAlgos, enabledAlgos);
+    }
+
+    @Test
+    public void testLoadOptionalAlgosWithInvalidAlgo() throws Exception {
+        doReturn(new String[] {"BestAlgorithm"})
+                .when(mMockResources)
+                .getStringArray(com.android.internal.R.array.config_optionalIpSecAlgorithms);
+
+        try {
+            IpSecAlgorithm.loadOptionalAlgos(mMockResources);
+            fail("Expected to fail due to invalid algorithm name");
+        } catch (Exception expected) {
+
+        }
     }
 }
