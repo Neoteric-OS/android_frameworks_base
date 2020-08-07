d82625e0c6e43ead929d8276a923fb940d6ca3d0
Mohammad Samiul Islam <samiul@google.com>
2020-08-07 11:24:49 +0100

    Switch InstallUtilsHost as static_lib dependency on all targets
    Bug: 160296318
    Test: presubmit
    Change-Id: I4a3a141b1a5fc1722e673a163e6df9bd9f9df0b2
    Merged-In: I4a3a141b1a5fc1722e673a163e6df9bd9f9df0b2
    (cherry picked from commit 6e0cfdad7c12034ee3fb359b8755f2ac437e7470)

--- tests/StagedInstallTest/src/com/android/tests/stagedinstallinternal/host/StagedInstallInternalTest.java
+++ tests/StagedInstallTest/src/com/android/tests/stagedinstallinternal/host/StagedInstallInternalTest.java
@@ -21,7 +21,8 @@ import static com.google.common.truth.Truth.assertThat;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assume.assumeTrue;
 
-import com.android.cts.install.lib.host.InstallUtilsHost;
+import android.cts.install.lib.host.InstallUtilsHost;
+
 import com.android.ddmlib.Log;
 import com.android.tests.util.ModuleTestUtils;
 import com.android.tradefed.testtype.DeviceJUnit4ClassRunner;
