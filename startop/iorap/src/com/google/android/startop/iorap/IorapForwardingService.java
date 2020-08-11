9630bf14e9234bec6fd1a6295c2292211293ec23
yawanng <yawanng@google.com>
2020-08-11 20:51:51 +0000

    IorapForwardingService: Suppress error log
    Bug: 160381575
    Test: make
    Change-Id: Iee7506205edc2863c76d0dae0568297565def3df

--- startop/iorap/src/com/google/android/startop/iorap/IorapForwardingService.java
+++ startop/iorap/src/com/google/android/startop/iorap/IorapForwardingService.java
@@ -386,7 +386,7 @@ public class IorapForwardingService extends SystemService {
             throw new AssertionError("unexpected remote error", t);
         } else {
             // Log to wtf which gets sent to dropbox, and in system_server this does not crash.
-            Log.wtf(TAG, t);
+            Log.w(TAG, t.getMessage());
         }
     }
 }
