1aa188bed548bdaa36fbdeff5d4c6ea9b145d4e8
Steven Moreland <smoreland@google.com>
2020-09-03 19:51:19 +0000

    libhwui: hwuimacro power test scripts
    May not merge these, would need to FIXME:
    - get arbitrary 'rock bottom' scripts from power teams
    - various cleanup
    
    Basically, this creates an init service which runs hwuimacro repeatedly
    for 30min and measures battery level before and after.
    
    For initial power measurements for heap initialization.
    
    Bug: 131355925
    Test: use scripts to run power tests
    Change-Id: If491cbc627744857764136f894dd7941adc5dc0b

---
+++ libs/hwui/tests/power/power-test.sh
@@ -0,0 +1,52 @@
+#
+# Copyright (C) 2020 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+
+
+function charge-level() {
+    echo "$(cat /sys/class/power_supply/battery/charge_counter) mAh"
+}
+
+function power-test() {
+    OUT=/data/power-test-output.txt
+    JUNK=/data/power-test-junk.txt
+
+    echo "START" > $OUT
+    echo "START" > $JUNK
+
+    getprop ro.build.fingerprint >> $OUT
+    date >> $OUT
+    echo "INITIAL CHARGE LEVEL:" >> $OUT
+    charge-level >> $OUT
+
+    local now_time=$(date +"%s")
+    local end_time=$(($now_time + 60 * 30))
+
+    while [[ $(date +"%s") -lt $end_time ]]; do
+        /data/benchmarktest64/hwuimacro/hwuimacro >> $JUNK 2>&1
+    done
+
+    date >> $OUT
+    echo "FINAL CHARGE LEVEL:" >> $OUT
+    charge-level >> $OUT
+
+    echo "END" >> $OUT
+    echo "END" >> $JUNK
+}
+
+power-test
+
