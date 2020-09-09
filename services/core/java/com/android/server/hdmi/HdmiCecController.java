ddceb26ae2ee9c27f63809d00eee0bcb7144b9cc
Marvin Ramin <marvinramin@google.com>
2020-09-09 10:56:28 +0200

    Reconnect CEC HAL callback
    When the HAL service crashes, reconnect to the HAL with retries and
    ensure that the callback is reconnected as well.
    
    Bug: 167649125
    Test: make; flash; kill cec hal process and ensure CEC messages are
    still sent and received afterwards
    Change-Id: I6a001e207f1c1a4dd4cb8b6c2e2dea689c9d9caa
    
    Change-Id: I5686f5dcf50c8f8bf67533596c39ac697f1e7263

--- services/core/java/com/android/server/hdmi/HdmiCecController.java
+++ services/core/java/com/android/server/hdmi/HdmiCecController.java
@@ -712,6 +712,7 @@ final class HdmiCecController {
         private IHdmiCec mHdmiCec;
         private final Object mLock = new Object();
         private int mPhysicalAddress = INVALID_PHYSICAL_ADDRESS;
+        private HdmiCecCallback mCallback;
 
         @Override
         public String nativeInit() {
@@ -720,7 +721,7 @@ final class HdmiCecController {
 
         boolean connectToHal() {
             try {
-                mHdmiCec = IHdmiCec.getService();
+                mHdmiCec = IHdmiCec.getService(true);
                 try {
                     mHdmiCec.linkToDeath(this, HDMI_CEC_HAL_DEATH_COOKIE);
                 } catch (RemoteException e) {
@@ -735,6 +736,7 @@ final class HdmiCecController {
 
         @Override
         public void setCallback(HdmiCecCallback callback) {
+            mCallback = callback;
             try {
                 mHdmiCec.setCallback(callback);
             } catch (RemoteException e) {
@@ -874,6 +876,10 @@ final class HdmiCecController {
             if (cookie == HDMI_CEC_HAL_DEATH_COOKIE) {
                 HdmiLogger.error(TAG, "Service died cokkie : " + cookie + "; reconnecting");
                 connectToHal();
+                // Reconnect the callback
+                if (mCallback != null) {
+                    setCallback(mCallback);
+                }
             }
         }
 
