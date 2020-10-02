ba6b790d05d4d3c3e03018c41d727baf62bada1e
Benedict Wong <benedictwong@google.com>
2020-10-01 17:50:14 -0700

    Implement DnsResolution state
    This change implements VCN DNS resolution using the DnsResolver API. DNS
    resolution will always be performed if a previous session was lost.
    
    Bug: 165827287
    Test: atest FrameworksVcnTests
    Change-Id: I8fdd82aeee78ebbfea9e8597943eec7aafecf7ec

--- tests/vcn/java/com/android/server/vcn/VcnGatewayConnectionDisconnectedStateTest.java
+++ tests/vcn/java/com/android/server/vcn/VcnGatewayConnectionDisconnectedStateTest.java
@@ -17,95 +17,25 @@
 package com.android.server.vcn;
 
 
-import static com.android.server.vcn.UnderlyingNetworkTracker.UnderlyingNetworkRecord;
-import static com.android.server.vcn.VcnTestUtils.setupIpSecManager;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.mock;
-
-import android.annotation.NonNull;
-import android.content.Context;
-import android.net.IpSecManager;
-import android.net.IpSecTunnelInterfaceResponse;
-import android.net.LinkProperties;
-import android.net.Network;
-import android.net.NetworkCapabilities;
-import android.net.vcn.VcnGatewayConnectionConfig;
-import android.net.vcn.VcnGatewayConnectionConfigTest;
-import android.os.ParcelUuid;
-import android.os.test.TestLooper;
 
 import androidx.test.filters.SmallTest;
 import androidx.test.runner.AndroidJUnit4;
 
-import com.android.server.IpSecService;
-
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import java.util.UUID;
 
 /** Tests for VcnGatewayConnection.DisconnectedState */
 @RunWith(AndroidJUnit4.class)
 @SmallTest
-public class VcnGatewayConnectionDisconnectedStateTest {
-    private static final ParcelUuid TEST_SUB_GRP = new ParcelUuid(UUID.randomUUID());
-    private static final int TEST_IPSEC_RESOURCE_ID = 1;
-    private static final String TEST_IPSEC_TUNNEL_IFACE = "IPSEC_IFACE";
-
-    @NonNull private final Context mContext;
-    @NonNull private final TestLooper mTestLooper;
-    @NonNull private final VcnNetworkProvider mVcnNetworkProvider;
-    @NonNull private final VcnContext mVcnContext;
-    @NonNull private final VcnGatewayConnectionConfig mConfig;
-    @NonNull private final VcnGatewayConnection.Dependencies mDeps;
-
-    @NonNull private final IpSecService mIpSecSvc;
-    @NonNull private final IpSecManager mIpSecMgr;
-
-    private VcnGatewayConnection mGatewayConnection;
-
-    public VcnGatewayConnectionDisconnectedStateTest() {
-        mContext = mock(Context.class);
-        mTestLooper = new TestLooper();
-        mVcnNetworkProvider = mock(VcnNetworkProvider.class);
-        mVcnContext = mock(VcnContext.class);
-        mConfig = VcnGatewayConnectionConfigTest.buildTestConfig();
-        mDeps = mock(VcnGatewayConnection.Dependencies.class);
-
-        mIpSecSvc = mock(IpSecService.class);
-        mIpSecMgr = setupIpSecManager(mContext, mIpSecSvc);
-
-        doReturn(mContext).when(mVcnContext).getContext();
-        doReturn(mTestLooper.getLooper()).when(mVcnContext).getLooper();
-        doReturn(mVcnNetworkProvider).when(mVcnContext).getVcnNetworkProvider();
-    }
-
-    @Before
-    public void setUp() throws Exception {
-        IpSecTunnelInterfaceResponse resp =
-                new IpSecTunnelInterfaceResponse(
-                        IpSecManager.Status.OK, TEST_IPSEC_RESOURCE_ID, TEST_IPSEC_TUNNEL_IFACE);
-        doReturn(resp).when(mIpSecSvc).createTunnelInterface(any(), any(), any(), any(), any());
-
-        mGatewayConnection = new VcnGatewayConnection(mVcnContext, TEST_SUB_GRP, mConfig, mDeps);
-    }
-
+public class VcnGatewayConnectionDisconnectedStateTest extends VcnGatewayConnectionTestBase {
     @Test
     public void testNetworkChangesTriggerStateTransitions() throws Exception {
         mGatewayConnection.transitionTo(mGatewayConnection.mDisconnectedState);
         mTestLooper.dispatchAll();
 
-        final UnderlyingNetworkRecord underlying =
-                new UnderlyingNetworkRecord(
-                        new Network(0),
-                        new NetworkCapabilities(),
-                        new LinkProperties(),
-                        false /* blocked */);
-        mGatewayConnection.onSelectedUnderlyingNetworkChanged(underlying);
+        mGatewayConnection.onSelectedUnderlyingNetworkChanged(TEST_UNDERLYING_NETWORK_RECORD_1);
         mTestLooper.dispatchAll();
 
         assertEquals(mGatewayConnection.mDnsResolutionState, mGatewayConnection.getCurrentState());
