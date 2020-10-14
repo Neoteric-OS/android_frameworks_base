6ce22fda8c0be6e5e1a2a5238bf58c4b14023036
Chienyuan <chienyuanhuang@google.com>
2020-10-14 10:45:19 +0800

    PAN: indicate tethring state when connecion state change
    Tag: #feature
    Bug: 156573624
    Test: manual
    Change-Id: I48906e265379ecb74fa1833ec31059a2e186468c

--- core/java/android/bluetooth/BluetoothPan.java
+++ core/java/android/bluetooth/BluetoothPan.java
@@ -65,6 +65,7 @@ public final class BluetoothPan implements BluetoothProfile {
      * <li> {@link BluetoothDevice#EXTRA_DEVICE} - The remote device. </li>
      * <li> {@link #EXTRA_LOCAL_ROLE} - Which local role the remote device is
      * bound to. </li>
+     * <li> {@link #EXTRA_TETHERING_STATUS} - Expected tethering status. </li>
      * </ul>
      *
      * <p>{@link #EXTRA_STATE} or {@link #EXTRA_PREVIOUS_STATE} can be any of
@@ -131,6 +132,31 @@ public final class BluetoothPan implements BluetoothProfile {
      * The local device is acting as a PAN User.
      */
     public static final int LOCAL_PANU_ROLE = 2;
+
+    /**
+     * Extra for {@link #ACTION_CONNECTION_STATE_CHANGED} intent
+     * Indicate expected tethering state
+     * Tethering module should listen this to determin tethering state
+     * It can be one of {@link #TETHERING_STATUS_START} or {@link #TETHERING_STATUS_STOP}.
+     */
+    @SuppressLint("ActionValue")
+    public static final String EXTRA_TETHERING_STATUS =
+            "android.bluetooth.pan.extra.TETHERING_STATUS";
+
+    /** @hide **/
+    @IntDef({TETHERING_STATUS_NONE, TETHERING_STATUS_START, TETHERING_STATUS_STOP})
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface TetheringStatus{}
+    public static final int TETHERING_STATUS_NONE = 0;
+    /**
+     * Switch tethering status to start
+     */
+    public static final int TETHERING_STATUS_START = 1;
+
+    /**
+     * Switch tethering status to stop
+     */
+    public static final int TETHERING_STATUS_STOP = 2;
 
     /** @hide */
     @IntDef({PAN_ROLE_NONE, REMOTE_NAP_ROLE, REMOTE_PANU_ROLE})
