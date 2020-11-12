cb87ca077edb04cb33c1ea78d4f9bf85631228d7
Hall Liu <hallliu@google.com>
2020-11-11 16:53:04 -0800

    Move usage of Runnable::run to TelephonyUtils
    Using Runnable::run in com.android.internal.telephony from this
    directory will conflict with any usages from frameworks/opt/telephony
    since the compiles apparently scopes method references at the package
    level. Move it to TelephonyUtils (which is in the .util sub-package) to
    avoid the conflict.
    
    Bug: 156779270
    Test: build
    Change-Id: I3471162eecde869aea66533de0355b3214859098

--- telephony/common/com/android/internal/telephony/util/TelephonyUtils.java
+++ telephony/common/com/android/internal/telephony/util/TelephonyUtils.java
@@ -44,6 +44,8 @@ public final class TelephonyUtils {
     public static boolean IS_USER = "user".equals(android.os.Build.TYPE);
     public static boolean IS_DEBUGGABLE = SystemProperties.getInt("ro.debuggable", 0) == 1;
 
+    public static final Executor DIRECT_EXECUTOR = Runnable::run;
+
     /**
      * Verify that caller holds {@link android.Manifest.permission#DUMP}.
      *
