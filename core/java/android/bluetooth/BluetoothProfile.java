eabc37102eafacc29b9eb5ae25747556543e5de6
Grzegorz Kołodziejczyk <grzegorz.kolodziejczyk@codecoup.pl>
2020-11-16 11:43:45 +0000

    Add Bluetooth LE Audio Profile (3/3)
    This is boilerplate code for Bluetooth LE Audio profile
    
    Bug: 150670922
    Test: compilation
    Tag: #feature
    Change-Id: Iadc3af12fd8b2808db2f4e933a1906a819824ade

--- core/java/android/bluetooth/BluetoothProfile.java
+++ core/java/android/bluetooth/BluetoothProfile.java
@@ -206,12 +206,18 @@ public interface BluetoothProfile {
     int HEARING_AID = 21;
 
     /**
+     * LE Audio Device
+     *
+     */
+    int LE_AUDIO = 22;
+
+    /**
      * Max profile ID. This value should be updated whenever a new profile is added to match
      * the largest value assigned to a profile.
      *
      * @hide
      */
-    int MAX_PROFILE_ID = 21;
+    int MAX_PROFILE_ID = 22;
 
     /**
      * Default priority for devices that we try to auto-connect to and
