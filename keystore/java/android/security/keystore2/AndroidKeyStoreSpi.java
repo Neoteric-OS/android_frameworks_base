11be446c1199c37b2a90c67c0923b6ade1a4bc8a
Janis Danisevskis <jdanis@google.com>
2020-11-17 09:50:06 -0800

    Keystore 2.0 SPI: Fix typos in comments.
    Test: N/A
    Change-Id: I2df8fedc89b9c23925602385459e306a2da70a6a

--- keystore/java/android/security/keystore2/AndroidKeyStoreSpi.java
+++ keystore/java/android/security/keystore2/AndroidKeyStoreSpi.java
@@ -251,9 +251,9 @@ public class AndroidKeyStoreSpi extends KeyStoreSpi {
         }
 
 
-        // TODO add modification time to key metadata.
+        // TODO add creation time to key metadata.
         return null;
-        // if (response.metadata.modificationTime == -1) {
+        // if (response.metadata.icationTime == -1) {
         //     return null;
         // }
         // return new Date(response.metadata.modificationTime);
@@ -341,7 +341,7 @@ public class AndroidKeyStoreSpi extends KeyStoreSpi {
         } else if (param instanceof KeyProtection) {
             spec = (KeyProtection) param;
             if (spec.isCriticalToDeviceEncryption()) {
-                // This key is should not be bound to the LSKF even if it is auth bound.
+                // This key should not be bound to the LSKF even if it is auth bound.
                 // This indicates that this key is used in the derivation for of the
                 // master key, that is used for the LSKF binding of other auth bound
                 // keys. This breaks up a circular dependency while retaining logical
