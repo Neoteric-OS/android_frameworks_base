11212a28fa14c2d83fc1b93b8249010a8706a4f9
Janis Danisevskis <jdanis@google.com>
2020-11-18 10:33:03 -0800

    Fix Broken test: libcore.java.security.ProviderTest#test_Provider_Properties
    Test: CtsLibcoreTestCases:libcore.java.security.ProviderTest#test_Provider_Properties
    Bug: 173480441
    Change-Id: I188cd778a25d221991280eb461a7ec052503790c

--- keystore/java/android/security/keystore/AndroidKeyStoreProvider.java
+++ keystore/java/android/security/keystore/AndroidKeyStoreProvider.java
@@ -84,7 +84,7 @@ public class AndroidKeyStoreProvider extends Provider {
 
         // java.security.KeyStore
         put("KeyStore.AndroidKeyStore", PACKAGE_NAME + ".AndroidKeyStoreSpi");
-        put("alg.alias.KeyStore.AndroidKeyStoreLegacy", "AndroidKeyStore");
+        put("alg.alias.KeyStore.AndroidKeyStoreLegacy", "KeyStore.AndroidKeyStore");
 
         // java.security.KeyPairGenerator
         put("KeyPairGenerator.EC", PACKAGE_NAME + ".AndroidKeyStoreKeyPairGeneratorSpi$EC");
