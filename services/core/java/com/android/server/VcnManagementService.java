474e8c0cfb1c22b79a5cfd10948c9038603d27b1
Cody Kesting <ckesting@google.com>
2020-12-15 15:27:49 -0800

    Define add and remove for VCN policy listeners.
    Bug: 175729529
    Test: atest FrameworksVcnTests
    Change-Id: I61dd789fe4a4ebd1ca95866494d0e42e97c202cc

--- services/core/java/com/android/server/VcnManagementService.java
+++ services/core/java/com/android/server/VcnManagementService.java
@@ -24,6 +24,7 @@ import android.net.ConnectivityManager;
 import android.net.NetworkProvider;
 import android.net.NetworkRequest;
 import android.net.vcn.IVcnManagementService;
+import android.net.vcn.IVcnUnderlyingNetworkPolicyListener;
 import android.net.vcn.VcnConfig;
 import android.os.Binder;
 import android.os.Handler;
@@ -343,6 +344,22 @@ public class VcnManagementService extends IVcnManagementService.Stub {
         synchronized (mLock) {
             return Collections.unmodifiableMap(mConfigs);
         }
+    }
+
+    /** Adds the provided listener for receiving VcnUnderlyingNetworkPolicy updates. */
+    @Override
+    public void addVcnUnderlyingNetworkPolicyListener(
+            IVcnUnderlyingNetworkPolicyListener listener) {
+        // TODO(b/175739863): implement policy listener registration
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    /** Removes the provided listener from receiving VcnUnderlyingNetworkPolicy updates. */
+    @Override
+    public void removeVcnUnderlyingNetworkPolicyListener(
+            IVcnUnderlyingNetworkPolicyListener listener) {
+        // TODO(b/175739863): implement policy listener unregistration
+        throw new UnsupportedOperationException("Not yet implemented");
     }
 
     /**
