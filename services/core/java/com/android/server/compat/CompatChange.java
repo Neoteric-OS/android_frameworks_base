cc30b342880c34b983d2ab3203dd11280dbf85bb
Andrei Onea <andreionea@google.com>
2020-12-16 16:07:57 +0000

    Do not enable changes newer than the current sdk
    Do not allow enabling compat changes gated on an sdk version newer than
    the current one.
    
    Test: atest OverrideValidatorImplTest
    Bug: 175790344
    Change-Id: Idd7d86f96a380bfe01940f356d38ad49d3f27678

--- services/core/java/com/android/server/compat/CompatChange.java
+++ services/core/java/com/android/server/compat/CompatChange.java
@@ -20,6 +20,7 @@ import android.annotation.Nullable;
 import android.compat.annotation.ChangeId;
 import android.compat.annotation.EnabledSince;
 import android.content.pm.ApplicationInfo;
+import android.os.Build;
 
 import com.android.internal.compat.CompatibilityChangeInfo;
 import com.android.server.compat.config.Change;
@@ -208,7 +209,10 @@ public final class CompatChange extends CompatibilityChangeInfo {
             return false;
         }
         if (getEnableSinceTargetSdk() != -1) {
-            return app.targetSdkVersion >= getEnableSinceTargetSdk();
+            // If the change is gated by a platform version newer than the one currently installed
+            // on the device, disregard the app's target sdk version.
+            int compareSdk = Math.min(app.targetSdkVersion, Build.VERSION.SDK_INT);
+            return compareSdk >= getEnableSinceTargetSdk();
         }
         return true;
     }
