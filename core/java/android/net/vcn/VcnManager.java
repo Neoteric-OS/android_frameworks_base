a491159949c7f88ff1d1d32129f986a2eebdd23d
Cody Kesting <ckesting@google.com>
2020-12-17 13:57:41 -0800

    Allow apps to request underlying network policies.
    This CL updates VcnManager to allow apps with permission NETWORK_FACTORY
    to request the current VcnUnderlyingNetworkPolicy given a
    NetworkCapabilities and LinkProperties.
    
    Bug: 175900686
    Test: atest FrameworksVcnTests
    Change-Id: I16416e619bdb03630582f5660260b0090730e9eb

--- core/java/android/net/vcn/VcnManager.java
+++ core/java/android/net/vcn/VcnManager.java
@@ -21,6 +21,8 @@ import android.annotation.NonNull;
 import android.annotation.RequiresPermission;
 import android.annotation.SystemService;
 import android.content.Context;
+import android.net.LinkProperties;
+import android.net.NetworkCapabilities;
 import android.os.ParcelUuid;
 import android.os.RemoteException;
 import android.os.ServiceSpecificException;
@@ -62,7 +64,7 @@ import java.util.concurrent.Executor;
  * @hide
  */
 @SystemService(Context.VCN_MANAGEMENT_SERVICE)
-public final class VcnManager {
+public class VcnManager {
     @NonNull private static final String TAG = VcnManager.class.getSimpleName();
 
     @VisibleForTesting
@@ -216,6 +218,31 @@ public final class VcnManager {
 
         try {
             mService.removeVcnUnderlyingNetworkPolicyListener(binder);
+        } catch (RemoteException e) {
+            throw e.rethrowFromSystemServer();
+        }
+    }
+
+    /**
+     * Get the {@link VcnUnderlyingNetworkPolicy} that matches the specified {@link
+     * NetworkCapabilities} and {@link LinkProperties}.
+     *
+     * @param networkCapabilities
+     * @param linkProperties
+     * @throws SecurityException if the caller does not have permission NETWORK_FACTORY
+     * @return
+     * @hide
+     */
+    @NonNull
+    @RequiresPermission(android.Manifest.permission.NETWORK_FACTORY)
+    public VcnUnderlyingNetworkPolicy getUnderlyingNetworkPolicy(
+            @NonNull NetworkCapabilities networkCapabilities,
+            @NonNull LinkProperties linkProperties) {
+        requireNonNull(networkCapabilities, "networkCapabilities must not be null");
+        requireNonNull(linkProperties, "linkProperties must not be null");
+
+        try {
+            return mService.getUnderlyingNetworkPolicy(networkCapabilities, linkProperties);
         } catch (RemoteException e) {
             throw e.rethrowFromSystemServer();
         }
