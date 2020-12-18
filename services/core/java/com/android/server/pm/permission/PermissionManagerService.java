ae9aee6788f89ab9c8263cda0da580e923ec0c27
Isak Lilja <isak.lilja@sony.com>
2020-12-18 14:48:05 +0100

    Don't attempt permission grant to users that are to be removed
    Don't call DefaultPermissionGrantPolicy#grantDefaultPermissions
    in systemReady for a user that is about to be removed. Partial
    and ephemeral users have not been cleaned up yet.
    
    This avoids a possible loopcrash at boot.
    
    Bug: 175955648
    Test: 1. adb shell pm create-user --ephemeral Test
          2. adb shell am switch-user <userid>
          3. Add a Google account
          4. adb reboot
          The device should reboot as expected
    Test: atest CtsMultiUserHostTestCases:EphemeralTest
    Change-Id: I5f4d96f98df9187cf3bc78699bd434aeb36f8949

--- services/core/java/com/android/server/pm/permission/PermissionManagerService.java
+++ services/core/java/com/android/server/pm/permission/PermissionManagerService.java
@@ -83,6 +83,7 @@ import android.content.pm.PackageParser;
 import android.content.pm.ParceledListSlice;
 import android.content.pm.PermissionGroupInfo;
 import android.content.pm.PermissionInfo;
+import android.content.pm.UserInfo;
 import android.content.pm.parsing.component.ParsedPermission;
 import android.content.pm.parsing.component.ParsedPermissionGroup;
 import android.content.pm.permission.SplitPermissionInfoParcelable;
@@ -4651,8 +4652,16 @@ public class PermissionManagerService extends IPermissionManager.Stub {
         mPermissionControllerManager = mContext.getSystemService(PermissionControllerManager.class);
         mPermissionPolicyInternal = LocalServices.getService(PermissionPolicyInternal.class);
 
+        UserManagerService userManagerService = UserManagerService.getInstance();
         int[] grantPermissionsUserIds = EMPTY_INT_ARRAY;
-        for (int userId : UserManagerService.getInstance().getUserIds()) {
+        for (int userId : userManagerService.getUserIds()) {
+            UserInfo ui = userManagerService.getUserInfo(userId);
+            boolean isUserToBeRemoved =
+                    ui.partial || ui.guestToRemove || (ui.isEphemeral() && !ui.preCreated);
+            if (isUserToBeRemoved && userId != UserHandle.USER_SYSTEM) {
+                // User is about to be removed.
+                continue;
+            }
             if (mPackageManagerInt.isPermissionUpgradeNeeded(userId)) {
                 grantPermissionsUserIds = ArrayUtils.appendInt(
                         grantPermissionsUserIds, userId);
