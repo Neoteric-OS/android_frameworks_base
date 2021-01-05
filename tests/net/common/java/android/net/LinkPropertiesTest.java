b80b56344a1e92a297de9e84e4f63d8c065239c4
Taras Antoshchuk <tantoshchuk@google.com>
2021-01-05 13:56:58 +0100

    Filter out excluded routes in LinkProperties
    Gate presence of excluded routes in LinkProperties on target sdk S.
    Add gating unit tests.
    
    Bug: 161114046
    Bug: 157330618
    Test: atest LinkPropertiesTest
    Change-Id: I07d67851e133a6e1ba43f422da95b5d87d26cd70

--- tests/net/common/java/android/net/LinkPropertiesTest.java
+++ tests/net/common/java/android/net/LinkPropertiesTest.java
@@ -30,7 +30,10 @@ import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
+import static org.junit.Assume.assumeTrue;
 
+import android.app.compat.CompatChanges;
+import android.compat.testing.PlatformCompatChangeRule;
 import android.net.LinkProperties.ProvisioningChange;
 import android.os.Build;
 import android.system.OsConstants;
@@ -44,6 +47,9 @@ import com.android.net.module.util.LinkPropertiesUtils.CompareResult;
 import com.android.testutils.DevSdkIgnoreRule;
 import com.android.testutils.DevSdkIgnoreRule.IgnoreAfter;
 import com.android.testutils.DevSdkIgnoreRule.IgnoreUpTo;
+
+import libcore.junit.util.compat.CoreCompatChangeRule.DisableCompatChanges;
+import libcore.junit.util.compat.CoreCompatChangeRule.EnableCompatChanges;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -63,6 +69,9 @@ import java.util.Set;
 public class LinkPropertiesTest {
     @Rule
     public final DevSdkIgnoreRule ignoreRule = new DevSdkIgnoreRule();
+
+    @Rule
+    public final PlatformCompatChangeRule compatChangeRule = new PlatformCompatChangeRule();
 
     private static final InetAddress ADDRV4 = address("75.208.6.1");
     private static final InetAddress ADDRV6 = address("2001:0db8:85a3:0000:0000:8a2e:0370:7334");
@@ -1012,7 +1021,6 @@ public class LinkPropertiesTest {
     @Test @IgnoreUpTo(Build.VERSION_CODES.Q)
     public void testLinkPropertiesParcelable() throws Exception {
         final LinkProperties source = makeLinkPropertiesForParceling();
-
         source.setWakeOnLanSupported(true);
         source.setCaptivePortalApiUrl(CAPPORT_API_URL);
         source.setCaptivePortalData((CaptivePortalData) getCaptivePortalData());
@@ -1125,6 +1133,38 @@ public class LinkPropertiesTest {
 
         lp.addRoute(new RouteInfo(GATEWAY61));
         assertTrue(lp.hasIPv6DefaultRoute());
+    }
+
+    @Test @IgnoreUpTo(Build.VERSION_CODES.R)
+    @EnableCompatChanges({LinkProperties.EXCLUDED_ROUTES})
+    public void testExcludedRoutesEnabled() {
+        final LinkProperties lp = new LinkProperties();
+        assertEquals(0, lp.getRoutes().size());
+
+        lp.addRoute(new RouteInfo(new IpPrefix(ADDRV4, 0), RTN_UNREACHABLE));
+        assertEquals(1, lp.getRoutes().size());
+
+        lp.addRoute(new RouteInfo(new IpPrefix(ADDRV6, 0), RTN_THROW));
+        assertEquals(2, lp.getRoutes().size());
+
+        lp.addRoute(new RouteInfo(GATEWAY1));
+        assertEquals(3, lp.getRoutes().size());
+    }
+
+    @Test @IgnoreUpTo(Build.VERSION_CODES.R)
+    @DisableCompatChanges({LinkProperties.EXCLUDED_ROUTES})
+    public void testExcludedRoutesDisabled() {
+        final LinkProperties lp = new LinkProperties();
+        assertEquals(0, lp.getRoutes().size());
+
+        lp.addRoute(new RouteInfo(new IpPrefix(ADDRV4, 0), RTN_UNREACHABLE));
+        assertEquals(0, lp.getRoutes().size());
+
+        lp.addRoute(new RouteInfo(new IpPrefix(ADDRV6, 0), RTN_THROW));
+        assertEquals(0, lp.getRoutes().size());
+
+        lp.addRoute(new RouteInfo(GATEWAY1));
+        assertEquals(1, lp.getRoutes().size());
     }
 
     @Test
@@ -1253,8 +1293,12 @@ public class LinkPropertiesTest {
         assertFalse(lp.hasIpv4UnreachableDefaultRoute());
     }
 
-    @Test @IgnoreUpTo(Build.VERSION_CODES.Q)
+    @Test
     public void testRouteAddWithSameKey() throws Exception {
+        // This test case can run with targetSdkVersion < S
+        // (i.e as part of the CtsNetTestCasesLatestSdk module)
+        assumeTrue(CompatChanges.isChangeEnabled(LinkProperties.EXCLUDED_ROUTES));
+
         LinkProperties lp = new LinkProperties();
         lp.setInterfaceName("wlan0");
         final IpPrefix v6 = new IpPrefix("64:ff9b::/96");
