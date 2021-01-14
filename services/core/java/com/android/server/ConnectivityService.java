6f3f6988e63a41ba2c5afb25474614807644a512
Lorenzo Colitti <lorenzo@google.com>
2021-01-15 01:29:01 +0900

    WIP: convert lockdown VPN to NetworkCallbacks
    Change-Id: I66d18512882efd468ee0ecec61f28786a195b357

--- services/core/java/com/android/server/ConnectivityService.java
+++ services/core/java/com/android/server/ConnectivityService.java
@@ -7532,6 +7532,7 @@ if (nri.request.requestId == mDefaultRequest.requestId) Log.d(TAG, "New best for
     }
 
     private void notifyLockdownVpn(NetworkAgentInfo nai) {
+        if (true) return;
         synchronized (mVpns) {
             if (mLockdownTracker != null) {
                 if (nai != null && nai.isVPN()) {
