d6f98df73cb82881305126d4fc4e54a34eb1446a
Cody Kesting <ckesting@google.com>
2021-01-20 15:37:41 -0800

    Notify UnderlyingNetworkTracker for Subscription changes.
    This CL updates VcnManagementService to notify active VCNs when it is
    notified of subscription changes by TelephonySubscriptionTracker. These
    subscription changes are passed down to each VCN's
    UnderlyingNetworkTracker, which determines whether it needs to register
    or unregister bringup NetworkRequests based on subIds within that VCN's
    Subscription Group.
    
    Bug: 177364490
    Test: atest FrameworksVcnTests
    Change-Id: I15bc9aaf3d5f97046d0ce9fcf1e12c9b1e0e1446

--- services/core/java/com/android/server/VcnManagementService.java
+++ services/core/java/com/android/server/VcnManagementService.java
@@ -290,8 +290,9 @@ public class VcnManagementService extends IVcnManagementService.Stub {
         public Vcn newVcn(
                 @NonNull VcnContext vcnContext,
                 @NonNull ParcelUuid subscriptionGroup,
+                @NonNull TelephonySubscriptionSnapshot snapshot,
                 @NonNull VcnConfig config) {
-            return new Vcn(vcnContext, subscriptionGroup, config);
+            return new Vcn(vcnContext, subscriptionGroup, snapshot, config);
         }
 
         /** Gets the subId indicated by the given {@link WifiInfo}. */
@@ -382,6 +383,7 @@ public class VcnManagementService extends IVcnManagementService.Stub {
                 // delay)
                 for (Entry<ParcelUuid, Vcn> entry : mVcns.entrySet()) {
                     final VcnConfig config = mConfigs.get(entry.getKey());
+
                     if (config == null
                             || !snapshot.packageHasPermissionsForSubscriptionGroup(
                                     entry.getKey(), config.getProvisioningPackageName())) {
@@ -399,6 +401,9 @@ public class VcnManagementService extends IVcnManagementService.Stub {
                                 }
                             }
                         }, instanceToTeardown, CARRIER_PRIVILEGES_LOST_TEARDOWN_DELAY_MS);
+                    } else {
+                        // If this VCN's status has not changed, update it with the new snapshot
+                        entry.getValue().updateSubscriptionSnapshot(mLastSnapshot);
                     }
                 }
             }
@@ -412,7 +417,7 @@ public class VcnManagementService extends IVcnManagementService.Stub {
         // TODO(b/176939047): Support multiple VCNs active at the same time, or limit to one active
         //                    VCN.
 
-        final Vcn newInstance = mDeps.newVcn(mVcnContext, subscriptionGroup, config);
+        final Vcn newInstance = mDeps.newVcn(mVcnContext, subscriptionGroup, mLastSnapshot, config);
         mVcns.put(subscriptionGroup, newInstance);
     }
 
