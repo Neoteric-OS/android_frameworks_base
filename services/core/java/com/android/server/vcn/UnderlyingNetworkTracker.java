a09bde0e32b64dbaf2e8a7a9c35c0cbcdab94d6c
Cody Kesting <ckesting@google.com>
2021-01-20 15:37:41 -0800

    Notify UnderlyingNetworkTracker for Subscription changes.
    This CL updates VcnManagementService to notify active VCNs when it is
    notified of subscription changes by TelephonySubscriptionTracker. These
    subscription changes are passed down to each VCN's
    UnderlyingNetworkTracker, which determines whether it needs to register
    or unregister bringup NetworkRequests based on subIds within that VCN's
    Subscription Group.
    
    Bug: 177364490
    Test: atest FrameworksVcnTests
    Change-Id: I15bc9aaf3d5f97046d0ce9fcf1e12c9b1e0e1446

--- services/core/java/com/android/server/vcn/UnderlyingNetworkTracker.java
+++ services/core/java/com/android/server/vcn/UnderlyingNetworkTracker.java
@@ -28,16 +28,14 @@ import android.net.NetworkRequest;
 import android.net.TelephonyNetworkSpecifier;
 import android.os.Handler;
 import android.os.ParcelUuid;
-import android.telephony.SubscriptionInfo;
-import android.telephony.SubscriptionManager;
 import android.util.ArraySet;
 import android.util.Slog;
 import android.util.SparseArray;
 
 import com.android.internal.annotations.VisibleForTesting;
 import com.android.internal.annotations.VisibleForTesting.Visibility;
+import com.android.server.vcn.TelephonySubscriptionTracker.TelephonySubscriptionSnapshot;
 
-import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 
@@ -58,7 +56,6 @@ public class UnderlyingNetworkTracker extends Handler {
     @NonNull private final UnderlyingNetworkTrackerCallback mCb;
     @NonNull private final Dependencies mDeps;
     @NonNull private final ConnectivityManager mConnectivityManager;
-    @NonNull private final SubscriptionManager mSubscriptionManager;
 
     @NonNull private final SparseArray<NetworkCallback> mCellBringupCallbacks = new SparseArray<>();
     @NonNull private final NetworkCallback mWifiBringupCallback = new NetworkBringupCallback();
@@ -68,17 +65,22 @@ public class UnderlyingNetworkTracker extends Handler {
 
     @NonNull private final Set<Integer> mRequiredUnderlyingNetworkCapabilities;
 
+    @NonNull private TelephonySubscriptionSnapshot mLastSnapshot;
+    private boolean mIsRunning = true;
+
     @Nullable private UnderlyingNetworkRecord mCurrentRecord;
     @Nullable private UnderlyingNetworkRecord.Builder mRecordInProgress;
 
     public UnderlyingNetworkTracker(
             @NonNull VcnContext vcnContext,
             @NonNull ParcelUuid subscriptionGroup,
+            @NonNull TelephonySubscriptionSnapshot snapshot,
             @NonNull Set<Integer> requiredUnderlyingNetworkCapabilities,
             @NonNull UnderlyingNetworkTrackerCallback cb) {
         this(
                 vcnContext,
                 subscriptionGroup,
+                snapshot,
                 requiredUnderlyingNetworkCapabilities,
                 cb,
                 new Dependencies());
@@ -87,12 +89,14 @@ public class UnderlyingNetworkTracker extends Handler {
     private UnderlyingNetworkTracker(
             @NonNull VcnContext vcnContext,
             @NonNull ParcelUuid subscriptionGroup,
+            @NonNull TelephonySubscriptionSnapshot snapshot,
             @NonNull Set<Integer> requiredUnderlyingNetworkCapabilities,
             @NonNull UnderlyingNetworkTrackerCallback cb,
             @NonNull Dependencies deps) {
         super(Objects.requireNonNull(vcnContext, "Missing vcnContext").getLooper());
         mVcnContext = vcnContext;
         mSubscriptionGroup = Objects.requireNonNull(subscriptionGroup, "Missing subscriptionGroup");
+        mLastSnapshot = Objects.requireNonNull(snapshot, "Missing snapshot");
         mCb = Objects.requireNonNull(cb, "Missing cb");
         mDeps = Objects.requireNonNull(deps, "Missing deps");
         mRequiredUnderlyingNetworkCapabilities =
@@ -100,7 +104,6 @@ public class UnderlyingNetworkTracker extends Handler {
                         requiredUnderlyingNetworkCapabilities,
                         "Missing requiredUnderlyingNetworkCapabilities");
         mConnectivityManager = mVcnContext.getContext().getSystemService(ConnectivityManager.class);
-        mSubscriptionManager = mVcnContext.getContext().getSystemService(SubscriptionManager.class);
 
         registerNetworkRequests();
     }
@@ -146,16 +149,17 @@ public class UnderlyingNetworkTracker extends Handler {
     private void updateSubIdsAndCellularRequests() {
         mVcnContext.ensureRunningOnLooperThread();
 
+        // Don't bother re-filing NetworkRequests if this Tracker has been torn down.
+        if (!mIsRunning) {
+            return;
+        }
+
         Set<Integer> prevSubIds = new ArraySet<>(mSubIds);
         mSubIds.clear();
 
         // Ensure NetworkRequests filed for all current subIds in mSubscriptionGroup
-        // STOPSHIP: b/177364490 use TelephonySubscriptionSnapshot to avoid querying Telephony
-        List<SubscriptionInfo> subInfos =
-                mSubscriptionManager.getSubscriptionsInGroup(mSubscriptionGroup);
-
-        for (SubscriptionInfo subInfo : subInfos) {
-            final int subId = subInfo.getSubscriptionId();
+        final Set<Integer> subIdsInSubGroup = mLastSnapshot.getAllSubIdsInGroup(mSubscriptionGroup);
+        for (final int subId : subIdsInSubGroup) {
             mSubIds.add(subId);
 
             if (!mCellBringupCallbacks.contains(subId)) {
@@ -176,6 +180,21 @@ public class UnderlyingNetworkTracker extends Handler {
         }
     }
 
+    /**
+     * Update this UnderlyingNetworkTracker's TelephonySubscriptionSnapshot.
+     *
+     * <p>Updating the TelephonySubscriptionSnapshot will cause this UnderlyingNetworkTracker to
+     * reevaluate its NetworkBringupCallbacks. This may result in NetworkRequests being registered
+     * or unregistered if the subIds mapped to the this Tracker's SubscriptionGroup change.
+     */
+    // package-private
+    void updateSubscriptionSnapshot(@NonNull TelephonySubscriptionSnapshot snapshot) {
+        Objects.requireNonNull(snapshot, "Missing snapshot");
+
+        mLastSnapshot = snapshot;
+        updateSubIdsAndCellularRequests();
+    }
+
     /** Tears down this Tracker, and releases all underlying network requests. */
     public void teardown() {
         mVcnContext.ensureRunningOnLooperThread();
@@ -188,6 +207,8 @@ public class UnderlyingNetworkTracker extends Handler {
             mConnectivityManager.unregisterNetworkCallback(cb);
         }
         mSubIds.clear();
+
+        mIsRunning = false;
     }
 
     /** Returns whether the currently selected Network matches the given network. */
