d7b2b81257d11223a10bd51428f74c428e1b8a84
Łukasz Rymanowski <lukasz.rymanowski@codecoup.pl>
2021-01-26 11:26:13 +0000

    vc: Add Volume Control Profile
    Test: compilation
    Tag: #feature
    Bug: 159786353
    Sponsor: jpawlowski@
    
    Change-Id: Iae585ecd9c2356b194161b967e96c28e96611752

--- core/java/android/bluetooth/BluetoothAdapter.java
+++ core/java/android/bluetooth/BluetoothAdapter.java
@@ -2812,6 +2812,9 @@ public final class BluetoothAdapter {
                 return true;
             }
             return false;
+        } else if (profile == BluetoothProfile.VOLUME_CONTROL) {
+            BluetoothVolumeControl vcs = new BluetoothVolumeControl(context, listener);
+            return true;
         } else {
             return false;
         }
@@ -2896,6 +2899,11 @@ public final class BluetoothAdapter {
             case BluetoothProfile.HEARING_AID:
                 BluetoothHearingAid hearingAid = (BluetoothHearingAid) proxy;
                 hearingAid.close();
+                break;
+            case BluetoothProfile.VOLUME_CONTROL:
+                BluetoothVolumeControl vcs = (BluetoothVolumeControl) proxy;
+                vcs.close();
+                break;
         }
     }
 
