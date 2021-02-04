f04e5ea97c0924e43f7dd70c85dbd1f791368848
Cody Kesting <ckesting@google.com>
2021-02-04 13:42:31 -0800

    DNS: Expose VCN underlying Network policy APIs.
    This CL exposes all APIs needed for Network factories to track
    VcnUnderlyingNetworkPolicies as decided by VcnManagementService.
    
    Bug: 177020190
    Test: atest FrameworksVcnTests
    Change-Id: Ied78a57973a4ceaa7ef4cd9feee64478137497b8

--- core/java/android/net/vcn/VcnManager.java
+++ core/java/android/net/vcn/VcnManager.java
@@ -19,6 +19,7 @@ import static java.util.Objects.requireNonNull;
 
 import android.annotation.NonNull;
 import android.annotation.RequiresPermission;
+import android.annotation.SystemApi;
 import android.annotation.SystemService;
 import android.content.Context;
 import android.net.LinkProperties;
@@ -64,10 +65,12 @@ import java.util.concurrent.Executor;
  *
  * @hide
  */
+@SystemApi
 @SystemService(Context.VCN_MANAGEMENT_SERVICE)
 public class VcnManager {
     @NonNull private static final String TAG = VcnManager.class.getSimpleName();
 
+    /** @hide */
     @VisibleForTesting(visibility = Visibility.PRIVATE)
     public static final Map<
                     VcnUnderlyingNetworkPolicyListener, VcnUnderlyingNetworkPolicyListenerBinder>
@@ -161,6 +164,7 @@ public class VcnManager {
      *
      * @hide
      */
+    @SystemApi
     public interface VcnUnderlyingNetworkPolicyListener {
         /**
          * Notifies the implementation that the VCN's underlying Network policy has changed.
@@ -182,6 +186,7 @@ public class VcnManager {
      *     already registered
      * @hide
      */
+    @SystemApi
     @RequiresPermission(android.Manifest.permission.NETWORK_FACTORY)
     public void addVcnUnderlyingNetworkPolicyListener(
             @NonNull Executor executor, @NonNull VcnUnderlyingNetworkPolicyListener listener) {
@@ -211,6 +216,7 @@ public class VcnManager {
      * @param listener the VcnUnderlyingNetworkPolicyListener that will be removed
      * @hide
      */
+    @SystemApi
     public void removeVcnUnderlyingNetworkPolicyListener(
             @NonNull VcnUnderlyingNetworkPolicyListener listener) {
         requireNonNull(listener, "listener must not be null");
@@ -244,6 +250,7 @@ public class VcnManager {
      * @return the VcnUnderlyingNetworkPolicy to be used for this Network.
      * @hide
      */
+    @SystemApi
     @NonNull
     @RequiresPermission(android.Manifest.permission.NETWORK_FACTORY)
     public VcnUnderlyingNetworkPolicy getUnderlyingNetworkPolicy(
@@ -350,7 +357,8 @@ public class VcnManager {
      *
      * @hide
      */
-    private static class VcnUnderlyingNetworkPolicyListenerBinder
+    @VisibleForTesting(visibility = Visibility.PRIVATE)
+    public static class VcnUnderlyingNetworkPolicyListenerBinder
             extends IVcnUnderlyingNetworkPolicyListener.Stub {
         @NonNull private final Executor mExecutor;
         @NonNull private final VcnUnderlyingNetworkPolicyListener mListener;
