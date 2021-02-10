cd072444b3f9672e07ce465f1e4ac47cb9207a48
Benedict Wong <benedictwong@google.com>
2021-02-10 15:44:26 -0800

    Allow NETWORK_LOST disconnections to retry
    This change adds the ability for disconnection requests to specify
    whether the full VcnGatewayConnection should be torn down. This is
    useful in cases where the cause is an ephemeral failure, and can be
    restarted at some later point (eg an underlying network appears, or a
    new NetworkRequest is filed that brings the VCN up.)
    
    Bug: 179944275
    Test: atest FrameworksVcnTests
    Change-Id: Id1813bcbbb80541be97cc383ea9d1fe08554b7ae

--- tests/vcn/java/com/android/server/vcn/VcnGatewayConnectionConnectingStateTest.java
+++ tests/vcn/java/com/android/server/vcn/VcnGatewayConnectionConnectingStateTest.java
@@ -19,6 +19,8 @@ package com.android.server.vcn;
 import static com.android.server.vcn.VcnGatewayConnection.VcnIkeSession;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
@@ -101,5 +103,23 @@ public class VcnGatewayConnectionConnectingStateTest extends VcnGatewayConnectio
 
         assertEquals(mGatewayConnection.mRetryTimeoutState, mGatewayConnection.getCurrentState());
         verify(mIkeSession).close();
+    }
+
+    @Test
+    public void testTeardown() throws Exception {
+        mGatewayConnection.teardownAsynchronously();
+        mTestLooper.dispatchAll();
+
+        assertEquals(mGatewayConnection.mDisconnectingState, mGatewayConnection.getCurrentState());
+        assertFalse(mGatewayConnection.isRunning());
+    }
+
+    @Test
+    public void testNonTeardownDisconnectRequest() throws Exception {
+        mGatewayConnection.sendDisconnectRequested("TEST", false);
+        mTestLooper.dispatchAll();
+
+        assertEquals(mGatewayConnection.mDisconnectingState, mGatewayConnection.getCurrentState());
+        assertTrue(mGatewayConnection.isRunning());
     }
 }
