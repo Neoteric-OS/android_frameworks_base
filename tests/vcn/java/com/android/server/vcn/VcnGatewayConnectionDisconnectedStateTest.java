601bf05fa1ed34d078041046b2388de6d62a453b
Benedict Wong <benedictwong@google.com>
2021-02-10 15:55:53 -0800

    Notify Vcn of VcnGatewayConnection quits
    This change updates the Vcn when any GatewayConnection StateMachine
    quits, removing it from the map of active GatewayConnections, and
    triggering a re-evaluation of all existing NetworkRequests. This may (in
    the event of a updated policy) result in an immediate restart.
    
    Bug: 179944275
    Test: atest FrameworksVcnTests
    Change-Id: I72abf1e270a772760a17a03173d5a110e15bd01d

--- tests/vcn/java/com/android/server/vcn/VcnGatewayConnectionDisconnectedStateTest.java
+++ tests/vcn/java/com/android/server/vcn/VcnGatewayConnectionDisconnectedStateTest.java
@@ -26,6 +26,7 @@ import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 
 import android.net.IpSecManager;
@@ -100,6 +101,7 @@ public class VcnGatewayConnectionDisconnectedStateTest extends VcnGatewayConnect
         assertNull(mGatewayConnection.getCurrentState());
         verify(mIpSecSvc).deleteTunnelInterface(eq(TEST_IPSEC_TUNNEL_RESOURCE_ID), any());
         assertFalse(mGatewayConnection.isRunning());
+        verify(mGatewayStatusCallback).onQuit();
     }
 
     @Test
@@ -109,5 +111,6 @@ public class VcnGatewayConnectionDisconnectedStateTest extends VcnGatewayConnect
 
         assertEquals(mGatewayConnection.mDisconnectedState, mGatewayConnection.getCurrentState());
         assertTrue(mGatewayConnection.isRunning());
+        verify(mGatewayStatusCallback, never()).onQuit();
     }
 }
