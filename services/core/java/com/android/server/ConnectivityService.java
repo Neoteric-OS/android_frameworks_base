ebd4d45fcd59c38350ae5496278e8c34b5902e53
Chalard Jean <jchalard@google.com>
2021-03-01 22:12:56 +0900

    Fix a bug where a request would not be refcounted
    This ends up crashing the system when the request
    is unregistered, because the ref counter finds the
    count associated with the relevant UID is zero.
    
    Test: FrameworksNetTests
    Test: TODO : this needs a new unit test
    Change-Id: I0ee0ce925a826d35d8fd58cefb8a870e98ce9add

--- services/core/java/com/android/server/ConnectivityService.java
+++ services/core/java/com/android/server/ConnectivityService.java
@@ -5253,6 +5253,7 @@ public class ConnectivityService extends IConnectivityManager.Stub
             mPid = nri.mPid;
             mUid = nri.mUid;
             mPendingIntent = nri.mPendingIntent;
+            mNetworkRequestCounter.incrementCountOrThrow(mUid);
             mCallingAttributionTag = nri.mCallingAttributionTag;
         }
 
