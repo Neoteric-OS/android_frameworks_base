50f8db1c0af25f7e0b2ca8b585f0405bab711b0b
Chienyuan <chienyuanhuang@google.com>
2021-03-12 20:46:17 +0800

    Le Scan: Add ambient discovery mode (1/2)
    Bug: 177466875
    Test: manual
    Change-Id: I4a5c8a0768903ef0838dcf55cf5cfba9a0a18eef

--- core/java/android/bluetooth/le/ScanSettings.java
+++ core/java/android/bluetooth/le/ScanSettings.java
@@ -52,6 +52,16 @@ public final class ScanSettings implements Parcelable {
     public static final int SCAN_MODE_LOW_LATENCY = 2;
 
     /**
+     * Perform Bluetooth LE scan in ambient discovery mode. This mode has lower duty cycle and more
+     * aggressive scan interval than balanced mode that provides a good trade-off between scan
+     * latency and power consumption.
+     *
+     * @hide
+     */
+    @SystemApi
+    public static final int SCAN_MODE_AMBIENT_DISCOVERY = 3;
+
+    /**
      * Trigger a callback for every Bluetooth advertisement found that matches the filter criteria.
      * If no filter is active, all advertisement packets are reported.
      */
@@ -277,7 +287,9 @@ public final class ScanSettings implements Parcelable {
          */
         public Builder setScanMode(int scanMode) {
             if (scanMode < SCAN_MODE_OPPORTUNISTIC || scanMode > SCAN_MODE_LOW_LATENCY) {
-                throw new IllegalArgumentException("invalid scan mode " + scanMode);
+                if (scanMode != SCAN_MODE_AMBIENT_DISCOVERY) {
+                    throw new IllegalArgumentException("invalid scan mode " + scanMode);
+                }
             }
             mScanMode = scanMode;
             return this;
