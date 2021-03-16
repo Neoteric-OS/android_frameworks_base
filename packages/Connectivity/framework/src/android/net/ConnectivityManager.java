bb101ce414f78105c6512b613894338f0fe0af72
Remi NGUYEN VAN <reminv@google.com>
2021-03-16 18:06:06 +0900

    Add startCaptivePortalApp to system API
    The API is already used by settings and should be usable by setup
    wizards. It is the only way for a caller outside of the system_server to
    trigger the captive portal application.
    
    The API is already CTS tested in android.net.cts.CaptivePortalTest.
    
    Fixes: 182871577
    Test: atest CtsNetTestCases:android.net.cts.CaptivePortalTest
    Change-Id: Ie8d9a546b54524ba837715baa94a07d1f993d8d3

--- packages/Connectivity/framework/src/android/net/ConnectivityManager.java
+++ packages/Connectivity/framework/src/android/net/ConnectivityManager.java
@@ -4442,12 +4442,16 @@ public class ConnectivityManager {
     /**
      * Requests that the system open the captive portal app on the specified network.
      *
+     * <p>This is to be used on networks where a captive portal was detected, as per
+     * {@link NetworkCapabilities#NET_CAPABILITY_CAPTIVE_PORTAL}.
+     *
      * @param network The network to log into.
      *
      * @hide
      */
+    @SystemApi(client = SystemApi.Client.MODULE_LIBRARIES)
     @RequiresPermission(android.Manifest.permission.NETWORK_SETTINGS)
-    public void startCaptivePortalApp(Network network) {
+    public void startCaptivePortalApp(@NonNull Network network) {
         try {
             mService.startCaptivePortalApp(network);
         } catch (RemoteException e) {
