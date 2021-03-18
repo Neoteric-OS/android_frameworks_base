988a571d284e677ba5991a4755bbf960d5b9fc7d
lifr <lifr@google.com>
2021-03-19 00:56:53 +0800

    [JS02]Remove hidden API usage of NetworkRequest.networkCapabilities
    The connection service will become the mainline module.
    Therefore, remove the caller of using NetworkRequest's
    hidden API outside the module.
    
    Bug: 170598012
    Test: atest ConnectivityControllerTest
    Change-Id: I263d2488472dae21e1cdca3760e0127cc0d20de2

--- apex/jobscheduler/service/java/com/android/server/job/controllers/ConnectivityController.java
+++ apex/jobscheduler/service/java/com/android/server/job/controllers/ConnectivityController.java
@@ -21,6 +21,7 @@ import static android.net.NetworkCapabilities.NET_CAPABILITY_NOT_METERED;
 
 import static com.android.server.job.JobSchedulerService.RESTRICTED_INDEX;
 
+import android.annotation.NonNull;
 import android.annotation.Nullable;
 import android.app.job.JobInfo;
 import android.net.ConnectivityManager;
@@ -373,15 +374,27 @@ public final class ConnectivityController extends RestrictingController implemen
         }
     }
 
+    private static NetworkCapabilities capsWithNotMetered(@NonNull final NetworkRequest request,
+            boolean hasNotMetered) {
+        final NetworkCapabilities.Builder builder = new NetworkCapabilities.Builder();
+        for (int transport : request.getTransportTypes()) builder.addTransportType(transport);
+        for (int capability : request.getCapabilities()) builder.addCapability(capability);
+        if (hasNotMetered) {
+            builder.addCapability(NET_CAPABILITY_NOT_METERED);
+        } else {
+            builder.removeCapability(NET_CAPABILITY_NOT_METERED);
+        }
+        return builder.build();
+    }
+
     private static boolean isStrictSatisfied(JobStatus jobStatus, Network network,
             NetworkCapabilities capabilities, Constants constants) {
         // A restricted job that's out of quota MUST use an unmetered network.
         if (jobStatus.getEffectiveStandbyBucket() == RESTRICTED_INDEX
                 && !jobStatus.isConstraintSatisfied(JobStatus.CONSTRAINT_WITHIN_QUOTA)) {
-            final NetworkCapabilities required = new NetworkCapabilities.Builder(
-                    jobStatus.getJob().getRequiredNetwork().networkCapabilities)
-                    .addCapability(NET_CAPABILITY_NOT_METERED).build();
-            return required.satisfiedByNetworkCapabilities(capabilities);
+            final NetworkCapabilities caps = capsWithNotMetered(jobStatus.getJob()
+                    .getRequiredNetwork(), true);
+            return caps.satisfiedByNetworkCapabilities(capabilities);
         } else {
             return jobStatus.getJob().getRequiredNetwork().canBeSatisfiedBy(capabilities);
         }
@@ -395,10 +408,9 @@ public final class ConnectivityController extends RestrictingController implemen
         }
 
         // See if we match after relaxing any unmetered request
-        final NetworkCapabilities relaxed = new NetworkCapabilities.Builder(
-                jobStatus.getJob().getRequiredNetwork().networkCapabilities)
-                        .removeCapability(NET_CAPABILITY_NOT_METERED).build();
-        if (relaxed.satisfiedByNetworkCapabilities(capabilities)) {
+        final NetworkCapabilities caps = capsWithNotMetered(jobStatus.getJob()
+                .getRequiredNetwork(), false);
+        if (caps.satisfiedByNetworkCapabilities(capabilities)) {
             // TODO: treat this as "maybe" response; need to check quotas
             return jobStatus.getFractionRunTime() > constants.CONN_PREFETCH_RELAX_FRAC;
         } else {
