fb3e13e4e9393045c2bb261b2a0085d55ac45498
Cody Kesting <ckesting@google.com>
2021-03-30 17:39:58 -0700

    Expose API for identifying GatewayConnections.
    This CL exposes the updated APIs for identifying GatewayConnections via
    a user-configured String set in VcnGatewayConnectionConfig.Builder, as
    requested by the API Council.
    
    Bug: 182345902
    Test: atest FrameworksVcnTests
    Change-Id: I10afd074906bc0f3831157dcee1da813b4cfce78

--- core/java/android/net/vcn/VcnGatewayConnectionConfig.java
+++ core/java/android/net/vcn/VcnGatewayConnectionConfig.java
@@ -255,7 +255,6 @@ public final class VcnGatewayConnectionConfig {
      * identifier in VcnStatusCallback invocations.
      *
      * @see VcnManager.VcnStatusCallback#onGatewayConnectionError
-     * @hide
      */
     @NonNull
     public String getGatewayConnectionName() {
@@ -437,16 +436,6 @@ public final class VcnGatewayConnectionConfig {
         /**
          * Construct a Builder object.
          *
-         * @param ctrlPlaneConfig the control plane configuration
-         * @see VcnControlPlaneConfig
-         */
-        public Builder(@NonNull VcnControlPlaneConfig ctrlPlaneConfig) {
-            this("" /* gatewayConnectionName */, ctrlPlaneConfig);
-        }
-
-        /**
-         * Construct a Builder object.
-         *
          * @param gatewayConnectionName the String GatewayConnection name for this
          *     VcnGatewayConnectionConfig. Each VcnGatewayConnectionConfig within a {@link
          *     VcnConfig} must be given a unique name. This name is used by the caller to
@@ -456,7 +445,6 @@ public final class VcnGatewayConnectionConfig {
          *     Throwable)}.
          * @param ctrlPlaneConfig the control plane configuration
          * @see VcnControlPlaneConfig
-         * @hide
          */
         public Builder(
                 @NonNull String gatewayConnectionName,
