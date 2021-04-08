fdabfc5b4b8354bda7363954a188d196c36137ca
Benedict Wong <benedictwong@google.com>
2021-04-08 03:18:45 -0700

    Disable INTERNET/DUN when mobile data toggled off
    This change disables INTERNET and DUN capabilities once the mobile data
    toggle is turned off. Additionally, when the toggle is flipped, any
    existing networks advertising either of those two capabilities will be
    restarted in order to remove the capabilities.
    
    Bug: 184864726
    Test: atest FrameworksVcnTests
    Change-Id: I81efeaa4b129443b08a597ff45e0037ddefeb414

--- tests/vcn/java/com/android/server/vcn/VcnGatewayConnectionDisconnectedStateTest.java
+++ tests/vcn/java/com/android/server/vcn/VcnGatewayConnectionDisconnectedStateTest.java
@@ -65,6 +65,7 @@ public class VcnGatewayConnectionDisconnectedStateTest extends VcnGatewayConnect
                         TEST_SUBSCRIPTION_SNAPSHOT,
                         mConfig,
                         mGatewayStatusCallback,
+                        true,
                         mDeps);
 
         vgc.setIsQuitting(true);
