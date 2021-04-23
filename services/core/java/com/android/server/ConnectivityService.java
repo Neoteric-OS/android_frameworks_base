16de48f8879410063c93dc3a9e1b8a3e3ea35097
Remi NGUYEN VAN <reminv@google.com>
2021-04-23 09:40:00 +0900

    Move getSupportedKeepalives to ConnectivityManager
    KeepaliveUtils.getSupportedKeepalives uses connectivity resources, which
    should only be queried by service-connectivity.
    
    Add a method in ConnectivityManager to query ConnectivityService for
    supported keepalives for a given network. This is currently the only
    usage of getSupportedKeepalives outside of KeepaliveTracker, and is only
    used from tests using the KeepaliveUtils hidden APIs.
    
    Bug: 186093901
    Test: atest CtsNetTestCases using the new API
    Change-Id: I645e1cf6f73c72654634a4700b706f3f907ed5c2

--- services/core/java/com/android/server/ConnectivityService.java
+++ services/core/java/com/android/server/ConnectivityService.java
@@ -180,6 +180,7 @@ import android.net.resolv.aidl.IDnsResolverUnsolicitedEventListener;
 import android.net.resolv.aidl.Nat64PrefixEventParcel;
 import android.net.resolv.aidl.PrivateDnsValidationEventParcel;
 import android.net.shared.PrivateDnsConfig;
+import android.net.util.KeepaliveUtils;
 import android.net.util.MultinetworkPolicyTracker;
 import android.net.util.NetdService;
 import android.os.BatteryStatsManager;
@@ -9955,5 +9956,18 @@ public class ConnectivityService extends IConnectivityManager.Stub
             netCap.setRequestorUidAndPackageName(Process.myUid(), mContext.getPackageName());
             return netCap;
         }
+    }
+
+    @Override
+    public int getSupportedKeepalivesForNetwork(@NonNull Network network) {
+        Objects.requireNonNull(network, "Network must not be null");
+        enforceSettingsPermission();
+
+        final int[] keepAlives = KeepaliveUtils.getSupportedKeepalives(mResources);
+        final NetworkCapabilities nc = getNetworkCapabilitiesInternal(network);
+        if (nc == null) {
+            throw new IllegalStateException("Network " + network + " is not connected");
+        }
+        return KeepaliveUtils.getSupportedKeepalivesForNetworkCapabilities(keepAlives, nc);
     }
 }
