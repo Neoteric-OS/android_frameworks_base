b649a696015929b26929caa7977e82ca45f699a3
Remi NGUYEN VAN <reminv@google.com>
2021-04-23 09:40:00 +0900

    Move getSupportedKeepalives to ConnectivityManager
    KeepaliveUtils.getSupportedKeepalives uses connectivity resources, which
    should only be queried by service-connectivity.
    
    Add a method in ConnectivityManager to query ConnectivityService for
    supported keepalives for a given network. This is currently the only
    usage of getSupportedKeepalives outside of KeepaliveTracker, and is only
    used from tests using the KeepaliveUtils hidden APIs.
    
    Bug: 186093901
    Test: atest CtsNetTestCases using the new API
    Change-Id: I645e1cf6f73c72654634a4700b706f3f907ed5c2

--- packages/Connectivity/framework/src/android/net/ConnectivityManager.java
+++ packages/Connectivity/framework/src/android/net/ConnectivityManager.java
@@ -5488,4 +5488,19 @@ public class ConnectivityManager {
         }
         Settings.Global.putString(context.getContentResolver(), PRIVATE_DNS_MODE, mode);
     }
+
+    /**
+     * Get supported keepalive count for the given {@link Network}.
+     *
+     * @return Supported keepalive count for the given {@link Network}.
+     * @hide
+     */
+    @RequiresPermission(android.Manifest.permission.NETWORK_SETTINGS)
+    public int getSupportedKeepalivesForNetwork(@NonNull Network network) {
+        try {
+            return mService.getSupportedKeepalivesForNetwork(network);
+        } catch (RemoteException e) {
+            throw e.rethrowFromSystemServer();
+        }
+    }
 }
