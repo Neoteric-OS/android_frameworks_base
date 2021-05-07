89e74236d6fc47d5d0fb32a60016e3a17ebedc52
Anton Hansson <hansson@google.com>
2021-05-07 16:32:10 +0100

    Inline two constants from CorePlatform
    Inlining these constants allow the public api metalava run to depend on
    ART's public API rather than the "internal" CorePlatform API, which
    prevents picking up unintentional classes (such as annotations) from the
    CorePlatform API in the public stubs.
    
    Because these constants get inlined in the API/stubs, there is no
    reason to reuse the constant across module boundaries, because they can
    never change anyway.
    
    Bug: 154796679
    Test: m checkapi
    Change-Id: I0b347a2964cfe5a1206145fdc56934727b890469

--- core/java/android/os/Debug.java
+++ core/java/android/os/Debug.java
@@ -75,7 +75,7 @@ public final class Debug
      * @deprecated Accurate counting is a burden on the runtime and may be removed.
      */
     @Deprecated
-    public static final int TRACE_COUNT_ALLOCS  = VMDebug.TRACE_COUNT_ALLOCS;
+    public static final int TRACE_COUNT_ALLOCS  = 1;
 
     /**
      * Flags for printLoadedClasses().  Default behavior is to only show
