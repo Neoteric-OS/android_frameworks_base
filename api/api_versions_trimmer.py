4fcf9618b48a2fa3551c68682cf26eb37bc947c6
Pedro Loureiro <pedroql@google.com>
2021-05-26 15:50:07 +0000

    Trim lint database
    lint database file (api-versions.xml) is used for NewApi checks. These checks
    were written thinking about app developers.
    
    When used inside the platform codebase, quite often it produces false positives
    which make relying on it cumbersome.
    
    This CL removes APIs introduced by mainline modules from the lint database to
    remove many such false positives. While some of them might be real issues, the
    alternative would be not turning these checks on at all.
    
    Test: m lint-check
    Test: m filter-api-versions
    Test: python api_versions_trimmer_unittests.py
    Change-Id: Ic45db0b71a2f13dc456463ce04b2f7744a0e330c

---
+++ api/api_versions_trimmer.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python3
+#
+# Copyright (C) 2021 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Script to remove mainline APIs from the api-versions.xml."""
+
+import argparse
+import xml.etree.ElementTree as ET
+import zipfile
+
+
+def file_to_class(filename):
+  # drop `.class` suffix
+  #
+  # a class present in a jar file, e.g. com.example.SomeClass$Inner is
+  # represented by the file com/example/SomeClass$Inner.class and then shows up
+  # in the api-versions.xml as com/example/SomeClass$Inner
+  return filename[:-6]
+
+
+def read_classes(stubs):
+  """read classes from the stubs file.
+
+  Args:
+    stubs: argument can be a path to a file (a string), a file-like object or a
+    path-like object
+
+  Returns:
+    a set of the classes found in the file (set of strings)
+  """
+  classes = set()
+  with zipfile.ZipFile(stubs) as z:
+    for info in z.infolist():
+      if (not info.is_dir()
+          and info.filename.endswith(".class")
+          and not info.filename.startswith("META-INF")):
+        classes.add(file_to_class(info.filename))
+  return classes
+
+
+def filter_method_signature(method, classes_to_remove):
+  """removes mentions of certain classes from this method signature.
+
+  Replaces any existing classes that need to be removed, with java/lang/Object
+
+  Args:
+    method: xml element that represents a method
+    classes_to_remove: set of classes you want to remove
+  """
+  signature = method.get("name")
+  start = signature.index("(")
+  end = signature.index(")")
+  classes = set(signature[start+1:end].split(";"))
+  for c in classes:
+    # c[1:] in order to drop the 'L' from the signature
+    if c[1:] in classes_to_remove:
+      signature = signature.replace(c+";", "Ljava/lang/Object;")
+  method.set("name", signature)
+
+
+def filter_lint_database(database, classes_to_remove, output):
+  """reads a lint database and writes a filtered version without some classes.
+
+  Reads database from api-versions.xml and removes any references to classes
+  in the second argument. Writes the result (another xml with the same format
+  of the database) to output.
+
+  Args:
+    database: path to xml with lint database to read
+    classes_to_remove: iterable (ideally a set or similar for quick
+    lookups) that enumerates the classes that should be removed
+    output: path to write the filtered database
+  """
+  xml = ET.parse(database)
+  root = xml.getroot()
+  for c in xml.findall("class"):
+    cname = c.get("name")
+    if cname in classes_to_remove:
+      root.remove(c)
+    else:
+      # find the <extends /> tag inside this class to see if the parent
+      # has been removed from the known classes (attribute called name)
+      super_classes = c.findall("implements")+c.findall("extends")
+      for super_class in super_classes:
+        super_class_name = super_class.get("name")
+        if super_class_name in classes_to_remove:
+          super_class.set("name", "java/lang/Object")
+      for method in c.findall("method"):
+        filter_method_signature(method, classes_to_remove)
+  xml.write(output)
+
+
+def main():
+  """run the program."""
+  parser = argparse.ArgumentParser(
+      description=
+      ("Read a lint database (api-versions.xml) and many stubs jar files. "
+       "Produce another database file that doesn't include the classes present "
+       "in the stubs file(s)."))
+  parser.add_argument("output", help="Destination of the result (xml file).")
+  parser.add_argument(
+      "api_versions",
+      help="The lint database (api-versions.xml file) to read data from"
+  )
+  parser.add_argument("stubs", nargs="+", help="The stubs jar file(s)")
+  parsed = parser.parse_args()
+  classes = set()
+  for stub in parsed.stubs:
+    classes.update(read_classes(stub))
+  filter_lint_database(parsed.api_versions, classes, parsed.output)
+
+
+if __name__ == "__main__":
+  main()
---
+++ api/api_versions_trimmer_unittests.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python3
+#
+# Copyright (C) 2021 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import io
+import unittest
+import xml.etree.ElementTree as ET
+import zipfile
+
+import api_versions_trimmer
+
+class virtual_zip_file:
+  def __init__(self):
+    self.zip = zipfile.ZipFile()
+
+def create_in_memory_zip_file(files):
+  f = io.BytesIO()
+  #f = io.StringIO()
+  with zipfile.ZipFile(f, "w") as z:
+    for fname in files:
+      path = zipfile.Path(z, fname)
+      with path.open("w") as class_file:
+        class_file.write("")
+  return f
+
+class api_versions_trimmer_unittest(unittest.TestCase):
+
+  def test_file_to_class(self):
+    name = api_versions_trimmer.file_to_class("a/b/C.class")
+    self.assertEqual("a/b/C", name)
+
+  def test_read_classes(self):
+    f = create_in_memory_zip_file(
+      [ "a/b/C.class",
+        "a/b/D.class",
+        "a/b/E.dex",
+        "f.dex",
+        "META-INFO/G.class"
+      ]
+    )
+    res = api_versions_trimmer.read_classes(f)
+    self.assertEqual({"a/b/C", "a/b/D"}, res)
+
+  def test_filter_method_signature(self):
+    xml = """
+    <method name="dispatchGesture(Landroid/accessibilityservice/GestureDescription;Landroid/accessibilityservice/AccessibilityService$GestureResultCallback;Landroid/os/Handler;)Z" since="24"/>
+    """
+    method = ET.fromstring(xml)
+    classes_to_remove = {"android/accessibilityservice/GestureDescription"}
+    expected = "dispatchGesture(Ljava/lang/Object;Landroid/accessibilityservice/AccessibilityService$GestureResultCallback;Landroid/os/Handler;)Z"
+    api_versions_trimmer.filter_method_signature(method, classes_to_remove)
+    self.assertEqual(expected, method.get("name"))
+
+  def test_filter_lint_database(self):
+    xml = """
+    <api version="2">
+      <!-- will be removed -->
+      <class name="a/b/C" since="1">
+        <extends name="java/lang/Object"/>
+      </class>
+
+      <class name="a/b/D" since="1">
+        <extends name="java/lang/Object"/>
+        <implements name="android/os/Parcelable"/>
+        <method name="dispatchGesture(Landroid/accessibilityservice/GestureDescription;Landroid/accessibilityservice/AccessibilityService$GestureRe
+sultCallback;Landroid/os/Handler;)Z" since="24"/>
+      </class>
+
+      <class name="a/b/E" since="1">
+        <!-- extends will be modified -->
+        <extends name="a/b/C"/>
+        <!-- first parameter will be modified -->
+        <method name="dispatchGesture(La/b/C;Landroid/os/Handler;)Z" since="24"/>
+        <!-- second should remain untouched -->
+        <method name="dispatchGesture(Landroid/accessibilityservice/GestureDescription;Landroid/accessibilityservice/AccessibilityService$GestureRe
+sultCallback;Landroid/os/Handler;)Z" since="24"/>
+      </class>
+
+      <class name="a/b/F" since="1">
+        <extends name="java/lang/Object"/>
+        <!-- implements will be modified -->
+        <implements name="a/b/C"/>
+      </class>
+    </api>
+    """
+    database = io.StringIO(xml)
+    classes_to_remove = {"a/b/C"}
+    output = io.BytesIO()
+    api_versions_trimmer.filter_lint_database(database, classes_to_remove,
+      output)
+    expected = """
+    <api version="2">
+
+      <class name="a/b/D" since="1">
+        <extends name="java/lang/Object"/>
+        <implements name="android/os/Parcelable"/>
+        <method name="dispatchGesture(Landroid/accessibilityservice/GestureDescription;Landroid/accessibilityservice/AccessibilityService$GestureRe
+sultCallback;Landroid/os/Handler;)Z" since="24"/>
+      </class>
+
+      <class name="a/b/E" since="1">
+        <extends name="java/lang/Object"/>
+        <method name="dispatchGesture(Ljava/lang/Object;Landroid/os/Handler;)Z" since="24"/>
+        <method name="dispatchGesture(Landroid/accessibilityservice/GestureDescription;Landroid/accessibilityservice/AccessibilityService$GestureRe
+sultCallback;Landroid/os/Handler;)Z" since="24"/>
+      </class>
+
+      <class name="a/b/F" since="1">
+        <extends name="java/lang/Object"/>
+        <implements name="java/lang/Object"/>
+      </class>
+    </api>
+    """
+    clean_whitespace = lambda x: x.replace(" ", "").replace("\n", "")
+    expected = clean_whitespace(expected)
+    res = clean_whitespace(output.getvalue().decode("utf-8"))
+
+    self.assertEqual(expected, res)
+
+  def testCi(self):
+    self.assertEqual(1, 2)
+
+
+if __name__ == '__main__':
+  unittest.main()
