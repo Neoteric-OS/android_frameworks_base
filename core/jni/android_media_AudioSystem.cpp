c10f9401234fda7231b9794b9ace95c3c3689fc0
Francois Gaffie <francois.gaffie@renault.com>
2021-06-22 10:19:41 +0200

    AudioService: move volume callback on AudioService
    Robustness to crash of AudioServer.
    Take benefit of future permission enforcement.
    
    Bug: 293236285
    Test: atest com.android.server.audio
    
    Signed-off-by: Francois Gaffie <francois.gaffie@renault.com>
    Change-Id: If556a668f45d608b98b6e8073426f5bb8128c7a2

--- core/jni/android_media_AudioSystem.cpp
+++ core/jni/android_media_AudioSystem.cpp
@@ -23,6 +23,7 @@
 #include <android/binder_ibinder_jni.h>
 #include <android/binder_libbinder.h>
 #include <android/media/AudioVibratorInfo.h>
+#include <android/media/INativeAudioVolumeGroupCallback.h>
 #include <android/media/INativeSpatializerCallback.h>
 #include <android/media/ISpatializer.h>
 #include <android/media/audio/common/AudioConfigBase.h>
@@ -3443,6 +3444,21 @@ static void android_media_AudioSystem_triggerSystemPropertyUpdate(JNIEnv *env,  
     }
 }
 
+static int android_media_AudioSystem_registerAudioVolumeGroupCallback(JNIEnv *env, jobject thiz,
+        jobject jIAudioVolumeGroupCallback) {
+    sp<media::INativeAudioVolumeGroupCallback> nIAudioVolumeGroupCallback
+            = interface_cast<media::INativeAudioVolumeGroupCallback>(
+                    ibinderForJavaObject(env, jIAudioVolumeGroupCallback));
+    return AudioSystem::addAudioVolumeGroupCallback(nIAudioVolumeGroupCallback);
+}
+
+static int android_media_AudioSystem_unregisterAudioVolumeGroupCallback(JNIEnv *env, jobject thiz,
+        jobject jIAudioVolumeGroupCallback) {
+    sp<media::INativeAudioVolumeGroupCallback> nIAudioVolumeGroupCallback
+            = interface_cast<media::INativeAudioVolumeGroupCallback>(
+                    ibinderForJavaObject(env, jIAudioVolumeGroupCallback));
+    return AudioSystem::removeAudioVolumeGroupCallback(nIAudioVolumeGroupCallback);
+}
 
 // ----------------------------------------------------------------------------
 
@@ -3614,6 +3630,12 @@ static const JNINativeMethod gMethods[] =
          MAKE_JNI_NATIVE_METHOD("clearPreferredMixerAttributes",
                                 "(Landroid/media/AudioAttributes;II)I",
                                 android_media_AudioSystem_clearPreferredMixerAttributes),
+         MAKE_JNI_NATIVE_METHOD("registerAudioVolumeGroupCallback",
+                                "(Landroid/media/INativeAudioVolumeGroupCallback;)I",
+                                android_media_AudioSystem_registerAudioVolumeGroupCallback),
+         MAKE_JNI_NATIVE_METHOD("unregisterAudioVolumeGroupCallback",
+                                "(Landroid/media/INativeAudioVolumeGroupCallback;)I",
+                                android_media_AudioSystem_unregisterAudioVolumeGroupCallback),
          MAKE_AUDIO_SYSTEM_METHOD(supportsBluetoothVariableLatency),
          MAKE_AUDIO_SYSTEM_METHOD(setBluetoothVariableLatencyEnabled),
          MAKE_AUDIO_SYSTEM_METHOD(isBluetoothVariableLatencyEnabled),
