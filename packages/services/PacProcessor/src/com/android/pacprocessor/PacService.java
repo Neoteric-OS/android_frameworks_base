b9b3917d8d28daf8fd167b5dcb3e231db74b8727
Pavel Grafov <pgrafov@google.com>
2021-10-11 16:01:26 +0100

    Delay instantiation of PacProcessor until unlocked
    Webkit PAC processor cannot be created before the user is unlocked,
    this is currently causing crashes during boot.
    With this CL if the user isn't unlocked when the service is created,
    PacProcessor is instantiated upon receiving ACTION_USER_UNLOCKED.
    
    If any requests come prior to that, the service returns null, which
    is the same as "DIRECT", i.e. no proxy is used for those requests.
    
    Bug: 200241231
    Test: manual, set PAC proxy and reboot
    Change-Id: Ib85531efbb67f7bcfd71a0a3a7afa4a058319afa

--- packages/services/PacProcessor/src/com/android/pacprocessor/PacService.java
+++ packages/services/PacProcessor/src/com/android/pacprocessor/PacService.java
@@ -15,12 +15,18 @@
  */
 package com.android.pacprocessor;
 
+import static android.content.Intent.ACTION_USER_UNLOCKED;
+
 import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Binder;
 import android.os.IBinder;
 import android.os.Process;
 import android.os.RemoteException;
+import android.os.UserManager;
 import android.util.Log;
 import android.webkit.PacProcessor;
 
@@ -33,16 +39,71 @@ import java.net.URL;
 public class PacService extends Service {
     private static final String TAG = "PacService";
 
-    private Object mLock = new Object();
+    private final Object mLock = new Object();
+
+    // Webkit PacProcessor cannot be instantiated before the user is unlocked, so this field is
+    // initialized lazily.
+    @GuardedBy("mLock")
+    private PacProcessor mPacProcessor;
+
+    // Stores PAC script when setPacFile is called before mPacProcessor is available. In case the
+    // script was already fed to the PacProcessor, it should be null.
+    @GuardedBy("mLock")
+    private String mScript;
 
     @GuardedBy("mLock")
-    private final PacProcessor mPacProcessor = PacProcessor.getInstance();
+    BroadcastReceiver mUnlockReceiver;
 
     private ProxyServiceStub mStub = new ProxyServiceStub();
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        UserManager um = getSystemService(UserManager.class);
+        if (um.isUserUnlocked()) {
+            instantiatePacProcessor();
+        } else {
+            mUnlockReceiver = new BroadcastReceiver() {
+                @Override
+                public void onReceive(Context context, Intent intent) {
+                    Log.d(TAG, "User became unlocked.");
+                    instantiatePacProcessor();
+                }
+            };
+            registerReceiver(mUnlockReceiver, new IntentFilter(ACTION_USER_UNLOCKED));
+
+            // In theory it's possible that the user became unlocked and we missed the broadcast.
+            if (um.isUserUnlocked()) {
+                instantiatePacProcessor();
+            }
+        }
+    }
+
+    /**
+     * Instantiates PacProcessor and feeds in the script if it has been set already. Should only be
+     * called when the user is unlocked.
+     */
+    private void instantiatePacProcessor() {
+        synchronized (mLock) {
+            if (mPacProcessor != null) {
+                return;
+            }
+
+            mPacProcessor = PacProcessor.getInstance();
+            if (mScript != null) {
+                if (!mPacProcessor.setProxyScript(mScript)) {
+                    Log.e(TAG, "Unable to parse proxy script.");
+                }
+                mScript = null;
+            }
+
+            if (mUnlockReceiver != null) {
+                unregisterReceiver(mUnlockReceiver);
+                mUnlockReceiver = null;
+            }
+        }
+        Log.d(TAG, "Instantiated the processor.");
     }
 
     @Override
@@ -74,7 +135,12 @@ public class PacService extends Service {
                 }
 
                 synchronized (mLock) {
-                    return mPacProcessor.findProxyForUrl(url);
+                    if (mPacProcessor != null) {
+                        return mPacProcessor.findProxyForUrl(url);
+                    } else {
+                        Log.e(TAG, "PacProcessor isn't ready, request will be direct");
+                        return null;
+                    }
                 }
             } catch (MalformedURLException e) {
                 throw new IllegalArgumentException("Invalid URL was passed");
@@ -88,8 +154,16 @@ public class PacService extends Service {
                 throw new SecurityException();
             }
             synchronized (mLock) {
-                if (!mPacProcessor.setProxyScript(script)) {
-                    Log.e(TAG, "Unable to parse proxy script.");
+                synchronized (mLock) {
+                    mScript = null;
+                    if (mPacProcessor != null) {
+                        if (!mPacProcessor.setProxyScript(script)) {
+                            Log.e(TAG, "Unable to parse proxy script.");
+                        }
+                    } else {
+                        // Script will be applied when PAC processor is instantiated
+                        mScript = script;
+                    }
                 }
             }
         }
