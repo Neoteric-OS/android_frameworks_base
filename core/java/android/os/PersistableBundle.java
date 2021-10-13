70977780d5699a5dd29887eb81ec3d5fff37b6da
Bernardo Rufino <brufino@google.com>
2021-10-13 16:24:58 +0100

    Improve Parcel.hasFileDescriptors(Object)
    Used by bundle to check if there are file descriptors in the map. In
    this CL:
    * Add support for List, ArrayMap and Map.
    * Support recursive checks (ie. a list inside a list).
    
    Test: atest -d android.os.cts.ParcelTest android.os.cts.BundleTest android.os.BundleTest android.os.ParcelTest
    Change-Id: I2e8ebc686453937ef7e31f4838c2ad547c5f5b2a

--- core/java/android/os/PersistableBundle.java
+++ core/java/android/os/PersistableBundle.java
@@ -155,11 +155,9 @@ public final class PersistableBundle extends BaseBundle implements Cloneable, Pa
         mFlags = FLAG_DEFUSABLE;
     }
 
-    /**
-     * Constructs a PersistableBundle without initializing it.
-     */
-    PersistableBundle(boolean doInit) {
-        super(doInit);
+    /** @hide */
+    PersistableBundle(PersistableBundle from, boolean deep) {
+        super(from, deep);
     }
 
     /**
@@ -190,9 +188,7 @@ public final class PersistableBundle extends BaseBundle implements Cloneable, Pa
      * are referenced as-is and not copied in any way.
      */
     public PersistableBundle deepCopy() {
-        PersistableBundle b = new PersistableBundle(false);
-        b.copyInternal(this, true);
-        return b;
+        return new PersistableBundle(this, /* deep */ false);
     }
 
     /**
