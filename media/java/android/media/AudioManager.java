5de008dd067d4f729743d7582a17f62eee975953
wescande <wescande@google.com>
2021-10-19 20:15:09 +0200

    Add `SystemApi` tag on getLastAudibleStreamVolume
    Bug: 190422401
    Tag: #refactor
    Test: Build
    Change-Id: I2cbb69292465b4271caeb0c563fa1db62d025dce

--- media/java/android/media/AudioManager.java
+++ media/java/android/media/AudioManager.java
@@ -1171,7 +1171,8 @@ public class AudioManager {
      *
      * @hide
      */
-    @UnsupportedAppUsage
+    @SystemApi
+    @RequiresPermission("android.permission.QUERY_AUDIO_STATE")
     public int getLastAudibleStreamVolume(int streamType) {
         final IAudioService service = getService();
         try {
