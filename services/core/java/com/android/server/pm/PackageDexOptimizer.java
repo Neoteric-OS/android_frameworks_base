6f7e74b2b020021b2c11fe1331969b6e3ac8fa95
Ken <kenjc.bian@rock-chips.com>
2021-11-06 01:54:38 +0000

    Release odex/vdex compressed blocks to save user space.
    Compression support will be checked in F2fsUtils.
    
    The following log will be printed if odex/vdex is set to compress_extension:
    D F2fsUtils: RELEASED 7750 blocks from "/data/app/~~tmgeKyrWzsiW2-xCsONrSA==/com.xx.xx==/oat/arm/base.vdex"
    D F2fsUtils: RELEASED 80 blocks from "/data/app/~~tmgeKyrWzsiW2-xCsONrSA==/com.xx.xx==/oat/arm/base.odex"
    
    Bug: 205257122
    Signed-off-by: Ken Bian <kenjc.bian@rock-chips.com>
    Change-Id: I5b554aa27a07c94d5a58c86c889131fe4b944720

--- services/core/java/com/android/server/pm/PackageDexOptimizer.java
+++ services/core/java/com/android/server/pm/PackageDexOptimizer.java
@@ -45,6 +45,7 @@ import static dalvik.system.DexFile.isProfileGuidedCompilerFilter;
 
 import android.annotation.NonNull;
 import android.annotation.Nullable;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.SharedLibraryInfo;
@@ -63,6 +64,7 @@ import android.util.Slog;
 import android.util.SparseArray;
 
 import com.android.internal.annotations.GuardedBy;
+import com.android.internal.content.F2fsUtils;
 import com.android.internal.util.IndentingPrintWriter;
 import com.android.server.pm.Installer.InstallerException;
 import com.android.server.pm.dex.ArtManagerService;
@@ -108,10 +110,12 @@ public class PackageDexOptimizer {
 
     private final ArtStatsLogger mArtStatsLogger = new ArtStatsLogger();
 
+    private final Context mContext;
     private static final Random sRandom = new Random();
 
     PackageDexOptimizer(Installer installer, Object installLock, Context context,
             String wakeLockTag) {
+        this.mContext = context;
         this.mInstaller = installer;
         this.mInstallLock = installLock;
 
@@ -120,6 +124,7 @@ public class PackageDexOptimizer {
     }
 
     protected PackageDexOptimizer(PackageDexOptimizer from) {
+        this.mContext = from.mContext;
         this.mInstaller = from.mInstaller;
         this.mInstallLock = from.mInstallLock;
         this.mDexoptWakeLock = from.mDexoptWakeLock;
@@ -349,6 +354,13 @@ public class PackageDexOptimizer {
             if (packageStats != null) {
                 long endTime = System.currentTimeMillis();
                 packageStats.setCompileTime(path, (int)(endTime - startTime));
+            }
+            if (oatDir != null) {
+                // Release odex/vdex compressed blocks to save user space.
+                // Compression support will be checked in F2fsUtils.
+                // The system app may be dexed, oatDir may be null, skip this situation.
+                final ContentResolver resolver = mContext.getContentResolver();
+                F2fsUtils.releaseCompressedBlocks(resolver, new File(oatDir));
             }
             return DEX_OPT_PERFORMED;
         } catch (InstallerException e) {
