39857f7034f17d292ee68e7f52d3e60f371b145f
Devin Moore <devinmoore@google.com>
2021-11-08 17:49:19 +0000

    Use new AIDL IR interface for ConsumerIr service
    Continue using the HIDL service if it is registered, otherwise use the
    AIDL service.
    
    Test: atest ConsumerIrTest VtsHalIrTargetTest hal_implementation_test
    Bug: 205000342
    Change-Id: I648aa765d14c4f5d4c862523a1bb69b2e7bc5cc1

--- services/core/java/com/android/server/ConsumerIrService.java
+++ services/core/java/com/android/server/ConsumerIrService.java
@@ -19,10 +19,12 @@ package com.android.server;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.hardware.IConsumerIrService;
+import android.hardware.ir.ConsumerIrFreqRange;
+import android.hardware.ir.IConsumerIr;
 import android.os.PowerManager;
+import android.os.RemoteException;
+import android.os.ServiceManager;
 import android.util.Slog;
-
-import java.lang.RuntimeException;
 
 public class ConsumerIrService extends IConsumerIrService.Stub {
     private static final String TAG = "ConsumerIrService";
@@ -37,6 +39,7 @@ public class ConsumerIrService extends IConsumerIrService.Stub {
     private final PowerManager.WakeLock mWakeLock;
     private final boolean mHasNativeHal;
     private final Object mHalLock = new Object();
+    private IConsumerIr mAidlService = null;
 
     ConsumerIrService(Context context) {
         mContext = context;
@@ -45,7 +48,8 @@ public class ConsumerIrService extends IConsumerIrService.Stub {
         mWakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, TAG);
         mWakeLock.setReferenceCounted(true);
 
-        mHasNativeHal = halOpen();
+        mHasNativeHal = openHal();
+
         if (mContext.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CONSUMER_IR)) {
             if (!mHasNativeHal) {
                 throw new RuntimeException("FEATURE_CONSUMER_IR present, but no IR HAL loaded!");
@@ -58,6 +62,17 @@ public class ConsumerIrService extends IConsumerIrService.Stub {
     @Override
     public boolean hasIrEmitter() {
         return mHasNativeHal;
+    }
+
+    private boolean openHal() {
+        // Attempt to get the HIDL hal first.
+        if (halOpen()) {
+            return true;
+        }
+        final String fqName = IConsumerIr.DESCRIPTOR + "/default";
+        mAidlService = IConsumerIr.Stub.asInterface(
+                        ServiceManager.waitForDeclaredService(fqName));
+        return mAidlService != null;
     }
 
     private void throwIfNoIrEmitter() {
@@ -91,10 +106,18 @@ public class ConsumerIrService extends IConsumerIrService.Stub {
 
         // Right now there is no mechanism to ensure fair queing of IR requests
         synchronized (mHalLock) {
-            int err = halTransmit(carrierFrequency, pattern);
+            if (mAidlService != null) {
+                try {
+                    mAidlService.transmit(carrierFrequency, pattern);
+                } catch (RemoteException ignore) {
+                    Slog.e(TAG, "Error transmitting frequency: " + carrierFrequency);
+                }
+            } else {
+                int err = halTransmit(carrierFrequency, pattern);
 
-            if (err < 0) {
-                Slog.e(TAG, "Error transmitting: " + err);
+                if (err < 0) {
+                    Slog.e(TAG, "Error transmitting: " + err);
+                }
             }
         }
     }
@@ -109,7 +132,24 @@ public class ConsumerIrService extends IConsumerIrService.Stub {
         throwIfNoIrEmitter();
 
         synchronized(mHalLock) {
-            return halGetCarrierFrequencies();
+            if (mAidlService != null) {
+                try {
+                    ConsumerIrFreqRange[] output = mAidlService.getCarrierFreqs();
+                    if (output.length <= 0) {
+                        Slog.e(TAG, "Error getting carrier frequencies.");
+                    }
+                    int[] result = new int[output.length * 2];
+                    for (int i = 0; i < output.length; i++) {
+                        result[i * 2] = output[i].minHz;
+                        result[i * 2 + 1] = output[i].maxHz;
+                    }
+                    return result;
+                } catch (RemoteException ignore) {
+                    return new int[0];
+                }
+            } else {
+                return halGetCarrierFrequencies();
+            }
         }
     }
 }
