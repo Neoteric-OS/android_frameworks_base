49efcc0fbe15ae63d1db641df6561da42209dcba
Kalesh Singh <kaleshsingh@google.com>
2021-11-18 12:56:27 -0800

    Fix GL memory not being removed from lost RAM
    Devices upgrading to S are not required to update their memtrack HAL
    implementation and won't support reporting the total GPU private memory.
    
    In these cases it's likely the HAL reports 0 for the total GPU private
    memory, since in S the HAL defition only overloads the already existing
    getMemory() API to report this. Prevent swapping the sum of GL type memory
    for all processes with the total GPU private memory when not supported,
    as doing this effectively leaves the GL memory as part of lost RAM.
    
    Bug: 203245390
    Test: dumpsys meminfo
    Change-Id: Ib095e4e1eab3ca20c2475382dea9b25ca638f495

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -11059,7 +11059,18 @@ public class ActivityManagerService extends IActivityManager.Stub
             final long gpuUsage = Debug.getGpuTotalUsageKb();
             if (gpuUsage >= 0) {
                 final long gpuPrivateUsage = Debug.getGpuPrivateMemoryKb();
-                if (gpuPrivateUsage >= 0) {
+
+                int apiLevel = SystemProperties.getInt("ro.product.first_api_level", 10000);
+                apiLevel = Math.min(apiLevel, SystemProperties.getInt("ro.board.api_level", 10000));
+                apiLevel = Math.min(apiLevel, SystemProperties.getInt("ro.first_board.api_level",
+                            10000));
+
+                // Check the first API level is at least S.
+                // Upgrading devices are not required to update their memtrack HAL version,
+                // as the policy is to not change old compatibility matrices
+                // Old HAL implementations may return 0 for GPU private memory if not supported
+                if (gpuPrivateUsage > 0
+                        || (apiLevel >= Build.VERSION_CODES.S && gpuPrivateUsage >= 0)) {
                     final long gpuDmaBufUsage = gpuUsage - gpuPrivateUsage;
                     pw.print("      GPU: ");
                     pw.print(stringifyKBSize(gpuUsage));
