f5bc0d7db925d4d4213e7885f55218b7ea7f238c
tadvana <tadvana@google.com>
2021-11-30 14:03:40 -0800

    Adds Car Projection role RFCOMM listener.
    This change introduces logic to the BluetoothManagerService which
    registers an RFCOMM socket server.
    The socket server registers a resource file defined UUID.
    Incoming socket connections are sent to the app holding the car
    projection role.
    
    Test: Run the CTS Bluetooth Car Projection Test. This requires two
    devices.
    
    Change-Id: I730aed198118dfc4fa690e4ba02e5fc6c65ea567

---
+++ core/java/android/bluetooth/BluetoothCarProjectionService.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.bluetooth;
+
+import android.annotation.SystemApi;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.os.ParcelFileDescriptor;
+
+/**
+ * The system application service intending to handle incoming Bluetooth RFCOMM connections for car
+ * projection should implement this class.
+ *
+ * @hide
+ */
+@SystemApi
+public abstract class BluetoothCarProjectionService extends Service {
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return new BluetoothCarProjectionBinder();
+    }
+
+    /** Handles incoming bluetooth socket connections intended for car projection. */
+    protected abstract void onSocketReceived(ParcelFileDescriptor fd, String hostAddress);
+
+    private final class BluetoothCarProjectionBinder extends IBluetoothCarProjection.Stub {
+        @Override
+        public void sendSocketFileDescriptor(ParcelFileDescriptor fd, String hostAddress) {
+            onSocketReceived(fd, hostAddress);
+        }
+    }
+}
