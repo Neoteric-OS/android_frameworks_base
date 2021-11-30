69b5efec92c273d5ce5d1935e97066134d303138
Jiakai Zhang <jiakaiz@google.com>
2021-11-30 10:45:09 +0000

    Prefetch standalone system server jars before switching domain.
    In b/203198541, we start to compile standalone system server jars, but
    the SELinux policy doesn't allow system_server to load the compiled code
    at runtime. As a solution, this change creates class loaders for
    standalone system server jars and caches them before switching to the
    "system_server" SELinux domain, which essentially loads the jars and
    compiled code into the memory.
    
    Bug: 203198541
    Test: manual -
      1. Patch aosp/1874113, aosp/1876173, aosp/1906158 (in order to have
         odrefresh compile a standalone system server jar).
      2. Build a system image and flash it to a device.
      3. Run "adb shell cat /proc/`adb shell pidof system_server`/maps" and
         see "apex@com.android.wifi@javalib@service-wifi.jar@classes.odex"
         being mmap'ed.
      4. Run "adb logcat | grep avc" and see no denial.
      5. atest odsign_e2e_tests
    
    Change-Id: I365be766cfb77c6e240d5871f1b3d8ed7abae6f5

--- core/java/com/android/internal/os/ZygoteInit.java
+++ core/java/com/android/internal/os/ZygoteInit.java
@@ -46,6 +46,7 @@ import android.system.OsConstants;
 import android.system.StructCapUserData;
 import android.system.StructCapUserHeader;
 import android.text.Hyphenator;
+import android.text.TextUtils;
 import android.util.EventLog;
 import android.util.Log;
 import android.util.Slog;
@@ -573,6 +574,30 @@ public class ZygoteInit {
             }
         }
         return sCachedSystemServerClassLoader;
+    }
+
+    /**
+     * Creates class loaders for standalone system server jars. This function may be called through
+     * JNI in system server startup, when the runtime is in a critically low state. Do not do
+     * extended computation etc here.
+     */
+    private static void prefetchStandaloneSystemServerJars() {
+        String envStr = Os.getenv("STANDALONE_SYSTEMSERVER_JARS");
+        if (TextUtils.isEmpty(envStr)) {
+            return;
+        }
+        for (String jar : envStr.split(":")) {
+            try {
+                SystemServerClassLoaderFactory.getOrCreateClassLoader(
+                        jar, getOrCreateSystemServerClassLoader());
+            } catch (Error e) {
+                // We don't want the process to crash for this error because prefetching is just an
+                // optimization.
+                Log.e(TAG,
+                        String.format("Failed to prefetch standalone system server jar \"%s\": %s",
+                                jar, e.toString()));
+            }
+        }
     }
 
     /**
