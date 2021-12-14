290794fa8091e23c7ae7d923e29d3c4d0ce7d1ae
paulhu <paulhu@google.com>
2021-12-14 23:09:55 +0800

    Split out the nsd API surface and use framework-connectivity-tiramisu
    - Split out nsd APIs and hidden APIs to Connectivity module.
    - Keep the all files in f/b/packages/Connectivity-T and add
      framework-connectivity-tiramisu library on p/m/Connectivity.
      Then make framework to use this module library.
    - Remove NsdService from SystemServer which will register from
      ConnectivityServiceInitializer
    
    Bug: 206893064
    Test: m online-sdk-dev-docs
    Test: atest FrameworksNetTests CtsNetTestCases
    CTS-Coverage-Bug: 207804007
    Merged-In: I72c7de124b9cc53f2263bd452ecd5f39e88e485b
    Change-Id: I72c7de124b9cc53f2263bd452ecd5f39e88e485b

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -1339,7 +1339,6 @@ public final class SystemServer implements Dumpable {
         VcnManagementService vcnManagement = null;
         NetworkStatsService networkStats = null;
         NetworkPolicyManagerService networkPolicy = null;
-        NsdService serviceDiscovery = null;
         WindowManagerService wm = null;
         SerialService serial = null;
         NetworkTimeUpdateService networkTimeUpdater = null;
@@ -1939,16 +1938,6 @@ public final class SystemServer implements Dumpable {
                 ServiceManager.addService(Context.VCN_MANAGEMENT_SERVICE, vcnManagement);
             } catch (Throwable e) {
                 reportWtf("starting VCN Management Service", e);
-            }
-            t.traceEnd();
-
-            t.traceBegin("StartNsdService");
-            try {
-                serviceDiscovery = NsdService.create(context);
-                ServiceManager.addService(
-                        Context.NSD_SERVICE, serviceDiscovery);
-            } catch (Throwable e) {
-                reportWtf("starting Service Discovery Service", e);
             }
             t.traceEnd();
 
