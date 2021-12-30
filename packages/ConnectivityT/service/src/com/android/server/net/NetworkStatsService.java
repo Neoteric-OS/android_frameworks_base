0fcdbd0aa8637ff29c3a56f81b8587fb1aed6710
Junyu Lai <junyulai@google.com>
2021-12-30 10:49:29 +0000

    [MS31] Fix several hidden API usages
    This includes:
    1. Replace UserHandle.getUid(userId, appId) with public API
    2. Remove Preconditions.checkArgument
    3. Replace MathUtil.constrain with the one in NetworkStatsUtils
    4. Remove Sets
    5. Replace NetworkStack.checkNetworkStackPermission with the
       one in the PermissionUtils
    6. Replace internal CollectionUtils with the one in the module.
    7. Inline ProtoOutputStream(fd)
    
    Test: atest NetworkStatsSubscriptionsMonitorTest \
          NetworkStatsServiceTest
    Bug: 204830222
    
    Change-Id: If06a27b04ad15c29052b670eefc2f6dc0d199420

--- packages/ConnectivityT/service/src/com/android/server/net/NetworkStatsService.java
+++ packages/ConnectivityT/service/src/com/android/server/net/NetworkStatsService.java
@@ -25,7 +25,6 @@ import static android.content.Intent.ACTION_USER_REMOVED;
 import static android.content.Intent.EXTRA_UID;
 import static android.content.pm.PackageManager.PERMISSION_GRANTED;
 import static android.net.NetworkIdentity.SUBTYPE_COMBINED;
-import static android.net.NetworkStack.checkNetworkStackPermission;
 import static android.net.NetworkStats.DEFAULT_NETWORK_ALL;
 import static android.net.NetworkStats.IFACE_ALL;
 import static android.net.NetworkStats.IFACE_VT;
@@ -158,6 +157,7 @@ import com.android.server.LocalServices;
 
 import java.io.File;
 import java.io.FileDescriptor;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.time.Clock;
@@ -985,7 +985,7 @@ public class NetworkStatsService extends INetworkStatsService.Stub {
             @NonNull NetworkStateSnapshot[] networkStates,
             @Nullable String activeIface,
             @NonNull UnderlyingNetworkInfo[] underlyingNetworkInfos) {
-        checkNetworkStackPermission(mContext);
+        PermissionUtils.enforceNetworkStackPermission(mContext);
 
         final long token = Binder.clearCallingIdentity();
         try {
@@ -1232,7 +1232,7 @@ public class NetworkStatsService extends INetworkStatsService.Stub {
         @Override
         public void limitReached(String limitName, String iface) {
             // only someone like NMS should be calling us
-            NetworkStack.checkNetworkStackPermission(mContext);
+            PermissionUtils.enforceNetworkStackPermission(mContext);
 
             if (LIMIT_GLOBAL_ALERT.equals(limitName)) {
                 // kick off background poll to collect network stats unless there is already
@@ -1661,7 +1661,7 @@ public class NetworkStatsService extends INetworkStatsService.Stub {
                 PackageManager.MATCH_ANY_USER
                 | PackageManager.MATCH_DISABLED_COMPONENTS);
         for (ApplicationInfo app : apps) {
-            final int uid = UserHandle.getUid(userId, app.uid);
+            final int uid = UserHandle.of(userId).getUid(app.uid);
             uids.add(uid);
         }
 
@@ -1869,7 +1869,7 @@ public class NetworkStatsService extends INetworkStatsService.Stub {
 
     @GuardedBy("mStatsLock")
     private void dumpProtoLocked(FileDescriptor fd) {
-        final ProtoOutputStream proto = new ProtoOutputStream(fd);
+        final ProtoOutputStream proto = new ProtoOutputStream(new FileOutputStream(fd));
 
         // TODO Right now it writes all history.  Should it limit to the "since-boot" log?
 
