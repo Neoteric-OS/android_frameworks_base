6a75c496752c9e0401cab8b91bacbc327d50c377
Roopa Sattiraju <sattiraju@google.com>
2022-01-04 09:42:52 -0800

    Migrating BT code in frameworks and services into packages/modules/Bluetooth.
    Note: Still builds the Bluetooth APK and not an APEX. APEX cannot be
    built on AOSP until Android T lands on AOSP.
    
    Bug: 196026708
    Test: Compile
    Change-Id: Ic4359a854de9f09587a9adb7359818deb6d2ce03

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -387,6 +387,7 @@ public final class SystemServer implements Dumpable {
     private static final String GAME_MANAGER_SERVICE_CLASS =
             "com.android.server.app.GameManagerService$Lifecycle";
     private static final String UWB_SERVICE_CLASS = "com.android.server.uwb.UwbService";
+    private static final String BLUETOOTH_SERVICE_CLASS = "com.android.server.bluetooth.BluetoothService";
 
     private static final String TETHERING_CONNECTOR_CLASS = "android.net.ITetheringConnector";
 
@@ -1538,7 +1539,7 @@ public final class SystemServer implements Dumpable {
                 Slog.i(TAG, "No Bluetooth Service (Bluetooth Hardware Not Present)");
             } else {
                 t.traceBegin("StartBluetoothService");
-                mSystemServiceManager.startService(BluetoothService.class);
+                mSystemServiceManager.startService(BLUETOOTH_SERVICE_CLASS);
                 t.traceEnd();
             }
 
