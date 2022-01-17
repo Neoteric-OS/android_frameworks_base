673e5ec8d58a39ad04c4ed68e6bf9eb00fbbf5f9
Etan Cohen <etancohen@google.com>
2022-01-17 19:38:34 +0000

    Suppress incorrect lint warnings about new API
    IpConfiguration and StaticIpConfiguration APIs were moved from SystemApi
    to public (SDK) APIs. The linter incorrectly triggers on code which
    built against the SystemApi as using new API.
    
    Tracking bug b/193460475 - TODO to remove once fixed.
    
    Bug: 209840828
    Test: builds errorprone
    Change-Id: Ie0622d4084144a6d7bdd16265b5f9cf138707022

--- core/java/android/net/DhcpResults.java
+++ core/java/android/net/DhcpResults.java
@@ -17,6 +17,7 @@
 package android.net;
 
 import android.annotation.Nullable;
+import android.annotation.SuppressLint;
 import android.compat.annotation.UnsupportedAppUsage;
 import android.os.Build;
 import android.os.Parcel;
@@ -79,6 +80,7 @@ public final class DhcpResults implements Parcelable {
     /**
      * Create a {@link StaticIpConfiguration} based on the DhcpResults.
      */
+    @SuppressLint("NewApi") // TODO: b/193460475 remove once fixed
     public StaticIpConfiguration toStaticIpConfiguration() {
         return new StaticIpConfiguration.Builder()
                 .setIpAddress(ipAddress)
@@ -88,6 +90,7 @@ public final class DhcpResults implements Parcelable {
                 .build();
     }
 
+    @SuppressLint("NewApi") // TODO: b/193460475 remove once fixed
     public DhcpResults(StaticIpConfiguration source) {
         if (source != null) {
             ipAddress = source.getIpAddress();
@@ -206,6 +209,7 @@ public final class DhcpResults implements Parcelable {
         return 0;
     }
 
+    @SuppressLint("NewApi") // TODO: b/193460475 remove once fixed
     private static DhcpResults readFromParcel(Parcel in) {
         final StaticIpConfiguration s = StaticIpConfiguration.CREATOR.createFromParcel(in);
         final DhcpResults dhcpResults = new DhcpResults(s);
