19d277c9d72ede7b054903b881b19284ba85b9b3
Lorenzo Colitti <lorenzo@google.com>
2022-01-20 11:40:22 +0900

    Revert "WIP: expose createConnectedLocalSocket."
    This reverts commit b11bf72c24fda272c47567577c907d61e84057ca.
    
    Change-Id: I9a993f4a2374d2ae8445782e4f32bc73e04d09fc

--- core/java/android/net/LocalSocket.java
+++ core/java/android/net/LocalSocket.java
@@ -16,11 +16,6 @@
 
 package android.net;
 
-import static android.annotation.SystemApi.Client.MODULE_LIBRARIES;
-
-import android.annotation.NonNull;
-import android.annotation.SuppressLint;
-import android.annotation.SystemApi;
 import android.compat.annotation.UnsupportedAppUsage;
 
 import java.io.Closeable;
@@ -86,10 +81,7 @@ public class LocalSocket implements Closeable {
      *
      * @hide - used by BluetoothSocket.
      */
-    @SystemApi(client = SystemApi.Client.MODULE_LIBRARIES)
-    @SuppressLint("UseParcelFileDescriptor")
-    @NonNull
-    public static LocalSocket createConnectedLocalSocket(@NonNull FileDescriptor fd) {
+    public static LocalSocket createConnectedLocalSocket(FileDescriptor fd) {
         return createConnectedLocalSocket(new LocalSocketImpl(fd), SOCKET_UNKNOWN);
     }
 
