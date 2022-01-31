e3ad4a5cece66892efd26a377ded7296229d82e8
Galia Peycheva <galinap@google.com>
2022-01-31 11:46:05 +0100

    Add background blur activity to silkfx
    Bug: 213292227
    Test: m SilkFX && adb install $OUT/testcases/SilkFX/arm/SilkFX.apk
    Test: adb shell am start -n com.android.test.silkfx/.materials.BackgroundBlurActivity
    Change-Id: I1692a53460b64b5a49d14736b2b680069217fa1d

--- tests/SilkFX/src/com/android/test/silkfx/Main.kt
+++ tests/SilkFX/src/com/android/test/silkfx/Main.kt
@@ -30,6 +30,7 @@ import com.android.test.silkfx.app.EXTRA_LAYOUT
 import com.android.test.silkfx.app.EXTRA_TITLE
 import com.android.test.silkfx.hdr.GlowActivity
 import com.android.test.silkfx.materials.GlassActivity
+import com.android.test.silkfx.materials.BackgroundBlurActivity
 import kotlin.reflect.KClass
 
 class Demo(val name: String, val makeIntent: (Context) -> Intent) {
@@ -51,7 +52,8 @@ private val AllDemos = listOf(
                 Demo("Blingy Notifications", R.layout.bling_notifications)
         )),
         DemoGroup("Materials", listOf(
-                Demo("Glass", GlassActivity::class)
+                Demo("Glass", GlassActivity::class),
+                Demo("Background Blur", BackgroundBlurActivity::class)
         ))
 )
 
