f355b5df5355f4f2ef9829c4616f68d9b074bc0a
ot903277 <ot_youyao.hu@mediatek.com>
2022-02-10 11:09:26 +0800

    Fix weak global reference overflow
    When the last item of sRunningAttributionSources is removed, the current
    check condition will cause the process of removing
    sRunningAttributionSources stopped and return directly, bring in weak
    global reference table overflow.
    
    Change the order for smooth removal.
    
    Bug: 218794347
    Test: run audio stress test pass
    
    Change-Id: I17e38cd8f2cb1ef922faee279e21e05c496c2103

--- services/core/java/com/android/server/pm/permission/PermissionManagerService.java
+++ services/core/java/com/android/server/pm/permission/PermissionManagerService.java
@@ -5689,15 +5689,17 @@ public class PermissionManagerService extends IPermissionManager.Stub {
                     appOpsManager.finishProxyOp(AppOpsManager.opToPublicName(op),
                             resolvedAttributionSource, skipCurrentFinish);
                 }
-
-                if (next == null || next.getNext() == null) {
-                    return;
-                }
-
                 RegisteredAttribution registered =
                         sRunningAttributionSources.remove(current.getToken());
-                if (registered != null) {
+                if (registered != null && next != null && next.getNext() != null)
                     registered.unregister();
+                }
+
+                if (next == null || next.getNext() == null) {
+                    if (next != null) {
+                        sRunningAttributionSources.remove(next.getToken());
+                    }
+                    return;
                 }
                 current = next;
             }
