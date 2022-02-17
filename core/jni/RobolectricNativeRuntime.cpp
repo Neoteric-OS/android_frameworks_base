a00eb9720a78a727a676e3f66dc9f78dedb49bfc
JuliaSullivanGoogle <JulianSull@google.com>
2022-02-17 19:33:04 +0000

    Add FontFamily to RobolectricNativeRuntime.cpp.
    Test: m -j robolectric_native_runtime dist
    Change-Id: I3f880c47c9768052af358bab9b2500dc1deca406

--- core/jni/RobolectricNativeRuntime.cpp
+++ core/jni/RobolectricNativeRuntime.cpp
@@ -8,6 +8,7 @@
 static JavaVM* javaVM;
 
 extern int register_android_graphics_Graphics(JNIEnv* env);
+extern int register_android_graphics_Typeface(JNIEnv* env);
 
 namespace android {
 
@@ -15,6 +16,7 @@ extern int register_android_database_CursorWindow(JNIEnv* env);
 extern int register_android_database_SQLiteConnection(JNIEnv* env);
 extern int register_android_graphics_Matrix(JNIEnv* env);
 extern int register_android_graphics_Paint(JNIEnv* env);
+extern int register_android_graphics_FontFamily(JNIEnv* env);
 
 #define REG_JNI(name) \
     { name }
@@ -31,6 +33,8 @@ static const RegJNIRec graphicsJNI[] = {
         REG_JNI(register_android_graphics_Matrix),
         REG_JNI(register_android_graphics_Graphics),
         REG_JNI(register_android_graphics_Paint),
+        REG_JNI(register_android_graphics_Typeface),
+        REG_JNI(register_android_graphics_FontFamily),
 };
 
 JNIEnv* AndroidRuntime::getJNIEnv() {
