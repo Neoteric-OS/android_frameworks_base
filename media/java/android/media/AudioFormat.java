0634dd2c5cf687a2b059a281f1030d7c73c2bc29
Rahul <rahul.mohan@xperi.com>
2022-02-21 10:25:08 +0530

    Audio: Add ENCODING_DTS_HD_MA, ENCODING_DTS_UHD_P2
    The DTS codec capability in sink devices can be DTS Digital Surround,
    DTS HD, DTS HD Master Audio(Coreless Lossless), DTS:X Profile 1,
    or DTS:X Profile 2.
    
    The AudioFormat class already supports ENCODING_DTS, ENCODING_DTS_HD,
    and ENCODING_DTS_UHD compressed formats. To distinguish all the
    five categories of DTS codec capabilities, two more compressed
    formats need to be added in AudioFormat class.
    
     - The ENCODING_DTS_HD_MA format represents codec capability for
       DTS HD Master Audio(coreless lossless)
    
     - The ENCODING_DTS_UHD_P1 format represents DTS:X Profile 1
       codec capability and is the same as existing ENCODING_DTS_UHD
    
     - The ENCODING_DTS_UHD_P2 format represents DTS:X Profile 2
       codec capability
    
    The codec capability represented by the existing formats are:
    
     - ENCODING_DTS is for DTS Digital Surround
    
     - ENCODING_DTS_HD is for DTS HD(DTS 8Ch Discrete, DTS High Resolution,
       DTS Express, DTS HD Master Audio with Core)
    
     - ENCODING_DTS_UHD is for DTS:X Profile 1
    
    Test: atest AudioFormatTest
    Change-Id: Id4d332cc75beee40cfe8637a4ecef18a0d2a0908

--- media/java/android/media/AudioFormat.java
+++ media/java/android/media/AudioFormat.java
@@ -350,10 +350,27 @@ public final class AudioFormat implements Parcelable {
     public static final int ENCODING_MPEGH_LC_L3 = 25;
     /** Audio data format: MPEG-H low complexity profile, level 4 */
     public static final int ENCODING_MPEGH_LC_L4 = 26;
-    /** Audio data format: DTS UHD compressed */
+    /** Audio data format: DTS UHD Profile-1 compressed (aka DTS:X Profile 1)
+     * Has the same meaning and value as ENCODING_DTS_UHD_P1 */
     public static final int ENCODING_DTS_UHD = 27;
     /** Audio data format: DRA compressed */
     public static final int ENCODING_DRA = 28;
+    /** Audio data format: DTS HD Master Audio compressed
+     * DTS HD Master Audio stream is variable bit rate and contains lossless audio.
+     * Use {@link #ENCODING_DTS_HD_MA} for lossless audio content(DTS-HD MA Coreless Lossless)
+     * and use {@link #ENCODING_DTS_HD} for other DTS bitstreams with extension substream
+     * (DTS 8Ch Discrete, DTS Hi Res, DTS Express, DTS-HD MA with core substream). */
+    public static final int ENCODING_DTS_HD_MA = 29;
+    /** Audio data format: DTS UHD Profile-1 compressed (aka DTS:X Profile 1)
+     * Has the same meaning and value as ENCODING_DTS_UHD */
+    public static final int ENCODING_DTS_UHD_P1 = 27;
+    /** Audio data format: DTS UHD Profile-2 compressed
+     * DTS-UHD Profile-2 supports delivery of Channel-Based Audio, Object-Based Audio
+     * and High Order Ambisonic presentations up to the fourth order.
+     * Use {@link #ENCODING_DTS_UHD} to transmit DTS UHD Profile 1(aka DTS:X Profile 1) bitstream.
+     * Use {@link #ENCODING_DTS_UHD_P2} to transmit DTS UHD Profile 2(aka DTS:X Profile 2)
+     * bitstream. */
+    public static final int ENCODING_DTS_UHD_P2 = 30;
 
     /** @hide */
     public static String toLogFriendlyEncoding(int enc) {
@@ -414,6 +431,10 @@ public final class AudioFormat implements Parcelable {
                 return "ENCODING_DTS_UHD";
             case ENCODING_DRA:
                 return "ENCODING_DRA";
+            case ENCODING_DTS_HD_MA:
+                return "ENCODING_DTS_HD_MA";
+            case ENCODING_DTS_UHD_P2:
+                return "ENCODING_DTS_UHD_P2";
             default :
                 return "invalid encoding " + enc;
         }
@@ -770,6 +791,8 @@ public final class AudioFormat implements Parcelable {
             case ENCODING_MPEGH_LC_L4:
             case ENCODING_DTS_UHD:
             case ENCODING_DRA:
+            case ENCODING_DTS_HD_MA:
+            case ENCODING_DTS_UHD_P2:
                 return true;
             default:
                 return false;
@@ -807,6 +830,8 @@ public final class AudioFormat implements Parcelable {
             case ENCODING_MPEGH_LC_L4:
             case ENCODING_DTS_UHD:
             case ENCODING_DRA:
+            case ENCODING_DTS_HD_MA:
+            case ENCODING_DTS_UHD_P2:
                 return true;
             default:
                 return false;
@@ -847,6 +872,8 @@ public final class AudioFormat implements Parcelable {
             case ENCODING_MPEGH_LC_L4:
             case ENCODING_DTS_UHD:
             case ENCODING_DRA:
+            case ENCODING_DTS_HD_MA:
+            case ENCODING_DTS_UHD_P2:
                 return false;
             case ENCODING_INVALID:
             default:
@@ -887,6 +914,8 @@ public final class AudioFormat implements Parcelable {
             case ENCODING_MPEGH_LC_L4:
             case ENCODING_DTS_UHD:
             case ENCODING_DRA:
+            case ENCODING_DTS_HD_MA:
+            case ENCODING_DTS_UHD_P2:
                 return false;
             case ENCODING_INVALID:
             default:
@@ -1175,6 +1204,8 @@ public final class AudioFormat implements Parcelable {
                 case ENCODING_MPEGH_LC_L4:
                 case ENCODING_DTS_UHD:
                 case ENCODING_DRA:
+                case ENCODING_DTS_HD_MA:
+                case ENCODING_DTS_UHD_P2:
                     mEncoding = encoding;
                     break;
                 case ENCODING_INVALID:
@@ -1403,7 +1434,9 @@ public final class AudioFormat implements Parcelable {
         ENCODING_MPEGH_LC_L3,
         ENCODING_MPEGH_LC_L4,
         ENCODING_DTS_UHD,
-        ENCODING_DRA }
+        ENCODING_DRA,
+        ENCODING_DTS_HD_MA,
+        ENCODING_DTS_UHD_P2 }
     )
     @Retention(RetentionPolicy.SOURCE)
     public @interface Encoding {}
@@ -1424,7 +1457,9 @@ public final class AudioFormat implements Parcelable {
             ENCODING_MPEGH_LC_L3,
             ENCODING_MPEGH_LC_L4,
             ENCODING_DTS_UHD,
-            ENCODING_DRA
+            ENCODING_DRA,
+            ENCODING_DTS_HD_MA,
+            ENCODING_DTS_UHD_P2
     };
 
     /** @hide */
@@ -1443,7 +1478,9 @@ public final class AudioFormat implements Parcelable {
             ENCODING_MPEGH_LC_L3,
             ENCODING_MPEGH_LC_L4,
             ENCODING_DTS_UHD,
-            ENCODING_DRA }
+            ENCODING_DRA,
+            ENCODING_DTS_HD_MA,
+            ENCODING_DTS_UHD_P2 }
     )
     @Retention(RetentionPolicy.SOURCE)
     public @interface SurroundSoundEncoding {}
@@ -1486,9 +1523,13 @@ public final class AudioFormat implements Parcelable {
             case ENCODING_MPEGH_LC_L4:
                 return "MPEG-H 3D Audio low complexity profile level 4";
             case ENCODING_DTS_UHD:
-                return "DTS UHD";
+                return "DTS UHD Profile 1";
             case ENCODING_DRA:
                 return "DRA";
+            case ENCODING_DTS_HD_MA:
+                return "DTS HD Master Audio";
+            case ENCODING_DTS_UHD_P2:
+                return "DTS UHD Profile 2";
             default:
                 return "Unknown surround sound format";
         }
