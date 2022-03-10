3f6582f9dec07df6ad6664f495eadf517bc91b40
Joe Bolinger <jbolinger@google.com>
2022-03-09 22:34:20 -0800

    Do not lock when biometric lock status is cleared.
    This can lead to a rapid unlock -> lock -> unlock cycle while keyguard is showing.
    
    Bug: 210897417
    Test: manual (unlock with fingerprint and verify adb logcat | grep keystore2::authorization shows 1 entry)
    Change-Id: I5e67025269da2f6c5e356d31279c409f9229f990

--- packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
+++ packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
@@ -1791,7 +1791,7 @@ public class KeyguardViewMediator extends SystemUI implements Dumpable,
         }
     };
 
-    public void keyguardDone() {
+    private void keyguardDone() {
         Trace.beginSection("KeyguardViewMediator#keyguardDone");
         if (DEBUG) Log.d(TAG, "keyguardDone()");
         userActivity();
@@ -1939,9 +1939,8 @@ public class KeyguardViewMediator extends SystemUI implements Dumpable,
             resetKeyguardDonePendingLocked();
         }
 
-
         if (mGoingToSleep) {
-            mUpdateMonitor.clearBiometricRecognized();
+            mUpdateMonitor.clearBiometricRecognizedWhenKeyguardDone(currentUser);
             Log.i(TAG, "Device is going to sleep, aborting keyguardDone");
             return;
         }
@@ -1962,7 +1961,7 @@ public class KeyguardViewMediator extends SystemUI implements Dumpable,
         }
 
         handleHide();
-        mUpdateMonitor.clearBiometricRecognized();
+        mUpdateMonitor.clearBiometricRecognizedWhenKeyguardDone(currentUser);
         Trace.endSection();
     }
 
