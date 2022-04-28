3982f4617eb60a6c4981712b519a028ae55683e2
cjh1249131356 <cjh1249131356@gmail.com>
2022-04-28 16:09:16 +0800

    SystemUI: QS tiles layout customizations [1/2]
    - Inspired from https://github.com/omnirom/android_frameworks_base/commit/4d71992f4446d828e0f5e7eb3841632ea28ee71c.
      To avoid breaking aosp performance of calculating stuff here and there, use overlay to implement this.
    
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -112,5 +112,7 @@ public class SystemSettings {
         Settings.System.STATUSBAR_CLOCK_DATE_POSITION,
         Settings.System.SHOW_FOURG_ICON,
         Settings.System.QS_TILE_LABEL_HIDE,
+        Settings.System.QS_LAYOUT,
+        Settings.System.QQS_LAYOUT,
     };
 }
