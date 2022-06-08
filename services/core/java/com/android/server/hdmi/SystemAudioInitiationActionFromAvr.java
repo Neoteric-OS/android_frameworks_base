dd1ae77fabae8bc93863f1528e637672814e321b
an.xi <an.xi@amlogic.com>
2022-06-08 14:53:47 +0800

    cec: fix wake up issue for testQuiescentBoot [1/1]
    isssue:233323608
    
    Problem:
    QuiescentBoot demands that the system is in standby state after the
    device reboots quiescently. But the sytem audio initiation of
    a soundbar with device type "4,5" could wake up the device.
    Besides, there could be tvs and source devices that don't maintain
    the active device state well. It may not send <Active Source> even
    though it's the active one. This usually happens when a tv wakes up.
    
    Solution:
    Even no device responds <Request Active Source>, the soundbar
    should not claim that it's the current active source.
    
    Verify:
    askey avo
    
    Change-Id: I4f322d0a79b16bed32e8a614af2e44b54b4a2954

--- services/core/java/com/android/server/hdmi/SystemAudioInitiationActionFromAvr.java
+++ services/core/java/com/android/server/hdmi/SystemAudioInitiationActionFromAvr.java
@@ -115,12 +115,7 @@ public class SystemAudioInitiationActionFromAvr extends HdmiCecFeatureAction {
 
     private void handleActiveSourceTimeout() {
         HdmiLogger.debug("Cannot get active source.");
-        // If not able to find Active Source and the current device has playbcak functionality,
-        // claim Active Source and start to query TV system audio mode support.
         if (audioSystem().mService.isPlaybackDevice()) {
-            audioSystem().mService.setAndBroadcastActiveSourceFromOneDeviceType(
-                    Constants.ADDR_BROADCAST, getSourcePath(),
-                    "SystemAudioInitiationActionFromAvr#handleActiveSourceTimeout()");
             mState = STATE_WAITING_FOR_TV_SUPPORT;
             queryTvSystemAudioModeSupport();
         } else {
