82dcff9cf1957cdf2c8d781302852004535a3ef8
xi yu <yuxi1@xiaomi.com>
2022-06-20 13:50:41 +0000

    [Bugfix][RemoteAnimation] Fix IndexOutOfBoundsException in onAnimationFinished of RemoteAnimationController
    Sometimes mPendingAnimations.remove will be called more than once in one cycle (line 255 - 270) of onAnimationFinished, e.g., mPendingAnimations.remove being called again in adapters.mAdapter.mCapturedFinishCallback.onAnimationFinished, resulting in IndexOutOfBoundsException.
    
    Bug:237989368
    
    Change-Id: Ica771f1ae4cb4ddddb7684bd964504ca9f31ea12
    Test: Monkey test

--- services/core/java/com/android/server/wm/RemoteAnimationController.java
+++ services/core/java/com/android/server/wm/RemoteAnimationController.java
@@ -70,6 +70,7 @@ class RemoteAnimationController implements DeathRecipient {
     private final Runnable mTimeoutRunnable = () -> cancelAnimation("timeoutRunnable");
 
     private FinishedCallback mFinishedCallback;
+    private boolean mIsFinishing;
     private boolean mCanceled;
     private boolean mLinkedToDeathOfRunner;
 
@@ -246,6 +247,7 @@ class RemoteAnimationController implements DeathRecipient {
                 mPendingAnimations.size());
         mHandler.removeCallbacks(mTimeoutRunnable);
         synchronized (mService.mGlobalLock) {
+            mIsFinishing = true;
             unlinkToDeathOfRunner();
             releaseFinishedCallback();
             mService.openSurfaceTransaction();
@@ -291,6 +293,7 @@ class RemoteAnimationController implements DeathRecipient {
             } finally {
                 mService.closeSurfaceTransaction("RemoteAnimationController#finished");
             }
+            mIsFinishing = false;
         }
         setRunningRemoteAnimation(false);
         ProtoLog.i(WM_DEBUG_REMOTE_ANIMATIONS, "Finishing remote animation");
@@ -501,6 +504,9 @@ class RemoteAnimationController implements DeathRecipient {
 
         @Override
         public void onAnimationCancelled(SurfaceControl animationLeash) {
+            if (mIsFinishing) {
+                return;
+            }
             if (mRecord.mAdapter == this) {
                 mRecord.mAdapter = null;
             } else {
