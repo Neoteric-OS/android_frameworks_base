c586fd4d43c5a9e867fa6c9f1e47f585ffb043fa
Caren Chang <carenc@google.com>
2022-07-13 23:27:21 +0000

    Add explanation about the purpose of accessibility heading and use case apps should consider.
    Change-Id: I01bdd1fc89d54e62e9ce1f8dc15a58d13c56fabc
    Test: N/A, documentation chang

--- core/java/android/view/View.java
+++ core/java/android/view/View.java
@@ -12810,6 +12810,10 @@ public class View implements Drawable.Callback, KeyEvent.Callback,
 
     /**
      * Set if view is a heading for a section of content for accessibility purposes.
+     * 
+     * Users of accessibility services can choose to navigate between headings
+     * instead of between paragraphs or between words. Apps that provide headings on
+     * sections of text can help the text navigation experience.
      *
      * @param isHeading {@code true} if the view is a heading, {@code false} otherwise.
      *
