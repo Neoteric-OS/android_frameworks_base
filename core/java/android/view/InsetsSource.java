d2a3da16cb6a996c9a804f491afca6aa2f429500
ot903107 <ot_zhenhua.deng@mediatek.com>
2022-08-08 10:19:09 +0800

    Add try-catch for ensure client get right last new insets
    While screen rotating, someone InsetsSources visibleframe update
    kind of late, no any logs present which type InsetsSource, when client
    request relayout window or getWindowInsets need writeToParce it, but
    multi-thread operate visibelFrame cause NPE even already check if null.
    
    Bug: 242037615
    Test: run autotest pass
    
    Change-Id: Ic82e7fd9ed8fbd747edd69b640fef6d5434d333c

--- core/java/android/view/InsetsSource.java
+++ core/java/android/view/InsetsSource.java
@@ -280,8 +280,12 @@ public class InsetsSource implements Parcelable {
         dest.writeInt(mType);
         mFrame.writeToParcel(dest, 0);
         if (mVisibleFrame != null) {
-            dest.writeInt(1);
-            mVisibleFrame.writeToParcel(dest, 0);
+            try {
+                dest.writeInt(1);
+                mVisibleFrame.writeToParcel(dest, 0);
+            } catch (NullPointerException e) {
+                dest.writeInt(0);
+            }
         } else {
             dest.writeInt(0);
         }
@@ -293,6 +297,7 @@ public class InsetsSource implements Parcelable {
         return "InsetsSource: {"
                 + "mType=" + InsetsState.typeToString(mType)
                 + ", mFrame=" + mFrame.toShortString()
+                + ", mVisibleFrame=" + mVisibleFrame
                 + ", mVisible=" + mVisible
                 + "}";
     }
