b14f11fb6a68bdf53becf18da99190b82e5087da
Han Wang <wanghan1995315@gmail.com>
2022-09-04 08:06:49 +0000

    Fix error log message for rename(2) workaround
    Change-Id: I36e8bf415a767078e9c5e8fa0c93d608711d8882

--- core/java/android/app/ActivityThread.java
+++ core/java/android/app/ActivityThread.java
@@ -7836,8 +7836,8 @@ public final class ActivityThread extends ClientTransactionHandler
                         Files.move(new File(oldPath).toPath(), new File(newPath).toPath(),
                                 StandardCopyOption.REPLACE_EXISTING);
                     } catch (IOException e2) {
-                        Log.e(TAG, "Rename recovery failed ", e);
-                        throw e;
+                        Log.e(TAG, "Rename recovery failed ", e2);
+                        throw new ErrnoException(e2.getMessage(), OsConstants.EIO);
                     }
                 } else {
                     throw e;
