e152b8f17ec9740a52323bf92507583badc8fda8
hupeng3 <hp121520@gmail.com>
2022-09-14 19:26:31 +0800

    Toast: Fix mCurrentUpdate concurrency issues in BrightnessSynchronizer
    There are multiple calls to runUpdate in BrightnessSynchronizer,
    and the mCurrentUpdate variable will also be called concurrently,
    which will cause NPE problems in mCurrentUpdate.
    
    Bug: 246667846
    
    Test: atest BrightnessSynchronizerTest
    
    Signed-off-by: hupeng3 <hp121520@gmail.com>
    Change-Id: I27b29115e5933e7d3cbf5577a775a4869cd1512a

--- core/java/com/android/internal/display/BrightnessSynchronizer.java
+++ core/java/com/android/internal/display/BrightnessSynchronizer.java
@@ -52,6 +52,8 @@ public class BrightnessSynchronizer {
     private static final long WAIT_FOR_RESPONSE_MILLIS = 200;
 
     private static final int MSG_RUN_UPDATE = 1;
+    private static final int MSG_UPDATE_INT = 2;
+    private static final int MSG_UPDATE_FLOAT = 3;
 
     // The tolerance within which we consider brightness values approximately equal to eachother.
     // This value is approximately 1/3 of the smallest possible brightness value.
@@ -62,7 +64,7 @@ public class BrightnessSynchronizer {
     private final Context mContext;
     private final BrightnessSyncObserver mBrightnessSyncObserver;
     private final Clock mClock;
-    private final Handler mHandler;
+    private final Handler mHandler = new BrightnessSynchronizerHandler(Looper.getMainLooper());
 
     private DisplayManager mDisplayManager;
     private int mLatestIntBrightness;
@@ -71,15 +73,14 @@ public class BrightnessSynchronizer {
     private BrightnessUpdate mPendingUpdate;
 
     public BrightnessSynchronizer(Context context) {
-        this(context, Looper.getMainLooper(), SystemClock::uptimeMillis);
+        this(context, SystemClock::uptimeMillis);
     }
 
     @VisibleForTesting
-    public BrightnessSynchronizer(Context context, Looper looper, Clock clock) {
+    public BrightnessSynchronizer(Context context, Clock clock) {
         mContext = context;
         mClock = clock;
         mBrightnessSyncObserver = new BrightnessSyncObserver();
-        mHandler = new BrightnessSynchronizerHandler(looper);
     }
 
     /**
@@ -293,8 +294,6 @@ public class BrightnessSynchronizer {
         static final int TYPE_FLOAT = 0x2;
 
         private static final int STATE_NOT_STARTED = 1;
-        private static final int STATE_RUNNING = 2;
-        private static final int STATE_COMPLETED = 3;
 
         private final int mSourceType;
         private final float mBrightness;
