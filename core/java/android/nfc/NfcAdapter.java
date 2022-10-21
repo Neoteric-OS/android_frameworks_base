63c04b82b7dae4ef8e5f97c26da599a37eec31ef
Alisher Alikhodjaev <alisher@google.com>
2022-10-21 13:27:27 -0700

    Use service parameter to get INfcAdapter
    The ServiceManager.getService() is a hidden API
    
    Bug: 244264995
    Test: build ok
    Change-Id: Ifcde87f4021238b4ebc67466f2d9b2e363586707

--- core/java/android/nfc/NfcAdapter.java
+++ core/java/android/nfc/NfcAdapter.java
@@ -38,9 +38,7 @@ import android.nfc.tech.NfcF;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.IBinder;
 import android.os.RemoteException;
-import android.os.ServiceManager;
 import android.util.Log;
 
 import java.io.IOException;
@@ -557,12 +555,16 @@ public final class NfcAdapter {
      * @hide
      */
     @UnsupportedAppUsage
-    public static synchronized NfcAdapter getNfcAdapter(Context context) {
+    public static synchronized NfcAdapter getNfcAdapter(Context context, INfcAdapter service) {
         if (context == null) {
             if (sNullContextNfcAdapter == null) {
                 sNullContextNfcAdapter = new NfcAdapter(null);
             }
             return sNullContextNfcAdapter;
+        }
+        if (service == null) {
+            Log.e(TAG, "could not retrieve NFC service");
+            throw new UnsupportedOperationException();
         }
         if (!sIsInitialized) {
             PackageManager pm;
@@ -577,14 +579,9 @@ public final class NfcAdapter {
                 Log.v(TAG, "this device does not have NFC support");
                 throw new UnsupportedOperationException();
             }
-            sService = getServiceInterface();
-            if (sService == null) {
-                Log.e(TAG, "could not retrieve NFC service");
-                throw new UnsupportedOperationException();
-            }
             if (sHasNfcFeature) {
                 try {
-                    sTagService = sService.getNfcTagInterface();
+                    sTagService = service.getNfcTagInterface();
                 } catch (RemoteException e) {
                     Log.e(TAG, "could not retrieve NFC Tag service");
                     throw new UnsupportedOperationException();
@@ -592,13 +589,13 @@ public final class NfcAdapter {
             }
             if (hasHceFeature) {
                 try {
-                    sNfcFCardEmulationService = sService.getNfcFCardEmulationInterface();
+                    sNfcFCardEmulationService = service.getNfcFCardEmulationInterface();
                 } catch (RemoteException e) {
                     Log.e(TAG, "could not retrieve NFC-F card emulation service");
                     throw new UnsupportedOperationException();
                 }
                 try {
-                    sCardEmulationService = sService.getNfcCardEmulationInterface();
+                    sCardEmulationService = service.getNfcCardEmulationInterface();
                 } catch (RemoteException e) {
                     Log.e(TAG, "could not retrieve card emulation service");
                     throw new UnsupportedOperationException();
@@ -607,22 +604,17 @@ public final class NfcAdapter {
 
             sIsInitialized = true;
         }
+        if (sService != service) {
+            sNfcAdapters.remove(context);
+            sService = service;
+            Log.i(TAG, "A new NFC service is created");
+        }
         NfcAdapter adapter = sNfcAdapters.get(context);
         if (adapter == null) {
             adapter = new NfcAdapter(context);
             sNfcAdapters.put(context, adapter);
         }
         return adapter;
-    }
-
-    /** get handle to NFC service interface */
-    private static INfcAdapter getServiceInterface() {
-        /* get a handle to NFC service */
-        IBinder b = ServiceManager.getService("nfc");
-        if (b == null) {
-            return null;
-        }
-        return INfcAdapter.Stub.asInterface(b);
     }
 
     /**
@@ -648,17 +640,18 @@ public final class NfcAdapter {
                     "context not associated with any application (using a mock context?)");
         }
 
-        if (getServiceInterface() == null) {
+        /* use getSystemService() for consistency */
+        NfcManager manager = context.getSystemService(NfcManager.class);
+        if (manager == null) {
+            Log.e(TAG, "NFC service is not available");
+            return null;
+        }
+
+        if (sService == null) {
             // NFC is not available
             return null;
         }
 
-        /* use getSystemService() for consistency */
-        NfcManager manager = (NfcManager) context.getSystemService(Context.NFC_SERVICE);
-        if (manager == null) {
-            // NFC not available
-            return null;
-        }
         return manager.getDefaultAdapter();
     }
 
@@ -680,7 +673,7 @@ public final class NfcAdapter {
         Log.w(TAG, "WARNING: NfcAdapter.getDefaultAdapter() is deprecated, use " +
                 "NfcAdapter.getDefaultAdapter(Context) instead", new Exception());
 
-        return NfcAdapter.getNfcAdapter(null);
+        return NfcAdapter.getNfcAdapter(null, null);
     }
 
     NfcAdapter(Context context) {
@@ -771,8 +764,8 @@ public final class NfcAdapter {
     @UnsupportedAppUsage
     public void attemptDeadServiceRecovery(Exception e) {
         Log.e(TAG, "NFC service dead - attempting to recover", e);
-        INfcAdapter service = getServiceInterface();
-        if (service == null) {
+        NfcManager manager = context.getSystemService(NfcManager.class);
+        if (manager == null || sService == null) {
             Log.e(TAG, "could not retrieve NFC service during service recovery");
             // nothing more can be done now, sService is still stale, we'll hit
             // this recovery path again later
@@ -780,7 +773,7 @@ public final class NfcAdapter {
         }
         // assigning to sService is not thread-safe, but this is best-effort code
         // and on a well-behaved system should never happen
-        sService = service;
+        INfcAdapter service = sService;
         try {
             sTagService = service.getNfcTagInterface();
         } catch (RemoteException ee) {
