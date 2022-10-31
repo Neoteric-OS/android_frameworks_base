19e555d16a67732c59c47fb1c1fad619a17fcd3d
Edward Savage-Jones <edward.savage-jones@sony.com>
2022-10-31 16:28:19 +0100

    Notifications are not locked at startup
    After first boot when doing S->T upgrade or when doing factory data
    reset from Android T Important system applications notification are
    not greyed out.
    
    This may lead to users permanently disabling important notifications.
    
    Bug: 254576888
    Test: See issue
    Change-Id: If73e294b9a732ef47ad685fdd19f51afd8a04eef

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -2285,7 +2285,6 @@ public class NotificationManagerService extends SystemService {
                 mAppOps,
                 new SysUiStatsEvent.BuilderFactory(),
                 mShowReviewPermissionsNotification);
-        mPreferencesHelper.updateFixedImportance(mUm.getUsers());
         mRankingHelper = new RankingHelper(getContext(),
                 mRankingHandler,
                 mPreferencesHelper,
@@ -2728,6 +2727,9 @@ public class NotificationManagerService extends SystemService {
             maybeShowInitialReviewPermissionsNotification();
         } else if (phase == SystemService.PHASE_ACTIVITY_MANAGER_READY) {
             mSnoozeHelper.scheduleRepostsForPersistedNotifications(System.currentTimeMillis());
+        } else if (phase == SystemService.PHASE_DEVICE_SPECIFIC_SERVICES_READY) {
+            mPreferencesHelper.updateFixedImportance(mUm.getUsers());
+            mPreferencesHelper.migrateNotificationPermissions(mUm.getUsers());
         }
     }
 
