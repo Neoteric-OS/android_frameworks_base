38dab0e6f2a7bc45280aa204e40e0c028a778fec
Stefano Cianciulli <scianciulli@google.com>
2022-11-01 17:49:12 +0000

    Stop reporting the duration including sleep for background DexOpt
    It is not needed anymore becaus the job scheduler holds a wake
    lock, so this duration is always the same as duration_ms.
    
    Bug: 255759883
    Test: atest BackgroundDexOptServiceUnitTest
    Change-Id: I0461f4f65f4c0b9a4368e3e0f83050de5b6ba141

--- services/core/java/com/android/server/pm/dex/ArtStatsLogUtils.java
+++ services/core/java/com/android/server/pm/dex/ArtStatsLogUtils.java
@@ -320,12 +320,14 @@ public class ArtStatsLogUtils {
     public static class BackgroundDexoptJobStatsLogger {
         /** Writes background dexopt job stats to statsd. */
         public void write(@BackgroundDexOptService.Status int status,
-                @JobParameters.StopReason int cancellationReason, long durationMs,
-                long durationIncludingSleepMs) {
-            ArtStatsLog.write(ArtStatsLog.BACKGROUND_DEXOPT_JOB_ENDED,
+                          @JobParameters.StopReason int cancellationReason,
+                          long durationMs) {
+            ArtStatsLog.write(
+                    ArtStatsLog.BACKGROUND_DEXOPT_JOB_ENDED,
                     STATUS_MAP.getOrDefault(status,
-                            ArtStatsLog.BACKGROUND_DEXOPT_JOB_ENDED__STATUS__STATUS_UNKNOWN),
-                    cancellationReason, durationMs, durationIncludingSleepMs);
+                        ArtStatsLog.BACKGROUND_DEXOPT_JOB_ENDED__STATUS__STATUS_UNKNOWN),
+                    cancellationReason,
+                    durationMs);
         }
     }
 }
