de19bc7700c7833901c7b3b2a6aec094798d4a35
Lars Greiss <kufikugel@googlemail.com>
2022-11-03 15:42:03 +0800

    SystemUI: HeadsUp timeout option [1/2]
    Forward ported to marshmallow/nougat By: @BeansTown106
    this was basically a rewrite as everything about it changed
    
    Change-Id: I9c88d67d25440dff8f545e1330da672be5d36913
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5752,6 +5752,12 @@ public final class Settings {
         public static final String FULLSCREEN_GESTURES = "fullscreen_gestures";
 
         /**
+         * Heads up timeout configuration
+         * @hide
+         */
+        public static final String HEADS_UP_TIMEOUT = "heads_up_timeout";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
