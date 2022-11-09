476c3e6065165edf383c01cfa3eba522d73bb6a2
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-11-10 01:38:14 +0530

    SystemUI: Allow hiding QS footer data usage [1/2]
    Change-Id: I7ac3093c12728ac6ed63225944d8a10da2ea69ed

--- packages/SystemUI/src/com/android/systemui/qs/QSFooterView.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSFooterView.java
@@ -69,22 +69,13 @@ public class QSFooterView extends FrameLayout {
     private boolean mExpanded;
     private float mExpansionAmount;
 
+    private boolean mHideDataUsage;
     private boolean mShouldShowUsageText;
     private boolean mShouldShowSuffix;
     private boolean mForceShowSuffix;
 
     @Nullable
     private OnClickListener mExpandClickListener;
-
-    private final ContentObserver mDeveloperSettingsObserver = new ContentObserver(
-            new Handler(mContext.getMainLooper())) {
-        @Override
-        public void onChange(boolean selfChange, Uri uri) {
-            super.onChange(selfChange, uri);
-            setBuildText();
-        }
-    };
-
     private DataUsageController mDataController;
     private SubscriptionManager mSubManager;
 
@@ -107,23 +98,17 @@ public class QSFooterView extends FrameLayout {
 
         updateResources();
         setImportantForAccessibility(IMPORTANT_FOR_ACCESSIBILITY_YES);
-        setBuildText();
-	setUsageText();
+        setClickable(false);
+        setUsageText();
+
         mUsageText.setOnClickListener(v -> {
             mForceShowSuffix = !mForceShowSuffix;
             setUsageText();
         });
     }
 
-    private void setBuildText() {
-        if (mBuildText == null) return;
-        mBuildText.setText(null);
-        mShouldShowBuildText = false;
-        mBuildText.setSelected(false);
-    }
-
     private void setUsageText() {
-        if (mUsageText == null || !mExpanded) return;
+        if (mUsageText == null || mHideDataUsage || !mExpanded) return;
         DataUsageController.DataUsageInfo info;
         String suffix;
         if (mIsWifiConnected) {
@@ -220,6 +205,13 @@ public class QSFooterView extends FrameLayout {
         }
     }
 
+    protected void setHideDataUsage(boolean hide) {
+        if (mHideDataUsage != hide) {
+            mHideDataUsage = hide;
+            updateVisibilities();
+        }
+    }
+
     @Override
     protected void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
@@ -269,7 +261,7 @@ public class QSFooterView extends FrameLayout {
             mFooterAnimator.setPosition(headerExpansionFraction);
         }
 
-	if (mUsageText == null) return;
+        if (mUsageText == null || mHideDataUsage) return;
         if (headerExpansionFraction == 1.0f) {
             postDelayed(() -> mUsageText.setSelected(true), 1000);
         } else if (headerExpansionFraction == 0.0f) {
@@ -277,23 +269,6 @@ public class QSFooterView extends FrameLayout {
             mForceShowSuffix = false;
         }
     }
-
-
-    @Override
-    protected void onAttachedToWindow() {
-        super.onAttachedToWindow();
-        mContext.getContentResolver().registerContentObserver(
-                Settings.Global.getUriFor(Settings.Global.DEVELOPMENT_SETTINGS_ENABLED), false,
-                mDeveloperSettingsObserver, UserHandle.USER_ALL);
-    }
-
-    @Override
-    @VisibleForTesting
-    public void onDetachedFromWindow() {
-        mContext.getContentResolver().unregisterContentObserver(mDeveloperSettingsObserver);
-        super.onDetachedFromWindow();
-    }
-
 
     void disable(int state2) {
         final boolean disabled = (state2 & DISABLE2_QUICK_SETTINGS) != 0;
@@ -306,11 +281,11 @@ public class QSFooterView extends FrameLayout {
         post(() -> {
             updateVisibilities();
             setUsageText();
-            setClickable(false);
         });
     }
 
     private void updateVisibilities() {
-        mUsageText.setVisibility(mExpanded && mShouldShowUsageText ? View.VISIBLE : View.INVISIBLE);
+        mUsageText.setVisibility(!mHideDataUsage && mExpanded && mShouldShowUsageText
+                ? View.VISIBLE : View.INVISIBLE);
     }
 }
