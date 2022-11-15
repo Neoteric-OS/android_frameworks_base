0f334d8cd3120af14a30e93652c5fd7fef88e0c6
WoongKi Min <wk0831.min@samsung.com>
2022-11-15 20:00:40 +0900

    Apply wakelock for notification sound
    There was often an issue that the notification sound was cut off during the play.
    NotificationPlayer wakelock guarantees that start() of MediaPlayer Java layer is performed, but can enter sleep at any time between NotificationPlayer rls() and MediaPlayer Native acq().
    
    Test: Check the notification sound of the app that does not wake up when sending the notification
    
    Change-Id: Id4d4986506c74f1ac80ad9ab023a35b4d2bd2642
    Signed-off-by: WoongKi Min <wk0831.min@samsung.com>

--- packages/SystemUI/src/com/android/systemui/media/NotificationPlayer.java
+++ packages/SystemUI/src/com/android/systemui/media/NotificationPlayer.java
@@ -104,6 +104,7 @@ public class NotificationPlayer implements OnCompletionListener, OnErrorListener
                     player.setOnCompletionListener(NotificationPlayer.this);
                     player.setOnErrorListener(NotificationPlayer.this);
                     player.prepare();
+                    player.setWakeMode(mCmd.context, PowerManager.PARTIAL_WAKE_LOCK);
                     if ((mCmd.uri != null) && (mCmd.uri.getEncodedPath() != null)
                             && (mCmd.uri.getEncodedPath().length() > 0)) {
                         if (!audioManager.isMusicActiveRemotely()) {
