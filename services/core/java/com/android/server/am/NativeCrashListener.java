0bb7c5df0b493e2ec78c7019a8d924e24582df51
xi yu <yuxi1@xiaomi.com>
2022-12-27 08:06:00 +0000

    [Bugfix] Fix the pid the log am_crash shows when a native crash occurs.
    When a native crash occurs, the pid the log am_crash shows is the pid of system_server, which is misleading.
    
    Test: manual
    
    Change-Id: I4f96008e4a2aaf3d2533e0fd023960168ed00c79

--- services/core/java/com/android/server/am/NativeCrashListener.java
+++ services/core/java/com/android/server/am/NativeCrashListener.java
@@ -85,7 +85,7 @@ final class NativeCrashListener extends Thread {
                 ci.stackTrace = mCrashReport;
 
                 if (DEBUG) Slog.v(TAG, "Calling handleApplicationCrash()");
-                mAm.handleApplicationCrashInner("native_crash", mApp, mApp.processName, ci);
+                mAm.handleApplicationCrashInner("native_crash", mApp, mApp.processName, ci, true);
                 if (DEBUG) Slog.v(TAG, "<-- handleApplicationCrash() returned");
             } catch (Exception e) {
                 Slog.e(TAG, "Unable to report native crash", e);
