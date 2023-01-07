c1d35f74bea8d7426379376f4784dcf3a54e0450
Vova Sharaienko <sharaienko@google.com>
2023-01-07 03:23:14 +0000

    [TeX] Introduced Telemetry Express Histogram Logging APIs
    - provided Java Counter Histogram metric logging API
    
    Bug: 262791652
    Test: m
    Change-Id: I5270ef327f39db28af3c3148f542b3f6bdad44b5

--- core/java/com/android/internal/expresslog/Counter.java
+++ core/java/com/android/internal/expresslog/Counter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022 The Android Open Source Project
+ * Copyright (C) 2023 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,9 +23,6 @@ import com.android.internal.util.FrameworkStatsLog;
 /** Counter encapsulates StatsD write API calls */
 public final class Counter {
 
-    // Not instantiable.
-    private Counter() {}
-
     /**
      * Increments Telemetry Express Counter metric by 1
      * @hide
@@ -39,9 +36,7 @@ public final class Counter {
      * @hide
      */
     public static void logIncrement(@NonNull String metricId, long amount) {
-        final long metricIdHash = hashString(metricId);
+        final long metricIdHash = Utils.hashString(metricId);
         FrameworkStatsLog.write(FrameworkStatsLog.EXPRESS_EVENT_REPORTED, metricIdHash, amount);
     }
-
-    private static native long hashString(String stringToHash);
 }
---
+++ core/java/com/android/internal/expresslog/CounterHistogram.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.expresslog;
+
+import android.annotation.NonNull;
+
+import com.android.internal.util.FrameworkStatsLog;
+
+/** CounterHistogram encapsulates StatsD write API calls */
+public final class CounterHistogram {
+
+    /** Used by CounterHistogram to map data sample to corresponding bin */
+    public interface BinOptions {
+        /**
+         * @brief Returns bins count to be used by counter histogram
+         * Does not include overflow & underflow bins
+         * @hide
+         */
+        int getBinsCount();
+
+        /**
+         * @brief Calculates bin index for the input sample value
+         * Returns zero based index - where zero stands for underflow
+         * @hide
+         */
+        int getBinForSample(float sample);
+    }
+
+    /** Used by CounterHistogram to map data sample to corresponding bin for on fixed range bins */
+    public static final class FixedRangeOptions implements BinOptions {
+
+        private final int mBinCount;
+        private final float mMinValue;
+        private final float mMaxValue;
+        private final float mBinSize;
+
+        public FixedRangeOptions(int binCount, float minValue, float maxValue) {
+            mBinCount = binCount;
+            mMinValue = minValue;
+            mMaxValue = maxValue;
+            mBinSize = (maxValue - minValue) / binCount;
+        }
+
+        @Override
+        public int getBinsCount() {
+            // include underflow & overflow bins
+            return mBinCount + 2;
+        }
+
+        @Override
+        public int getBinForSample(float sample) {
+            if (sample < mMinValue) {
+                // goes to underflow
+                return mBinCount;
+            } else if (sample > mMaxValue) {
+                // goes to overflow
+                return mBinCount + 1;
+            }
+            return (int) ((sample - mMinValue)  / mBinSize);
+        }
+    }
+
+    private final long mMetricIdHash;
+
+    private final BinOptions mBinOptions;
+
+    public CounterHistogram(@NonNull String metricId, @NonNull BinOptions binOptions) {
+        mMetricIdHash = Utils.hashString(metricId);
+        mBinOptions = binOptions;
+    }
+
+    /**
+     * Increments samples count for automatically calculated bin
+     * @hide
+     */
+    public void logSample(float sample) {
+        final int binIndex = mBinOptions.getBinForSample(sample);
+        FrameworkStatsLog.write(FrameworkStatsLog.EXPRESS_HISTOGRAM_SAMPLE_REPORTED,
+                mMetricIdHash, 1, binIndex);
+    }
+}
