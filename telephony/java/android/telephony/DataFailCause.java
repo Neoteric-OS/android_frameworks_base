54714a2f8c699bda3df37b35ca781f3f436c91e3
Tairu Wang <tairuw@google.com>
2023-01-18 12:49:42 -0800

    Add new DataFailCause for Iwlan
    Add DataFailCause IWLAN_SIM_NOT_READY
    - Split IWLAN_PDN_CONNECTION_REJECTION usage to its own DataFailCause
      for clarity.Change-Id:
    
    Add DataFailCause IWLAN_TUNNEL_TRANSFORM_FAILED
    - Error happens when tunnel transform failed (e.g. no local addresses
      found, fail to create tunnel interface
    
    Add DataFailCause IWLAN_IKE_INIT_TIMEOUT
    - Split IWLAN_IKEV2_MSG_TIMEOUT into different data fail cause.
    
    Add DataFailCause IWLAN_IKE_NETWORK_LOST_EXCEPTION
    - Add new DatafailCause to match IKE_NETWORK_LOST_EXCEPTION and report to framework module instead of ERROR_UNSPECIFIED
    
    Add DataFailCause IWLAN_IKE_PRIVATE_ERROR_TYPE
    - Carrier-specific error codes during IKEv2 SA setup all default to IWLAN_NETWORK_FAILURE, which is noisy.
    
    Add new DataFailCause IWLAN_IKE_SESSION_CLOSED_BEFORE_CHILD_SESSION_OPENED
    - replace NETWORK_FAILURE usage in IwlanError with IWLAN_IKE_SESSION_CLOSED_BEFORE_CHILD_SESSION_OPENED
    
    Add DataFailCause IWLAN_DPD_TIMEOUT
    - Split IWLAN_IKEV2_MSG_TIMEOUT into different data fail cause.
    
    Add DataFailCause IWLAN_IKE_REKEY_MOBILITY_TIMEOUT and IWLAN_IKE_MOBIKE_TIMEOUT
    - Split IWLAN_IKEV2_MSG_TIMEOUT into different data fail cause.
    
    Bug: 269160037
    Test: atest IwlanTest
    Change-Id: I05e5c7e2fdd66a439fc9e9b2d763202a1cf6e515

--- telephony/java/android/telephony/DataFailCause.java
+++ telephony/java/android/telephony/DataFailCause.java
@@ -997,7 +997,8 @@ public final class DataFailCause {
      */
     public static final int IWLAN_CONGESTION = 0x3C8C;
 
-    /** IKE configuration error resulting in failure  */
+    /** IKE configuration error resulting in failure,
+     * below IWLAN error codes are defined by the UE and do not relate to any 3GPP spec value */
     public static final int IWLAN_IKEV2_CONFIG_FAILURE = 0x4000;
     /**
      * Sent in the response to an IKE_AUTH message when, for some reason,
@@ -1014,6 +1015,25 @@ public final class DataFailCause {
     public static final int IWLAN_DNS_RESOLUTION_TIMEOUT = 0x4005;
     /** Expected to update or bring down an ePDG tunnel, but no tunnel found*/
     public static final int IWLAN_TUNNEL_NOT_FOUND = 0x4006;
+    /** SIM information not available for IWLAN to bring up*/
+    public static final int IWLAN_SIM_NOT_READY = 0x4007;
+    /** Failed to apply tunnel transform*/
+    public static final int IWLAN_TUNNEL_TRANSFORM_FAILED = 0x4008;
+    /** IKE Init timeout, no response from EPDG*/
+    public static final int IWLAN_IKE_INIT_TIMEOUT = 0x4009;
+    /** IWLAN PDN setup failed due to Wi-Fi lost during IKE tunnel setup,
+     * match exception reported by IKE module*/
+    public static final int IWLAN_IKE_NETWORK_LOST_EXCEPTION = 0x400A;
+    /** Carrier-specific error codes during IKEv2 SA setup*/
+    public static final int IWLAN_IKE_PRIVATE_ERROR_TYPE = 0x400B;
+    /** IKE Session closed before child session opened*/
+    public static final int IWLAN_IKE_SESSION_CLOSED_BEFORE_CHILD_SESSION_OPENED = 0x400C;
+    /** DPD message does not get an ack after the re-tx attempts and duration, i.e., times out.*/
+    public static final int IWLAN_IKE_DPD_TIMEOUT = 0x400D;
+    /** The Wifi -> Wifi handover of the IMS PDN fails because the network does not respond to the
+     * MOBIKE / rekey mobility message in the expected manner */
+    public static final int IWLAN_IKE_REKEY_MOBILITY_TIMEOUT = 0x400E;
+    public static final int IWLAN_IKE_MOBIKE_TIMEOUT = 0x400F;
 
     // OEM sepecific error codes. To be used by OEMs when they don't
     // want to reveal error code which would be replaced by ERROR_UNSPECIFIED
@@ -1508,6 +1528,16 @@ public final class DataFailCause {
         sFailCauseMap.put(IWLAN_DNS_RESOLUTION_NAME_FAILURE, "IWLAN_DNS_RESOLUTION_NAME_FAILURE");
         sFailCauseMap.put(IWLAN_DNS_RESOLUTION_TIMEOUT, "IWLAN_DNS_RESOLUTION_TIMEOUT");
         sFailCauseMap.put(IWLAN_TUNNEL_NOT_FOUND, "IWLAN_TUNNEL_NOT_FOUND");
+        sFailCauseMap.put(IWLAN_SIM_NOT_READY, "IWLAN_SIM_NOT_READY");
+        sFailCauseMap.put(IWLAN_TUNNEL_TRANSFORM_FAILED, "IWLAN_TUNNEL_TRANSFORM_FAILED");
+        sFailCauseMap.put(IWLAN_IKE_INIT_TIMEOUT, "IWLAN_IKE_INIT_TIMEOUT");
+        sFailCauseMap.put(IWLAN_IKE_NETWORK_LOST_EXCEPTION, "IWLAN_IKE_NETWORK_LOST_EXCEPTION");
+        sFailCauseMap.put(IWLAN_IKE_PRIVATE_ERROR_TYPE, "IWLAN_IKE_PRIVATE_ERROR_TYPE");
+        sFailCauseMap.put(IWLAN_IKE_SESSION_CLOSED_BEFORE_CHILD_SESSION_OPENED,
+                "IWLAN_IKE_SESSION_CLOSED_BEFORE_CHILD_SESSION_OPENED");
+        sFailCauseMap.put(IWLAN_IKE_DPD_TIMEOUT, "IWLAN_IKE_DPD_TIMEOUT");
+        sFailCauseMap.put(IWLAN_IKE_REKEY_MOBILITY_TIMEOUT, "IWLAN_IKE_REKEY_MOBILITY_TIMEOUT");
+        sFailCauseMap.put(IWLAN_IKE_MOBIKE_TIMEOUT, "IWLAN_IKE_MOBIKE_TIMEOUT");
         sFailCauseMap.put(OEM_DCFAILCAUSE_1, "OEM_DCFAILCAUSE_1");
         sFailCauseMap.put(OEM_DCFAILCAUSE_2, "OEM_DCFAILCAUSE_2");
         sFailCauseMap.put(OEM_DCFAILCAUSE_3, "OEM_DCFAILCAUSE_3");
