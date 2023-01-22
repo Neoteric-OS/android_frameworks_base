4902c8539400dbfeb5f7641c2c3c142710c10834
SpiritCroc <dev@spiritcroc.de>
2023-01-22 23:11:14 +0300

    SystemUI: Add Locale Tile
    Toggle the primary default language
    
    - Bring it to Oreo - LorDClockaN
    - Bring it to Pie, 11, 12 - eyosen
    - Bring it to T - semdoc
    
    Change-Id: I3e336eddb360ea1a796727657652ad9301007380

--- packages/SystemUI/src/com/android/systemui/neoteric/NeotericModule.kt
+++ packages/SystemUI/src/com/android/systemui/neoteric/NeotericModule.kt
@@ -24,6 +24,7 @@ import com.android.systemui.qs.tiles.CellularTile
 import com.android.systemui.qs.tiles.CompassTile
 import com.android.systemui.qs.tiles.DataSwitchTile
 import com.android.systemui.qs.tiles.HeadsUpTile
+import com.android.systemui.qs.tiles.LocaleTile
 import com.android.systemui.qs.tiles.SoundTile
 import com.android.systemui.qs.tiles.SyncTile
 import com.android.systemui.qs.tiles.UsbTetherTile
@@ -78,6 +79,12 @@ interface NeotericModule {
     @IntoMap
     @StringKey(HeadsUpTile.TILE_SPEC)
     fun bindHeadsUpTile(headsUpTile: HeadsUpTile): QSTileImpl<*>
+
+    /** Inject LocaleTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(LocaleTile.TILE_SPEC)
+    fun bindLocaleTile(localeTile: LocaleTile): QSTileImpl<*>
 
     /** Inject SoundTile into tileMap in QSModule */
     @Binds
