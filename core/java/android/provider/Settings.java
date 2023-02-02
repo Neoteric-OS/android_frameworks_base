venoomdev/platform_frameworks_base

base: Implement background process killer [1/2] * rebranded/adapted from Descendant-XI/frameworks_base@4d6a3fa * nuked stuffs we dont want to mess with like sync and location
Signed-off-by: dlwlrma123 <alexfinhart@gmail.com>

core/java/android/provider/Settings.java
@@ -6103,6 +6103,13 @@ public final class Settings {
          */
         public static final String STATUSBAR_ICONS_STYLE = "statusbar_icons_style";
 
+        /**
+         * Arcane Idle Manager
+         * @hide
+         */
+        @Readable
+        public static final String ARCANE_IDLE_MANAGER = "arcane_idle_manager";
+
         /**          
           * Setting to enable/disable gaming mode
           * @hide

Neoteric-OS/android_frameworks_base

core/java/android/provider/Settings.java
@@ -5620,6 +5620,13 @@ public final class Settings {
         public static final String BACK_GESTURE_HAPTIC = "back_gesture_haptic";
 
         /**
+         * Zephyrus Idle Manager
+         * @hide
+         */
+        @Readable
+        public static final String ZEPHYRUS_IDLE_MANAGER = "zephyrus_idle_manager";
+
+        /**
          * @hide
          */
         public static final String STATUS_BAR_BRIGHTNESS_CONTROL = "status_bar_brightness_control";