00c5da96c1963283e6006b181af432428f35d027
Nanase Mitsuhashi <Nanase.X.Mitsuhashi@sony.com>
2023-02-13 15:27:30 +0900

    Add a new carrier config to shows a warning to before network scan
    This is used to warn that mobile data will be temporarily disabled
    while finding the network.
    
    Bug: 211380761
    Test: manual - Checked that a warning dialog shall be displayed before
    finding the network.
    
    Change-Id: I96bf0bfd2bd5aa16d57290ddca473576da8f1b14

--- telephony/java/android/telephony/CarrierConfigManager.java
+++ telephony/java/android/telephony/CarrierConfigManager.java
@@ -8614,6 +8614,18 @@ public class CarrierConfigManager {
     public static final String KEY_IWLAN_HANDOVER_POLICY_STRING_ARRAY =
             "iwlan_handover_policy_string_array";
 
+    /**
+     * Boolean indicating whether to warn that mobile data will be temporarily disabled while
+     * finding the network.
+     *
+     * {@code true}: show warning dialog before finding the network.
+     * {@code false}: default value, not show warning dialog before finding the network.
+     *
+     * @hide
+     */
+    public static final String KEY_SHOW_WARNING_DISABLE_DATA_FOR_FINDING_NETWORK_BOOL =
+            "show_warning_disable_data_for_finding_network_bool";
+
     /** The default value for every variable. */
     private final static PersistableBundle sDefaults;
 
@@ -9261,6 +9273,7 @@ public class CarrierConfigManager {
                 new long[] {180000, 180000, 180000, 180000});
         sDefaults.putBooleanArray(KEY_DATA_STALL_RECOVERY_SHOULD_SKIP_BOOL_ARRAY,
                 new boolean[] {false, false, true, false, false});
+        sDefaults.putBoolean(KEY_SHOW_WARNING_DISABLE_DATA_FOR_FINDING_NETWORK_BOOL, false);
     }
 
     /**
