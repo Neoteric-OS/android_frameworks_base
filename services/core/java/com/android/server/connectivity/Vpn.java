c68a13e45a7acc917dcd0714bcffee1e6fc48545
chiachangwang <chiachangwang@google.com>
2023-03-17 02:10:03 +0000

    Add more logs into dumpsys of VPN
    For improving debuggability of VPN, this commit adds below logs
    into dump section.
    - Logical bits in Ikev2VpnProfile
    - Underlying network cap change and link properties change
    - Historical calls to set always on and lockdown mode
    - CarrierConfig change history
    
    Test: atest FrameworksNetTests
    Test: "adb shell dumpsys vpn_management" with VPN connected
    Change-Id: I1a7aa81b207a0fa3cdec08e839fd8916fbff0f3c

--- services/core/java/com/android/server/connectivity/Vpn.java
+++ services/core/java/com/android/server/connectivity/Vpn.java
@@ -366,8 +366,12 @@ public class Vpn {
     }
 
     private static final int MAX_EVENTS_LOGS = 20;
-    private final LocalLog mUnderlyNetworkChanges = new LocalLog(MAX_EVENTS_LOGS);
+    private final LocalLog mUnderlyingNetworkChanges = new LocalLog(MAX_EVENTS_LOGS);
+    private final LocalLog mUnderlyingNetworkNetCapChanges = new LocalLog(MAX_EVENTS_LOGS);
+    private final LocalLog mUnderlyingNetworkLpChanges = new LocalLog(MAX_EVENTS_LOGS);
     private final LocalLog mVpnManagerEvents = new LocalLog(MAX_EVENTS_LOGS);
+    private final LocalLog mCarrierConfigChange = new LocalLog(MAX_EVENTS_LOGS);
+    private final LocalLog mAlwaysOnLockdownChange = new LocalLog(MAX_EVENTS_LOGS);
 
     /**
      * Cached Map of <subscription ID, CarrierConfigInfo> since retrieving the PersistableBundle
@@ -1107,6 +1111,8 @@ public class Vpn {
         mLockdownAllowlist = (mLockdown && lockdownAllowlist != null)
                 ? Collections.unmodifiableList(new ArrayList<>(lockdownAllowlist))
                 : Collections.emptyList();
+        mAlwaysOnLockdownChange.log("Mode changed: lockdown=" + mLockdown + " alwaysOn="
+                + mAlwaysOn + " calling from " + Binder.getCallingUid());
 
         if (isCurrentPreparedPackage(packageName)) {
             updateAlwaysOnNotification(mNetworkInfo.getDetailedState());
@@ -1723,7 +1729,7 @@ public class Vpn {
     }
 
     private void logUnderlyNetworkChanges(List<Network> networks) {
-        mUnderlyNetworkChanges.log("Switch to "
+        mUnderlyingNetworkChanges.log("Switch to "
                 + ((networks != null) ? TextUtils.join(", ", networks) : "null"));
     }
 
@@ -2990,6 +2996,9 @@ public class Vpn {
                     @Override
                     public void onCarrierConfigChanged(int slotIndex, int subId, int carrierId,
                             int specificCarrierId) {
+                        mCarrierConfigChange.log("Changed on slot " + slotIndex + " subId="
+                                + subId + " carrerId=" + carrierId
+                                + " specificCarrierId=" + specificCarrierId);
                         synchronized (Vpn.this) {
                             mCachedCarrierConfigInfoPerSubId.remove(subId);
 
@@ -3693,6 +3702,8 @@ public class Vpn {
 
         /** Called when the NetworkCapabilities of underlying network is changed */
         public void onDefaultNetworkCapabilitiesChanged(@NonNull NetworkCapabilities nc) {
+            mUnderlyingNetworkNetCapChanges.log(
+                    "Changed from " + mUnderlyingNetworkCapabilities + " to " + nc);
             final NetworkCapabilities oldNc = mUnderlyingNetworkCapabilities;
             mUnderlyingNetworkCapabilities = nc;
             if (oldNc == null) {
@@ -3706,6 +3717,8 @@ public class Vpn {
 
         /** Called when the LinkProperties of underlying network is changed */
         public void onDefaultNetworkLinkPropertiesChanged(@NonNull LinkProperties lp) {
+            mUnderlyingNetworkLpChanges.log(
+                    "Changed from " + mUnderlyingLinkProperties + " to " + lp);
             mUnderlyingLinkProperties = lp;
         }
 
@@ -5046,21 +5059,47 @@ public class Vpn {
                 final IkeV2VpnRunner runner = ((IkeV2VpnRunner) mVpnRunner);
                 pw.println("Token: " + runner.mSessionKey);
                 pw.println("MOBIKE " + (runner.mMobikeEnabled ? "enabled" : "disabled"));
+                pw.println("Profile: " + runner.mProfile);
                 if (mDataStallSuspected) pw.println("Data stall suspected");
                 if (runner.mScheduledHandleDataStallFuture != null) {
                     pw.println("Reset session scheduled");
                 }
             }
+            pw.println();
             pw.println("mCachedCarrierConfigInfoPerSubId=" + mCachedCarrierConfigInfoPerSubId);
 
-            pw.println("mUnderlyNetworkChanges (most recent first):");
+            pw.println("mUnderlyingNetworkChanges (most recent first):");
             pw.increaseIndent();
-            mUnderlyNetworkChanges.reverseDump(pw);
+            mUnderlyingNetworkChanges.reverseDump(pw);
             pw.decreaseIndent();
 
             pw.println("mVpnManagerEvent (most recent first):");
             pw.increaseIndent();
             mVpnManagerEvents.reverseDump(pw);
+            pw.decreaseIndent();
+            pw.println();
+
+            pw.println("mCarrierConfigChange (most recent first):");
+            pw.increaseIndent();
+            mCarrierConfigChange.reverseDump(pw);
+            pw.decreaseIndent();
+            pw.println();
+
+            pw.println("mUnderlyingNetworkNetCapChanges (most recent first):");
+            pw.increaseIndent();
+            mUnderlyingNetworkNetCapChanges.reverseDump(pw);
+            pw.decreaseIndent();
+            pw.println();
+
+            pw.println("mUnderlyingNetworkLpChanges (most recent first):");
+            pw.increaseIndent();
+            mUnderlyingNetworkLpChanges.reverseDump(pw);
+            pw.decreaseIndent();
+            pw.println();
+
+            pw.println("mAlwaysOnLockdownChange (most recent first):");
+            pw.increaseIndent();
+            mAlwaysOnLockdownChange.reverseDump(pw);
             pw.decreaseIndent();
         }
     }
