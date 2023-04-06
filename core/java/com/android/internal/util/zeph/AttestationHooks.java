e83ea2ee6660ee86d93db9ce22aa1148c46ccbd4
HELLBOY017 <abhaygill017@gmail.com>
2023-04-06 21:17:23 +0530

    base: com.android.internal.util.zephyrus -> com.android.internal.util.zeph
    Should have done it from the start.

--- core/java/com/android/internal/util/zeph/AttestationHooks.java
+++ core/java/com/android/internal/util/zeph/AttestationHooks.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.internal.util.zephyrus;
+package com.android.internal.util.zeph;
 
 import android.app.Application;
 import android.os.Build;
--- core/java/com/android/internal/util/zeph/PixelPropsUtils.java
+++ core/java/com/android/internal/util/zeph/PixelPropsUtils.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.internal.util.zephyrus;
+package com.android.internal.util.zeph;
 
 import android.app.Application;
 import android.os.Build;
