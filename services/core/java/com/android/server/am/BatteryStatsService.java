21ed4b9e62912682acbf97e9b3e45a8ef8f5582a
Motomu Utsumi <motomuman@google.com>
2023-04-25 15:59:55 +0900

    Remove INetworkManagementEventObserver in BatteryStatsService
    Battery stats can not be updated by INetworkManagementEventObserver
    since the idletimer label is changed from transportType to netId by
    aosp/2560791 if trackMultiNetwork flag is enabled
    Wifi/Mobile radio power state will be updated by ConnectivityService
    instead of INetworkManagementEventObserver.
    
    Bug: 279380356
    Bug: 267870186
    Test: TH
    Change-Id: I6ce6045e5ab33323b97d391c003d04446a70657a

--- services/core/java/com/android/server/am/BatteryStatsService.java
+++ services/core/java/com/android/server/am/BatteryStatsService.java
@@ -17,7 +17,6 @@
 package com.android.server.am;
 
 import static android.Manifest.permission.BATTERY_STATS;
-import static android.Manifest.permission.BLUETOOTH_CONNECT;
 import static android.Manifest.permission.DEVICE_POWER;
 import static android.Manifest.permission.NETWORK_STACK;
 import static android.Manifest.permission.POWER_SAVER;
@@ -103,6 +102,7 @@ import com.android.internal.util.DumpUtils;
 import com.android.internal.util.FrameworkStatsLog;
 import com.android.internal.util.ParseUtils;
 import com.android.internal.util.function.pooled.PooledLambda;
+import com.android.net.flags.Flags;
 import com.android.net.module.util.NetworkCapabilitiesUtils;
 import com.android.server.LocalServices;
 import com.android.server.Watchdog;
@@ -427,7 +427,9 @@ public final class BatteryStatsService extends IBatteryStats.Stub
         final ConnectivityManager cm = mContext.getSystemService(ConnectivityManager.class);
         try {
             nms.registerObserver(mActivityChangeObserver);
-            cm.registerDefaultNetworkCallback(mNetworkCallback);
+            if (!Flags.trackMultipleNetworkActivities()) {
+                cm.registerDefaultNetworkCallback(mNetworkCallback);
+            }
         } catch (RemoteException e) {
             Slog.e(TAG, "Could not register INetworkManagement event observer " + e);
         }
