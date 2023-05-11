d9fbe6a43862a30fba0306307fb853b5bf4c2247
Christopher Ferris <cferris@google.com>
2023-05-10 18:37:27 -0700

    Allow logging internal allocator stats.
    Add new option --logstats (dumpsys meminfo --logstats PID|SERVICE).
    
    Bug: 281729079
    
    Test: Ran the command on a scudo based system and verified log contains
    Test: scudo stats.
    Test: Ran the command on a jemalloc based system and verified log contains
    Test: jemalloc stats.
    Change-Id: Iaf8f8cce8663471cc21a54d8ab70208a7e70eaa8

--- core/java/android/app/ActivityThread.java
+++ core/java/android/app/ActivityThread.java
@@ -1451,11 +1451,12 @@ public final class ActivityThread extends ClientTransactionHandler
         @Override
         public void dumpMemInfo(ParcelFileDescriptor pfd, Debug.MemoryInfo mem, boolean checkin,
                 boolean dumpFullInfo, boolean dumpDalvik, boolean dumpSummaryOnly,
-                boolean dumpUnreachable, String[] args) {
+                boolean dumpUnreachable, boolean dumpAllocatorStats, String[] args) {
             FileOutputStream fout = new FileOutputStream(pfd.getFileDescriptor());
             PrintWriter pw = new FastPrintWriter(fout);
             try {
-                dumpMemInfo(pw, mem, checkin, dumpFullInfo, dumpDalvik, dumpSummaryOnly, dumpUnreachable);
+                dumpMemInfo(pw, mem, checkin, dumpFullInfo, dumpDalvik, dumpSummaryOnly,
+                            dumpUnreachable, dumpAllocatorStats);
             } finally {
                 pw.flush();
                 IoUtils.closeQuietly(pfd);
@@ -1463,7 +1464,8 @@ public final class ActivityThread extends ClientTransactionHandler
         }
 
         private void dumpMemInfo(PrintWriter pw, Debug.MemoryInfo memInfo, boolean checkin,
-                boolean dumpFullInfo, boolean dumpDalvik, boolean dumpSummaryOnly, boolean dumpUnreachable) {
+                boolean dumpFullInfo, boolean dumpDalvik, boolean dumpSummaryOnly,
+                boolean dumpUnreachable, boolean dumpAllocatorStats) {
             long nativeMax = Debug.getNativeHeapSize() / 1024;
             long nativeAllocated = Debug.getNativeHeapAllocatedSize() / 1024;
             long nativeFree = Debug.getNativeHeapFreeSize() / 1024;
@@ -1615,6 +1617,9 @@ public final class ActivityThread extends ClientTransactionHandler
                 pw.println(" ");
                 pw.println(" Unreachable memory");
                 pw.print(Debug.getUnreachableMemory(100, showContents));
+            }
+            if (dumpAllocatorStats) {
+                Debug.logAllocatorStats();
             }
         }
 
--- core/java/android/os/Debug.java
+++ core/java/android/os/Debug.java
@@ -2684,4 +2684,13 @@ public final class Debug
      * @hide
      */
     public static native boolean isVmapStack();
+
+    /**
+     * Log internal statistics about the allocator.
+     * @return true if the statistics were logged properly, false if not.
+     *
+     * @hide
+     */
+    public static native boolean logAllocatorStats();
+
 }
