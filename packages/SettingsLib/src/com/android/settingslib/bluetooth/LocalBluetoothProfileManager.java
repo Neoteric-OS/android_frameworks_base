514bdd070a1e55729ab99c5e778e8f6d4ac33b70
Alice Kuo <aliceypkuo@google.com>
2023-05-16 23:52:02 +0800

    Add the connection state change handle for the volume control profile
    Volume control profile broadcast the connection state change only for the connected when the bluetooth service get connected with connected VCP device. The other connection state didn't be handled. In some case, it would causes the system ui or setting got the vcp profile connected and then no further update for the real connection state. The patch add the VCP profile connection state handle that is the same as the other profile.
    
    Bug: 278982782
    Test: NA
    Change-Id: I41bb5e97eae588e718c45e78f702e1959ab856e4

--- packages/SettingsLib/src/com/android/settingslib/bluetooth/LocalBluetoothProfileManager.java
+++ packages/SettingsLib/src/com/android/settingslib/bluetooth/LocalBluetoothProfileManager.java
@@ -232,8 +232,8 @@ public class LocalBluetoothProfileManager {
                 Log.d(TAG, "Adding local Volume Control profile");
             }
             mVolumeControlProfile = new VolumeControlProfile(mContext, mDeviceManager, this);
-            // Note: no event handler for VCP, only for being connectable.
-            mProfileNameMap.put(VolumeControlProfile.NAME, mVolumeControlProfile);
+            addProfile(mVolumeControlProfile, VolumeControlProfile.NAME,
+                    BluetoothVolumeControl.ACTION_CONNECTION_STATE_CHANGED);
         }
         if (mLeAudioProfile == null && supportedList.contains(BluetoothProfile.LE_AUDIO)) {
             if (DEBUG) {
