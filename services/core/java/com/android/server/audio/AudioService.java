2b24804e4a8850f7351a4949daab027defd82a99
Ryan Shih <ryan.shih@mediatek.com>
2023-05-25 20:49:50 +0800

    Summary:Add property to configure key repeat delay.
    [Description]
    1. sys.key_repeat_delay
    2. sys.volume_key_repeat_delay
    3. sys.repeated_key_vol_steps
    
    Property 1 is used to configure global key repeat delay, property 2
    is for volume key repeat delay and property 3 is for volume steps
    while long pressing volume key.
    
    We use property 2 to reduce CPU usage of changing volume continuously
    and property 3 to increase speed of volume change for good user
    experience.
    
    [Root Cause]
    Adjusting volume causes high CPU usage.
    
    [Solution]
    Enlarge key repeat delay to reduce CPU usage.
    
    Test:
    AC on - ok
    DC on - ok
    Change volume - ok
    Mute/unmute - ok
    
    Bug: 289987890
    Bug: 304908948
    
    Change-Id: I8b2c1ed18ab5763bb18361c1c94628ca598e560f

--- services/core/java/com/android/server/audio/AudioService.java
+++ services/core/java/com/android/server/audio/AudioService.java
@@ -3190,8 +3190,17 @@ public class AudioService extends IAudioService.Stub
             return;
         }
 
+        int delay = SystemProperties.getInt("sys.volume_key_repeat_delay_ms", 0);
         int flags = AudioManager.FLAG_SHOW_UI | AudioManager.FLAG_PLAY_SOUND
                 | AudioManager.FLAG_FROM_KEY;
+
+        if (event.getRepeatCount() > 0) {
+            SystemProperties.set("sys.dynamic_key_repeat_delay_ms",
+                                    delay > 0 ? Integer.toString(delay) : "");
+            flags |= AudioManager.FLAG_FROM_REPEATED_KEY;
+        } else {
+            SystemProperties.set("sys.dynamic_key_repeat_delay_ms", "");
+        }
 
         switch (event.getKeyCode()) {
             case KeyEvent.KEYCODE_VOLUME_UP:
@@ -3455,7 +3464,11 @@ public class AudioService extends IAudioService.Stub
             }
         } else {
             // convert one UI step (+/-1) into a number of internal units on the stream alias
-            step = rescaleStep(10, streamType, streamTypeAlias);
+            int volSteps = 10;
+            if ((flags & AudioManager.FLAG_FROM_REPEATED_KEY) != 0) {
+                volSteps = SystemProperties.getInt("ro.repeated_key_vol_steps", 10);
+            }
+            step = rescaleStep(volSteps, streamType, streamTypeAlias);
         }
 
         // If either the client forces allowing ringer modes for this adjustment,
