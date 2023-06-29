0da298b02b9f6397c4ed2508b3c38660df21b350
Jaeyoon Lee <joyful.lee@samsung.corp-partner.google.com>
2023-06-29 16:05:54 +0900

    Insert MGF_DIGEST tag on ImportWrappedKey logic
    SecureKeyImport is failed because of MGF_DIGEST tag mismatch.
    wrapping key has MGF_DIGEST tag when generate or import key
    but importWrappedKey logic does not have MGF_DIGEST tag on WrappedKeyEntry
    So MGF_DIGEST tat mismatch error occur when decrypt wrapped key using wrapping key
    
    Bug: 277853193
    Test: android.keystore.cts.ImportWrappedKeyTest#testKeyStore_ImportWrappedKey
    Change-Id: Id7229a763e3041ffbe73989a2bb24306b7beb7a5
    Signed-off-by: Jaeyoon Lee <joyful.lee@samsung.corp-partner.google.com>

--- keystore/java/android/security/keystore2/AndroidKeyStoreSpi.java
+++ keystore/java/android/security/keystore2/AndroidKeyStoreSpi.java
@@ -913,6 +913,28 @@ public class AndroidKeyStoreSpi extends KeyStoreSpi {
                         padding
                 ));
             }
+            if (padding == KeymasterDefs.KM_PAD_RSA_OAEP) {
+                if (spec.isDigestsSpecified()) {
+                    boolean hasDefaultMgf1DigestBeenAdded = false;
+                    for (String digest : spec.getDigests()) {
+                        args.add(KeyStore2ParameterUtils.makeEnum(
+                                KeymasterDefs.KM_TAG_RSA_OAEP_MGF_DIGEST,
+                                KeyProperties.Digest.toKeymaster(digest)
+                        ));
+                        hasDefaultMgf1DigestBeenAdded |= digest.equals(DEFAULT_MGF1_DIGEST);
+                    }
+                    /* Because of default MGF1 digest is SHA-1. It has to be added in Key
+                     * characteristics. Otherwise, crypto operations will fail with Incompatible
+                     * MGF1 digest.
+                     */
+                    if (!hasDefaultMgf1DigestBeenAdded) {
+                        args.add(KeyStore2ParameterUtils.makeEnum(
+                                KeymasterDefs.KM_TAG_RSA_OAEP_MGF_DIGEST,
+                                KeyProperties.Digest.toKeymaster(DEFAULT_MGF1_DIGEST)
+                        ));
+                    }
+                }
+            }
         }
 
         KeyGenParameterSpec spec = (KeyGenParameterSpec) entry.getAlgorithmParameterSpec();
