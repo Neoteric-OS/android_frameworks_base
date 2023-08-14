fc184c6564196b5790b239bc06fd41b42cd82a3d
Yilin Wu <Reflecter510@gmail.com>
2023-08-14 22:21:54 +0800

    BatteryStatsImpl: Fix wrong synchronized with mDailyFile.
    DailyFile is synchronized with wrong Object 'mCheckinFile' when writing data to mDailyFile, which may cause unexpected errors in BatteryStats.
    
    Bug: N/A
    Test: N/A
    Change-Id: I212460aff20b2135d987ab6cdbbdc1c4eb841c16

--- core/java/com/android/internal/os/BatteryStatsImpl.java
+++ core/java/com/android/internal/os/BatteryStatsImpl.java
@@ -12694,7 +12694,7 @@ public class BatteryStatsImpl extends BatteryStats {
                 BackgroundThread.getHandler().post(new Runnable() {
                     @Override
                     public void run() {
-                        synchronized (mCheckinFile) {
+                        synchronized (mDailyFile) {
                             final long startTimeMs2 = SystemClock.uptimeMillis();
                             FileOutputStream stream = null;
                             try {
