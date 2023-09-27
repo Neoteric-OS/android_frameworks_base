663fd9b89edb1460da27476698947399ccf20fc7
Maciej Żenczykowski <maze@google.com>
2023-09-27 21:28:56 +0000

    let's at least log the src mac as being the src mac, not the dst
    changing the field names is much harder due to it going via AIDL
    
    Test: TreeHugger
    Bug: 292404319
    Signed-off-by: Maciej Żenczykowski <maze@google.com>
    Change-Id: I4101caf3b0d2abfadbabf3f389155c29a4d64e0f

--- core/java/android/net/metrics/WakeupEvent.java
+++ core/java/android/net/metrics/WakeupEvent.java
@@ -44,7 +44,7 @@ public class WakeupEvent {
         j.add(iface);
         j.add("uid: " + Integer.toString(uid));
         j.add("eth=0x" + Integer.toHexString(ethertype));
-        j.add("dstHw=" + dstHwAddr);
+        j.add("srcMac=" + dstHwAddr);  // really!! http://b/292404319#comment11
         if (ipNextHeader > 0) {
             j.add("ipNxtHdr=" + ipNextHeader);
             j.add("srcIp=" + srcIp);
