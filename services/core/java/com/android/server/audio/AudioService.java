3c677f2d49000a2846c650bbaaabe8a0874dc284
François Gaffie <francois.gaffie@renault.com>
2023-12-14 15:14:48 +0100

    Audio: add dump in AudioVolumeGroup and Strategy
    Bug: 316643994
    Test: adb shell dumpsys audio
    
    Change-Id: Icb95f845c49b0260fd014b3c29f5fff34b41ec78
    Signed-off-by: François Gaffie <francois.gaffie@renault.com>

--- services/core/java/com/android/server/audio/AudioService.java
+++ services/core/java/com/android/server/audio/AudioService.java
@@ -11918,6 +11918,8 @@ public class AudioService extends IAudioService.Stub
         pw.println("\n");
         dumpSupportedSystemUsage(pw);
 
+        AudioProductStrategy.dump(pw);
+
         pw.println("\n");
         pw.println("\nSpatial audio:");
         pw.println("mHasSpatializerEffect:" + mHasSpatializerEffect + " (effect present)");
