98fb97dd4ae78caab7f1f7b0125cba35f7a615fb
Arun Johnson <arunjohnson@google.com>
2024-01-04 18:46:00 +0000

    Enabling large audio for BLOCK_MODEL
    Bug: 298052174
    
    Change-Id: I22d261f424c1d890d311f593c652d946fcb065fa

--- media/java/android/media/MediaCodec.java
+++ media/java/android/media/MediaCodec.java
@@ -1961,7 +1961,17 @@ final public class MediaCodec {
                                         index, infos);
                                 break;
                             case BUFFER_MODE_BLOCK:
-                                // TODO
+                                while (mOutputFrames.size() <= index) {
+                                    mOutputFrames.add(null);
+                                }
+                                OutputFrame frame = mOutputFrames.get(index);
+                                if (frame == null) {
+                                    frame = new OutputFrame(index);
+                                    mOutputFrames.set(index, frame);
+                                }
+                                frame.setBufferInfos(infos);
+                                frame.setAccessible(true);
+
                             default:
                                 throw new IllegalArgumentException(
                                         "Unrecognized buffer mode: for large frame audio");
@@ -3518,6 +3528,24 @@ final public class MediaCodec {
         }
 
         /**
+         * MediaCodec.BufferInfo objects for a large buffer containing multiple
+         * access-units.
+         *
+         * @param infos Represents {@link MediaCodec.BufferInfo} objects to marks
+         *              individual access-unit boundaries and the timestamps associated with it.
+         *              The buffer is expected to contain the data is a continuous manner.
+         * @return this object
+         */
+        @FlaggedApi(FLAG_LARGE_AUDIO_FRAME)
+        public @NonNull QueueRequest setBufferInfos(@NonNull ArrayDeque<BufferInfo> infos) {
+          if (!isAccessible()) {
+              throw new IllegalStateException("The request is stale");
+          }
+          mBufferInfos = infos;
+          return this;
+        }
+
+        /**
          * Add an integer parameter.
          * See {@link MediaFormat} for an exhaustive list of supported keys with
          * values of type int, that can also be set with {@link MediaFormat#setInteger}.
@@ -3634,9 +3662,17 @@ final public class MediaCodec {
             }
             setAccessible(false);
             if (mLinearBlock != null) {
+                if (mBufferInfos.isEmpty()) {
+                    BufferInfo info = new BufferInfo();
+                    info.size = mSize;
+                    info.offset = mOffset;
+                    info.presentationTimeUs = mPresentationTimeUs;
+                    info.flags = mFlags;
+                    mBufferInfos.add(info);
+                }
                 mCodec.native_queueLinearBlock(
-                        mIndex, mLinearBlock, mOffset, mSize, mCryptoInfo,
-                        mPresentationTimeUs, mFlags,
+                        mIndex, mLinearBlock, mCryptoInfo,
+                        mBufferInfos.toArray(),
                         mTuningKeys, mTuningValues);
             } else if (mHardwareBuffer != null) {
                 mCodec.native_queueHardwareBuffer(
@@ -3654,6 +3690,7 @@ final public class MediaCodec {
             mHardwareBuffer = null;
             mPresentationTimeUs = 0;
             mFlags = 0;
+            mBufferInfos = null;
             mTuningKeys.clear();
             mTuningValues.clear();
             return this;
@@ -3677,6 +3714,7 @@ final public class MediaCodec {
         private HardwareBuffer mHardwareBuffer = null;
         private long mPresentationTimeUs = 0;
         private @BufferFlag int mFlags = 0;
+        private ArrayDeque<BufferInfo> mBufferInfos = null;
         private final ArrayList<String> mTuningKeys = new ArrayList<>();
         private final ArrayList<Object> mTuningValues = new ArrayList<>();
 
@@ -3686,11 +3724,8 @@ final public class MediaCodec {
     private native void native_queueLinearBlock(
             int index,
             @NonNull LinearBlock block,
-            int offset,
-            int size,
             @Nullable CryptoInfo cryptoInfo,
-            long presentationTimeUs,
-            int flags,
+            @NonNull Object[] bufferInfos,
             @NonNull ArrayList<String> keys,
             @NonNull ArrayList<Object> values);
 
@@ -4488,6 +4523,14 @@ final public class MediaCodec {
             return mFlags;
         }
 
+        /*
+         * Returns the BufferInfos associated with this OutputFrame
+         */
+        @FlaggedApi(FLAG_LARGE_AUDIO_FRAME)
+        public @NonNull ArrayDeque<BufferInfo> getBufferInfos() {
+            return mBufferInfos;
+        }
+
         /**
          * Returns a read-only {@link MediaFormat} for this frame. The returned
          * object is valid only until the client calls {@link MediaCodec#releaseOutputBuffer}.
@@ -4513,6 +4556,7 @@ final public class MediaCodec {
             mLinearBlock = null;
             mHardwareBuffer = null;
             mFormat = null;
+            mBufferInfos = null;
             mChangedKeys.clear();
             mKeySet.clear();
             mLoaded = false;
@@ -4531,6 +4575,10 @@ final public class MediaCodec {
             mFlags = info.flags;
         }
 
+        void setBufferInfos(ArrayDeque<BufferInfo> infos) {
+          mBufferInfos = infos;
+        }
+
         boolean isLoaded() {
             return mLoaded;
         }
@@ -4545,6 +4593,7 @@ final public class MediaCodec {
         private long mPresentationTimeUs = 0;
         private @BufferFlag int mFlags = 0;
         private MediaFormat mFormat = null;
+        private ArrayDeque<BufferInfo> mBufferInfos = null;
         private final ArrayList<String> mChangedKeys = new ArrayList<>();
         private final Set<String> mKeySet = new HashSet<>();
         private boolean mAccessible = false;
