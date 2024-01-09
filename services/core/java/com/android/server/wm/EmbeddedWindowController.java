99a8b0ac6b21276bbf23495acbbe2da9d756e09a
ot903107 <ot_zhenhua.deng@mediatek.com>
2024-01-09 22:15:29 +0800

    Update Embedded window focus again while been removed
    Since rare race click event for Embedded window grant focus and
    activity focus, if activity get focus first, and Embedded window
    did not setFocusedWindow again after focus leave, it will easily
    hit no focused widnow ANR.
    
    Bug: 319369153
    Test: frequency inject click event for customized animated
           splashscheen icon apk,such as Google Maps
    
    Change-Id: Icdc7d0f505b46b526a960ed130a6e315faf5bd3b

--- services/core/java/com/android/server/wm/EmbeddedWindowController.java
+++ services/core/java/com/android/server/wm/EmbeddedWindowController.java
@@ -51,6 +51,7 @@ class EmbeddedWindowController {
         new ArrayMap<>();
     private final Object mGlobalLock;
     private final ActivityTaskManagerService mAtmService;
+    private boolean mGrantFocusComplete = false;
 
     EmbeddedWindowController(ActivityTaskManagerService atmService) {
         mAtmService = atmService;
@@ -213,6 +214,9 @@ class EmbeddedWindowController {
 
         void onRemoved() {
             if (mInputChannel != null) {
+                if (mGrantFocusComplete) {
+                    mWmService.grantEmbeddedWindowFocus(mSession, mFocusGrantToken, false);
+                }
                 mWmService.mInputManager.removeInputChannel(mInputChannel.getToken());
                 mInputChannel.dispose();
                 mInputChannel = null;
@@ -286,6 +290,7 @@ class EmbeddedWindowController {
 
         private void handleTap(boolean grantFocus) {
             if (mInputChannel != null) {
+                mGrantFocusComplete = grantFocus;
                 if (mHostWindowState != null) {
                     // Use null session since this is being granted by system server and doesn't
                     // require the host session to be passed in
