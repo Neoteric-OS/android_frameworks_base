7c06f2b3f5cf75fbf97d3802fc26affc64bcabb0
Isak Lilja <isak.lilja@sony.com>
2024-01-12 09:54:56 +0100

    Wake up the device on internal key events
    Add a setting for waking up the device on any internal key event. This
    is useful for when the device is placed in a gimbal or a case so that
    the power key is inaccessible. When this setting is enabled, the user
    can wake up the device by pressing any key on the side of the device
    (volume keys, camera key).
    
    Bug: 319691231
    Test: 1) Run 'adb shell settings put secure \
                 wake_up_on_any_internal_key_event_enabled 1'
          2) Press the power key to turn off the display.
          3) Press the volume down key to wake up the display.
    Change-Id: I138d71a75ac1d38fb2f1c7fb507779415883805c

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -36,6 +36,7 @@ import static android.content.pm.PackageManager.PERMISSION_GRANTED;
 import static android.os.Build.VERSION_CODES.M;
 import static android.os.Build.VERSION_CODES.O;
 import static android.os.IInputConstants.INVALID_INPUT_DEVICE_ID;
+import static android.provider.Settings.Secure.WAKE_UP_ON_ANY_INTERNAL_KEY_EVENT_ENABLED;
 import static android.provider.Settings.Secure.VOLUME_HUSH_OFF;
 import static android.view.Display.DEFAULT_DISPLAY;
 import static android.view.Display.INVALID_DISPLAY;
@@ -705,6 +706,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     // Timeout for showing the keyguard after the screen is on, in case no "ready" is received.
     private int mKeyguardDrawnTimeout = 1000;
 
+    private boolean mWakeUpOnAnyInternalKeyEvent;
+
     private static final int MSG_DISPATCH_MEDIA_KEY_WITH_WAKE_LOCK = 3;
     private static final int MSG_DISPATCH_MEDIA_KEY_REPEAT_WITH_WAKE_LOCK = 4;
     private static final int MSG_KEYGUARD_DRAWN_COMPLETE = 5;
@@ -905,6 +908,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.Secure.getUriFor(
                     Settings.Secure.NAV_BAR_KIDS_MODE), false, this,
+                    UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.Secure.getUriFor(
+                    Settings.Secure.WAKE_UP_ON_ANY_INTERNAL_KEY_EVENT_ENABLED), false, this,
                     UserHandle.USER_ALL);
             updateSettings();
         }
@@ -2954,6 +2960,12 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 mKidsModeEnabled = kidsModeEnabled;
                 updateKidsModeSettings();
             }
+
+            mWakeUpOnAnyInternalKeyEvent = Settings.Secure.getIntForUser(
+                    resolver,
+                    WAKE_UP_ON_ANY_INTERNAL_KEY_EVENT_ENABLED,
+                    0 /* def */,
+                    UserHandle.USER_CURRENT) == 1;
         }
         if (updateRotation) {
             updateRotation(true);
@@ -4516,7 +4528,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         final int keyCode = event.getKeyCode();
         final boolean down = event.getAction() == KeyEvent.ACTION_DOWN;
         boolean isWakeKey = (policyFlags & WindowManagerPolicy.FLAG_WAKE) != 0
-                || event.isWakeKey();
+                || event.isWakeKey()
+                || (mWakeUpOnAnyInternalKeyEvent
+                && event.getDevice() != null & !event.getDevice().isExternal());
 
         if (!mSystemBooted) {
             // If we have not yet booted, don't let key events do anything.
