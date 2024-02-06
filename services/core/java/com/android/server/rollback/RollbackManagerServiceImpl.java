7898ca8115e2a8230659cd238019a14bfcf88f4e
Shrinidhi Hegde <shrinidhihegde@google.com>
2024-02-06 15:50:18 +0000

    Added high impact rollback in RollbackPackageHealthObserver
    Added high impact rollback as a last resort after low impact rollbacks
    are tried. Also removed reference to automatic rollback deny list, since
    we will be isung impact level instead of the deny list going forward.
    
    Bug: 291137901
    Test: Unit tests
    Change-Id: I4d75dd513b3f43c179baf93ece832f9007173ff4

--- services/core/java/com/android/server/rollback/RollbackManagerServiceImpl.java
+++ services/core/java/com/android/server/rollback/RollbackManagerServiceImpl.java
@@ -1212,14 +1212,20 @@ class RollbackManagerServiceImpl extends IRollbackManager.Stub implements Rollba
         rollback.makeAvailable();
         mPackageHealthObserver.notifyRollbackAvailable(rollback.info);
 
-        // TODO(zezeozue): Provide API to explicitly start observing instead
-        // of doing this for all rollbacks. If we do this for all rollbacks,
-        // should document in PackageInstaller.SessionParams#setEnableRollback
-        // After enabling and committing any rollback, observe packages and
-        // prepare to rollback if packages crashes too frequently.
-        mPackageHealthObserver.startObservingHealth(rollback.getPackageNames(),
-                mRollbackLifetimeDurationInMillis);
+        if (rollback.info.getRollbackImpactLevel() == PackageManager.ROLLBACK_USER_IMPACT_LOW) {
+            // TODO(zezeozue): Provide API to explicitly start observing instead
+            // of doing this for all rollbacks. If we do this for all rollbacks,
+            // should document in PackageInstaller.SessionParams#setEnableRollback
+            // After enabling and committing any rollback, observe packages and
+            // prepare to rollback if packages crashes too frequently.
+            mPackageHealthObserver.startObservingHealth(rollback.getPackageNames(),
+                    mRollbackLifetimeDurationInMillis);
+        }
         runExpiration();
+    }
+
+    private boolean isRollbackLowImpact(RollbackInfo rollbackInfo) {
+        return rollbackInfo.getRollbackImpactLevel() == PackageManager.ROLLBACK_USER_IMPACT_LOW;
     }
 
     /*
