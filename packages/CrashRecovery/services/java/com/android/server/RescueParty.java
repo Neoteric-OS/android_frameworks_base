54b8f252b131b53f9951ac95489de6c53a40c40d
Harshit Mahajan <harshitmahajan@google.com>
2024-03-11 16:28:09 +0000

    [CrashRecovery] Add System Apis from RescueParty
    RescueParty will be moved to a new module CrashRecovery.
    We need to create an API layer for all the platform intraction.
    Making all the interacting methods as SystemApi
    More Details: go/v-crashrecovery-module-design-doc
    
    Bug: 289203818
    Test: TH
    Change-Id: I1cd9b04f27362d144dcac0844f50ddfe42fac558

--- packages/CrashRecovery/services/java/com/android/server/RescueParty.java
+++ packages/CrashRecovery/services/java/com/android/server/RescueParty.java
@@ -23,6 +23,7 @@ import static com.android.server.pm.PackageManagerServiceUtils.logCriticalInfo;
 import android.annotation.IntDef;
 import android.annotation.NonNull;
 import android.annotation.Nullable;
+import android.annotation.SystemApi;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.pm.ApplicationInfo;
@@ -77,7 +78,10 @@ import java.util.concurrent.TimeUnit;
  *
  * @hide
  */
+@SystemApi(client = SystemApi.Client.SYSTEM_SERVER)
 public class RescueParty {
+    /** Fully-static utility classes must not have constructor */
+    private RescueParty() {}
     @VisibleForTesting
     static final String PROP_ENABLE_RESCUE = "persist.sys.enable_rescue";
     @VisibleForTesting
@@ -156,7 +160,7 @@ public class RescueParty {
             | ApplicationInfo.FLAG_SYSTEM;
 
     /** Register the Rescue Party observer as a Package Watchdog health observer */
-    public static void registerHealthObserver(Context context) {
+    public static void registerHealthObserver(@NonNull Context context) {
         PackageWatchdog.getInstance(context).registerHealthObserver(
                 RescuePartyObserver.getInstance(context));
     }
@@ -201,6 +205,7 @@ public class RescueParty {
      * Check if we're currently attempting to reboot for a factory reset. This method must
      * return true if RescueParty tries to reboot early during a boot loop, since the device
      * will not be fully booted at this time.
+     * @hide
      */
     public static boolean isRecoveryTriggeredReboot() {
         return isFactoryResetPropertySet() || isRebootPropertySet();
@@ -214,25 +219,31 @@ public class RescueParty {
         return CrashRecoveryProperties.attemptingReboot().orElse(false);
     }
 
+    /** @hide **/
     protected static long getLastFactoryResetTimeMs() {
         return CrashRecoveryProperties.lastFactoryResetTimeMs().orElse(0L);
     }
 
+    /** @hide **/
     protected static int getMaxRescueLevelAttempted() {
         return CrashRecoveryProperties.maxRescueLevelAttempted().orElse(LEVEL_NONE);
     }
 
+    /** @hide **/
     protected static void setFactoryResetProperty(boolean value) {
         CrashRecoveryProperties.attemptingFactoryReset(value);
     }
+    /** @hide **/
     protected static void setRebootProperty(boolean value) {
         CrashRecoveryProperties.attemptingReboot(value);
     }
 
+    /** @hide **/
     protected static void setLastFactoryResetTimeMs(long value) {
         CrashRecoveryProperties.lastFactoryResetTimeMs(value);
     }
 
+    /** @hide **/
     protected static void setMaxRescueLevelAttempted(int level) {
         CrashRecoveryProperties.maxRescueLevelAttempted(level);
     }
@@ -241,7 +252,7 @@ public class RescueParty {
      * Called when {@code SettingsProvider} has been published, which is a good
      * opportunity to reset any settings depending on our rescue level.
      */
-    public static void onSettingsProviderPublished(Context context) {
+    public static void onSettingsProviderPublished(@NonNull Context context) {
         handleNativeRescuePartyResets();
         ContentResolver contentResolver = context.getContentResolver();
         DeviceConfig.setMonitorCallback(
@@ -256,7 +267,7 @@ public class RescueParty {
      * to avoid rolled back modules consuming flag values only expected to work
      * on modules of newer versions.
      */
-    public static void resetDeviceConfigForPackages(List<String> packageNames) {
+    public static void resetDeviceConfigForPackages(@NonNull List<String> packageNames) {
         if (packageNames == null) {
             return;
         }
@@ -404,8 +415,8 @@ public class RescueParty {
     /**
      * Get the rescue level to perform if this is the n-th attempt at mitigating failure.
      *
-     * @param mitigationCount: the mitigation attempt number (1 = first attempt etc.)
-     * @param mayPerformReboot: whether or not a reboot and factory reset may be performed
+     * @param mitigationCount the mitigation attempt number (1 = first attempt etc.)
+     * @param mayPerformReboot whether or not a reboot and factory reset may be performed
      *                          for the given failure.
      * @return the rescue level for the n-th mitigation attempt.
      */
@@ -787,6 +798,7 @@ public class RescueParty {
      * Handle mitigation action for package failures. This observer will be register to Package
      * Watchdog and will receive calls about package failures. This observer is persistent so it
      * may choose to mitigate failures for packages it has not explicitly asked to observe.
+     * @hide
      */
     public static class RescuePartyObserver implements PackageHealthObserver {
 
@@ -801,7 +813,9 @@ public class RescueParty {
             mContext = context;
         }
 
-        /** Creates or gets singleton instance of RescueParty. */
+        /** Creates or gets singleton instance of RescueParty.
+         * @hide
+         */
         public static RescuePartyObserver getInstance(Context context) {
             synchronized (RescuePartyObserver.class) {
                 if (sRescuePartyObserver == null) {
