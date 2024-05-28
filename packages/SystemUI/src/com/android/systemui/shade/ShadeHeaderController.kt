752c323fbddd03b01fa922536d11dda21934591b
Dhina17 <dhinalogu@gmail.com>
2024-05-28 22:05:42 +0530

    SystemUI: Update QS header fields text appearance on theme change
    - Now, It's showing correct font for QS header clock, date
      and carrier text after changing system font.
    
    Change-Id: I75d7a065e93e17ebba01aa9cbe705ac6ab5a781d

--- packages/SystemUI/src/com/android/systemui/shade/ShadeHeaderController.kt
+++ packages/SystemUI/src/com/android/systemui/shade/ShadeHeaderController.kt
@@ -338,6 +338,11 @@ constructor(
                 updateResources()
             }
 
+            override fun onThemeChanged() {
+                clock.setTextAppearance(R.style.TextAppearance_QS_Status)
+                date.setTextAppearance(R.style.TextAppearance_QS_Status)
+                mShadeCarrierGroup.updateTextAppearance(R.style.TextAppearance_QS_Status)
+            }
         }
 
     private val nextAlarmCallback =
