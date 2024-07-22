fff11e8cc09d6e2f3ef8802902ea0b595af4131b
Yi Kong <yikong@google.com>
2024-07-22 14:28:34 +0900

    profcollect: Collect camera traces for a longer duration
    Increase the duration so that we can get a more complete and accurate
    trace.
    
    Test: manual
    Bug: 319394981
    Change-Id: Id6bc62d44da418c5e21bc7876971a4b64f59c25d

--- services/profcollect/src/com/android/server/profcollect/ProfcollectForwardingService.java
+++ services/profcollect/src/com/android/server/profcollect/ProfcollectForwardingService.java
@@ -404,7 +404,8 @@ public final class ProfcollectForwardingService extends SystemService {
                 String traceTag = traceInitialization ? "camera_init" : "camera";
                 BackgroundThread.get().getThreadHandler().postDelayed(() -> {
                     try {
-                        mIProfcollect.trace_process(traceTag, "android.hardware.camera.provider");
+                        mIProfcollect.trace_process(traceTag, "android.hardware.camera.provider",
+                                5000);
                     } catch (RemoteException e) {
                         Log.e(LOG_TAG, "Failed to initiate trace: " + e.getMessage());
                     }
