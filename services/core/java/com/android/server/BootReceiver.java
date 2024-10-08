b50bb84d81982811e031dc0c2407ed9898ee6a18
gang.huang <gang.huang@unisoc.com>
2024-10-08 12:03:55 +0800

    Fix FD leaks in BootReceiver.
    Bug: 372123174
    Test: Manual.
    Change-Id: I32e8030c025ad2403c961eadacafc97abdf76085

--- services/core/java/com/android/server/BootReceiver.java
+++ services/core/java/com/android/server/BootReceiver.java
@@ -147,6 +147,10 @@ public class BootReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(final Context context, Intent intent) {
+        if (!Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+            return;
+        }
+
         // Log boot events in the background to avoid blocking the main thread with I/O
         new Thread() {
             @Override
@@ -212,6 +216,8 @@ public class BootReceiver extends BroadcastReceiver {
                 } catch (Exception e) {
                     Slog.wtf(TAG, "Error watching for trace events", e);
                     return 0;  // Unregister the handler.
+                } finally {
+                    IoUtils.closeQuietly(fd);
                 }
                 return OnFileDescriptorEventListener.EVENT_INPUT;
             }
