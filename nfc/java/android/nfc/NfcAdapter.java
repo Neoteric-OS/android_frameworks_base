d4072767f28ad41ed13feb52f1a39124b1e03a0a
George Chang <georgekgchang@google.com>
2024-10-28 09:38:14 +0000

    Add NFC IsTagIntentAllowed API
    Flag: android.nfc.nfc_check_tag_intent_preference
    Bug: 335916336
    Test: m framework-nfc.stubs.source-update-current-api
    Test: manual
    Change-Id: I502c31ec8ba876d9a0c35a8d5447343d27ef678d

--- nfc/java/android/nfc/NfcAdapter.java
+++ nfc/java/android/nfc/NfcAdapter.java
@@ -49,6 +49,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.RemoteException;
+import android.os.UserHandle;
 import android.util.Log;
 
 import java.io.IOException;
@@ -2894,5 +2895,29 @@ public final class NfcAdapter {
             }
         }
         return mNfcOemExtension;
+    }
+
+    /**
+     * Activity action: ask the user to change allowlist of the Tag app preference.
+     *
+     */
+    @SdkConstant(SdkConstantType.ACTIVITY_INTENT_ACTION)
+    @FlaggedApi(Flags.FLAG_NFC_CHECK_TAG_INTENT_PREFERENCE)
+    public static final String ACTIOIN_CHANGE_TAG_INTENT_PREFERENCE =
+            "android.nfc.ACTIOIN_CHANGE_TAG_INTENT_PREFERENCE";
+
+    /**
+     * Checks if the Activity disabled from TagIntentPreference
+     *
+     * @return True if Tag Intent allowed
+     * @throws UnsupportedOperationException if FEATURE_NFC is unavailable
+     */
+    @FlaggedApi(Flags.FLAG_NFC_CHECK_TAG_INTENT_PREFERENCE)
+    public boolean isTagIntentAllowed() {
+        if (!sHasNfcFeature) {
+            throw new UnsupportedOperationException();
+        }
+        return callServiceReturn(() ->  sService.isTagIntentAllowed(mContext.getPackageName(),
+                UserHandle.myUserId()), false);
     }
 }
