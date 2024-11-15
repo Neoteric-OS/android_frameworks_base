dcac92fdd3df42a92f422398e26682cd2a09f2d2
prochy-exe <dominikprochazka100@gmail.com>
2024-11-15 16:55:43 +0530

    SystemUI: Allow single tap on keyguard affordances [1/2]
    * add an option to always single tap on keyguard affordance, because long pressing on them is quite frankly abysmal
    
    HELLBOY017: Adapt to 15
    
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -13696,6 +13696,12 @@ public final class Settings {
          * @hide
          */
         public static final String BERRY_BLACK_THEME = "berry_black_theme";
+
+	/**
+         * Boolean value whether activate keyguard affordance shortcuts on short press
+         * @hide
+         */
+        public static final String KEYGUARD_AFFORDANCE_SINGLE_TAP = "keyguard_affordance_single_tap";
     }
 
     /**
