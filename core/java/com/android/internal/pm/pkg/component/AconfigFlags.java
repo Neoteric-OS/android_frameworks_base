ca81a32482a3db50b50cd5af85ed908b5fd4ee8f
Thomas Makin <halorocker89@gmail.com>
2024-11-23 17:53:45 +0000

    pm: aconfig: do not error on missing flag files
    Change-Id: Ifd9368b153197f2f5d9bf594cf56bb33413c0f19

--- core/java/com/android/internal/pm/pkg/component/AconfigFlags.java
+++ core/java/com/android/internal/pm/pkg/component/AconfigFlags.java
@@ -72,6 +72,9 @@ public class AconfigFlags {
                 (Process.myUid() == Process.SYSTEM_UID) ? DeviceProtos.parsedFlagsProtoPaths()
                         : Arrays.asList(DeviceProtos.PATHS);
         for (String fileName : defaultFlagProtoFiles) {
+            File f = new File(fileName);
+            if (!f.isFile() || !f.canRead())
+                continue;
             try (var inputStream = new FileInputStream(fileName)) {
                 loadAconfigDefaultValues(inputStream.readAllBytes());
             } catch (IOException e) {
@@ -107,6 +110,8 @@ public class AconfigFlags {
 
         final var settingsFile = new File(Environment.getUserSystemDirectory(0),
                 "settings_config.xml");
+        if (!settingsFile.isFile() || !settingsFile.canRead())
+            return;
         try (var inputStream = new FileInputStream(settingsFile)) {
             TypedXmlPullParser parser = Xml.resolvePullParser(inputStream);
             if (parser.next() != XmlPullParser.END_TAG && "settings".equals(parser.getName())) {
@@ -150,8 +155,8 @@ public class AconfigFlags {
                     if (flagPackageAndName == null) {
                         continue;
                     }
-                    // We ignore all settings that aren't for flags. We'll know they are for flags
-                    // if they correspond to flags read from the proto files.
+                    // We ignore all settings that aren't for flags. We'll know they are
+                    // for flags if they correspond to flags read from the proto files.
                     if (!mFlagValues.containsKey(flagPackageAndName)) {
                         continue;
                     }
@@ -160,7 +165,8 @@ public class AconfigFlags {
                     final Integer currentPriority = flagPriority.get(flagPackageAndName);
                     if (currentPriority != null && currentPriority >= priority) {
                         Slog.i(LOG_TAG, "Skipping " + prefix + " flag " + flagPackageAndName
-                                + " because of the existing one with priority " + currentPriority);
+                                + " because of the existing one with priority "
+                                + currentPriority);
                         continue;
                     }
                     flagPriority.put(flagPackageAndName, priority);
