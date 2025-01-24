1bdbae8d6d72d70219eacddcd3d8de8bc6008fac
Jared Duke <jdduke@google.com>
2025-01-24 23:57:21 +0000

    Integrate SDK system feature cache behind a flag
    Add the SDK system feature cache to the hasSystemFeature query lookup.
    This involves populating the initial cache array in system server, and
    handing this as a compact int array (<1KB) to each process.
    
    An alternative would be to stash this in the ApplicationSharedMemory,
    which we may do as part of a follow-up, but that would require
    additional complexity in routing build constants to the fixed native
    array sizes. We can adopt that model in the future after validating
    improvements with a trunk stable aconfig flag.
    
    Flag: android.content.pm.cache_sdk_system_features
    Bug: 326623529
    Test: presubmit
    Change-Id: I5549260cafff3ae08543c0c8d944e8cf396da224

--- core/java/android/app/ActivityThread.java
+++ core/java/android/app/ActivityThread.java
@@ -102,6 +102,7 @@ import android.content.pm.PermissionInfo;
 import android.content.pm.ProviderInfo;
 import android.content.pm.ProviderInfoList;
 import android.content.pm.ServiceInfo;
+import android.content.pm.SystemFeaturesCache;
 import android.content.res.AssetManager;
 import android.content.res.CompatibilityInfo;
 import android.content.res.Configuration;
@@ -580,6 +581,8 @@ public final class ActivityThread extends ClientTransactionHandler
 
     Bundle mCoreSettings = null;
 
+    private SystemFeaturesCache mSystemFeaturesCache;
+
     /**
      * The lock word for the {@link #mCoreSettings}.
      */
@@ -976,6 +979,12 @@ public final class ActivityThread extends ClientTransactionHandler
         AutofillOptions autofillOptions;
 
         /**
+         * Non-null iff android.content.pm.Flags.cacheSdkSystemFeatures() is enabled.
+         */
+        @Nullable
+        SystemFeaturesCache mSystemFeaturesCache;
+
+        /**
          * Content capture options for the application - when null, it means ContentCapture is not
          * enabled for the package.
          */
@@ -1297,6 +1306,7 @@ public final class ActivityThread extends ClientTransactionHandler
                 Bundle coreSettings,
                 String buildSerial,
                 AutofillOptions autofillOptions,
+                SystemFeaturesCache systemFeaturesCache,
                 ContentCaptureOptions contentCaptureOptions,
                 long[] disabledCompatChanges,
                 long[] loggableCompatChanges,
@@ -1352,6 +1362,7 @@ public final class ActivityThread extends ClientTransactionHandler
             data.initProfilerInfo = profilerInfo;
             data.buildSerial = buildSerial;
             data.autofillOptions = autofillOptions;
+            data.mSystemFeaturesCache = systemFeaturesCache;
             data.contentCaptureOptions = contentCaptureOptions;
             data.disabledCompatChanges = disabledCompatChanges;
             data.mLoggableCompatChanges = loggableCompatChanges;
@@ -3249,6 +3260,20 @@ public final class ActivityThread extends ClientTransactionHandler
             //Slog.i(TAG, "**** WE DO, WE DO WANT TO GC!");
             BinderInternal.forceGc(reason);
         }
+    }
+
+    /** @hide */
+    public void setSystemFeaturesCache(@Nullable SystemFeaturesCache systemFeaturesCache) {
+        if (mSystemFeaturesCache != null) {
+            throw new IllegalArgumentException("SystemFeaturesCache already initialized.");
+        }
+        mSystemFeaturesCache = systemFeaturesCache;
+    }
+
+    /** @hide */
+    @Nullable
+    public SystemFeaturesCache getSystemFeaturesCache() {
+        return mSystemFeaturesCache;
     }
 
     private static final String HEAP_FULL_COLUMN =
@@ -7389,6 +7414,8 @@ public final class ActivityThread extends ClientTransactionHandler
          * Set the LocaleList. This may change once we create the App Context.
          */
         LocaleList.setDefault(data.config.getLocales());
+
+        setSystemFeaturesCache(data.mSystemFeaturesCache);
 
         if (Typeface.ENABLE_LAZY_TYPEFACE_INITIALIZATION) {
             try {
