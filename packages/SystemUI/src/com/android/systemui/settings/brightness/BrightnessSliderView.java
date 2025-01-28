aospa/platform_frameworks_base

SystemUI: Animate brightness slider background
Both, the progress slider as well as the toggle icon are scaled while
expanding the QS. Animate the background as well to get a more unified
experience.

Change-Id: I4336a12dbae1db7911cf9c08e04dbc8432586286

packages/SystemUI/src/com/android/systemui/settings/brightness/BrightnessSliderView.java
@@ -53,6 +53,8 @@ public class BrightnessSliderView extends LinearLayout {
     @Nullable
     private Drawable mProgressDrawable;
     @Nullable
+    private Drawable mProgressBgDrawable;
+    @Nullable
     private Drawable mToggleBgDrawable;
     private float mScale = 1f;
     private final Rect mSystemGestureExclusionRect = new Rect();
@@ -78,6 +80,7 @@ public class BrightnessSliderView extends LinearLayout {
         // Finds the progress drawable. Assumes brightness_progress_drawable.xml
         try {
             LayerDrawable progress = (LayerDrawable) mSlider.getProgressDrawable();
+            mProgressBgDrawable = progress.findDrawableByLayerId(android.R.id.background);
             DrawableWrapper progressSlider = (DrawableWrapper) progress
                     .findDrawableByLayerId(android.R.id.progress);
             LayerDrawable actualProgressSlider = (LayerDrawable) progressSlider.getDrawable();
@@ -273,6 +276,12 @@ public class BrightnessSliderView extends LinearLayout {
             int height = (int) (mProgressDrawable.getIntrinsicHeight() * mScale);
             int inset = (mProgressDrawable.getIntrinsicHeight() - height) / 2;
             mProgressDrawable.setBounds(r.left, inset, r.right, inset + height);
+            if (mProgressBgDrawable != null) {
+                final Drawable sliderBackground = mSlider.getCurrentDrawable();
+                final Rect rBgBounds = sliderBackground.getBounds();
+                mProgressBgDrawable.setBounds(rBgBounds.left, inset, rBgBounds.right,
+                        inset + height);
+            }
             if (mToggleBgDrawable != null) {
                 final Rect rToggle = mToggleBgDrawable.getBounds();
                 // The slider & toggle share the same height

Neoteric-OS/android_frameworks_base

packages/SystemUI/src/com/android/systemui/settings/brightness/BrightnessSliderView.java
@@ -53,6 +53,8 @@ public class BrightnessSliderView extends LinearLayout {
     @Nullable
     private Drawable mProgressDrawable;
     @Nullable
+    private Drawable mProgressBgDrawable;
+    @Nullable
     private Drawable mToggleBgDrawable;
     private float mScale = 1f;
     private final Rect mSystemGestureExclusionRect = new Rect();
@@ -78,6 +80,7 @@ public class BrightnessSliderView extends LinearLayout {
         // Finds the progress drawable. Assumes brightness_progress_drawable.xml
         try {
             LayerDrawable progress = (LayerDrawable) mSlider.getProgressDrawable();
+            mProgressBgDrawable = progress.findDrawableByLayerId(android.R.id.background);
             DrawableWrapper progressSlider = (DrawableWrapper) progress
                     .findDrawableByLayerId(android.R.id.progress);
             LayerDrawable actualProgressSlider = (LayerDrawable) progressSlider.getDrawable();
@@ -283,6 +286,11 @@ public class BrightnessSliderView extends LinearLayout {
             int height = (int) (mProgressDrawable.getIntrinsicHeight() * mScale);
             int inset = (mProgressDrawable.getIntrinsicHeight() - height) / 2;
             mProgressDrawable.setBounds(r.left, inset, r.right, inset + height);
+            if (mProgressBgDrawable != null) {
+                final Rect rBackground = mProgressBgDrawable.getBounds();
+                mProgressBgDrawable.setBounds(rBackground.left, inset, rBackground.right,
+                        inset + height);
+            }
             if (mToggleBgDrawable != null) {
                 final Rect rToggle = mToggleBgDrawable.getBounds();
                 // The slider & toggle share the same height