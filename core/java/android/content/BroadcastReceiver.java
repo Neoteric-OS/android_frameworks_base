a8c8a712edcbb406d44f889b36f0249f5d762f20
Ben Miles <benmiles@google.com>
2025-02-11 05:47:06 -0800

    Add debug store logging for more app lifecycle events
    New debug store events:
    * BindApp = bind application
    * SchRec = schedule receiver
    * SchRegRec = schedule registered receiver
    * BcRcvReg = broadcast receive (registered receiver)
    
    Also changed the semantics of some existing debug store events:
    * GoAsync / Finish now include the pending result id (so readers can see which finish is for which goAsync)
    * Finish no longer includes the receiver name (redundant)
    * HandleReceiver is renamed to BrRcvComp for consistency with BrRcvReg
    
    Also:
    * Added some debug logging for the debug store
    
    The rationale for adding these events is to make broadcast ANRs and
    bind_application ANRs more debuggable.
    
    Change-Id: I28166f10a51db728cbdd177704263486d12c7ef8
    Test: atest FrameworksCoreTests:com.android.internal.os.DebugStoreTest
    Test: Manual testing
    Flag: debug_store_enabled

--- core/java/android/content/BroadcastReceiver.java
+++ core/java/android/content/BroadcastReceiver.java
@@ -261,7 +261,7 @@ public abstract class BroadcastReceiver {
                         1);
             }
             if (DEBUG_STORE_ENABLED) {
-                DebugStore.recordFinish(mReceiverClassName);
+                DebugStore.recordFinish(System.identityHashCode(this));
             }
 
             if (mType == TYPE_COMPONENT) {
@@ -442,7 +442,8 @@ public abstract class BroadcastReceiver {
         PendingResult res = mPendingResult;
         mPendingResult = null;
         if (DEBUG_STORE_ENABLED) {
-            DebugStore.recordGoAsync(getClass().getName());
+            DebugStore.recordGoAsync(
+                    getClass().getName(), res == null ? 0 : System.identityHashCode(res));
         }
         if (res != null && Trace.isTagEnabled(Trace.TRACE_TAG_ACTIVITY_MANAGER)) {
             res.mReceiverClassName = getClass().getName();
