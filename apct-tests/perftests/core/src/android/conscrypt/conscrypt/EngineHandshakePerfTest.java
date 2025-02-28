d31fbb2ed07680c34144cedb30c2d188c729443a
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2025-02-27 21:07:26 -0800

    Revert "Extend list of ciphers in conscrypt perftests"
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/60e5dcbc-08fb-41e1-ac2c-b7fa74c8ff1a). It will not be committed, and can be ignored. If you need to commit this revert, please don't use this - create a new revert of the original topic.
    
    Change-Id: Ib676c07634b13365476e37a4299d396d61151399
    Signed-off-by: boq-android-culprit-assistant@system.gserviceaccount.com

--- apct-tests/perftests/core/src/android/conscrypt/conscrypt/EngineHandshakePerfTest.java
+++ apct-tests/perftests/core/src/android/conscrypt/conscrypt/EngineHandshakePerfTest.java
@@ -87,13 +87,11 @@ public final class EngineHandshakePerfTest {
         }
     }
 
-
     public Collection getParams() {
         final List<Object[]> params = new ArrayList<>();
         for (BufferType bufferType : BufferType.values()) {
-            for (String cipher : ConscryptParams.ciphers) {
-                params.add(new Object[] {new Config(bufferType, cipher, 100)});
-            }
+            params.add(new Object[] {new Config(bufferType,
+                "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256", 100)});
         }
         return params;
     }
