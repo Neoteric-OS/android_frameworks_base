3ba39460fe6ab05ff54318ed8c13251c99f705a4
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2025-03-04 04:35:42 -0800

    Revert "Extend list of ciphers in conscrypt perftests"
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/020e34f2-5162-41ac-a4a6-9c3a2c474990). It will not be committed, and can be ignored. If you need to commit this revert, please don't use this - create a new revert of the original topic.
    
    Change-Id: I6feda1262b57a338022b1ac9f95aa9f406828986
    Signed-off-by: boq-android-culprit-assistant@system.gserviceaccount.com

--- apct-tests/perftests/core/src/android/conscrypt/conscrypt/ServerSocketPerfTest.java
+++ apct-tests/perftests/core/src/android/conscrypt/conscrypt/ServerSocketPerfTest.java
@@ -102,12 +102,15 @@ public final class ServerSocketPerfTest {
         final List<Object[]> params = new ArrayList<>();
         for (EndpointFactory endpointFactory : EndpointFactory.values()) {
             for (ChannelType channelType : ChannelType.values()) {
-                for (int messageSize : ConscryptParams.messageSizes) {
-                    for (String cipher : ConscryptParams.ciphers) {
-                        params.add(new Object[] {new Config(endpointFactory,
-                            endpointFactory, messageSize, cipher, channelType)});
-                    }
-                }
+                params.add(new Object[] {new Config(endpointFactory,
+                    endpointFactory, 64,
+                    "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256", channelType)});
+                params.add(new Object[] {new Config(endpointFactory,
+                    endpointFactory, 512,
+                    "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256", channelType)});
+                params.add(new Object[] {new Config(endpointFactory,
+                    endpointFactory, 4096,
+                    "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256", channelType)});
             }
         }
         return params;
