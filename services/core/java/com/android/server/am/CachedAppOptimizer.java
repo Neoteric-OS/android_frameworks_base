845d397c312ccd9e4d231ec249a7a3ba16469e2e
Li Li <dualli@google.com>
2025-05-12 11:18:34 -0700

    Check the freeze policy when re-enabling Freezer, which might have changed when Freezer was temporarily disabled.
    CRs-Fixed: 4177267
    Flag: EXEMPT bugfix
    Bug: 404682210
    Bug: 414479084
    Change-Id: Ia354779d265ed0ab056f3be5162e88f28c736713
    (cherry picked from commit b76a4e3e80d47f84071cdcbff1c6e487dd36c112)

--- services/core/java/com/android/server/am/CachedAppOptimizer.java
+++ services/core/java/com/android/server/am/CachedAppOptimizer.java
@@ -985,7 +985,9 @@ public class CachedAppOptimizer {
 
                     final ProcessCachedOptimizerRecord opt = process.mOptRecord;
                     if (enable && opt.hasFreezerOverride()) {
-                        freezeAppAsyncLSP(process);
+                        if (OomAdjuster.getFreezePolicy(process)) {
+                            freezeAppAsyncLSP(process);
+                        }
                         opt.setFreezerOverride(false);
                     }
 
