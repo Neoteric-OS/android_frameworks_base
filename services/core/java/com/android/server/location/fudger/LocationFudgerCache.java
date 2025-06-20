eca2ca4ab8e3711054857e7080a3e196ad311f51
Ludovic Barman <ludovicb@google.com>
2025-06-20 10:08:12 -0700

    Flag cleanups: density_based_coarse_locations and population_density_provider
    Both are eligible for cleanup:
    - https://releasetracker.partner.android.com/changes/5s3gfni000001?stage_id=c4tq20q000001&build_unique_id=AndroidLocationFlagsDensityBasedCoarseLocationsLaunch-platform&apply_default_filter=true&enable_workflow_timeline=true
    - https://releasetracker.partner.android.com/changes/fgbgfni000001?stage_id=e0jq20q000001&build_unique_id=AndroidLocationFlagsPopulationDensityProviderLaunch-platform&apply_default_filter=true&enable_workflow_timeline=true
    
    Change-Id: I1cff14d981f6441c8a922156a3cba29b522b5a86

--- services/core/java/com/android/server/location/fudger/LocationFudgerCache.java
+++ services/core/java/com/android/server/location/fudger/LocationFudgerCache.java
@@ -40,7 +40,6 @@ import java.util.Objects;
  * The cache is first-in, first-out, and has a fixed size. Cache entries are valid until evicted by
  * another value.
  */
-@FlaggedApi(Flags.FLAG_POPULATION_DENSITY_PROVIDER)
 public class LocationFudgerCache {
 
     // The maximum number of S2 cell ids stored in the cache.
