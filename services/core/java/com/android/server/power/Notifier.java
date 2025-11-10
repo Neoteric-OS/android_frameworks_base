7772a74690c66ca27351eb7edd21e4a95628bf61
Your Name <zhangzhihao6@xiaomi.corp-partner.google.com>
2025-11-10 09:36:20 +0800

    Fix memory leak issues
    Fixes:447452228
    
    Change-Id: I10664c0865f757360c0ecedf936ef8cec01fc498
    Signed-off-by: zhangzhihao6@xiaomi.com <zhangzhihao6@xiaomi.com>

--- services/core/java/com/android/server/power/Notifier.java
+++ services/core/java/com/android/server/power/Notifier.java
@@ -196,6 +196,7 @@ public class Notifier {
     private final AtomicBoolean mIsPlayingChargingStartedFeedback = new AtomicBoolean(false);
 
     private final Injector mInjector;
+    int mCurrentScreenPolicy;
 
     private final PowerManagerFlags mFlags;
 
@@ -904,11 +905,18 @@ public class Notifier {
      * Called when the screen policy changes.
      */
     public void onScreenPolicyUpdate(int displayGroupId, int newPolicy) {
-        if (DEBUG) {
-            Slog.d(TAG, "onScreenPolicyUpdate: newPolicy=" + newPolicy);
-        }
         mWakefulnessSessionObserver.onScreenPolicyUpdate(
                 SystemClock.uptimeMillis(), displayGroupId, newPolicy);
+
+        if (displayGroupId != Display.DEFAULT_DISPLAY_GROUP || newPolicy == mCurrentScreenPolicy) {
+            return;
+        }
+
+        if (DEBUG) {
+            Slog.d(TAG, "onScreenPolicyUpdate: Screen policy transition " + mCurrentScreenPolicy + " -> " + newPolicy);
+        }
+
+        mCurrentScreenPolicy = newPolicy;
 
         synchronized (mLock) {
             Message msg = mHandler.obtainMessage(MSG_SCREEN_POLICY);
